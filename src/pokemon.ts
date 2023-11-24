8ad08edd53ca58d2d4daaff74c4b5f6e5a85935a
Flashfyre <flashfireex@gmail.com>
2023-11-24 08:31:52 -0500

    Fix level up moves not working for spliced Pokemon

--- src/pokemon.ts
+++ src/pokemon.ts
@@ -562,6 +562,10 @@ export default abstract class Pokemon extends Phaser.GameObjects.Container {
     if (this.fusionSpecies) {
       const fusionLevelMoves = this.getFusionSpeciesForm().getLevelMoves();
       const newLevelMoves: LevelMoves = [];
+      while (levelMoves[0][0] < startingLevel)
+        levelMoves.shift();
+      while (fusionLevelMoves[0][0] < startingLevel)
+        fusionLevelMoves.shift();
       for (let l = startingLevel; l <= this.level; l++) {
         while (levelMoves.length && levelMoves[0][0] === l) {
           const levelMove = levelMoves.shift();
