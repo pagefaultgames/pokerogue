c0aac06fab547bdcc5c31e655564601a2600ff7f
Flashfyre <flashfireex@gmail.com>
2024-01-02 17:09:36 -0500

    Fix mobile bug on starter select screen

--- src/ui/starter-select-ui-handler.ts
+++ src/ui/starter-select-ui-handler.ts
@@ -357,8 +357,6 @@ export default class StarterSelectUiHandler extends MessageUiHandler {
     if (button === Button.SUBMIT) {
       if (this.tryStart(true))
         success = true;
-      else
-        error = true;
     } else if (this.startCursorObj.visible) {
       switch (button) {
         case Button.ACTION:
