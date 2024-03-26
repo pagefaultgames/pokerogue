dfc330fb72c4299c83746f52aae3883c89c69d82
Up <upcraftedlp@gmail.com>
2024-03-26 01:02:54 +0100

    fix production builds
    need to keep class names due to serialization depending on them

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
-import fs from 'vite-plugin-fs';
+// import fs from 'vite-plugin-fs';
 
 export default defineConfig({
 	plugins: [/*fs()*/],
 	server: { host: '0.0.0.0', port: 8000 },
 	clearScreen: false,
+	build: {
+		minify: 'esbuild',
+	},
+	esbuild: {
+		keepNames: true,
+	}
 })
