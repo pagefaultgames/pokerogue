fab415c1563f86261bc64b9579d49ab4b863322f
Stophles <71789013+Stophles@users.noreply.github.com>
2024-04-04 19:33:07 -0500

    Toxic Spikes - Properly removed by statused Poison types
    Toxic spikes should now be removed by poison types who are statused before they are switched to, instead of ignoring their entry

--- src/data/arena-tag.ts
+++ src/data/arena-tag.ts
@@ -17,7 +17,21 @@ export enum ArenaTagSide {
   ENEMY
 }
 
-export abstract class ArenaTag {
+export abstract class ArenaTag {activateTrap(pokemon: Pokemon): boolean {
+    if (pokemon.isOfType(Type.POISON) && pokemon.isGrounded()) {
+      this.neutralized = true;
+      if (pokemon.scene.arena.removeTag(this.tagType)) {
+        pokemon.scene.queueMessage(getPokemonMessage(pokemon, ` absorbed the ${this.getMoveName()}!`));
+        return true;
+      }
+    }
+    else if (!pokemon.status && pokemon.isGrounded()) {
+      const toxic = this.layers > 1;
+      if (pokemon.trySetStatus(!toxic ? StatusEffect.POISON : StatusEffect.TOXIC, true, null, `the ${this.getMoveName()}`))
+        return true;      
+    }
+    return false;
+  }
   public tagType: ArenaTagType;
   public turnCount: integer;
   public sourceMove: Moves;
@@ -211,18 +225,20 @@ class ToxicSpikesTag extends ArenaTrapTag {
   }
 
   activateTrap(pokemon: Pokemon): boolean {
-    if (!pokemon.status && pokemon.isGrounded()) {
-      const toxic = this.layers > 1;
-      if (pokemon.trySetStatus(!toxic ? StatusEffect.POISON : StatusEffect.TOXIC, true, null, `the ${this.getMoveName()}`))
+    if (pokemon.isOfType(Type.POISON) && pokemon.isGrounded()) {
+      this.neutralized = true;
+      if (pokemon.scene.arena.removeTag(this.tagType)) {
+        pokemon.scene.queueMessage(getPokemonMessage(pokemon, ` absorbed the ${this.getMoveName()}!`));
         return true;
-      else if (pokemon.isOfType(Type.POISON)) {
-        this.neutralized = true;
-        if (pokemon.scene.arena.removeTag(this.tagType)) {
-          pokemon.scene.queueMessage(getPokemonMessage(pokemon, ` absorbed the ${this.getMoveName()}!`));
-          return true;
-        }
       }
     }
+    else if (!pokemon.status && pokemon.isGrounded()) {
+      const toxic = this.layers > 1;
+      if (pokemon.trySetStatus(!toxic ? StatusEffect.POISON : StatusEffect.TOXIC, true, null, `the ${this.getMoveName()}`))
+        return true;      
+    }
+    return false;
+  }
 
     return false;
   }
