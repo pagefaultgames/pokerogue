8cce54453697adfe69700eb160f807394f95609e
Flashfyre <flashfireex@gmail.com>
2024-04-15 14:45:00 -0400

    Replace intro with smaller file

--- src/plugins/cache-busted-loader-plugin.ts
+++ src/plugins/cache-busted-loader-plugin.ts
@@ -1,7 +1,5 @@
 let cacheBuster = '';
 
-const ignoredFiles = [ 'intro_dark' ];
-
 export default class CacheBustedLoaderPlugin extends Phaser.Loader.LoaderPlugin {
     constructor(scene: Phaser.Scene) {
         super(scene)
@@ -19,8 +17,7 @@ export default class CacheBustedLoaderPlugin extends Phaser.Loader.LoaderPlugin 
         if (!Array.isArray(file))
             file = [ file ];
     
-        if (!ignoredFiles.includes(file?.key) && cacheBuster)
-            file.forEach(item => item.url += '?v=' + cacheBuster);
+        file.forEach(item => item.url += '?v=' + cacheBuster);
 
         super.addFile(file);
     }
