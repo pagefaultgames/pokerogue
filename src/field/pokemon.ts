4fc194295b40d38da0760bb8ec895639005b789a
Flashfyre <flashfireex@gmail.com>
2024-04-21 19:58:55 -0400

    Remove event shiny bonus

--- src/field/pokemon.ts
+++ src/field/pokemon.ts
@@ -986,11 +986,8 @@ export default abstract class Pokemon extends Phaser.GameObjects.Container {
 
     let shinyThreshold = new Utils.IntegerHolder(32);
     if (thresholdOverride === undefined) {
-      if (!this.hasTrainer()) {
-        if (new Date() < new Date('4/22/2024'))
-          shinyThreshold.value *= 3;
+      if (!this.hasTrainer())
         this.scene.applyModifiers(ShinyRateBoosterModifier, true, shinyThreshold);
-      }
     } else
       shinyThreshold.value = thresholdOverride;
 
