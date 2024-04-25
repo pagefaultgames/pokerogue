a762101f64f5045be0134817b6434d7cb2cde294
lucfd <83493765+lucfd@users.noreply.github.com>
2024-04-25 01:07:08 -0400

    fixing merge conflicts

--- src/data/ability.ts
+++ src/data/ability.ts
@@ -3195,8 +3195,6 @@ export function initAbilities() {
       .attr(NoFusionAbilityAbAttr),
     new Ability(Abilities.QUICK_DRAW, "Quick Draw", "Enables the Pokémon to move first occasionally.", 8)
       .attr(IncrementMovePriorityAbAttr, (pokemon, move) => move.category !== MoveCategory.STATUS && Math.random() <= 1/3, 0.01),
-    new Ability(Abilities.QUICK_DRAW, 8)
-      .unimplemented(),
     new Ability(Abilities.UNSEEN_FIST, 8)
       .unimplemented(),
     new Ability(Abilities.CURIOUS_MEDICINE, 8)
