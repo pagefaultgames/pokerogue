4607782578eec09ad143e5be41993922d4f2daa6
Flashfyre <flashfireex@gmail.com>
2024-04-26 19:57:38 -0400

    Add temporary double wild shiny chance

--- src/field/pokemon.ts
+++ src/field/pokemon.ts
@@ -997,8 +997,11 @@ export default abstract class Pokemon extends Phaser.GameObjects.Container {
 
     let shinyThreshold = new Utils.IntegerHolder(32);
     if (thresholdOverride === undefined) {
-      if (!this.hasTrainer())
+      if (!this.hasTrainer()) {
+        if (new Date() < new Date('4/29/2024'))
+          shinyThreshold.value *= 2;
         this.scene.applyModifiers(ShinyRateBoosterModifier, true, shinyThreshold);
+      }
     } else
       shinyThreshold.value = thresholdOverride;
 
