0b438d0ff4acd2bcf0641ec1a3daf8a8da2bb844
Flashfyre <flashfireex@gmail.com>
2024-04-28 20:30:12 -0400

    Revert "Add temporary double wild shiny chance"
    This reverts commit 4607782578eec09ad143e5be41993922d4f2daa6.

--- src/field/pokemon.ts
+++ src/field/pokemon.ts
@@ -997,11 +997,8 @@ export default abstract class Pokemon extends Phaser.GameObjects.Container {
 
     let shinyThreshold = new Utils.IntegerHolder(32);
     if (thresholdOverride === undefined) {
-      if (!this.hasTrainer()) {
-        if (new Date() < new Date('4/29/2024'))
-          shinyThreshold.value *= 2;
+      if (!this.hasTrainer())
         this.scene.applyModifiers(ShinyRateBoosterModifier, true, shinyThreshold);
-      }
     } else
       shinyThreshold.value = thresholdOverride;
 
