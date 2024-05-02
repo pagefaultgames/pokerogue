9d8f7ddcf0b8afb07df95a10ec7839b8f83b1594
Benjamin Odom <bennybroseph@gmail.com>
2024-05-02 14:10:21 -0500

    Implement Tailwind (#398)
    Added a new ArenaTagType for Tailwind. Lasts 4 turns including the turn used.

--- src/data/arena-tag.ts
+++ src/data/arena-tag.ts
@@ -482,6 +482,20 @@ export class GravityTag extends ArenaTag {
   }
 }
 
+class TailwindTag extends ArenaTag {
+  constructor(turnCount: integer, sourceId: integer, side: ArenaTagSide) {
+    super(ArenaTagType.TAILWIND, turnCount, Moves.TAILWIND, sourceId, side);
+  }
+
+  onAdd(arena: Arena): void {
+    arena.scene.queueMessage(`The Tailwind blew from behind${this.side === ArenaTagSide.PLAYER ? '\nyour' : this.side === ArenaTagSide.ENEMY ? '\nthe opposing' : ''} team!`);
+  }
+
+  onRemove(arena: Arena): void {
+    arena.scene.queueMessage(`${this.side === ArenaTagSide.PLAYER ? 'Your' : this.side === ArenaTagSide.ENEMY ? 'The opposing' : ''} team's Tailwind petered out!`);
+  }
+}
+
 export function getArenaTag(tagType: ArenaTagType, turnCount: integer, sourceMove: Moves, sourceId: integer, targetIndex?: BattlerIndex, side: ArenaTagSide = ArenaTagSide.BOTH): ArenaTag {
   switch (tagType) {
     case ArenaTagType.MIST:
@@ -513,5 +527,7 @@ export function getArenaTag(tagType: ArenaTagType, turnCount: integer, sourceMov
       return new LightScreenTag(turnCount, sourceId, side);
     case ArenaTagType.AURORA_VEIL:
       return new AuroraVeilTag(turnCount, sourceId, side);
+    case ArenaTagType.TAILWIND:
+      return new TailwindTag(turnCount, sourceId, side);
   }
 }
