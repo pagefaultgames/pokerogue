dc3ee071d5689e8183636283e55574cafc2c20f3
Lugiad <adrien.grivel@hotmail.fr>
2024-05-02 14:34:59 +0200

    Update Spanish fight-ui-handler.ts

--- src/locales/es/fight-ui-handler.ts
+++ src/locales/es/fight-ui-handler.ts
@@ -2,5 +2,5 @@ import { SimpleTranslationEntries } from "#app/plugins/i18n";
 
 export const fightUiHandler: SimpleTranslationEntries = {
     "pp": "PP",
-    "power": "POWER",
+    "power": "POTENCIA",
 } as const;
