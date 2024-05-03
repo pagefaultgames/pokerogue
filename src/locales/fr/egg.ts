d7cff1bc0c1ab24792dea7192b1b88afb616a603
TeKrop <tekrop@proton.me>
2024-05-03 21:10:41 +0200

    fixes on french translations after review

--- src/locales/fr/egg.ts
+++ src/locales/fr/egg.ts
@@ -8,14 +8,14 @@ export const egg: SimpleTranslationEntries = {
   "defaultTier": "Commun",
   "hatchWavesMessageSoon": "Il fait du bruit. Il va éclore !",
   "hatchWavesMessageClose": "Il bouge de temps en temps. Il devrait bientôt éclore.",
-  "hatchWavesMessageNotClose": "Qu'est-ce qui va en sortir ? Ça va mettre du temps.",
-  "hatchWavesMessageLongTime": "Cet œuf va sûrement mettre du temps à éclore.",
+  "hatchWavesMessageNotClose": "Qu’est-ce qui va en sortir ? Ça va mettre du temps.",
+  "hatchWavesMessageLongTime": "Cet Œuf va sûrement mettre du temps à éclore.",
   "gachaTypeLegendary": "Taux de Légendaires élevé",
   "gachaTypeMove": "Taux de Capacité Œuf Rare élevé",
   "gachaTypeShiny": "Taux de Chromatiques élevé",
   "selectMachine": "Sélectionnez une machine.",
-  "notEnoughVouchers": "Vous n'avez pas assez de coupons!",
-  "tooManyEggs": "Vous avez trop d'œufs!",
+  "notEnoughVouchers": "Vous n’avez pas assez de coupons !",
+  "tooManyEggs": "Vous avez trop d’Œufs !",
   "pull": "Tirage",
   "pulls": "Tirages"
 } as const;
