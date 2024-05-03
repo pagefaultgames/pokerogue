9447cd01df498d6c0396dc7d5bd075735f69baac
Madi Simpson <madibyee@gmail.com>
2024-05-03 14:06:50 -0700

    bugfix: ensure hit recoil moves always do at least 1hp in recoil

--- src/data/move.ts
+++ src/data/move.ts
@@ -685,7 +685,7 @@ export class RecoilAttr extends MoveEffectAttr {
       return false;
 
     const recoilDamage = Math.max(Math.floor((!this.useHp ? user.turnData.damageDealt : user.getMaxHp()) * this.damageRatio),
-      !this.useHp && user.turnData.damageDealt ? 1 : 0);
+      user.turnData.damageDealt ? 1 : 0);
     if (!recoilDamage)
       return false;
 
