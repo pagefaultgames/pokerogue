4aad37a75800e75d88dd3f8015c761e243038f8f
lucfd <83493765+lucfd@users.noreply.github.com>
2024-05-05 14:04:19 -0400

    Implement Take Heart Sleep Talk edge case (#508)
    * removed take heart condition, added sleep talk edge case
    
    * removed freeze heal
    
    * removed testing stuff

--- src/data/move.ts
+++ src/data/move.ts
@@ -6249,7 +6249,7 @@ export function initMoves() {
       .partial(),
     new SelfStatusMove(Moves.TAKE_HEART, Type.PSYCHIC, -1, 10, -1, 0, 8)
       .attr(StatChangeAttr, [ BattleStat.SPATK, BattleStat.SPDEF ], 1, true)
-      .attr(HealStatusEffectAttr, true, StatusEffect.PARALYSIS, StatusEffect.POISON, StatusEffect.TOXIC, StatusEffect.BURN),
+      .attr(HealStatusEffectAttr, true, StatusEffect.PARALYSIS, StatusEffect.POISON, StatusEffect.TOXIC, StatusEffect.BURN, StatusEffect.SLEEP),
     /* Unused
     new AttackMove(Moves.G_MAX_WILDFIRE, Type.FIRE, MoveCategory.PHYSICAL, 10, -1, 10, -1, 0, 8)
       .target(MoveTarget.ALL_NEAR_ENEMIES)
