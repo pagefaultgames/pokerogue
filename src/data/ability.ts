c3bec2548917f810399638ba1440f08bd3523f0f
shayebeadlingkl <shayebeadling@gmail.com>
2024-05-06 19:18:59 -0400

    bypasses faint

--- src/data/ability.ts
+++ src/data/ability.ts
@@ -3081,7 +3081,8 @@ export function initAbilities() {
       .attr(BattleStatMultiplierAbAttr, BattleStat.SPATK, 0.5)
       .condition((pokemon) => pokemon.getHpRatio() <= 0.5),
     new Ability(Abilities.CURSED_BODY, 5)
-      .attr(PostDefendMoveDisableAbAttr, 30),
+      .attr(PostDefendMoveDisableAbAttr, 30)
+      .bypassFaint(),
     new Ability(Abilities.HEALER, 5)
       .unimplemented(),
     new Ability(Abilities.FRIEND_GUARD, 5)
