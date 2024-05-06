6ba8892d0a211b4c06a84a70a34439627e521a47
Matthew Ross <mcross98@gmail.com>
2024-05-05 19:50:17 -0700

    Facade should ignore burn damage reduction

--- src/field/pokemon.ts
+++ src/field/pokemon.ts
@@ -1366,7 +1366,7 @@ export default abstract class Pokemon extends Phaser.GameObjects.Container {
 
           if (!isTypeImmune) {
             damage.value = Math.ceil(((((2 * source.level / 5 + 2) * power.value * sourceAtk.value / targetDef.value) / 50) + 2) * stabMultiplier.value * typeMultiplier.value * arenaAttackTypeMultiplier * screenMultiplier.value * ((this.scene.randBattleSeedInt(15) + 85) / 100) * criticalMultiplier.value);
-            if (isPhysical && source.status && source.status.effect === StatusEffect.BURN) {
+            if (isPhysical && source.status && source.status.effect === StatusEffect.BURN && move.id !== Moves.FACADE) {
               const burnDamageReductionCancelled = new Utils.BooleanHolder(false);
               applyAbAttrs(BypassBurnDamageReductionAbAttr, source, burnDamageReductionCancelled);
               if (!burnDamageReductionCancelled.value)
