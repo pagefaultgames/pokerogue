eb126c10975b8d3347f60d61b81e174f51f34dc8
Nekiritan <138871410+Nekiritan@users.noreply.github.com>
2024-05-07 19:20:13 +0200

    Update menu-ui-handler.ts

--- src/locales/de/menu-ui-handler.ts
+++ src/locales/de/menu-ui-handler.ts
@@ -2,7 +2,7 @@ import { SimpleTranslationEntries } from "#app/plugins/i18n";
 
 export const menuUiHandler: SimpleTranslationEntries = {
     "GAME_SETTINGS": 'Spieleinstellungen',
-    "ACHIEVEMENTS": "Achievements",
+    "ACHIEVEMENTS": "Errungenschaften",
     "STATS": "Statistiken",
     "VOUCHERS": "Gutscheine",
     "EGG_LIST": "Eierliste",
