b6a29537e87ffb7a0fed6700dfe7eef53d2d2c55
Nekiritan <138871410+Nekiritan@users.noreply.github.com>
2024-05-07 19:21:20 +0200

    Update fight-ui-handler.ts

--- src/locales/de/fight-ui-handler.ts
+++ src/locales/de/fight-ui-handler.ts
@@ -1,6 +1,6 @@
 import { SimpleTranslationEntries } from "#app/plugins/i18n";
 
 export const fightUiHandler: SimpleTranslationEntries = {
-    "pp": "PP",
-    "power": "POWER",
+    "pp": "AP",
+    "power": "Stärke",
 } as const;
