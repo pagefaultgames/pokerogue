1b124e17307f81439bcbdbc5f656b7b9f5f2a7a4
SamuelHudson <samuel.hudson2017@gmail.com>
2024-05-07 15:54:56 -0500

    Implemented Ball Fetch

--- src/phases.ts
+++ src/phases.ts
@@ -4152,6 +4152,7 @@ export class AttemptCapturePhase extends PokemonPhase {
       scale: 1
     });
     
+    this.scene.currentBattle.lastUsedPokeball = this.pokeballType;
     this.removePb();
     this.end();
   }
