2bc1cfb0fc9e7364a71e6290704bb52d534fb38c
Flashfyre <flashfireex@gmail.com>
2024-05-09 20:16:29 -0400

    Extend the temporary shiny chance increase by 1 day

--- src/field/pokemon.ts
+++ src/field/pokemon.ts
@@ -1054,7 +1054,7 @@ export default abstract class Pokemon extends Phaser.GameObjects.Container {
     let shinyThreshold = new Utils.IntegerHolder(32);
     if (thresholdOverride === undefined) {
       if (!this.hasTrainer()) {
-        if (new Date() < new Date('2024-05-12'))
+        if (new Date() < new Date('2024-05-13'))
           shinyThreshold.value *= 3;
         this.scene.applyModifiers(ShinyRateBoosterModifier, true, shinyThreshold);
       }
