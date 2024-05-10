7fefaa13dae8e3b41c4c427edecb013d29939c02
notpatchmaybe <104580041+notpatchmaybe@users.noreply.github.com>
2024-05-10 14:18:58 +0100

    Removed an unneccesary line

--- src/data/move.ts
+++ src/data/move.ts
@@ -1782,7 +1782,6 @@ export class SwapStatsAttr extends MoveEffectAttr
         if (!super.apply(user, target, move, args))
             return false;
         let priorBoostArray : integer[] = [ 0, 0, 0, 0, 0, 0, 0 ];
-            priorBoostArray[s] = user.summonData.battleStats[s];
         for (let s = 0; s < target.summonData.battleStats.length; s++)
           {
             priorBoostArray[s] = user.summonData.battleStats[s];
