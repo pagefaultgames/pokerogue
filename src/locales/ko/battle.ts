0a90df66761459eb68661ff61d19a7eecfc53c65
GyeongMin Lee <59150207+gyeongent@users.noreply.github.com>
2024-05-11 14:53:45 +0900

    battle.ts 업데이트

--- src/locales/ko/battle.ts
+++
@@ -1,9 +0,0 @@
-import { SimpleTranslationEntries } from "#app/plugins/i18n";
-
-export const commandUiHandler: SimpleTranslationEntries = {
-    "fight": "싸운다",
-    "ball": "몬스터볼",
-    "pokemon": "포켓몬",
-    "run": "도망친다",
-    "actionMessage": "{{pokemonName}}은(는)\n무엇을 할까?",
-} as const;
