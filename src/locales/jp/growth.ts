9f011f67d28fcb569b6ff7208e20f41819d99d52
Lugiad <adrien.grivel@hotmail.fr>
2024-05-12 15:23:23 +0200

    Add japanese growth.ts growth

---
+++ src/locales/jp/growth.ts
@@ -0,0 +1,10 @@
+import { SimpleTranslationEntries } from "#app/plugins/i18n";
+
+export const growth: SimpleTranslationEntries = {
+    "Erratic": "60まんタイプ",
+    "Fast": "80まんタイプ",
+    "Medium_Fast": "100まんタイプ",
+    "Medium_Slow": "105まんタイプ",
+    "Slow": "125まんタイプ",
+    "Fluctuating": "164まんタイプ"
+} as const;
