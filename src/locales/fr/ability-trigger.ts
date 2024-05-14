0f9fd3b780393b0ce3eef8cc0d5b6c994bf89c8e
Lugiad <adrien.grivel@hotmail.fr>
2024-05-14 02:18:03 +0200

    Update French ability-trigger.ts

--- src/locales/fr/ability-trigger.ts
+++ src/locales/fr/ability-trigger.ts
@@ -1,5 +1,5 @@
 import { SimpleTranslationEntries } from "#app/plugins/i18n";
 
 export const abilityTriggers: SimpleTranslationEntries = {
-    'blockRecoilDamage' : `{{pokemonName}}'s {{abilityName}}\nprotected it from recoil!`,
+    'blockRecoilDamage' : `{{abilityName}}\nde {{pokemonName}} le protège du contrecoup !`,
 } as const;
