96d27fd44b55b2142dfc73673742832aedea9834
Xavion3 <xavion333@gmail.com>
2024-05-15 01:29:12 +1000

    Removes now unused getSunday function

--- src/utils.ts
+++ src/utils.ts
@@ -116,13 +116,6 @@ export function randSeedEasedWeightedItem<T>(items: T[], easingFunction: string 
   return items[Math.floor(easedValue * items.length)];
 }
 
-export function getSunday(date: Date): Date {
-  const day = date.getUTCDay();
-  const diff = date.getUTCDate() - day;
-  const newDate = new Date(date.setUTCDate(diff));
-  return new Date(Date.UTC(newDate.getUTCFullYear(), newDate.getUTCMonth(), newDate.getUTCDate()));
-}
-
 export function getFrameMs(frameCount: integer): integer {
   return Math.floor((1 / 60) * 1000 * frameCount);
 }
