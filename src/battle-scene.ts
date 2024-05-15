1e224a6ac48ace2e4b64a2da341307c304289bca
Madmadness65 <blaze.the.fireman@gmail.com>
2024-05-15 01:46:24 -0500

    Allow Original Color Magearna to be obtained
    Another completely cosmetic Pokémon form (and a nice looking one at that!).

--- src/battle-scene.ts
+++ src/battle-scene.ts
@@ -1012,6 +1012,7 @@ export default class BattleScene extends SceneBase {
 			case Species.FLORGES:
 			case Species.FURFROU:
 			case Species.ORICORIO:
+			case Species.MAGEARNA:
 			case Species.SQUAWKABILLY:
 			case Species.TATSUGIRI:
 			case Species.PALDEA_TAUROS:
