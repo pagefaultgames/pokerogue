fcffa000c5285beb2b31199cdfc3c64612771602
Lugiad <adrien.grivel@hotmail.fr>
2024-05-15 17:45:15 +0200

    French translation Save and Quit

--- src/locales/fr/menu-ui-handler.ts
+++ src/locales/fr/menu-ui-handler.ts
@@ -9,7 +9,7 @@ export const menuUiHandler: SimpleTranslationEntries = {
     "EGG_GACHA": "Gacha-Œufs",
     "MANAGE_DATA": "Mes données",
     "COMMUNITY": "Communauté",
-    "SAVE_AND_QUIT": "Save and Quit",
+    "SAVE_AND_QUIT": "Sauver & quitter",
     "LOG_OUT": "Déconnexion",
     "slot": "Emplacement {{slotNumber}}",
     "importSession": "Importer session",
