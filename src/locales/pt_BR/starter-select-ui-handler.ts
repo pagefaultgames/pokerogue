8abb340e4c8463915e36f0ff4430e0edc48977b0
Lugiad <adrien.grivel@hotmail.fr>
2024-05-15 15:24:32 +0200

    Added Gen names localizations in Starter Selection UI

--- src/locales/pt_BR/starter-select-ui-handler.ts
+++ src/locales/pt_BR/starter-select-ui-handler.ts
@@ -7,6 +7,15 @@ import { SimpleTranslationEntries } from "#app/plugins/i18n";
  */
 export const starterSelectUiHandler: SimpleTranslationEntries = {
     "confirmStartTeam": 'Começar com esses Pokémon?',
+    "gen1": "I",
+    "gen2": "II",
+    "gen3": "III",
+    "gen4": "IV",
+    "gen5": "V",
+    "gen6": "VI",
+    "gen7": "VII",
+    "gen8": "VII",
+    "gen9": "IX",
     "growthRate": "Crescimento:",
     "ability": "Hab.:",
     "passive": "Passiva:",
