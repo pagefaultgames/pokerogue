7f49b088039dde2efd31064e93730a84bcd2d66d
José Ricardo Fleury Oliveira <josefleury@discente.ufg.br>
2024-05-16 11:54:06 -0300

    Organized localization configs

--- src/locales/en/config.ts
+++ src/locales/en/config.ts
@@ -13,12 +13,11 @@ import { nature } from "./nature";
 import { pokeball } from "./pokeball";
 import { pokemon } from "./pokemon";
 import { pokemonStat } from "./pokemon-stat";
+import { splashMessages } from "./splash-messages";
 import { starterSelectUiHandler } from "./starter-select-ui-handler";
+import { titles, trainerClasses, trainerNames } from "./trainers";
 import { tutorial } from "./tutorial";
-import { titles,trainerClasses,trainerNames } from "./trainers";
-import { splashMessages } from "./splash-messages"
 import { weather } from "./weather";
-
 
 export const enConfig = {    
     ability: ability,
@@ -27,20 +26,20 @@ export const enConfig = {
     commandUiHandler: commandUiHandler,
     egg: egg,
     fightUiHandler: fightUiHandler,
-    menuUiHandler: menuUiHandler,
+    growth: growth,
     menu: menu,
+    menuUiHandler: menuUiHandler,
+    modifierType: modifierType,
     move: move,
+    nature: nature,
     pokeball: pokeball,
-    pokemonStat: pokemonStat,
     pokemon: pokemon,
+    pokemonStat: pokemonStat,
+    splashMessages: splashMessages,
     starterSelectUiHandler: starterSelectUiHandler,
     titles: titles,
     trainerClasses: trainerClasses,
     trainerNames: trainerNames,
     tutorial: tutorial,
-    splashMessages: splashMessages,
-    nature: nature,
-    growth: growth,
     weather: weather,
-    modifierType: modifierType,
 }
