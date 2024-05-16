452841d76c4b351d076d1536a6c9c6580cfff34f
hjh <j1441257360@163.com>
2024-05-16 13:13:57 +0800

    Chinese localization supplement and fixes

--- src/locales/zh_CN/growth.ts
+++ src/locales/zh_CN/growth.ts
@@ -1,10 +1,10 @@
 import { SimpleTranslationEntries } from "#app/plugins/i18n";
 
 export const growth: SimpleTranslationEntries = {
-    "Erratic": "Erratic",
-    "Fast": "Fast",
-    "Medium_Fast": "Medium Fast",
-    "Medium_Slow": "Medium Slow",
-    "Slow": "Slow",
-    "Fluctuating": "Fluctuating"
+    "Erratic": "最快",
+    "Fast": "快",
+    "Medium_Fast": "较快",
+    "Medium_Slow": "较慢",
+    "Slow": "慢",
+    "Fluctuating": "最慢"
 } as const;
