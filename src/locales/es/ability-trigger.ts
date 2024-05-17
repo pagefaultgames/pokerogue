8f017f3e899bf66b3a852b787ea2c2d3a61f6064
Sbug98 <68464158+Sbug98@users.noreply.github.com>
2024-05-17 18:05:01 +0200

    fixed ability name

--- src/locales/es/ability-trigger.ts
+++ src/locales/es/ability-trigger.ts
@@ -1,6 +1,6 @@
 import { SimpleTranslationEntries } from "#app/plugins/i18n";
 
 export const abilityTriggers: SimpleTranslationEntries = {
-    'blockRcoilDamage' : `{{pokemonName}}'s {{abilityName}}\nprotected it from recoil!`,
+    'blockRecoilDamage' : `{{pokemonName}}'s {{abilityName}}\nprotected it from recoil!`,
     'badDreams': `{{pokemonName}} Está atormentado!`
 } as const;
