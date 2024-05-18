8e0f4744479aeed75af7ab644a8e0cbbb3e4a5fd
MadridPawmot <42167718+Alekemon@users.noreply.github.com>
2024-05-18 21:38:32 +0200

    Added Spanish translations

--- src/locales/es/ability-trigger.ts
+++ src/locales/es/ability-trigger.ts
@@ -1,5 +1,5 @@
 import { SimpleTranslationEntries } from "#app/plugins/i18n";
 
 export const abilityTriggers: SimpleTranslationEntries = {
-    'blockRecoilDamage' : `{{pokemonName}}'s {{abilityName}}\nprotected it from recoil!`,
+    'blockRecoilDamage' : `¡{{abilityName}} de {{pokemonName}}\nle protegió de recibir\ndaño de retroceso!`,
 } as const;
