20604268571f5cddccad6722900fac9e4c13136b
MadridPawmot <42167718+Alekemon@users.noreply.github.com>
2024-05-18 23:33:01 +0200

    More translations

---
+++ src/ui/egg.ts
@@ -0,0 +1,21 @@
+import { SimpleTranslationEntries } from "#app/plugins/i18n";
+
+export const egg: SimpleTranslationEntries = {
+  "egg": "Huevo",
+  "greatTier": "Raro",
+  "ultraTier": "Épico",
+  "masterTier": "Legendario",
+  "defaultTier": "Común",
+  "hatchWavesMessageSoon": "Se escuchan sonidos. ¡Pronto saldrá!",
+  "hatchWavesMessageClose": "A veces se mueve. Debe estar a punto de salir.",
+  "hatchWavesMessageNotClose": "¿Qué habrá dentro? Tendrás que esperar un poco más.",
+  "hatchWavesMessageLongTime": "Parece que a este huevo le va a costar mucho abrirse.",
+  "gachaTypeLegendary": "Tasa de Legendarios incrementada",
+  "gachaTypeMove": "Tasa de Movimientos Huevo raros incrementada",
+  "gachaTypeShiny": "Tasa de Variocolor incrementada",
+  "selectMachine": "Elige una máquina.",
+  "notEnoughVouchers": "¡No tienes suficientes cupones!",
+  "tooManyEggs": "¡Tienes demasiados huevos!",
+  "pull": "Tirar",
+  "pulls": "Tiradas"
+} as const;
