c6f8a6564b663a21cf9e9f38b94939f2731e5bcf
MadridPawmot <42167718+Alekemon@users.noreply.github.com>
2024-05-18 21:49:14 +0200

    Add more Spanish translations

--- src/locales/es/egg.ts
+++ src/locales/es/egg.ts
@@ -1,21 +1,21 @@
 import { SimpleTranslationEntries } from "#app/plugins/i18n";
 
 export const egg: SimpleTranslationEntries = {
-  "egg": "Egg",
-  "greatTier": "Rare",
-  "ultraTier": "Epic",
-  "masterTier": "Legendary",
-  "defaultTier": "Common",
+  "egg": "Huevo",
+  "greatTier": "Raro",
+  "ultraTier": "Épico",
+  "masterTier": "Legendario",
+  "defaultTier": "Común",
   "hatchWavesMessageSoon": "Sounds can be heard coming from inside! It will hatch soon!",
   "hatchWavesMessageClose": "It appears to move occasionally. It may be close to hatching.",
   "hatchWavesMessageNotClose": "What will hatch from this? It doesn't seem close to hatching.",
   "hatchWavesMessageLongTime": "It looks like this Egg will take a long time to hatch.",
-  "gachaTypeLegendary": "Legendary Rate Up",
-  "gachaTypeMove": "Rare Egg Move Rate Up",
-  "gachaTypeShiny": "Shiny Rate Up",
-  "selectMachine": "Select a machine.",
-  "notEnoughVouchers": "You don't have enough vouchers!",
-  "tooManyEggs": "You have too many eggs!",
-  "pull": "Pull",
-  "pulls": "Pulls"
+  "gachaTypeLegendary": "Tasa de Legendarios incrementada",
+  "gachaTypeMove": "Tasa de Movimientos Huevo raros incrementada",
+  "gachaTypeShiny": "Tasa de Variocolor incrementada",
+  "selectMachine": "Elige una máquina.",
+  "notEnoughVouchers": "¡No tienes suficientes cupones!",
+  "tooManyEggs": "¡Tienes demasiados huevos!",
+  "pull": "Tirar",
+  "pulls": "Tiradas"
 } as const;
