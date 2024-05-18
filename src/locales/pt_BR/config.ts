2c5fd7373e41794e391ba5f31d8c7823b9cf7022
Lugiad <adrien.grivel@hotmail.fr>
2024-05-18 17:42:17 +0200

    Update config.ts

--- src/locales/pt_BR/config.ts
+++ src/locales/pt_BR/config.ts
@@ -19,6 +19,7 @@ import { titles, trainerClasses, trainerNames } from "./trainers";
 import { tutorial } from "./tutorial";
 import { weather } from "./weather";
 import { berry } from "./berry";
+import { gameStatsUiHandler } from "./game-stats-ui-handler";
 
 
 export const ptBrConfig = {    
@@ -45,4 +46,5 @@ export const ptBrConfig = {
     weather: weather,
     modifierType: modifierType,
     berry: berry,
+    gameStatsUiHandler: gameStatsUiHandler,
 }
