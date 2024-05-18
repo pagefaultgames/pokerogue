13e0c16ac28849f84c98727fdd1e49dbfd0aa8c1
Ned Pfeiffer <nedpfeiffer@proton.me>
2024-05-18 13:41:08 -0700

    shortened translation

--- src/locales/es/battle-message-ui-handler.ts
+++ src/locales/es/battle-message-ui-handler.ts
@@ -2,7 +2,7 @@ import { SimpleTranslationEntries } from "#app/plugins/i18n";
 
 export const battleMessageUiHandler: SimpleTranslationEntries = {
   "ivBest": "Inmejorable",
-  "ivFantastic": "Espectacular",
+  "ivFantastic": "Fantástico",
   "ivVeryGood": "Notable",
   "ivPrettyGood": "Genial",
   "ivDecent": "No está mal",
