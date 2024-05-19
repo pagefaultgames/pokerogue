f91c4339ce7f2ccf3013136dd949af6323e5ec35
GhostFlys <zhanghai91@gmail.com>
2024-05-20 06:27:17 +0800

    add egg.ts (#1138)

---
+++ src/locales/zh_CN/egg.ts
@@ -0,0 +1,21 @@
+import { SimpleTranslationEntries } from "#app/plugins/i18n";
+
+export const egg: SimpleTranslationEntries = {
+  "egg": "蛋",
+  "greatTier": "稀有",
+  "ultraTier": "史诗",
+  "masterTier": "传说",
+  "defaultTier": "普通",
+  "hatchWavesMessageSoon": "里面传来声音！似乎快要孵化了！",
+  "hatchWavesMessageClose": "有时好像会动一下。就快孵化了吧？",
+  "hatchWavesMessageNotClose": "会孵化出什么呢？看来还需要很长时间才能孵化。",
+  "hatchWavesMessageLongTime": "这个蛋需要很长时间才能孵化。",
+  "gachaTypeLegendary": "传说概率上升",
+  "gachaTypeMove": "稀有概率上升",
+  "gachaTypeShiny": "闪光概率上升",
+  "selectMachine": "选择一个机器。",
+  "notEnoughVouchers": "你没有足够的兑换券！",
+  "tooManyEggs": "你的蛋太多啦！",
+  "pull": "抽",
+  "pulls": "连抽"
+} as const;
