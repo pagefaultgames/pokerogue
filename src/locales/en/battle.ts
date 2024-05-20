29984b3ba1ffc2196fe395ff9f10dd94d9b24cd7
Lugiad <adrien.grivel@hotmail.fr>
2024-05-20 02:15:18 +0200

    Update battle.ts

--- src/locales/en/battle.ts
+++ src/locales/en/battle.ts
@@ -4,7 +4,7 @@ export const battle: SimpleTranslationEntries = {
   "bossAppeared": "{{bossName}} appeared.",
   "trainerAppeared": "{{trainerName}}\nwould like to battle!",
   "trainerAppearedDouble": "{{trainerName}}\nwould like to battle!",
-  "trainerSendOut": "{{trainerName}} sent out\n{{pokemonName}}!",//
+  "trainerSendOut": "{{trainerName}} sent out\n{{pokemonName}}!",
   "singleWildAppeared": "A wild {{pokemonName}} appeared!",
   "multiWildAppeared": "A wild {{pokemonName1}}\nand {{pokemonName2}} appeared!",
   "playerComeBack": "Come back, {{pokemonName}}!",
@@ -14,7 +14,7 @@ export const battle: SimpleTranslationEntries = {
   "switchQuestion": "Will you switch\n{{pokemonName}}?",
   "trainerDefeated": `You defeated\n{{trainerName}}!`,
   "moneyWon": "You got\n₽{{moneyAmount}} for winning!",
-  "modifierReceived": "You received\n{{modifierName}}!",//
+  "modifierReceived": "You received\n{{modifierName}}!",
   "pokemonCaught": "{{pokemonName}} was caught!",
   "partyFull": "Your party is full.\nRelease a Pokémon to make room for {{pokemonName}}",
   "pokemon": "Pokémon",
