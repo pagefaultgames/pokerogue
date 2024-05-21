42e2be22bc8eac3a184095418bed49df3a810c5d
Sbug98 <68464158+Sbug98@users.noreply.github.com>
2024-05-21 09:26:01 +0200

    Implements bad dreams (#633)
    * test commit
    
    * de modified readme
    
    * first implementation, needs testing with healer
    
    * fixed beahviour if 2+ opponents are on field, also added message
    
    * further fixed
    
    * reset of overrides.ts before merge to main
    
    * fixed grammar
    
    Co-authored-by: Jonas Pinson <jonas.pinson@gmail.com>
    
    * Implemented changes suggested by @bennybroseph and @TempsRay
    
    * Added more docs to the class
    
    * removed old comments
    
    * fixed ability name in comments
    
    * added translation and made message localized
    
    * fixed ability name
    
    * added missing bracket
    
    ---------
    
    Co-authored-by: Jonas Pinson <jonas.pinson@gmail.com>

--- src/locales/de/ability-trigger.ts
+++ src/locales/de/ability-trigger.ts
@@ -2,4 +2,5 @@ import { SimpleTranslationEntries } from "#app/plugins/i18n";
 
 export const abilityTriggers: SimpleTranslationEntries = {
     'blockRecoilDamage' : `{{pokemonName}} wurde durch {{abilityName}}\nvor Rückstoß geschützt!`,
+    'badDreams': `{{pokemonName}} wird gequält!`,
 } as const;
