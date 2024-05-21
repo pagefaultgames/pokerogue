6405bcab875864f221cd9627206c45e3a157b262
Lugiad <adrien.grivel@hotmail.fr>
2024-05-21 15:53:06 +0200

    Correction to French ability-trigger.ts (#1200)

--- src/locales/fr/ability-trigger.ts
+++ src/locales/fr/ability-trigger.ts
@@ -2,5 +2,5 @@ import { SimpleTranslationEntries } from "#app/plugins/i18n";
 
 export const abilityTriggers: SimpleTranslationEntries = {
     'blockRecoilDamage' : `{{abilityName}}\nde {{pokemonName}} le protège du contrecoup !`,
-    'badDreams': `{{pokemonName}} est tourmenté!`
+    'badDreams': `{{pokemonName}} a le sommeil agité !`
 } as const;
