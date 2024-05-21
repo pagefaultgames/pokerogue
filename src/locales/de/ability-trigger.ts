cac6db0aafba7a3118d8144fd7bff42b15fceb81
Jannik Tappert <tappertjannik@googlemail.com>
2024-05-21 16:06:01 +0200

    Fixed german localization of bad dreams

--- src/locales/de/ability-trigger.ts
+++ src/locales/de/ability-trigger.ts
@@ -2,5 +2,5 @@ import { SimpleTranslationEntries } from "#app/plugins/i18n";
 
 export const abilityTriggers: SimpleTranslationEntries = {
     'blockRecoilDamage' : `{{pokemonName}} wurde durch {{abilityName}}\nvor Rückstoß geschützt!`,
-    'badDreams': `{{pokemonName}} wird gequält!`,
+    'badDreams': `{{pokemonName}} ist in einem Alptraum gefangen!`,
 } as const;
