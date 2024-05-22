0f56f7614fd6b337fe891a2291081b9f501a50e1
torranx <torranicles@gmail.com>
2024-05-23 02:26:40 +0800

    fix force switch move

--- src/phases.ts
+++ src/phases.ts
@@ -1464,11 +1464,12 @@ export class SwitchSummonPhase extends SummonPhase {
 
   onEnd(): void {
     super.onEnd();
-
+    
     const pokemon = this.getPokemon();
-    const moveId = pokemon.scene.currentBattle.turnCommands[this.fieldIndex]?.move?.move;
-    const lastUsedMove = moveId ? allMoves[moveId] : undefined;
 
+    const moveId = this.lastPokemon?.scene.currentBattle.lastMove;
+    const lastUsedMove = moveId ? allMoves[moveId] : undefined;
+    
     // Compensate for turn spent summoning
     if (pokemon.scene.currentBattle.turnCommands[this.fieldIndex]?.command === Command.POKEMON || !!lastUsedMove?.findAttr(attr => attr instanceof ForceSwitchOutAttr)) //check if hard switch OR pivot move was used
       pokemon.battleSummonData.turnCount--;
