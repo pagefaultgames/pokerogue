f712853d261074c2a81721c0d70a496fb4d60fb9
Benjamin Odom <bennybroseph@gmail.com>
2024-05-22 03:12:19 -0500

    Update pokemon.ts

--- src/field/pokemon.ts
+++ src/field/pokemon.ts
@@ -3455,6 +3455,18 @@ export enum HitResult {
 
 export type DamageResult = HitResult.EFFECTIVE | HitResult.SUPER_EFFECTIVE | HitResult.NOT_VERY_EFFECTIVE | HitResult.ONE_HIT_KO | HitResult.OTHER;
 
+export class PpUsedEvent extends Event {
+  public pokemonMove: PokemonMove;
+  public count: number;
+
+  constructor(pokemonMove: PokemonMove, count: number) {
+    super('PpUsed');
+
+    this.pokemonMove = pokemonMove;
+    this.count = count;
+  }
+}
+
 export class PokemonMove {
   public moveId: Moves;
   public ppUsed: integer;
@@ -3462,6 +3474,8 @@ export class PokemonMove {
   /** Tracks if a move has ever been used before */
   public used: boolean;
   public virtual: boolean;
+
+  public eventTarget: EventTarget;
 
   constructor(moveId: Moves, ppUsed?: integer, ppUp?: integer, virtual?: boolean) {
     this.moveId = moveId;
@@ -3487,8 +3501,10 @@ export class PokemonMove {
   usePp(count: number = 1) {
     if (count > 0)
       this.used = true; // If PP has been used, the move has been used
-    
+
     this.ppUsed = Math.min(this.ppUsed + count, this.getMovePp());
+
+    this.eventTarget.dispatchEvent(new PpUsedEvent(this, count)); // Alert Subscribers
   }
 
   getMovePp(): integer {
