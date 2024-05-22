742dfa09cc4188db25850d5aaddc3fb2c223eb25
Laeticia PIERRE <laeticia.pierre06200@gmail.com>
2024-05-22 14:47:39 +0200

    New test for missing localization case

--- src/data/trainer-config.test.ts
+++ src/data/trainer-config.test.ts
@@ -2,6 +2,7 @@ import { expect, describe, it, beforeAll, vi, afterAll, beforeEach } from "vites
 import { TrainerConfig, TrainerSlot } from "./trainer-config";
 import { TrainerType } from "./enums/trainer-type";
 import { TrainerVariant } from "#app/field/trainer.js";
+import i18next from "i18next";
 
 describe("trainer-config", () => {
   describe("getTitle", () => {
@@ -54,6 +55,16 @@ describe("trainer-config", () => {
     it("returns the trainer type name when gender selected but trainer variant default", () => {
       trainerConfig.hasGenders = true;
       expect(trainerConfig.getTitle(TrainerSlot.NONE, TrainerVariant.DEFAULT)).toBe("Artist");
+    });
+
+    it("returns the trainer type name when want to add _female but female version does not exist in the localization", () => {
+      trainerConfig.hasGenders = true;
+      const i18nextExistsSpy = jest.spyOn(i18next, 'exists').mockReturnValue(false);
+
+      const title = trainerConfig.getTitle(TrainerSlot.NONE, TrainerVariant.FEMALE);
+
+      expect(i18nextExistsSpy).toBeCalled();
+      expect(title).toBe("Artist");
     });
   });
 });
