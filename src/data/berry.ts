92212ffd47e3ce1c893bd4cd540a820680bb5d22
Ethan <hensley.ethan64@gmail.com>
2024-05-24 10:48:39 -0400

    Fix berry conflicts

--- src/data/berry.ts
+++ src/data/berry.ts
@@ -77,59 +77,67 @@ export type BerryEffectFunc = (pokemon: Pokemon) => void;
 
 export function getBerryEffectFunc(berryType: BerryType): BerryEffectFunc {
   switch (berryType) {
-    case BerryType.SITRUS:
-    case BerryType.ENIGMA:
-      return (pokemon: Pokemon) => {
-        if (pokemon.battleData)
-          pokemon.battleData.berriesEaten.push(berryType);
-        const hpHealed = new Utils.NumberHolder(Math.floor(pokemon.getMaxHp() / 4));
-        applyAbAttrs(DoubleBerryEffectAbAttr, pokemon, null, hpHealed);
-        pokemon.scene.unshiftPhase(new PokemonHealPhase(pokemon.scene, pokemon.getBattlerIndex(),
-          hpHealed.value, getPokemonMessage(pokemon, `'s ${getBerryName(berryType)}\nrestored its HP!`), true));
-      };
-    case BerryType.LUM:
-      return (pokemon: Pokemon) => {
-        if (pokemon.battleData)
-          pokemon.battleData.berriesEaten.push(berryType);
-        if (pokemon.status)
-          pokemon.scene.queueMessage(getPokemonMessage(pokemon, getStatusEffectHealText(pokemon.status.effect)));
-        pokemon.resetStatus(true, true)
-        pokemon.updateInfo();
-      };
-    case BerryType.LIECHI:
-    case BerryType.GANLON:
-    case BerryType.PETAYA:
-    case BerryType.APICOT:
-    case BerryType.SALAC:
-      return (pokemon: Pokemon) => {
-        if (pokemon.battleData)
-          pokemon.battleData.berriesEaten.push(berryType);
-        const battleStat = (berryType - BerryType.LIECHI) as BattleStat;
-        const statLevels = new Utils.NumberHolder(1);
-        applyAbAttrs(DoubleBerryEffectAbAttr, pokemon, null, statLevels);
-        pokemon.scene.unshiftPhase(new StatChangePhase(pokemon.scene, pokemon.getBattlerIndex(), true, [ battleStat ], statLevels.value));
-      };
-    case BerryType.LANSAT:
-      return (pokemon: Pokemon) => {
-        if (pokemon.battleData)
-          pokemon.battleData.berriesEaten.push(berryType);
-        pokemon.addTag(BattlerTagType.CRIT_BOOST);
-      };
-    case BerryType.STARF:
-      return (pokemon: Pokemon) => {
-        if (pokemon.battleData)
-          pokemon.battleData.berriesEaten.push(berryType);
-        const statLevels = new Utils.NumberHolder(2);
-        applyAbAttrs(DoubleBerryEffectAbAttr, pokemon, null, statLevels);
-        pokemon.scene.unshiftPhase(new StatChangePhase(pokemon.scene, pokemon.getBattlerIndex(), true, [ BattleStat.RAND ], statLevels.value));
-      };
-    case BerryType.LEPPA:
-      return (pokemon: Pokemon) => {
-        if (pokemon.battleData)
-          pokemon.battleData.berriesEaten.push(berryType);
-        const ppRestoreMove = pokemon.getMoveset().find(m => !m.getPpRatio());
+  case BerryType.SITRUS:
+  case BerryType.ENIGMA:
+    return (pokemon: Pokemon) => {
+      if (pokemon.battleData) {
+        pokemon.battleData.berriesEaten.push(berryType);
+      }
+      const hpHealed = new Utils.NumberHolder(Math.floor(pokemon.getMaxHp() / 4));
+      applyAbAttrs(DoubleBerryEffectAbAttr, pokemon, null, hpHealed);
+      pokemon.scene.unshiftPhase(new PokemonHealPhase(pokemon.scene, pokemon.getBattlerIndex(),
+        hpHealed.value, getPokemonMessage(pokemon, `'s ${getBerryName(berryType)}\nrestored its HP!`), true));
+    };
+  case BerryType.LUM:
+    return (pokemon: Pokemon) => {
+      if (pokemon.battleData) {
+        pokemon.battleData.berriesEaten.push(berryType);
+      }
+      if (pokemon.status)
+        pokemon.scene.queueMessage(getPokemonMessage(pokemon, getStatusEffectHealText(pokemon.status.effect)));
+      pokemon.resetStatus(true, true)
+      pokemon.updateInfo();
+    };
+  case BerryType.LIECHI:
+  case BerryType.GANLON:
+  case BerryType.PETAYA:
+  case BerryType.APICOT:
+  case BerryType.SALAC:
+    return (pokemon: Pokemon) => {
+      if (pokemon.battleData) {
+        pokemon.battleData.berriesEaten.push(berryType);
+      }
+      const battleStat = (berryType - BerryType.LIECHI) as BattleStat;
+      const statLevels = new Utils.NumberHolder(1);
+      applyAbAttrs(DoubleBerryEffectAbAttr, pokemon, null, statLevels);
+      pokemon.scene.unshiftPhase(new StatChangePhase(pokemon.scene, pokemon.getBattlerIndex(), true, [ battleStat ], statLevels.value));
+    };
+  case BerryType.LANSAT:
+    return (pokemon: Pokemon) => {
+      if (pokemon.battleData) {
+        pokemon.battleData.berriesEaten.push(berryType);
+      }
+      pokemon.addTag(BattlerTagType.CRIT_BOOST);
+    };
+  case BerryType.STARF:
+    return (pokemon: Pokemon) => {
+      if (pokemon.battleData) {
+        pokemon.battleData.berriesEaten.push(berryType);
+      }
+      const statLevels = new Utils.NumberHolder(2);
+      applyAbAttrs(DoubleBerryEffectAbAttr, pokemon, null, statLevels);
+      pokemon.scene.unshiftPhase(new StatChangePhase(pokemon.scene, pokemon.getBattlerIndex(), true, [ BattleStat.RAND ], statLevels.value));
+    };
+  case BerryType.LEPPA:
+    return (pokemon: Pokemon) => {
+      if (pokemon.battleData) {
+        pokemon.battleData.berriesEaten.push(berryType);
+      }
+      const ppRestoreMove = pokemon.getMoveset().find(m => !m.getPpRatio());
+      if (ppRestoreMove !== undefined) {
         ppRestoreMove.ppUsed = Math.max(ppRestoreMove.ppUsed - 10, 0);
         pokemon.scene.queueMessage(getPokemonMessage(pokemon, ` restored PP to its move ${ppRestoreMove.getName()}\nusing its ${getBerryName(berryType)}!`));
-      };
+      }
+    };
   }
 }
