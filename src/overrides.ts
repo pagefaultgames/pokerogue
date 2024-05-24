d68629ba93039665047c11ec05ec41655a83bc45
Benjamin Odom <bennybroseph@gmail.com>
2024-05-24 18:33:11 -0500

    Update overrides.ts

--- src/overrides.ts
+++ src/overrides.ts
@@ -26,7 +26,7 @@ export const SEED_OVERRIDE: string = "";
 export const WEATHER_OVERRIDE: WeatherType = WeatherType.NONE;
 export const DOUBLE_BATTLE_OVERRIDE: boolean = false;
 export const STARTING_WAVE_OVERRIDE: integer = 0;
-export const STARTING_BIOME_OVERRIDE: Biome = Biome.GRASSY_FIELD;
+export const STARTING_BIOME_OVERRIDE: Biome = Biome.TOWN;
 // default 1000
 export const STARTING_MONEY_OVERRIDE: integer = 0;
 export const POKEBALL_OVERRIDE: { active: boolean, pokeballs: PokeballCounts } = {
