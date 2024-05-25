9ba7fe325ac75522417b660241addddb1a6858b8
Benjamin Odom <bennybroseph@gmail.com>
2024-05-25 11:08:42 -0500

    Update battle-scene-events.ts

--- src/battle-scene-events.ts
+++ src/battle-scene-events.ts
@@ -2,13 +2,25 @@ import Move from "./data/move";
 
 /** Alias for all {@linkcode BattleScene} events */
 export enum BattleSceneEventType {
-  /** @see {@linkcode MoveUsedEvent} */
+  /**
+   * Triggers when a move is successfully used
+   * @see {@linkcode MoveUsedEvent}
+   */
   MOVE_USED = "onMoveUsed",
-  /** @see {@linkcode TurnInitEvent} */
+  /**
+   * Triggers on the first turn of a new battle
+   * @see {@linkcode TurnInitEvent}
+   */
   TURN_INIT = "onTurnInit",
-  /** @see {@linkcode TurnEndEvent} */
+  /**
+   * Triggers after a turn ends in battle
+   * @see {@linkcode TurnEndEvent}
+   */
   TURN_END  = "onTurnEnd",
-  /** @see {@linkcode NewArenaEvent} */
+  /**
+   * Triggers when a new {@linkcode Arena} is created during initialization
+   * @see {@linkcode NewArenaEvent}
+   */
   NEW_ARENA = "onNewArena",
 }
 
