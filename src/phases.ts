1131fcfdb4d48af06c1a49487122208cbbce50be
Jakub Hanko <60473007+JakubHanko@users.noreply.github.com>
2024-05-25 15:01:08 +0200

    Implement Honey Gather (#1360)
    * Implement Honey Gather
    
    * Updated Ability Description
    
    ---------
    
    Co-authored-by: Benjamin Odom <bennybroseph@gmail.com>

--- src/phases.ts
+++ src/phases.ts
@@ -2301,9 +2301,6 @@ export class BattleEndPhase extends BattlePhase {
     super.start();
 
     this.scene.currentBattle.addBattleScore(this.scene);
-    if (this.scene.currentBattle.moneyScattered) {
-      this.scene.currentBattle.pickUpScatteredMoney(this.scene);
-    }
 
     this.scene.gameData.gameStats.battles++;
     if (this.scene.currentBattle.trainer) {
@@ -2321,6 +2318,10 @@ export class BattleEndPhase extends BattlePhase {
 
     for (const pokemon of this.scene.getParty().filter(p => !p.isFainted())) {
       applyPostBattleAbAttrs(PostBattleAbAttr, pokemon);
+    }
+
+    if (this.scene.currentBattle.moneyScattered) {
+      this.scene.currentBattle.pickUpScatteredMoney(this.scene);
     }
 
     this.scene.clearEnemyHeldItemModifiers();
