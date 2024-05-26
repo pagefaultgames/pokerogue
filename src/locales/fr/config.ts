d0818d34c72d4be0dc510170b49be2d1b8d9de92
Lugiad <adrien.grivel@hotmail.fr>
2024-05-26 22:42:16 +0200

    Update config.ts

--- src/locales/fr/config.ts
+++ src/locales/fr/config.ts
@@ -22,7 +22,7 @@ import { battleMessageUiHandler } from "./battle-message-ui-handler";
 import { berry } from "./berry";
 import { voucher } from "./voucher";
 import { biome } from "./biome";
-
+import { pokemonInfoContainer } from "./pokemon-info-container";
 
 export const frConfig = {
   ability: ability,
@@ -51,4 +51,5 @@ export const frConfig = {
   berry: berry,
   voucher: voucher,
   biome: biome,
+  pokemonInfoContainer: pokemonInfoContainer,
 };
