813880a5169664b4423bf870b0b20f3d1d4b3b63
Lugiad <adrien.grivel@hotmail.fr>
2024-05-26 22:43:40 +0200

    Update config.ts

--- src/locales/zh_TW/config.ts
+++ src/locales/zh_TW/config.ts
@@ -22,6 +22,7 @@ import { battleMessageUiHandler } from "./battle-message-ui-handler";
 import { berry } from "./berry";
 import { voucher } from "./voucher";
 import { biome } from "./biome";
+import { pokemonInfoContainer } from "./pokemon-info-container";
 
 export const zhTWConfig = {
   ability: ability,
@@ -50,4 +51,5 @@ export const zhTWConfig = {
   berry: berry,
   voucher: voucher,
   biome: biome,
+  pokemonInfoContainer: pokemonInfoContainer,
 };
