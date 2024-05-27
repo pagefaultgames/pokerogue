4f87d39da5270102e5868e8e64eace33cbc936ed
Franck Trouillez <francktrouillez99@gmail.com>
2024-05-27 13:49:18 +0200

    Make EnemyPokemon stronger depending on Prestige Level if in Prestige Mode
    Add POKEMON_STATS Prestige modifiers.
    Allow to modify EnemyPokemon Battle Stats if in Prestige Mode, depending on the Prestige.

--- src/system/prestige.ts
+++ src/system/prestige.ts
@@ -1,4 +1,13 @@
-export enum PrestigeModifierAttribute {}
+import { Stat } from "#app/data/pokemon-stat";
+
+export enum PrestigeModifierAttribute {
+  WILD_POKEMON_ATTACK,
+  WILD_POKEMON_DEFENSE,
+  WILD_POKEMON_SPEED,
+  TRAINER_POKEMON_ATTACK,
+  TRAINER_POKEMON_DEFENSE,
+  TRAINER_POKEMON_SPEED
+}
 
 enum PrestigeModifierOperation {
   ADD,
@@ -21,25 +30,50 @@ const PRESTIGE_MODIFIERS: PrestigeModifier[][] = [
   // Level 0 - Should be empty
   [],
   // Level 1
-  [],
+  [
+    new PrestigeModifier(PrestigeModifierAttribute.TRAINER_POKEMON_ATTACK, PrestigeModifierOperation.MULTIPLY, 1.05),
+    new PrestigeModifier(PrestigeModifierAttribute.TRAINER_POKEMON_DEFENSE, PrestigeModifierOperation.MULTIPLY, 1.05)
+  ],
   // Level 2
   [],
   // Level 3
-  [],
+  [
+    new PrestigeModifier(PrestigeModifierAttribute.WILD_POKEMON_ATTACK, PrestigeModifierOperation.MULTIPLY, 1.05),
+    new PrestigeModifier(PrestigeModifierAttribute.WILD_POKEMON_DEFENSE, PrestigeModifierOperation.MULTIPLY, 1.05)
+  ],
   // Level 4
   [],
   // Level 5
-  [],
+  [
+    new PrestigeModifier(PrestigeModifierAttribute.WILD_POKEMON_ATTACK, PrestigeModifierOperation.MULTIPLY, 1.1),
+    new PrestigeModifier(PrestigeModifierAttribute.WILD_POKEMON_DEFENSE, PrestigeModifierOperation.MULTIPLY, 1.1),
+    new PrestigeModifier(PrestigeModifierAttribute.TRAINER_POKEMON_ATTACK, PrestigeModifierOperation.MULTIPLY, 1.1),
+    new PrestigeModifier(PrestigeModifierAttribute.TRAINER_POKEMON_DEFENSE, PrestigeModifierOperation.MULTIPLY, 1.1),
+    new PrestigeModifier(PrestigeModifierAttribute.WILD_POKEMON_SPEED, PrestigeModifierOperation.MULTIPLY, 1.3),
+    new PrestigeModifier(PrestigeModifierAttribute.TRAINER_POKEMON_SPEED, PrestigeModifierOperation.MULTIPLY, 1.3)
+  ],
   // Level 6
   [],
   // Level 7
-  [],
+  [
+    new PrestigeModifier(PrestigeModifierAttribute.WILD_POKEMON_ATTACK, PrestigeModifierOperation.MULTIPLY, 1.1),
+    new PrestigeModifier(PrestigeModifierAttribute.WILD_POKEMON_DEFENSE, PrestigeModifierOperation.MULTIPLY, 1.1)
+  ],
   // Level 8
   [],
   // Level 9
-  [],
+  [
+    new PrestigeModifier(PrestigeModifierAttribute.TRAINER_POKEMON_ATTACK, PrestigeModifierOperation.MULTIPLY, 1.1),
+    new PrestigeModifier(PrestigeModifierAttribute.TRAINER_POKEMON_DEFENSE, PrestigeModifierOperation.MULTIPLY, 1.1)
+  ],
   // Level 10
-  []
+  [
+    new PrestigeModifier(PrestigeModifierAttribute.TRAINER_POKEMON_ATTACK, PrestigeModifierOperation.MULTIPLY, 1.1),
+    new PrestigeModifier(PrestigeModifierAttribute.TRAINER_POKEMON_DEFENSE, PrestigeModifierOperation.MULTIPLY, 1.1),
+    new PrestigeModifier(PrestigeModifierAttribute.WILD_POKEMON_ATTACK, PrestigeModifierOperation.MULTIPLY, 1.1),
+    new PrestigeModifier(PrestigeModifierAttribute.WILD_POKEMON_DEFENSE, PrestigeModifierOperation.MULTIPLY, 1.1),
+    new PrestigeModifier(PrestigeModifierAttribute.TRAINER_POKEMON_SPEED, PrestigeModifierOperation.MULTIPLY, 2)
+  ]
 ];
 
 export abstract class Prestige {
@@ -67,6 +101,28 @@ export abstract class Prestige {
           return acc * modifier.value;
         }
       }, value);
+  }
+
+  /**
+   * Get the modifier attribute for the given stat and if the pokemon is wild or not
+   *
+   * @param stat
+   * @param isWildPokemon
+   * @returns the modifier attribute
+   */
+  public static getModifierAttributeFromStat(stat: Stat, isWildPokemon: boolean = true): PrestigeModifierAttribute {
+    switch (stat) {
+    case Stat.ATK:
+    case Stat.SPATK:
+      return isWildPokemon ? PrestigeModifierAttribute.WILD_POKEMON_ATTACK : PrestigeModifierAttribute.TRAINER_POKEMON_ATTACK;
+    case Stat.DEF:
+    case Stat.SPDEF:
+      return isWildPokemon ? PrestigeModifierAttribute.WILD_POKEMON_DEFENSE : PrestigeModifierAttribute.TRAINER_POKEMON_DEFENSE;
+    case Stat.SPD:
+      return isWildPokemon ? PrestigeModifierAttribute.WILD_POKEMON_SPEED : PrestigeModifierAttribute.TRAINER_POKEMON_SPEED;
+    default:
+      return null;
+    }
   }
 
   /**
