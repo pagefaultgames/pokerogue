102355881fa253a152b059ee46a2dfe10433c9eb
MadridPawmot <42167718+Alekemon@users.noreply.github.com>
2024-05-27 18:07:22 +0200

    Update trainers.ts

--- src/locales/pt_BR/trainers.ts
+++ src/locales/pt_BR/trainers.ts
@@ -94,6 +94,7 @@ export const trainerClasses: SimpleTranslationEntries = {
   "rich_kid_female": "Garota Rica",
   "rich_kids": "Garotos Ricos",
   "roughneck": "Arruaceiro",
+  "sailor": "Sailor",
   "scientist": "Cientista",
   "scientist_female": "Cientista",
   "scientists": "Cientistas",
