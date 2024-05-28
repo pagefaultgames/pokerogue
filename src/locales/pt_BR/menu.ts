bd260b7fa9c966e5654c41304d3da7145b32bc5a
torranx <torranicles@gmail.com>
2024-05-29 03:47:14 +0800

    [LOCALIZATION] add pt_BR

--- src/locales/pt_BR/menu.ts
+++ src/locales/pt_BR/menu.ts
@@ -11,7 +11,7 @@ export const menu: SimpleTranslationEntries = {
   "dailyRun": "Desafio Diário (Beta)",
   "loadGame": "Carregar Jogo",
   "newGame": "Novo Jogo",
-  "settings": "Settings",
+  "settings": "Configurações",
   "selectGameMode": "Escolha um modo de jogo.",
   "logInOrCreateAccount": "Inicie uma sessão ou crie uma conta para começar. Não é necessário email!",
   "username": "Nome de Usuário",
