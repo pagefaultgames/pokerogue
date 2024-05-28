fb8fa6140dbdf2ff1542fccd5959b2aa125b6aba
Jannik Tappert <tappertjannik@googlemail.com>
2024-05-28 19:41:12 +0200

    The Achivment Bar now grows with the context

--- src/ui/achv-bar.ts
+++ src/ui/achv-bar.ts
@@ -2,7 +2,11 @@ import BattleScene from "../battle-scene";
 import {Achv, getAchievementDescription} from "../system/achv";
 import { Voucher } from "../system/voucher";
 import { TextStyle, addTextObject } from "./text";
-import i18next from "i18next";
+
+const ICON_Y_OFFSET = 4;
+const FONT_SIZE = 72;
+const X_OFFSET = 10;
+const ACHEIVEMENT_BAR_MIN_HEIGHT = 40;
 
 export default class AchvBar extends Phaser.GameObjects.Container {
   private bg: Phaser.GameObjects.NineSlice;
@@ -20,52 +24,27 @@ export default class AchvBar extends Phaser.GameObjects.Container {
   }
 
   setup(): void {
-    let xNineSlice = 0;
-    let heightNineSlice = 40;
-    let xicon= 4;
-    let titleTextX = 40;
-    let descriptionTextX = 43;
-    let wordWrapWidth = 664;
-    let yicon = 4;
-    let bgWidth = 160;
-    if (i18next.language === "de") {
-      xNineSlice = -20;
-      heightNineSlice = 50;
-      xicon = -16;
-      titleTextX = 20;
-      descriptionTextX = 23;
-      wordWrapWidth = 720;
-      bgWidth = 180;
-
-    }
-
-    this.bg = this.scene.add.nineslice(xNineSlice , 0, "achv_bar", null,bgWidth , heightNineSlice, 41, 6, 16, 4);
+    this.bg = this.scene.add.nineslice(0 , 0, "achv_bar", null,160 , ACHEIVEMENT_BAR_MIN_HEIGHT, 41, 6, 16, 4);
     this.bg.setOrigin(0, 0);
-
-    // This can not be done earlier because the bg is not yet created
-    if (i18next.language === "de") {
-      yicon = this.bg.height/2.5 - this.bg.height/4;
-    }
-
     this.add(this.bg);
 
-    this.icon = this.scene.add.sprite(xicon, yicon, "items");
+    this.icon = this.scene.add.sprite(4, ICON_Y_OFFSET, "items");
     this.icon.setOrigin(0, 0);
     this.add(this.icon);
 
-    this.titleText = addTextObject(this.scene, titleTextX, 3, "", TextStyle.MESSAGE, { fontSize: "72px" });
+    this.titleText = addTextObject(this.scene, 40, 3, "", TextStyle.MESSAGE, { fontSize: `${FONT_SIZE}px` });
     this.titleText.setOrigin(0, 0);
     this.add(this.titleText);
 
-    this.scoreText = addTextObject(this.scene, 150, 3, "", TextStyle.MESSAGE, { fontSize: "72px" });
+    this.scoreText = addTextObject(this.scene, 150, 3, "", TextStyle.MESSAGE, { fontSize: `${FONT_SIZE}px` });
     this.scoreText.setOrigin(1, 0);
     this.add(this.scoreText);
 
-    this.descriptionText = addTextObject(this.scene, descriptionTextX, 16, "", TextStyle.WINDOW_ALT, { fontSize: "72px" });
+    this.descriptionText = addTextObject(this.scene, 43, 16, "", TextStyle.WINDOW_ALT, { fontSize:`${FONT_SIZE}px` });
     this.descriptionText.setOrigin(0, 0);
     this.add(this.descriptionText);
 
-    this.descriptionText.setWordWrapWidth(wordWrapWidth);
+    this.descriptionText.setWordWrapWidth(680);
     this.descriptionText.setLineSpacing(-5);
 
     this.setScale(0.5);
@@ -84,9 +63,42 @@ export default class AchvBar extends Phaser.GameObjects.Container {
     this.bg.setTexture(`achv_bar${tier ? `_${tier + 1}` : ""}`);
     this.icon.setFrame(achv.getIconImage());
     this.titleText.setText(achv.getName());
-    this.scoreText.setVisible(achv instanceof Achv);
-    this.descriptionText.setText(getAchievementDescription((achv as Achv).localizationKey));
 
+    // Calculate the size and position of the achievement bar based on the length of the achievement name
+    this.titleText.width = achv.getName().length*FONT_SIZE;
+    this.titleText.x = ((achv.getName().length-30) * -1)+X_OFFSET;
+    this.bg.x = ((achv.getName().length+10) * -1) +X_OFFSET;
+    this.bg.width = achv.getName().length*FONT_SIZE-50;
+
+    // The description text is decides the height of the achievement bar
+    this.descriptionText.setText(getAchievementDescription((achv as Achv).localizationKey));
+    this.descriptionText.x = this.titleText.x+3;
+    if (this.descriptionText.getBounds().height > ACHEIVEMENT_BAR_MIN_HEIGHT) {
+      if (this.descriptionText.getBounds().height/2 < ACHEIVEMENT_BAR_MIN_HEIGHT) {
+        this.bg.height = ACHEIVEMENT_BAR_MIN_HEIGHT;
+      } else {
+        const descriptionHeight = this.descriptionText.getBounds().height/2;
+        // Round up to the nearest 10
+        const roundedHeight = Math.ceil(descriptionHeight / 10) * 10;
+        this.bg.height = roundedHeight;
+      }
+    } else {
+      this.bg.height = ACHEIVEMENT_BAR_MIN_HEIGHT;
+    }
+
+    // The icon is positioned based on the height of the achievement bar (centered)
+    this.icon.x = ((achv.getName().length+4) * -1)+8;
+
+    // If the height of the achievement bar is more than 40, the icon is positioned at the center of the bar
+    if (this.bg.height > 40 && this.bg.height<60) {
+      this.icon.y = this.bg.height/2.5 - this.bg.height/4;
+    } else if (this.bg.height >= 60) {
+      this.icon.y = this.bg.height/2 - this.bg.height/4;
+    } else {
+      this.icon.y = ICON_Y_OFFSET;
+    }
+
+    this.scoreText.setVisible(achv instanceof Achv);
     if (achv instanceof Achv) {
       this.scoreText.setText(`+${(achv as Achv).score}pt`);
     }
