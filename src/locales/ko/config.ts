30f4f45cf678268ab08128d1a5119370f1885e79
Benjamin Odom <bennybroseph@gmail.com>
2024-05-29 18:02:02 -0500

    Update to Main Localization Changes

--- src/locales/ko/config.ts
+++ src/locales/ko/config.ts
@@ -1,19 +1,9 @@
 import { ability } from "./ability";
 import { abilityTriggers } from "./ability-trigger";
 import { battle } from "./battle";
-import { battleMessageUiHandler } from "./battle-message-ui-handler";
-import { berry } from "./berry";
-import { biome } from "./biome";
 import { commandUiHandler } from "./command-ui-handler";
-import {
-  PGMdialogue, PGFdialogue,
-  PGMbattleSpecDialogue, PGFbattleSpecDialogue,
-  PGMmiscDialogue, PGFmiscDialogue,
-  PGMdoubleBattleDialogue, PGFdoubleBattleDialogue
-} from "./dialogue";
 import { egg } from "./egg";
 import { fightUiHandler } from "./fight-ui-handler";
-import { gameStatsUiHandler } from "./game-stats-ui-handler";
 import { growth } from "./growth";
 import { menu } from "./menu";
 import { menuUiHandler } from "./menu-ui-handler";
@@ -23,50 +13,62 @@ import { nature } from "./nature";
 import { pokeball } from "./pokeball";
 import { pokemon } from "./pokemon";
 import { pokemonInfo } from "./pokemon-info";
-import { pokemonInfoContainer } from "./pokemon-info-container";
 import { splashMessages } from "./splash-messages";
 import { starterSelectUiHandler } from "./starter-select-ui-handler";
 import { titles, trainerClasses, trainerNames } from "./trainers";
 import { tutorial } from "./tutorial";
-import { voucher } from "./voucher";
 import { weather } from "./weather";
-
-
-export const koConfig = {
-  ability,
-  abilityTriggers,
-  battle,
-  battleMessageUiHandler,
-  berry,
-  biome,
-  commandUiHandler,
-  egg,
-  fightUiHandler,
-  gameStatsUiHandler,
-  growth,
-  menu,
-  menuUiHandler,
-  modifierType,
-  move,
-  nature,
+import { battleMessageUiHandler } from "./battle-message-ui-handler";
+import { berry } from "./berry";
+import { gameStatsUiHandler } from "./game-stats-ui-handler";
+import { voucher } from "./voucher";
+import {
   PGMdialogue,
   PGFdialogue,
   PGMbattleSpecDialogue,
   PGFbattleSpecDialogue,
   PGMmiscDialogue,
-  PGFmiscDialogue,
-  PGMdoubleBattleDialogue,
-  PGFdoubleBattleDialogue,
-  pokeball,
-  pokemon,
-  pokemonInfo,
-  pokemonInfoContainer,
-  splashMessages,
-  starterSelectUiHandler,
-  titles,
-  trainerClasses,
-  trainerNames,
-  tutorial,
-  voucher,
-  weather,
+  PGFmiscDialogue, PGMdoubleBattleDialogue, PGFdoubleBattleDialogue
+} from "./dialogue";
+import { biome } from "./biome";
+import { pokemonInfoContainer } from "./pokemon-info-container";
+
+
+export const koConfig = {
+  ability: ability,
+  abilityTriggers: abilityTriggers,
+  battle: battle,
+  commandUiHandler: commandUiHandler,
+  egg: egg,
+  fightUiHandler: fightUiHandler,
+  growth: growth,
+  menu: menu,
+  menuUiHandler: menuUiHandler,
+  modifierType: modifierType,
+  move: move,
+  nature: nature,
+  pokeball: pokeball,
+  pokemon: pokemon,
+  pokemonInfo: pokemonInfo,
+  splashMessages: splashMessages,
+  starterSelectUiHandler: starterSelectUiHandler,
+  titles: titles,
+  trainerClasses: trainerClasses,
+  trainerNames: trainerNames,
+  tutorial: tutorial,
+  weather: weather,
+  battleMessageUiHandler: battleMessageUiHandler,
+  berry: berry,
+  gameStatsUiHandler: gameStatsUiHandler,
+  voucher: voucher,
+  biome: biome,
+  pokemonInfoContainer: pokemonInfoContainer,
+  PGMdialogue: PGMdialogue,
+  PGFdialogue: PGFdialogue,
+  PGMbattleSpecDialogue: PGMbattleSpecDialogue,
+  PGFbattleSpecDialogue: PGFbattleSpecDialogue,
+  PGMmiscDialogue: PGMmiscDialogue,
+  PGFmiscDialogue: PGFmiscDialogue,
+  PGMdoubleBattleDialogue: PGMdoubleBattleDialogue,
+  PGFdoubleBattleDialogue: PGFdoubleBattleDialogue
 };
