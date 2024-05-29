42b64b0868c70686c0c1278ef46a14af49a41b0d
Lugiad <adrien.grivel@hotmail.fr>
2024-05-29 18:29:59 +0200

    Update config.ts

--- src/locales/en/config.ts
+++ src/locales/en/config.ts
@@ -32,6 +32,7 @@ import {
 } from "./dialogue";
 import { biome } from "./biome";
 import { pokemonInfoContainer } from "./pokemon-info-container";
+import { modifierSelectUiHandler } from "./modifier-select-ui-handler";
 
 export const enConfig = {
   ability: ability,
@@ -69,5 +70,6 @@ export const enConfig = {
   PGMmiscDialogue: PGMmiscDialogue,
   PGFmiscDialogue: PGFmiscDialogue,
   PGMdoubleBattleDialogue: PGMdoubleBattleDialogue,
-  PGFdoubleBattleDialogue: PGFdoubleBattleDialogue
+  PGFdoubleBattleDialogue: PGFdoubleBattleDialogue,
+  modifierSelectUiHandler: modifierSelectUiHandler
 };
