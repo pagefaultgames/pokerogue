115f23f6c872a2ebb3641fd7af28c8f04f3a4f9b
hayuna <marek.kowalonek@gmail.com>
2024-05-29 18:29:45 +0200

    Replace null with 0 as init value

--- src/overrides.ts
+++ src/overrides.ts
@@ -79,7 +79,7 @@ export const OPP_GENDER_OVERRIDE: Gender = null;
 export const OPP_MOVESET_OVERRIDE: Array<Moves> = [];
 export const OPP_SHINY_OVERRIDE: boolean = false;
 export const OPP_VARIANT_OVERRIDE: Variant = 0;
-export const OPP_LEVEL_OVERRIDE: number | null = null;
+export const OPP_LEVEL_OVERRIDE: number = 0;
 
 /**
  * MODIFIER / ITEM OVERRIDES
