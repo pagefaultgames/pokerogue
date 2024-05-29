3b2b7e456aff8850c610a8bbe91be31bd4d4ced4
Franck Trouillez <francktrouillez99@gmail.com>
2024-05-29 03:13:44 +0200

    Show current prestige in Battle Scene
    Allows to show what level of prestige we are in when playing

--- src/battle-scene.ts
+++ src/battle-scene.ts
@@ -58,6 +58,7 @@ import {InputsController} from "./inputs-controller";
 import {UiInputs} from "./ui-inputs";
 import { MoneyFormat } from "./enums/money-format";
 import { NewArenaEvent } from "./battle-scene-events";
+import { FEATURE_FLAGS, FeatureFlag } from "./feature-flags";
 
 export const bypassLogin = import.meta.env.VITE_BYPASS_LOGIN === "1";
 
@@ -170,6 +171,7 @@ export default class BattleScene extends SceneBase {
   private party: PlayerPokemon[];
   /** Combined Biome and Wave count text */
   private biomeWaveText: Phaser.GameObjects.Text;
+  private prestigeLevelText: Phaser.GameObjects.Text;
   private moneyText: Phaser.GameObjects.Text;
   private scoreText: Phaser.GameObjects.Text;
   private luckLabelText: Phaser.GameObjects.Text;
@@ -372,6 +374,12 @@ export default class BattleScene extends SceneBase {
     this.biomeWaveText.setOrigin(1, 0);
     this.fieldUI.add(this.biomeWaveText);
 
+    if (FEATURE_FLAGS[FeatureFlag.PRESTIGE_MODE]) {
+      this.prestigeLevelText = addTextObject(this, (this.game.canvas.width / 6) - 2, 0, "", TextStyle.BATTLE_INFO);
+      this.prestigeLevelText.setOrigin(1, 0);
+      this.fieldUI.add(this.prestigeLevelText);
+    }
+
     this.moneyText = addTextObject(this, (this.game.canvas.width / 6) - 2, 0, "", TextStyle.MONEY);
     this.moneyText.setOrigin(1, 0);
     this.fieldUI.add(this.moneyText);
@@ -498,6 +506,9 @@ export default class BattleScene extends SceneBase {
       }
     });
 
+    if (FEATURE_FLAGS[FeatureFlag.PRESTIGE_MODE]) {
+      this.updatePrestigeLevelText();
+    }
     this.updateBiomeWaveText();
     this.updateMoneyText();
     this.updateScoreText();
@@ -814,6 +825,11 @@ export default class BattleScene extends SceneBase {
     }
 
     this.currentBattle = null;
+
+    if (FEATURE_FLAGS[FeatureFlag.PRESTIGE_MODE]) {
+      this.prestigeLevelText.setText(`Prestige ${this.prestigeLevel}`);
+      this.prestigeLevelText.setVisible(false);
+    }
 
     this.biomeWaveText.setText(startingWave.toString());
     this.biomeWaveText.setVisible(false);
@@ -1271,6 +1287,18 @@ export default class BattleScene extends SceneBase {
     });
   }
 
+  updatePrestigeLevelText(): void {
+    if (!FEATURE_FLAGS[FeatureFlag.PRESTIGE_MODE]) {
+      return;
+    }
+    if (!this.prestigeLevel || this.gameMode.modeId !== GameModes.CLASSIC) {
+      this.prestigeLevelText.setVisible(false);
+      return;
+    }
+    this.prestigeLevelText.setText(`Prestige ${this.prestigeLevel}`);
+    this.prestigeLevelText.setVisible(true);
+  }
+
   updateBiomeWaveText(): void {
     const isBoss = !(this.currentBattle.waveIndex % 10);
     const biomeString: string = getBiomeName(this.arena.biomeType);
@@ -1331,8 +1359,16 @@ export default class BattleScene extends SceneBase {
   }
 
   updateUIPositions(): void {
+    const baseY = -(this.game.canvas.height / 6);
     const enemyModifierCount = this.enemyModifiers.filter(m => m.isIconVisible(this)).length;
-    this.biomeWaveText.setY(-(this.game.canvas.height / 6) + (enemyModifierCount ? enemyModifierCount <= 12 ? 15 : 24 : 0));
+    const offsetEnemyModifier = (enemyModifierCount ? enemyModifierCount <= 12 ? 15 : 24 : 0);
+    if (this.gameMode?.modeId === GameModes.CLASSIC && this.prestigeLevel && FEATURE_FLAGS[FeatureFlag.PRESTIGE_MODE]) {
+      this.prestigeLevelText.setY(baseY + offsetEnemyModifier);
+      this.biomeWaveText.setY(this.prestigeLevelText.y + 10);
+    } else {
+      this.biomeWaveText.setY(baseY + offsetEnemyModifier);
+    }
+
     this.moneyText.setY(this.biomeWaveText.y + 10);
     this.scoreText.setY(this.moneyText.y + 10);
     [ this.luckLabelText, this.luckText ].map(l => l.setY((this.scoreText.visible ? this.scoreText : this.moneyText).y + 10));
