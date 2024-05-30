46ee15b9cb8f4162d6342dd0ad30779a6c4cc252
Greenlamp2 <44787002+Greenlamp2@users.noreply.github.com>
2024-05-30 16:46:27 +0200

    correction

--- src/test/phases.test.ts
+++ src/test/phases.test.ts
@@ -3,13 +3,15 @@ import { LoginPhase } from "#app/phases.js";
 import { Mode } from "#app/ui/ui.js";
 import { describe, expect, it } from "vitest";
 import GameWrapper from "./essentials/gameWrapper";
+import {waitUntil} from "#app/test/essentials/utils";
 
 describe("Phases", () => {
   describe("LoginPhase", () => {
-    it("should start the login phase", () => {
+    it("should start the login phase", async () => {
       const scene = new BattleScene();
       const game = new GameWrapper();
       game.scene.add("battle", scene);
+      await waitUntil(() => scene.ui?.getMode() === Mode.OPTION_SELECT);
       const battleScene = game.scenes["battle"] as BattleScene;
       const loginPhase = new LoginPhase(battleScene);
       loginPhase.start();
