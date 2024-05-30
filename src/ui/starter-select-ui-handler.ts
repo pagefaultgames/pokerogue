cae7d7606da8a0a2e2e29126d458b279204586e1
Benjamin Odom <bennybroseph@gmail.com>
2024-05-29 19:29:59 -0500

    Adds a Small Flyout Panel to the Battle Info Object (#1377)
    * Initial Commit
    
    * Update pbinfo_enemy_boss_stats.png
    
    * Move to Separate Key
    
    * Add Separate Mobile Control for Flyout
    
    * Add Setting to Enable/Disable
    
    * Add to the Tutorial
    
    * Change to BUTTON.V

--- src/ui/starter-select-ui-handler.ts
+++ src/ui/starter-select-ui-handler.ts
@@ -1313,7 +1313,7 @@ export default class StarterSelectUiHandler extends MessageUiHandler {
             success = true;
           }
           break;
-        case Button.CYCLE_VARIANT:
+        case Button.V:
           if (this.canCycleVariant) {
             let newVariant = props.variant;
             do {
