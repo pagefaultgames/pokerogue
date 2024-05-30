67ab5c533b659f61af6d36d948baabdc5e222918
Adrian T <68144167+torranx@users.noreply.github.com>
2024-05-31 05:42:46 +0800

    [Ability][Move] Finish wind rider/power + tailwind implementation (#1566)
    * finish wind moves + tailwind implementation
    
    * move code and add documentations
    
    * remove partial tag of wind_power
    
    * add translations, fix move bugs
    
    * fix ability trigger message
    
    * fix ability trigger message
    
    * add es localization
    
    * move out of else statement

--- src/locales/en/ability-trigger.ts
+++ src/locales/en/ability-trigger.ts
@@ -3,5 +3,6 @@ import { SimpleTranslationEntries } from "#app/plugins/i18n";
 export const abilityTriggers: SimpleTranslationEntries = {
   "blockRecoilDamage" : "{{pokemonName}}'s {{abilityName}}\nprotected it from recoil!",
   "badDreams": "{{pokemonName}} is tormented!",
+  "windPowerCharged": "Being hit by {{moveName}} charged {{pokemonName}} with power!",
   "perishBody": "{{pokemonName}}'s {{abilityName}}\n will faint both pokemon in 3 turns!",
 } as const;
