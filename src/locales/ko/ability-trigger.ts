1703837e886eace44264c961cda32db09805497d
MrWaterT <87186129+MrWaterT@users.noreply.github.com>
2024-05-31 05:40:31 +0900

    [Localization] Update Korean rival dialogue (#1597)
    * Update small Korean locales
    
    * Translate Korean classic story conversation
    
    * Minor translation edit in Korean
    
    3: translate 'not the same back home'
      from 'atmosphere of home is not like before'
      to 'returning to previous state is impossible'
    3_female: now 'home' clearly means their hometown, not only player's house
    6, 6_female: simple nuance-of-sentence modification

--- src/locales/ko/ability-trigger.ts
+++ src/locales/ko/ability-trigger.ts
@@ -2,5 +2,5 @@ import { SimpleTranslationEntries } from "#app/plugins/i18n";
 
 export const abilityTriggers: SimpleTranslationEntries = {
   "blockRecoilDamage" : "{{pokemonName}}(는)은 {{abilityName}} 때문에\n반동 데미지를 받지 않는다!",
-  "badDreams": "{{pokemonName}} is tormented!",
+  "badDreams": "{{pokemonName}}(는)은\n나이트메어 때문에 시달리고 있다!",
 } as const;
