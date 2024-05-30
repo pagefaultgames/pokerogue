cbe63456f8b2194183ceb32bc02b8bde4e4c65b4
torranx <torranicles@gmail.com>
2024-05-31 02:05:02 +0800

    add es localization

--- src/locales/es/ability-trigger.ts
+++ src/locales/es/ability-trigger.ts
@@ -3,5 +3,5 @@ import { SimpleTranslationEntries } from "#app/plugins/i18n";
 export const abilityTriggers: SimpleTranslationEntries = {
   "blockRecoilDamage" : "¡{{abilityName}} de {{pokemonName}}\nlo protegió del daño de retroceso!",
   "badDreams": "¡{{pokemonName}} está atormentado!",
-  "windPowerCharged": "Being hit by {{moveName}} charged {{pokemonName}} with power!",
+  "windPowerCharged": "¡{{pokemonName}} se ha cargado de electricidad gracias a {{moveName}}!",
 } as const;
