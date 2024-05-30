8cfab039d263d2274dfb33bae20890fc357ca0d3
Lugiad <adrien.grivel@hotmail.fr>
2024-05-31 01:49:11 +0200

    Update src/locales/ko/modifier-select-ui-handler.ts
    Co-authored-by: returntoice <dieandbecome@gmail.com>

--- src/locales/ko/modifier-select-ui-handler.ts
+++ src/locales/ko/modifier-select-ui-handler.ts
@@ -4,7 +4,7 @@ export const modifierSelectUiHandler: SimpleTranslationEntries = {
   "transfer": "도구 이동",
   "reroll": "새로고침",
   "lockRarities": "Lock Rarities",
-  "transferDesc": "Transfer a held item from one Pokémon to another.",
+  "transferDesc": "포켓몬이 지닌 도구를 다른 포켓몬에게 옮깁니다.",
   "rerollDesc": "Spend money to reroll your item options.",
   "lockRaritiesDesc": "Lock item rarities on reroll (affects reroll cost).",
   "rerollCost": "₽{{cost}}",
