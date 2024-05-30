1e4b004d7356856938fb588c412df8bfe8faa355
Jacob Hatchett <152253273+newmattock@users.noreply.github.com>
2024-05-30 08:36:12 -0700

    [Ability] Added Perish Body ability (#1554)
    * Added Perish Body ability
    
    * fix linting issues
    
    * Documentation + Checking if either pokemon has perish song tag
    
    * Fixed typo and improved TriggerMessage

--- src/locales/en/ability-trigger.ts
+++ src/locales/en/ability-trigger.ts
@@ -3,4 +3,5 @@ import { SimpleTranslationEntries } from "#app/plugins/i18n";
 export const abilityTriggers: SimpleTranslationEntries = {
   "blockRecoilDamage" : "{{pokemonName}}'s {{abilityName}}\nprotected it from recoil!",
   "badDreams": "{{pokemonName}} is tormented!",
+  "perishBody": "{{pokemonName}}'s {{abilityName}}\n will faint both pokemon in 3 turns!",
 } as const;
