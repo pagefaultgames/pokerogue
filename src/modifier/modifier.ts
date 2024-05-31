620dfae533cc5c592925235a873e278b1b05e85d
Tempoanon <163687446+Tempo-anon@users.noreply.github.com>
2024-05-31 10:55:48 -0400

    Revert "[Bug] Full Heals and Full Restore cure confusion (#1112)"
    This reverts commit 6c4b60a2faca4f52fd83808e1bd6c331aa98a2c3.

--- src/modifier/modifier.ts
+++ src/modifier/modifier.ts
@@ -1057,14 +1057,16 @@ export class PokemonHpRestoreModifier extends ConsumablePokemonModifier {
       let restorePoints = this.restorePoints;
       if (!this.fainted) {
         restorePoints = Math.floor(restorePoints * (args[1] as number));
-        if (this.fainted || this.healStatus) {
-          pokemon.resetStatus(true, true);
-        }
-        pokemon.hp = Math.min(pokemon.hp + Math.max(Math.ceil(Math.max(Math.floor((this.restorePercent * 0.01) * pokemon.getMaxHp()), restorePoints)), 1), pokemon.getMaxHp());
-        return true;
       }
-      return false;
+      if (this.fainted || this.healStatus) {
+        pokemon.resetStatus();
+      }
+      pokemon.hp = Math.min(pokemon.hp + Math.max(Math.ceil(Math.max(Math.floor((this.restorePercent * 0.01) * pokemon.getMaxHp()), restorePoints)), 1), pokemon.getMaxHp());
+
+      return true;
     }
+
+    return false;
   }
 }
 
@@ -1075,7 +1077,8 @@ export class PokemonStatusHealModifier extends ConsumablePokemonModifier {
 
   apply(args: any[]): boolean {
     const pokemon = args[0] as Pokemon;
-    pokemon.resetStatus(true, true);
+    pokemon.resetStatus();
+
     return true;
   }
 }
