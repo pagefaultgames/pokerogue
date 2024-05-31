64232edcacac7f623fd7e93e164dcc0863162c4e
Frederik Hobein <frederik.hobein@nterra.com>
2024-05-31 16:54:09 +0200

    oops

--- src/overrides.ts
+++ src/overrides.ts
@@ -27,7 +27,7 @@ import { Gender } from "./data/gender";
 export const SEED_OVERRIDE: string = "";
 export const WEATHER_OVERRIDE: WeatherType = WeatherType.NONE;
 export const DOUBLE_BATTLE_OVERRIDE: boolean = false;
-export const STARTING_WAVE_OVERRIDE: integer = 8;
+export const STARTING_WAVE_OVERRIDE: integer = 0;
 export const STARTING_BIOME_OVERRIDE: Biome = Biome.TOWN;
 export const ARENA_TINT_OVERRIDE: TimeOfDay = null;
 // Multiplies XP gained by this value including 0. Set to null to ignore the override
