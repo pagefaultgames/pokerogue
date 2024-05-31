a7e0e5270fc6a623d6f445ec9b46df1d9ae8dfbd
Benjamin Odom <bennybroseph@gmail.com>
2024-05-31 17:38:05 -0500

    Update overrides.ts

--- src/overrides.ts
+++ src/overrides.ts
@@ -11,6 +11,9 @@ import { Type } from "./data/type";
 import { Stat } from "./data/pokemon-stat";
 import { PokeballCounts } from "./battle-scene";
 import { PokeballType } from "./data/pokeball";
+import {TimeOfDay} from "#app/data/enums/time-of-day";
+import { Gender } from "./data/gender";
+import { StatusEffect } from "./data/status-effect";
 import { modifierTypes } from "./modifier/modifier-type";
 
 /**
