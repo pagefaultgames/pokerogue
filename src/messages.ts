03a7888c7ae583c3b11852881ccfa6281a39b37e
Laeticia PIERRE <laeticia.pierre06200@gmail.com>
2024-05-31 15:16:28 +0200

    Doc getPokemonNameWithAffix + getPokemonMessage + fix

--- src/messages.ts
+++ src/messages.ts
@@ -2,10 +2,22 @@ import { BattleSpec } from "./enums/battle-spec";
 import Pokemon from "./field/pokemon";
 import i18next from "./plugins/i18n";
 
+/**
+ * Builds a message by concatenating the Pokemon name with its potential affix and the given text
+ * @param pokemon {@linkcode Pokemon} name and battle context will be retrieved from this instance for {@linkcode getPokemonNameWithAffix}
+ * @param {string} content any text
+ * @returns {string} ex: "Wild Gengar fainted!", "Ectoplasma sauvage est K.O!"
+ * @see {@linkcode getPokemonNameWithAffix} for the Pokemon's name and potentiel affix
+ */
 export function getPokemonMessage(pokemon: Pokemon, content: string): string {
-  return `${getPokemonNameWithAffix(pokemon)} ${content}`;
+  return `${getPokemonNameWithAffix(pokemon)}${content}`;
 }
 
+/**
+ * Retrieves the Pokemon's name, potentially with an affix indicating its role (wild or foe) in the current battle context, translated
+ * @param pokemon {@linkcode Pokemon} name and battle context will be retrieved from this instance
+ * @returns {string} ex: "Wild Gengar", "Ectoplasma sauvage"
+ */
 export function getPokemonNameWithAffix(pokemon: Pokemon): string {
   switch (pokemon.scene.currentBattle.battleSpec) {
   case BattleSpec.DEFAULT:
@@ -23,6 +35,6 @@ export function getPokemonNameWithAffix(pokemon: Pokemon): string {
       ? i18next.t("battle:foePokemonWithAffix", { pokemonName: pokemon.name })
       : pokemon.name;
   default:
-    pokemon.name;
+    return pokemon.name;
   }
 }
