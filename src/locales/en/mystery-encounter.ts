e47e77269961ad3b512d2bc6099264723fb682ad
InnocentGameDev <asdargmng@gmail.com>
2024-05-31 12:19:25 +0200

    started working on locale/en, still lots left

--- src/locales/en/mystery-encounter.ts
+++ src/locales/en/mystery-encounter.ts
@@ -1,13 +1,34 @@
 import { SimpleTranslationEntries } from "#app/plugins/i18n";
 
+/**Encounter descriptions should be about 7-9 words long per line, meaning 3 lines (21-27 words) are usually best to formulate the situation.
+*/
+
 export const mysteryEncounter: SimpleTranslationEntries = {
   "encounter_0": "Mysterious challengers have appeared!",
-  "encounter_0_title": "Mysterious Challengers",
+  "encounter_0_title": "Mysterious Challenger",
   "encounter_0_description": "If you defeat a challenger, you might impress them enough to receive a boon. But if you lose, they might not be so kind.",
   "encounter_0_query": "Who will you battle?",
   "encounter_0_option_1": "1. A weak but clever foe",
   "encounter_0_option_2": "2. A strong foe",
   "encounter_0_option_3": "3. The mightiest foe",
   "encounter_0_option_4": "4. Leave",
-  "encounter1": "This is mystery encounter 1!"
+
+  "encounter_1": "You keep going, you find... nothing?",
+  "encounter_1_title": "A mysterious chest",
+  "encounter_1_description": "A beautifully ornamented chest ",
+  "encounter_1_query": "Who will you battle?",
+  "encounter_1_option_1": "1. A weak but clever foe",
+  "encounter_1_option_2": "2. A strong foe",
+  "encounter_1_option_3": "3. The mightiest foe",
+  "encounter_1_option_4": "4. Leave",
+
+  "encounter_2": "This is mystery encounter 1!",
+  "encounter_2_title": "Mysterious Challengers",
+  "encounter_2_description": "If you defeat a challenger, you might impress them enough to receive a boon. But if you lose, they might not be so kind.",
+  "encounter_2_query": "Who will you battle?",
+  "encounter_2_option_1": "1. A weak but clever foe",
+  "encounter_2_option_2": "2. A strong foe",
+  "encounter_2_option_3": "3. The mightiest foe",
+  "encounter_2_option_4": "4. Leave",
+
 } as const;
