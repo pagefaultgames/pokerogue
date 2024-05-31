c30363eccd2e333561330b78e2c78f974a15fe28
José Ricardo Fleury Oliveira <josefleury@discente.ufg.br>
2024-05-31 10:49:08 -0300

    korean translations

--- src/locales/ko/achv.ts
+++ src/locales/ko/achv.ts
@@ -2,170 +2,170 @@ import { AchievementTranslationEntries } from "#app/plugins/i18n.js";
 
 export const achv: AchievementTranslationEntries = {
   "Achievements": {
-    name: "Achievements",
+    name: "업적",
   },
   "Locked": {
-    name: "Locked",
+    name: "미완료",
   },
 
   "MoneyAchv": {
-    description: "Accumulate a total of ₽{{moneyAmount}}",
+    description: "누적 소지금 ₽{{moneyAmount}} 달성",
   },
   "10K_MONEY": {
-    name: "Money Haver",
+    name: "돈 좀 있나?",
   },
   "100K_MONEY": {
-    name: "Rich",
+    name: "부자",
   },
   "1M_MONEY": {
-    name: "Millionaire",
+    name: "백만장자",
   },
   "10M_MONEY": {
-    name: "One Percenter",
+    name: "상위 1프로",
   },
 
   "DamageAchv": {
-    description: "Inflict {{damageAmount}} damage in one hit",
+    description: "한 번의 공격만으로 {{damageAmount}} 대미지",
   },
   "250_DMG": {
-    name: "Hard Hitter",
+    name: "강타자",
   },
   "1000_DMG": {
-    name: "Harder Hitter",
+    name: "최강타자",
   },
   "2500_DMG": {
-    name: "That's a Lotta Damage!",
+    name: "때릴 줄 아시는군요!",
   },
   "10000_DMG": {
-    name: "One Punch Man",
+    name: "원펀맨",
   },
 
   "HealAchv": {
-    description: "Heal {{healAmount}} {{HP}} at once with a move, ability, or held item",
+    description: "기술이나 특성, 지닌 도구로 한 번에 {{healAmount}} {{HP}} 회복",
   },
   "250_HEAL": {
-    name: "Novice Healer",
+    name: "견습 힐러",
   },
   "1000_HEAL": {
-    name: "Big Healer",
+    name: "상급 힐러",
   },
   "2500_HEAL": {
-    name: "Cleric",
+    name: "클레릭",
   },
   "10000_HEAL": {
-    name: "Recovery Master",
+    name: "회복 마스터",
   },
 
   "LevelAchv": {
-    description: "Level up a Pokémon to Lv{{level}}",
+    description: "포켓몬 Lv{{level}} 달성",
   },
   "LV_100": {
-    name: "But Wait, There's More!",
+    name: "잠깐, 여기가 끝이 아니라구!",
   },
   "LV_250": {
-    name: "Elite",
+    name: "엘리트",
   },
   "LV_1000": {
-    name: "To Go Even Further Beyond",
+    name: "더 먼 곳을 향해",
   },
 
   "RibbonAchv": {
-    description: "Accumulate a total of {{ribbonAmount}} Ribbons",
+    description: "총 {{ribbonAmount}}개의 리본 획득",
   },
   "10_RIBBONS": {
-    name: "Pokémon League Champion",
+    name: "포켓몬 리그 챔피언",
   },
   "25_RIBBONS": {
-    name: "Great League Champion",
+    name: "슈퍼 리그 챔피언",
   },
   "50_RIBBONS": {
-    name: "Ultra League Champion",
+    name: "하이퍼 리그 챔피언",
   },
   "75_RIBBONS": {
-    name: "Rogue League Champion",
+    name: "로그 리그 챔피언",
   },
   "100_RIBBONS": {
-    name: "Master League Champion",
+    name: "마스터 리그 챔피언",
   },
 
   "TRANSFER_MAX_BATTLE_STAT": {
-    name: "Teamwork",
-    description: "Baton pass to another party member with at least one stat maxed out",
+    name: "팀워크",
+    description: "한 개 이상의 능력치가 최대 랭크일 때 배턴터치 사용",
   },
   "MAX_FRIENDSHIP": {
-    name: "Friendmaxxing",
-    description: "Reach max friendship on a Pokémon",
+    name: "친밀 맥스",
+    description: "최대 친밀도 달성",
   },
   "MEGA_EVOLVE": {
-    name: "Megamorph",
-    description: "Mega evolve a Pokémon",
+    name: "메가변환",
+    description: "포켓몬을 메가진화",
   },
   "GIGANTAMAX": {
-    name: "Absolute Unit",
-    description: "Gigantamax a Pokémon",
+    name: "엄청난 것",
+    description: "포켓몬을 다이맥스",
   },
   "TERASTALLIZE": {
-    name: "STAB Enthusiast",
-    description: "Terastallize a Pokémon",
+    name: "반짝반짝",
+    description: "포켓몬을 테라스탈",
   },
   "STELLAR_TERASTALLIZE": {
-    name: "The Hidden Type",
-    description: "Stellar Terastallize a Pokémon",
+    name: "숨겨진 타입",
+    description: "포켓몬을 스텔라 테라스탈",
   },
   "SPLICE": {
-    name: "Infinite Fusion",
-    description: "Splice two Pokémon together with DNA Splicers",
+    name: "끝없는 융합",
+    description: "유전자쐐기로 두 포켓몬을 융합",
   },
   "MINI_BLACK_HOLE": {
-    name: "A Hole Lot of Items",
-    description: "Acquire a Mini Black Hole",
+    name: "도구가 가득한 구멍",
+    description: "미니 블랙홀 획득",
   },
   "CATCH_MYTHICAL": {
-    name: "Mythical",
-    description: "Catch a mythical Pokémon",
+    name: "환상",
+    description: "환상의 포켓몬 포획",
   },
   "CATCH_SUB_LEGENDARY": {
-    name: "(Sub-)Legendary",
-    description: "Catch a sub-legendary Pokémon",
+    name: "(준)전설",
+    description: "준전설 포켓몬 포획",
   },
   "CATCH_LEGENDARY": {
-    name: "Legendary",
-    description: "Catch a legendary Pokémon",
+    name: "전설",
+    description: "전설의 포켓몬 포획",
   },
   "SEE_SHINY": {
-    name: "Shiny",
-    description: "Find a shiny Pokémon in the wild",
+    name: "다른 색",
+    description: "야생의 색이 다른 포켓몬 발견",
   },
   "SHINY_PARTY": {
-    name: "That's Dedication",
-    description: "Have a full party of shiny Pokémon",
+    name: "찐사랑",
+    description: "색이 다른 포켓몬만으로 파티 구성",
   },
   "HATCH_MYTHICAL": {
-    name: "Mythical Egg",
-    description: "Hatch a mythical Pokémon from an egg",
+    name: "환상의 알",
+    description: "알에서 환상의 포켓몬이 부화",
   },
   "HATCH_SUB_LEGENDARY": {
-    name: "Sub-Legendary Egg",
-    description: "Hatch a sub-legendary Pokémon from an egg",
+    name: "준전설 알",
+    description: "알에서 준전설 포켓몬이 부화",
   },
   "HATCH_LEGENDARY": {
-    name: "Legendary Egg",
-    description: "Hatch a legendary Pokémon from an egg",
+    name: "전설의 알",
+    description: "알에서 전설의 포켓몬이 부화",
   },
   "HATCH_SHINY": {
-    name: "Shiny Egg",
-    description: "Hatch a shiny Pokémon from an egg",
+    name: "빛나는 알",
+    description: "알에서 색이 다른 포켓몬이 부화",
   },
   "HIDDEN_ABILITY": {
-    name: "Hidden Potential",
-    description: "Catch a Pokémon with a hidden ability",
+    name: "숨은 잠재력",
+    description: "숨겨진 특성을 지닌 포켓몬을 포획",
   },
   "PERFECT_IVS": {
-    name: "Certificate of Authenticity",
-    description: "Get perfect IVs on a Pokémon",
+    name: "진짜배기 증명서",
+    description: "최고의 개체값을 지닌 포켓몬 획득",
   },
   "CLASSIC_VICTORY": {
-    name: "Undefeated",
-    description: "Beat the game in classic mode",
+    name: "무패",
+    description: "클래식 모드 클리어",
   },
 } as const;
