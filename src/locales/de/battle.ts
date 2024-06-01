25135a0eeac811211ca23d14e976d055326b5ca1
Ohmry <o.ohmry@gmail.com>
2024-06-02 07:44:54 +0900

    [Localization] Add deutsch localization for situation of pokemon fainted

--- src/locales/de/battle.ts
+++ src/locales/de/battle.ts
@@ -61,5 +61,5 @@ export const battle: SimpleTranslationEntries = {
   "useMove": "{{pokemonNameWithAffix}} setzt {{moveName}} ein!",
   "drainMessage": "{{pokemonName}} wurde Energie abgesaugt",
   "regainHealth": "KP von {{pokemonName}} wurden wieder aufgefrischt!",
-  "fainted": "{{pokemonNameWithAffix}} fainted!"
+  "fainted": "{{pokemonNameWithAffix}} kampfunfähig!"
 } as const;
