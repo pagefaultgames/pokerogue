1acabee2731f193c96292e6b42dab98c66b7964d
InnocentGameDev <asdargmng@gmail.com>
2024-06-01 12:42:53 +0200

    Update modifier-type.ts
    Added DerTapp translations without the "Ein Item zum Tragen." part for consistency throughout the file and with the rest of the locales, and according to what was briefly discussed in the Discord last night.

--- src/locales/de/modifier-type.ts
+++ src/locales/de/modifier-type.ts
@@ -209,8 +209,8 @@ export const modifierType: ModifierTypeTranslationEntries = {
     "LEFTOVERS": { name: "Überreste", description: "Heilt 1/16 der maximalen KP eines Pokémon pro Runde" },
     "SHELL_BELL": { name: "Muschelglocke", description: "Heilt den Anwender um 1/8 des von ihm zugefügten Schadens" },
 
-    "TOXIC_ORB": { name: "Heiß-Orb", description: "It's a bizarre orb that exudes toxins when touched and will badly poison the holder during battle" },
-    "FLAME_ORB": { name: "Toxik-Orb", description: "It's a bizarre orb that gives off heat when touched and will affect the holder with a burn during battle" },
+    "TOXIC_ORB": { name: "Toxik-Orb", description: "Dieser bizarre Orb vergiftet seinen Träger im Kampf schwer." },
+    "FLAME_ORB": { name: "Heiß-Orb", description: "Dieser bizarre Orb fügt seinem Träger im Kampf Verbrennungen zu." },
 
     "BATON": { name: "Stab", description: "Ermöglicht das Weitergeben von Effekten beim Wechseln von Pokémon, wodurch auch Fallen umgangen werden." },
 
