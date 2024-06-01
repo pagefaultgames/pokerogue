64f4b71b079500ab2267aaec807db180d9816017
StaalNS <staal@xistence.org>
2024-06-01 14:06:14 +0200

    add

---
+++ src/locales/fr/pokemon-info-container.ts
@@ -0,0 +1,11 @@
+import { SimpleTranslationEntries } from "#app/plugins/i18n";
+
+export const pokemonInfoContainer: SimpleTranslationEntries = {
+  "moveset": "Capacités",
+  "gender": "Sexe :",
+  "ability": "Talent :",
+  "nature": "Nature :",
+  "epic": "Épique",
+  "rare": "Rare",
+  "common": "Commun"
+} as const;
---
+++ src/locales/zh_TW/pokemon-info-container.ts
@@ -0,0 +1,11 @@
+import { SimpleTranslationEntries } from "#app/plugins/i18n";
+
+export const pokemonInfoContainer: SimpleTranslationEntries = {
+  "moveset": "Moveset",
+  "gender": "Gender:",
+  "ability": "Ability:",
+  "nature": "Nature:",
+  "epic": "Epic",
+  "rare": "Rare",
+  "common": "Common"
+} as const;
