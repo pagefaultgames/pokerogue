dba647cd074a5fe594a442e204983d9f25230fcc
Ohmry <o.ohmry@gmail.com>
2024-06-03 06:51:20 +0900

    [Localization] Translated party-ui-handler's localization to Deutsch

--- src/locales/de/party-ui-handler.ts
+++ src/locales/de/party-ui-handler.ts
@@ -1,10 +1,10 @@
 import { SimpleTranslationEntries } from "#app/plugins/i18n";
 
 export const partyUiHandler: SimpleTranslationEntries = {
-  "SEND_OUT": "Send Out",
-  "SUMMARY": "Summary",
-  "CANCEL": "Cancel",
-  "RELEASE": "Release",
-  "APPLY": "Apply",
-  "TEACH": "Teach"
+  "SEND_OUT": "Einwechseln",
+  "SUMMARY": "Bericht",
+  "CANCEL": "Abbrechen",
+  "RELEASE": "Freilassen",
+  "APPLY": "Anwenden",
+  "TEACH": "Erlernen"
 } as const;
