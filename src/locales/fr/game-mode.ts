6d5cce6f96ed80041786778ecbea7bb0cb470e2f
Lugiad <adrien.grivel@hotmail.fr>
2024-06-03 13:47:54 +0200

    Update French game-mode.ts

--- src/locales/fr/game-mode.ts
+++ src/locales/fr/game-mode.ts
@@ -3,7 +3,7 @@ import { SimpleTranslationEntries } from "#app/plugins/i18n";
 export const gameMode: SimpleTranslationEntries = {
   "classic": "Classique",
   "endless": "Infini",
-  "endlessSpliced": "Infini (Fusionnés)",
+  "endlessSpliced": "Infini (Fusions)",
   "dailyRun": "Défi du jour",
   "unknown": "Inconnu",
 } as const;
