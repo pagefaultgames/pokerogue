65051cd3d4b0022e080d4b14d88a5333dd8a188b
Matthew Olker <matthew.olker@gmail.com>
2024-06-04 21:45:36 -0400

    revert pokemon info container depth change

--- src/ui/pokemon-info-container.ts
+++ src/ui/pokemon-info-container.ts
@@ -62,7 +62,6 @@ export default class PokemonInfoContainer extends Phaser.GameObjects.Container {
 
   private initialX: number;
   private movesContainerInitialX: number;
-  private initialParentDepth: number;
 
   public statsContainer: StatsContainer;
 
@@ -263,8 +262,6 @@ export default class PokemonInfoContainer extends Phaser.GameObjects.Container {
 
       this.setVisible(true);
       this.shown = true;
-      this.initialParentDepth = this.parentContainer.depth;
-      this.parentContainer.setDepth(3);
     });
   }
 
@@ -285,7 +282,6 @@ export default class PokemonInfoContainer extends Phaser.GameObjects.Container {
   hide(speedMultiplier: number = 1): Promise<void> {
     return new Promise(resolve => {
       if (!this.shown) {
-        this.parentContainer.setDepth(this.initialParentDepth);
         return resolve();
       }
 
@@ -306,7 +302,6 @@ export default class PokemonInfoContainer extends Phaser.GameObjects.Container {
           this.pokemonShinyIcon.off("pointerover");
           this.pokemonShinyIcon.off("pointerout");
           (this.scene as BattleScene).ui.hideTooltip();
-          this.parentContainer.setDepth(this.initialParentDepth);
           resolve();
         }
       });
