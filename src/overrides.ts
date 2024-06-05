1c9cc00beec0f78ea45e03408e6710c7663db7c4
sirzento <sirzento@gmx.de>
2024-06-05 11:32:44 +0200

    Renamed egg hatch override

--- src/overrides.ts
+++ src/overrides.ts
@@ -90,7 +90,7 @@ export const OPP_VARIANT_OVERRIDE: Variant = 0;
  * EGG OVERRIDES
  */
 
-export const IMMEDIATE_HATCH_EGGS_OVERRIDE: boolean = false;
+export const EGG_IMMEDIATE_HATCH_OVERRIDE: boolean = false;
 export const EGG_TIER_OVERRIDE: EggTier = EggTier.COMMON;
 export const EGG_SHINY_OVERRIDE: boolean = false;
 export const EGG_VARIANT_OVERRIDE: VariantTier = VariantTier.COMMON;
