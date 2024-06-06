606577e0abad55dcbbc41703ca72c1888f036854
Mr.WaterT <water.t.works@gmail.com>
2024-06-07 05:31:03 +0900

    Translate iceFaceAvoidedDamage

--- src/locales/ko/ability-trigger.ts
+++ src/locales/ko/ability-trigger.ts
@@ -6,5 +6,5 @@ export const abilityTriggers: SimpleTranslationEntries = {
   "windPowerCharged": "{{pokemonName}}[[는]]\n{{moveName}}에 맞아 충전되었다!",
   "perishBody": "{{pokemonName}}의 {{abilityName}} 때문에\n양쪽 포켓몬 모두는 3턴 후에 쓰러져 버린다!",
   "poisonHeal": "{{pokemonName}}[[는]] {{abilityName}}[[로]]인해\n조금 회복했다.",
-  "iceFaceAvoidedDamage": "{{pokemonName}} avoided\ndamage with {{abilityName}}!",
+  "iceFaceAvoidedDamage": "{{pokemonName}}[[는]] {{abilityName}} 때문에\n데미지를 받지 않는다!",
 } as const;
