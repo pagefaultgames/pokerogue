96a5326852fbc31119f67784fd7c368bac15b65d
Mr.WaterT <water.t.works@gmail.com>
2024-06-06 20:03:24 +0900

    Edit wrong space

--- src/locales/ko/ability-trigger.ts
+++ src/locales/ko/ability-trigger.ts
@@ -5,5 +5,5 @@ export const abilityTriggers: SimpleTranslationEntries = {
   "badDreams": "{{pokemonName}}[[는]]\n나이트메어 때문에 시달리고 있다!",
   "windPowerCharged": "{{pokemonName}}[[는]]\n{{moveName}}에 맞아 충전되었다!",
   "perishBody": "{{pokemonName}}의 {{abilityName}} 때문에\n양쪽 포켓몬 모두는 3턴 후에 쓰러져 버린다!",
-  "poisonHeal": "{{pokemonName}}[[는]] {{abilityName}}[[로]]인해  조금 회복했다.",
+  "poisonHeal": "{{pokemonName}}[[는]] {{abilityName}}[[로]]인해\n조금 회복했다.",
 } as const;
