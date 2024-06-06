8b82d3955863af2956c1f3cf074e5d814fec2a21
MadridPawmot <42167718+Alekemon@users.noreply.github.com>
2024-06-06 12:17:41 +0200

    Added Portuguese translation

--- src/locales/pt_BR/trainers.ts
+++ src/locales/pt_BR/trainers.ts
@@ -95,7 +95,7 @@ export const trainerClasses: SimpleTranslationEntries = {
   "rich_kid_female": "Garota Rica",
   "rich_kids": "Garotos Ricos",
   "roughneck": "Arruaceiro",
-  "sailor": "Sailor",
+  "sailor": "Marinheiro",
   "scientist": "Cientista",
   "scientist_female": "Cientista",
   "scientists": "Cientistas",
