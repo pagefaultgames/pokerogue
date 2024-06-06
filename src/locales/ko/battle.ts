9e5f4d5ce6364fb2b3e3e88cbecf40227f65b097
Ohmry <o.ohmry@gmail.com>
2024-06-06 23:06:35 +0900

    Reflect lint results in code

--- src/locales/ko/battle.ts
+++ src/locales/ko/battle.ts
@@ -58,6 +58,6 @@ export const battle: SimpleTranslationEntries = {
   "ivScannerUseQuestion": "{{pokemonName}}에게 개체값탐지기를 사용하시겠습니까?",
   "wildPokemonWithAffix": "야생 {{pokemonName}}",
   "foePokemonWithAffix": "상대 {{pokemonName}}",
-  "useMove": "{{pokemonNameWithAffix}}의 {{moveName}}!",  
+  "useMove": "{{pokemonNameWithAffix}}의 {{moveName}}!",
   "fainted": "{{pokemonNameWithAffix}}은(는) 쓰러졌다!"
 } as const;
