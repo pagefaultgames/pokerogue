e471e40c11373040b581d07f9fef957a2edf0d56
Jannik Tappert <38758606+CodeTappert@users.noreply.github.com>
2024-06-07 22:30:06 +0200

    Ability Bar can now be localized. In a way that pokemonName abilityName and passive string can be ordered freely

--- src/locales/es/fight-ui-handler.ts
+++ src/locales/es/fight-ui-handler.ts
@@ -4,4 +4,6 @@ export const fightUiHandler: SimpleTranslationEntries = {
   "pp": "PP",
   "power": "Potencia",
   "accuracy": "Precisión",
+  "abilityFlyInText": " {{pokemonName}}'s {{passive}}{{abilityName}}",
+  "passive": "Passive ", // The space at the end is important
 } as const;
