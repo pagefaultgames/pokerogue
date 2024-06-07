cc443a7ac0389cd9e4d4ed5913764997173aeeab
GoldTra <ericpazos02@gmail.com>
2024-06-07 22:56:16 +0200

    Updated Spanish translations

--- src/locales/es/party-ui-handler.ts
+++ src/locales/es/party-ui-handler.ts
@@ -1,10 +1,10 @@
 import { SimpleTranslationEntries } from "#app/plugins/i18n";
 
 export const partyUiHandler: SimpleTranslationEntries = {
-  "SEND_OUT": "Send Out",
-  "SUMMARY": "Summary",
-  "CANCEL": "Cancel",
-  "RELEASE": "Release",
-  "APPLY": "Apply",
-  "TEACH": "Teach"
+  "SEND_OUT": "Cambiar",
+  "SUMMARY": "Datos",
+  "CANCEL": "Cancelar",
+  "RELEASE": "Liberar",
+  "APPLY": "Usar",
+  "TEACH": "Aprender"
 } as const;
