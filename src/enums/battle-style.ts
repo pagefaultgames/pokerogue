2852978d260b05e991ba5c5048fe6a0513564c5f
hayuna <marek.kowalonek@gmail.com>
2024-06-07 10:40:21 +0200

    Move enum BattleStyle into separated file

---
+++ src/enums/battle-style.ts
@@ -0,0 +1,9 @@
+/**
+   * Determines the selected battle style.
+   * - 0 = 'Shift'
+   * - 1 = 'Set' - The option to switch the active pokemon at the start of a battle will not display.
+   */
+export enum BattleStyle {
+    SHIFT,
+    SET
+}
