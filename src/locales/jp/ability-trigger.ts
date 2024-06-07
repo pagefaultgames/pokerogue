7bf51ad1aad28ec8123a439f1b1bb3ecc3dbc916
DESKTOP-G36G2KQ\chepy <chepyu@naver.com>
2024-06-07 18:31:03 +0900

    ability-trigger.ts translated

--- src/locales/jp/ability-trigger.ts
+++ src/locales/jp/ability-trigger.ts
@@ -1,10 +1,10 @@
 import { SimpleTranslationEntries } from "#app/plugins/i18n";
 
 export const abilityTriggers: SimpleTranslationEntries = {
-  "blockRecoilDamage" : "{{pokemonName}}'s {{abilityName}}\nprotected it from recoil!",
-  "badDreams": "{{pokemonName}} is tormented!",
-  "iceFaceAvoidedDamage": "{{pokemonName}} avoided\ndamage with {{abilityName}}!",
-  "windPowerCharged": "Being hit by {{moveName}} charged {{pokemonName}} with power!",
-  "perishBody": "{{pokemonName}}'s {{abilityName}}\nwill faint both pokemon in 3 turns!",
-  "poisonHeal": "{{pokemonName}}'s {{abilityName}}\nrestored its HP a little!"
+  "blockRecoilDamage" : "{{pokemonName}}は {{abilityName}}で\nはんどうダメージを うけない！",
+  "badDreams": "{{pokemonName}}は ナイトメアに うなされている！",
+  "iceFaceAvoidedDamage": "{{pokemonName}}は\n{{abilityName}}で ダメージを うけない！",
+  "windPowerCharged": "{{pokemonName}}は\n{{moveName}}を うけて じゅうでんした！",
+  "perishBody": "{{pokemonName}}の {{abilityName}}で\nおたがいは 3ターンごに ほろびてしまう！",
+  "poisonHeal": "{{pokemonName}}は {{abilityName}}で\nかいふくした！"
 } as const;
