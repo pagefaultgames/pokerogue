f8ce7833c49ea13a8577e171f2409e1e0a3651d2
José Ricardo Fleury Oliveira <josefleury@discente.ufg.br>
2024-06-08 11:53:25 -0300

    [ptBR Localization] Added many translations (#1951)
    * translations
    
    * english parity

--- src/locales/pt_BR/party-ui-handler.ts
+++ src/locales/pt_BR/party-ui-handler.ts
@@ -1,10 +1,10 @@
 import { SimpleTranslationEntries } from "#app/plugins/i18n";
 
 export const partyUiHandler: SimpleTranslationEntries = {
-  "SEND_OUT": "Send Out",
-  "SUMMARY": "Summary",
-  "CANCEL": "Cancel",
-  "RELEASE": "Release",
-  "APPLY": "Apply",
-  "TEACH": "Teach"
+  "SEND_OUT": "Trocar",
+  "SUMMARY": "Sumário",
+  "CANCEL": "Cancelar",
+  "RELEASE": "Soltar",
+  "APPLY": "Aplicar",
+  "TEACH": "Ensinar",
 } as const;
