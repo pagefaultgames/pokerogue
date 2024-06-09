d24e38e9851e3dd71130eecee7225cb58cc676fd
DESKTOP-G36G2KQ\chepy <chepyu@naver.com>
2024-06-09 12:27:08 +0900

    Loading asset, game mode and more

---
+++ src/locales/jp/save-slot-select-ui-handler.ts
@@ -0,0 +1,9 @@
+import { SimpleTranslationEntries } from "#app/plugins/i18n";
+
+export const saveSlotSelectUiHandler: SimpleTranslationEntries = {
+  "overwriteData": "せんたくしたスロットにデータをうわがきしますか？",
+  "loading": "ローディング...",
+  "wave": "Wave",
+  "lv": "Lv",
+  "empty": "なし",
+} as const;
