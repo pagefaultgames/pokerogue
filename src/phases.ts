942c1198149011f6c4077d4103a6024a28112685
whatanoob <pedrito3_@hotmail.com>
2024-06-10 21:33:34 +0100

    [Bug] Fix toxic damage increment not resetting upon switching (#1844)
    * Update phases.ts
    
    Add reset toxic turn counter in PostSummonPhase
    
    * Update phases.ts
    
    Fix linting

--- src/phases.ts
+++ src/phases.ts
@@ -1162,6 +1162,9 @@ export class PostSummonPhase extends PokemonPhase {
 
     const pokemon = this.getPokemon();
 
+    if (pokemon.status?.effect === StatusEffect.TOXIC) {
+      pokemon.status.turnCount = 0;
+    }
     this.scene.arena.applyTags(ArenaTrapTag, pokemon);
     applyPostSummonAbAttrs(PostSummonAbAttr, pokemon).then(() => this.end());
   }
