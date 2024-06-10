cfd83aef66f99747c24b05630035699823c5c371
Madmadness65 <59298170+Madmadness65@users.noreply.github.com>
2024-06-10 08:58:34 -0500

    Implement Arceus Plates and Silvally Memories (#1195)
    * Implement Arceus Plates and Silvally Memories
    
    * German localization for items
    
    * Run linter on changes, add items to zh_TW localization
    
    * linter cleanup
    
    * Finish localization for other languages
    
    Names for items sourced from Bulbapedia. Names for the items in Portuguese could not be reliably sourced, so they were as directly translated as possible.
    
    * Add custom Blank Memory item
    
    Currently does nothing, but the idea is that it would be used if the all-in-one form change item idea is followed through.
    
    * Update item atlas
    
    * Use move.type instead of type.value
    
    The type effectiveness now works again.
    
    * Update comments
    
    * Fix type changing affecting only Silvally
    
    * Condense switch cases down to a single line
    
    As per Brain Frog's suggestion.

--- src/data/ability.ts
+++ src/data/ability.ts
@@ -4135,8 +4135,7 @@ export function initAbilities() {
       .attr(UncopiableAbilityAbAttr)
       .attr(UnswappableAbilityAbAttr)
       .attr(UnsuppressableAbilityAbAttr)
-      .attr(NoFusionAbilityAbAttr)
-      .unimplemented(),
+      .attr(NoFusionAbilityAbAttr),
     new Ability(Abilities.FLOWER_GIFT, 4)
       .conditionalAttr(getWeatherCondition(WeatherType.SUNNY || WeatherType.HARSH_SUN), BattleStatMultiplierAbAttr, BattleStat.ATK, 1.5)
       .conditionalAttr(getWeatherCondition(WeatherType.SUNNY || WeatherType.HARSH_SUN), BattleStatMultiplierAbAttr, BattleStat.SPDEF, 1.5)
@@ -4488,8 +4487,7 @@ export function initAbilities() {
       .attr(UncopiableAbilityAbAttr)
       .attr(UnswappableAbilityAbAttr)
       .attr(UnsuppressableAbilityAbAttr)
-      .attr(NoFusionAbilityAbAttr)
-      .unimplemented(),
+      .attr(NoFusionAbilityAbAttr),
     new Ability(Abilities.ELECTRIC_SURGE, 7)
       .attr(PostSummonTerrainChangeAbAttr, TerrainType.ELECTRIC)
       .attr(PostBiomeChangeTerrainChangeAbAttr, TerrainType.ELECTRIC),
