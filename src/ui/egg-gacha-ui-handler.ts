32ddf985dfff14c6a042012933f1b44f38bbf21e
Matthew Olker <matthew.olker@gmail.com>
2024-06-10 21:55:25 -0400

    account for new font

--- src/ui/egg-gacha-ui-handler.ts
+++ src/ui/egg-gacha-ui-handler.ts
@@ -170,7 +170,7 @@ export default class EggGachaUiHandler extends MessageUiHandler {
     const pullOptionsText = pullOptions.map(option =>{
       const desc = option.description.split(" ");
       if (desc[0].length < 2) {
-        desc[0] += resolvedLanguage === "ko" ? " " : "  ";
+        desc[0] += ["zh", "ko"].includes(resolvedLanguage.substring(0,2)) ? " " : "  ";
       }
       if (option.multiplier === multiplierOne) {
         desc[0] = " " + desc[0];
