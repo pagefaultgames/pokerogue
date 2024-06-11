350e3eeb1a8fa9957198eb50b6fd35c6c37f2c83
Felix Staud <felix.staud@headwire.com>
2024-06-11 10:14:41 -0700

    add prettier

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  printWidth: 120,
+};
+
+export default config;
