27a7c2bd3ec6c255dfbd36835c8c9bbe6c8739bf
Frisk17 <shaazib@outlook.com>
2024-06-12 10:09:11 +0530

    Created a magnetRiseCondition separately

--- src/data/move.ts
+++ src/data/move.ts
@@ -5281,6 +5281,12 @@ const failIfDampCondition: MoveConditionFunc = (user, target, move) => {
   return !cancelled.value;
 };
 
+const failIfIngrainCondition: MoveConditionFunc = (user, target, move) => !user.getTag(BattlerTagType.INGRAIN);
+
+const magnetRiseCondition: MoveConditionFunc = (user, target, move) => {
+  return failOnGravityCondition(user, target, move) && failIfIngrainCondition(user, target, move) && !user.getTag(BattlerTagType.IGNORE_FLYING) && !user.getTag(BattlerTagType.MAGNET_RISEN);
+};
+
 const userSleptOrComatoseCondition: MoveConditionFunc = (user: Pokemon, target: Pokemon, move: Move) =>  user.status?.effect === StatusEffect.SLEEP || user.hasAbility(Abilities.COMATOSE);
 
 const targetSleptOrComatoseCondition: MoveConditionFunc = (user: Pokemon, target: Pokemon, move: Move) =>  target.status?.effect === StatusEffect.SLEEP || target.hasAbility(Abilities.COMATOSE);
@@ -6498,9 +6504,7 @@ export function initMoves() {
       .attr(AddBattlerTagAttr, BattlerTagType.AQUA_RING, true, true),
     new SelfStatusMove(Moves.MAGNET_RISE, Type.ELECTRIC, -1, 10, -1, 0, 4)
       .attr(AddBattlerTagAttr, BattlerTagType.MAGNET_RISEN, true, true)
-      .condition((user, target, move) => !user.scene.arena.getTag(ArenaTagType.GRAVITY) &&
-      !user.getTag(BattlerTagType.IGNORE_FLYING) && !user.getTag(BattlerTagType.INGRAIN) &&
-      !user.getTag(BattlerTagType.MAGNET_RISEN)),
+      .condition(magnetRiseCondition),
     new AttackMove(Moves.FLARE_BLITZ, Type.FIRE, MoveCategory.PHYSICAL, 120, 100, 15, 10, 0, 4)
       .attr(RecoilAttr, false, 0.33)
       .attr(HealStatusEffectAttr, true, StatusEffect.FREEZE)
