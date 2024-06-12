a55c77d09dc3c72c2deae2c21cf1f8d065cd09c4
Frutescens <info@laptop>
2024-06-12 12:17:29 -0700

    Text Changes to MonoGen Challenges

--- src/locales/en/achv.ts
+++ src/locales/en/achv.ts
@@ -172,39 +172,39 @@ export const PGMachv: AchievementTranslationEntries = {
 
   "MONO_GEN_ONE": {
     name: "The Original Rival",
-    description: "Complete the generation one only challenge.",
+    description: "Complete the Generation I only challenge.",
   },
   "MONO_GEN_TWO": {
     name: "Generation 1.5",
-    description: "Complete the generation two only challenge.",
+    description: "Complete the Generation II only challenge.",
   },
   "MONO_GEN_THREE": {
     name: "Too much water?",
-    description: "Complete the generation three only challenge.",
+    description: "Complete the Generation III only challenge.",
   },
   "MONO_GEN_FOUR": {
     name: "Is she really the hardest?",
-    description: "Complete the generation four only challenge.",
+    description: "Complete the Generation IV only challenge.",
   },
   "MONO_GEN_FIVE": {
     name: "All Original",
-    description: "Complete the generation five only challenge.",
+    description: "Complete the Generation V only challenge.",
   },
   "MONO_GEN_SIX": {
     name: "Almost Royalty",
-    description: "Complete the generation six only challenge.",
+    description: "Complete the Generation VI only challenge.",
   },
   "MONO_GEN_SEVEN": {
     name: "Only Technically",
-    description: "Complete the generation seven only challenge.",
+    description: "Complete the Generation VII only challenge.",
   },
   "MONO_GEN_EIGHT": {
     name: "A Champion Time!",
-    description: "Complete the generation eight only challenge.",
+    description: "Complete the Generation VIII only challenge.",
   },
   "MONO_GEN_NINE": {
     name: "She was going easy on you",
-    description: "Complete the generation nine only challenge.",
+    description: "Complete the Generation IX challenge.",
   },
 
   "MonoType": {
