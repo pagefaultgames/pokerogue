2266ca6364a80ffec463a733fc16ab48eb9a4c26
Jannik Tappert <38758606+CodeTappert@users.noreply.github.com>
2024-06-15 20:49:31 +0200

    Localized the statname in the X Item description

--- src/locales/fr/modifier-type.ts
+++ src/locales/fr/modifier-type.ts
@@ -248,6 +248,19 @@ export const modifierType: ModifierTypeTranslationEntries = {
     "x_accuracy": "Précision +",
     "dire_hit": "Muscle +",
   },
+
+  TempBattleStatBoosterStatName: {
+    "ATK": "Attack",
+    "DEF": "Defense",
+    "SPATK": "Sp. Atk",
+    "SPDEF": "Sp. Def",
+    "SPD": "Speed",
+    "ACC": "Accuracy",
+    "CRIT": "Critical Hit Ratio",
+    "EVA": "Evasiveness",
+    "DEFAULT": "???",
+  },
+
   AttackTypeBoosterItem: {
     "silk_scarf": "Mouchoir Soie",
     "black_belt": "Ceinture Noire",
