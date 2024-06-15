b995346d524add9427a14e89e669f6a28225d274
Alexis <alexis.faizeau@animedigitalnetwork.fr>
2024-06-15 11:56:50 +0200

    Translate missing fr keys for trainers

--- src/locales/fr/ability-trigger.ts
+++ src/locales/fr/ability-trigger.ts
@@ -4,7 +4,7 @@ export const abilityTriggers: SimpleTranslationEntries = {
   "blockRecoilDamage" : "{{abilityName}}\nde {{pokemonName}} le protège du contrecoup !",
   "badDreams": "{{pokemonName}} a le sommeil agité !",
   "windPowerCharged": "{{pokemonName}} a été touché par la capacité {{moveName}} et se charge en électricité !",
-  "perishBody": "{{abilityName}} de {{pokemonName}} \névanouira les deux pokémons en 3 tours !",
-  "poisonHeal": "{{abilityName}} de {{pokemonName}} \nrétablit un peu ses HP !",
+  "perishBody": "{{abilityName}} de {{pokemonName}} \nmettra KO les deux pokémons dans 3 tours !",
+  "poisonHeal": "{{abilityName}} de {{pokemonName}} \nrétablit un peu ses PV !",
   "iceFaceAvoidedDamage": "{{pokemonName}} a évité les\ndommages avec {{abilityName}}!"
 } as const;
