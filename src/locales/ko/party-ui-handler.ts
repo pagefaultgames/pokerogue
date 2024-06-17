9d98df499a9c4765da9a46cd761aa51399c25b20
Alexis <alexis.faizeau@animedigitalnetwork.fr>
2024-06-17 12:46:00 +0200

    Translate forgotten party sentences to ko

--- src/locales/ko/party-ui-handler.ts
+++ src/locales/ko/party-ui-handler.ts
@@ -6,5 +6,11 @@ export const partyUiHandler: SimpleTranslationEntries = {
   "CANCEL": "그만둔다",
   "RELEASE": "놓아준다",
   "APPLY": "사용한다",
-  "TEACH": "가르친다"
+  "TEACH": "가르친다",
+  "chooseAPokemon": "Choose a Pokémon.",
+  "doWhatWithPokemon": "Do what with this Pokémon?",
+  "selectMove": "Select a move.",
+  "selectItemTransfer": "Select a held item to transfer.\nUse < and > to change the quantity.",
+  "selectPokemonSplice": "Select another Pokémon to splice.",
+  "cancelButton": "Cancel",
 } as const;
