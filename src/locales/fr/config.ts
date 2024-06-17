426a5d9fc62f44110edb69739b42689d4ce18df6
Alexis <alexis.faizeau@animedigitalnetwork.fr>
2024-06-17 15:00:32 +0200

    Translate pokemon summary to fr

--- src/locales/fr/config.ts
+++ src/locales/fr/config.ts
@@ -40,6 +40,7 @@ import { voucher } from "./voucher";
 import { weather } from "./weather";
 import { partyUiHandler } from "./party-ui-handler";
 import { settings } from "#app/locales/fr/settings.js";
+import { pokemonSummary } from "./pokemon-summary";
 
 export const frConfig = {
   ability: ability,
@@ -74,6 +75,7 @@ export const frConfig = {
   pokemon: pokemon,
   pokemonInfo: pokemonInfo,
   pokemonInfoContainer: pokemonInfoContainer,
+  pokemonSummary: pokemonSummary,
   saveSlotSelectUiHandler: saveSlotSelectUiHandler,
   settings: settings,
   splashMessages: splashMessages,
