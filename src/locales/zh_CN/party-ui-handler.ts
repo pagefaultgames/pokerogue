674008beec0476b6bf9ceaa90edefbccbdb56ac0
Alexis <alexis.faizeau@animedigitalnetwork.fr>
2024-06-17 12:47:01 +0200

    Translate forgotten party sentences to zh cn

--- src/locales/zh_CN/party-ui-handler.ts
+++ src/locales/zh_CN/party-ui-handler.ts
@@ -6,5 +6,11 @@ export const partyUiHandler: SimpleTranslationEntries = {
   "CANCEL": "取消",
   "RELEASE": "放生",
   "APPLY": "应用",
-  "TEACH": "教授"
+  "TEACH": "教授",
+  "chooseAPokemon": "Choose a Pokémon.",
+  "doWhatWithPokemon": "Do what with this Pokémon?",
+  "selectMove": "Select a move.",
+  "selectItemTransfer": "Select a held item to transfer.\nUse < and > to change the quantity.",
+  "selectPokemonSplice": "Select another Pokémon to splice.",
+  "cancelButton": "Cancel",
 } as const;
