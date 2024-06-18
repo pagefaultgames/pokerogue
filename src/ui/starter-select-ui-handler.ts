b64e5dce1c539ae2063d016043bdf7fa89235612
ReneGV <renegv94@gmail.com>
2024-06-17 21:41:29 -0600

    [QoL] Move up form text when candies and hatched eggs are hidden (#2359)

--- src/ui/starter-select-ui-handler.ts
+++ src/ui/starter-select-ui-handler.ts
@@ -1975,6 +1975,7 @@ export default class StarterSelectUiHandler extends MessageUiHandler {
             this.pokemonHatchedIcon,
             this.pokemonHatchedCountText
           ].map(c => c.setVisible(false));
+          this.pokemonFormText.setY(25);
         } else if (species.speciesId === Species.ETERNATUS) {
           this.pokemonHatchedIcon.setVisible(false);
           this.pokemonHatchedCountText.setVisible(false);
@@ -1988,6 +1989,7 @@ export default class StarterSelectUiHandler extends MessageUiHandler {
           this.pokemonCandyCountText.setText(`x${this.scene.gameData.starterData[species.speciesId].candyCount}`);
           this.pokemonCandyCountText.setVisible(true);
           this.pokemonFormText.setVisible(true);
+          this.pokemonFormText.setY(42);
           this.pokemonHatchedIcon.setVisible(true);
           this.pokemonHatchedCountText.setVisible(true);
 
