cf15036055b355e6c538f0617e8b39520cc250d1
Enoch <enoch.jwsong@gmail.com>
2024-06-19 01:20:31 +0900

    Update src/locales/de/battle.ts
    Co-authored-by: Jannik Tappert <38758606+CodeTappert@users.noreply.github.com>

--- src/locales/de/battle.ts
+++ src/locales/de/battle.ts
@@ -62,7 +62,7 @@ export const battle: SimpleTranslationEntries = {
   "drainMessage": "{{pokemonName}} wurde Energie abgesaugt",
   "regainHealth": "KP von {{pokemonName}} wurden wieder aufgefrischt!",
   "fainted": "{{pokemonNameWithAffix}} wurde besiegt!",
-  "statChange": "{{pokemonNameWithAffix}}'s {{statsFragment}} {{battleStatLevelChangeDescription}}!",
+  "statChange": "{{statsFragment}} von {{pokemonNameWithAffix}} {{battleStatLevelChangeDescription}}!",
   "statsAnd": " and ",
   "statRose": "steigt",
   "statSharplyRose": "steigt stark",
