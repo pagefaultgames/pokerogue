ae68082a5fbcc19fefc3c6488c07ad82b209b9a3
Frutescens <info@laptop>
2024-06-19 20:17:15 -0700

    Run History Extended Mode - Modifiers still buggy

--- src/locales/en/run-history-ui-handler.ts
+++ src/locales/en/run-history-ui-handler.ts
@@ -6,8 +6,23 @@ export const runHistory: SimpleTranslationEntries = {
   "defeatedTrainer": "Defeated by ",
   "defeatedTrainerDouble": "Defeated by Duo",
   "defeatedRival": "Defeated by Rival",
+  "defeated":"Defeated",
   "luck":"Luck",
-  "score":"Score"
+  "score":"Score",
+  "mode":"Mode",
+  "challengeRules":"Rule(s)",
+  "challengeMonoGen1":"Gen I",
+  "challengeMonoGen2":"Gen II",
+  "challengeMonoGen3":"Gen III",
+  "challengeMonoGen4":"Gen IV",
+  "challengeMonoGen5":"Gen V",
+  "challengeMonoGen6":"Gen VI",
+  "challengeMonoGen7":"Gen VII",
+  "challengeMonoGen8":"Gen VIII",
+  "challengeMonoGen9":"Gen IX",
+  "playerItems":"Player Items",
+  "runInfo":"Run Info",
+  "money":"Money",
 } as const;
 
 //Mode Information found in game-mode.ts
