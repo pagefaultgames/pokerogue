6625d4e5b1d4eb3fefba4df0761fb78feb15e4f1
watchakorn-18k <wk18k@proton.me>
2024-06-20 13:33:38 +0000

    chore : change "#app/interfaces/locales"

--- src/locales/th/tutorial.ts
+++ src/locales/th/tutorial.ts
@@ -1,4 +1,4 @@
-import { SimpleTranslationEntries } from "#app/plugins/i18n";
+import { SimpleTranslationEntries } from "#app/interfaces/locales";
 
 export const tutorial: SimpleTranslationEntries = {
   "intro": `ยินดีต้อนรับสู่ PokéRogue! เกมแฟนโฟกัสที่เน้นการต่อสู้โปเกมอนพร้อมคุณลักษณะของเกมแนวโรแกไลท์
