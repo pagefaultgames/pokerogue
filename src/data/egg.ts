cc73531e895dbc145d2b6d3c1ff7b239235b3df5
Sirzento <sirzento@gmx.de>
2024-06-22 11:43:12 +0200

    Hotfix: Override varaint tier if species was given. Fixed that some common egg where displayed as manaphy egg.

--- src/data/egg.ts
+++ src/data/egg.ts
@@ -163,6 +163,12 @@ export class Egg {
     if (eggOptions.species) {
       this._tier = this.getEggTierFromSpeciesStarterValue();
       this._hatchWaves = eggOptions.hatchWaves ?? this.getEggTierDefaultHatchWaves();
+      // If species has no variant, set variantTier to common. This needs to
+      // be done because species with no variants get filtered at rollSpecies but since the
+      // species is set the check never happens
+      if (!getPokemonSpecies(this.species).hasVariants()) {
+        this._variantTier = VariantTier.COMMON;
+      }
     }
     if (eggOptions.pulled) {
       this.addEggToGameData(eggOptions.scene);
@@ -175,7 +181,7 @@ export class Egg {
 
   public isManaphyEgg(): boolean {
     return (this._species === Species.PHIONE || this._species === Species.MANAPHY) ||
-       this._tier === EggTier.COMMON && !(this._id % 204);
+       this._tier === EggTier.COMMON && !(this._id % 204) && !this._species;
   }
 
   public getKey(): string {
