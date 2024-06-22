b1b3e1775e53f46f3c0148a20fa762b62ca5f499
Tempoanon <163687446+Tempo-anon@users.noreply.github.com>
2024-06-22 09:55:28 -0400

    Update src/locales/fr/weather.ts
    Co-authored-by: Jannik Tappert <38758606+CodeTappert@users.noreply.github.com>

--- src/locales/fr/weather.ts
+++ src/locales/fr/weather.ts
@@ -40,6 +40,6 @@ export const weather: SimpleTranslationEntries = {
 
   "strongWindsStartMessage": "Un vent mystérieux se lève !",
   "strongWindsLapseMessage": "Le vent mystérieux souffle violemment !",
-  "strongWindsEffectMessage": "The mysterious air current weakened the attack!",
+  "strongWindsEffectMessage": "Le courant aérien mystérieux affaiblit l’attaque !",
   "strongWindsClearMessage": "Le vent mystérieux s’est dissipé…"
 };
