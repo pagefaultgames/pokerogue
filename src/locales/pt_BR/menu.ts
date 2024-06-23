73266567b64352bc7a6af46ea8472928283b5ae7
José Ricardo Fleury Oliveira <josefleury@discente.ufg.br>
2024-06-23 14:11:23 -0300

    fix

--- src/locales/pt_BR/menu.ts
+++ src/locales/pt_BR/menu.ts
@@ -52,6 +52,5 @@ export const menu: SimpleTranslationEntries = {
   "yes": "Sim",
   "no": "Não",
   "disclaimer": "AVISO",
-  "disclaimerDescription": "Este jogo é um produto inacabado; ele pode ter problemas de jogabilidade (incluindo possíveis\n perdas de dados salvos), sofrer alterações sem aviso prévio e pode ou não ser atualizado ou concluído.",
-  "choosePokemon": "Escolha um Pokémon.",
+  "disclaimerDescription": "Este jogo é um produto inacabado; ele pode ter problemas de jogabilidade (incluindo possíveis\n perdas de dados salvos), sofrer alterações sem aviso prévio e pode ou não ser atualizado ou concluído."
 } as const;
