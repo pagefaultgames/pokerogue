799e15127f9754761aaa74e669af4c4b9eee84f6
mewtwo2387 <mewtwo2387@gmail.com>
2024-06-23 18:47:01 +0100

    Corrected all English punctuation into Chinese in zh_TW

--- src/locales/zh_TW/ability-trigger.ts
+++ src/locales/zh_TW/ability-trigger.ts
@@ -1,8 +1,8 @@
 import { SimpleTranslationEntries } from "#app/interfaces/locales";
 
 export const abilityTriggers: SimpleTranslationEntries = {
-  "blockRecoilDamage" : "{{pokemonName}} 的 {{abilityName}}\n抵消了反作用力!",
-  "badDreams": "{{pokemonName}} 被折磨着!",
+  "blockRecoilDamage" : "{{pokemonName}} 的 {{abilityName}}\n抵消了反作用力！",
+  "badDreams": "{{pokemonName}} 被折磨着！",
   "costar": "{{pokemonName}} copied {{allyName}}'s stat changes!",
   "iceFaceAvoidedDamage": "{{pokemonName}} avoided\ndamage with {{abilityName}}!",
   "trace": "{{pokemonName}} copied {{targetName}}'s\n{{abilityName}}!",
