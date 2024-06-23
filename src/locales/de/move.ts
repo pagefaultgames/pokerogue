be78280c2ed34654c6329598e1dcfb253804067b
InnocentGameDev <asdargmng@gmail.com>
2024-06-23 20:34:20 +0200

    Update src/locales/de/move.ts
    German locale commit.
    
    Co-authored-by: Jannik Tappert <38758606+CodeTappert@users.noreply.github.com>

--- src/locales/de/move.ts
+++ src/locales/de/move.ts
@@ -2975,7 +2975,7 @@ export const move: MoveTranslationEntries = {
   },
   "dynamaxCannon": {
     name: "Dynamax-Kanone",
-    effect: "Der Anwender schießt einen Strahl aus seinem Kern ab. Dynamaximierte Ziele erleiden doppelten Schaden." // Check new EN locale
+    effect: "Der Anwender schießt einen Strahl aus seinem Kern ab. Verursacht bis zu doppelt so viel Schaden, wenn der Gegner ein höheres Level als der Anwender hat."
   },
   "snipeShot": {
     name: "Präzisionsschuss",
