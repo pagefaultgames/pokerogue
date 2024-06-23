326ae013fe9ed6d7369b92abb8305d4a404452fd
José Ricardo Fleury Oliveira <josefleury@discente.ufg.br>
2024-06-23 12:50:05 -0300

    updated costar and trace (#2539)

--- src/locales/pt_BR/ability-trigger.ts
+++ src/locales/pt_BR/ability-trigger.ts
@@ -3,10 +3,10 @@ import { SimpleTranslationEntries } from "#app/interfaces/locales";
 export const abilityTriggers: SimpleTranslationEntries = {
   "blockRecoilDamage" : "{{abilityName}} de {{pokemonName}}\nprotegeu-o do dano de recuo!",
   "badDreams": "{{pokemonName}} está tendo pesadelos!",
-  "costar": "{{pokemonName}} copied {{allyName}}'s stat changes!",
+  "costar": "{{pokemonName}} copiou as mudanças\nde atributo de {{allyName}}!",
   "iceFaceAvoidedDamage": "{{pokemonName}} evitou\ndanos com sua {{abilityName}}!",
   "perishBody": "{{abilityName}} de {{pokemonName}}\nirá desmaiar ambos os Pokémon em 3 turnos!",
   "poisonHeal": "{{abilityName}} de {{pokemonName}}\nrestaurou seus PS um pouco!",
-  "trace": "{{pokemonName}} copied {{targetName}}'s\n{{abilityName}}!",
+  "trace": "{{pokemonName}} copiou {{abilityName}}\nde {{targetName}}!",
   "windPowerCharged": "Ser atingido por {{moveName}} carregou {{pokemonName}} com poder!",
 } as const;
