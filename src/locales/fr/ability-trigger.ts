111cfd686d4e9f34c5fb7f9c18685bccb0b384a2
Lugiad <adrien.grivel@hotmail.fr>
2024-06-23 18:37:04 +0200

    [Localization(fr)] Some missing translations + some corrections (#2538)
    * Update French ability-trigger.ts
    
    * Update French fight-ui-handler.ts
    
    * Update French menu.ts
    
    * Update French settings.ts
    
    * Update French egg.ts
    
    * Update French modifier-type.ts
    
    * Update modifier-type.ts
    
    * Update modifier-type.ts

--- src/locales/fr/ability-trigger.ts
+++ src/locales/fr/ability-trigger.ts
@@ -3,10 +3,10 @@ import { SimpleTranslationEntries } from "#app/interfaces/locales";
 export const abilityTriggers: SimpleTranslationEntries = {
   "blockRecoilDamage" : "{{abilityName}}\nde {{pokemonName}} le protège du contrecoup !",
   "badDreams": "{{pokemonName}} a le sommeil agité !",
-  "costar": "{{pokemonName}} copied {{allyName}}'s stat changes!",
+  "costar": "{{pokemonName}} copie les changements de stats\nde {{allyName}} !",
   "iceFaceAvoidedDamage": "{{pokemonName}} évite les dégâts\navec {{abilityName}} !",
   "perishBody": "{{abilityName}} de {{pokemonName}}\nmettra les deux Pokémon K.O. dans trois tours !",
   "poisonHeal": "{{abilityName}} de {{pokemonName}}\nrestaure un peu ses PV !",
-  "trace": "{{pokemonName}} copied {{targetName}}'s\n{{abilityName}}!",
+  "trace": "{{pokemonName}} copie le talent {{abilityName}}\nde {{targetName}} !",
   "windPowerCharged": "{{pokemonName}} a été touché par la capacité {{moveName}} et se charge en électricité !",
 } as const;
