8c8ddd26b55302b633303bb021713a7a9a73d717
cadi <dbtmdgns4296@naver.com>
2024-06-25 00:22:15 +0900

    [Ability] Implement Quick Draw (#2287)
    * implement quick draw
    
    * add line
    
    * change ability success condition
    
    * quick draw comes before quick claw effect
    
    * now ability has chance variable to test conditional situation
    
    * add unit test
    
    * Merge branch 'main' into ability-quick-draw
    
    * modifed: ability.getTriggerMessage
    
    * modified: to use pokemon.battleData.abilityRevealed
    
    * modified: changed private variable to public
    
    * erase expect interceptor log
    
    * add localizations
    
    * modified: add comma in locales
    
    * modified: ability docs & trigger logic
    
    * modified: changed test logic

--- src/locales/zh_TW/ability-trigger.ts
+++ src/locales/zh_TW/ability-trigger.ts
@@ -7,4 +7,5 @@ export const abilityTriggers: SimpleTranslationEntries = {
   "iceFaceAvoidedDamage": "{{pokemonName}} 因爲 {{abilityName}}\n避免了傷害！",
   "trace": "{{pokemonName}} 複製了 {{targetName}} 的\n{{abilityName}}!",
   "windPowerCharged": "受 {{moveName}} 的影響， {{pokemonName}} 提升了能力！",
+  "quickDraw":"{{pokemonName}} can act faster than normal, thanks to its Quick Draw!",
 } as const;
