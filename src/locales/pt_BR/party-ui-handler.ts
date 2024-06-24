f1b33d68388ef0eb8a9a14a8ab4d02a7de271cfa
José Ricardo Fleury Oliveira <josefleury@discente.ufg.br>
2024-06-24 11:19:57 -0300

    Localization: Add new translations for party UI handler

--- src/locales/pt_BR/party-ui-handler.ts
+++ src/locales/pt_BR/party-ui-handler.ts
@@ -1,10 +1,46 @@
 import { SimpleTranslationEntries } from "#app/interfaces/locales";
 
 export const partyUiHandler: SimpleTranslationEntries = {
+  "ALL": "Tudo",
   "SEND_OUT": "Trocar",
   "SUMMARY": "Sumário",
   "CANCEL": "Cancelar",
   "RELEASE": "Soltar",
   "APPLY": "Aplicar",
   "TEACH": "Ensinar",
+  "ACTIVATE": "Ativar",
+  "DEACTIVATE": "Desativar",
+
+  "noEnergy": "{{pokemonName}} não pode\nmais batalhar!",
+  "hasEnergy": "{{pokemonName}} ainda\npode batalhar!",
+  "cantBeUsed": "{{pokemonName}} não pode\nser usado neste desafio!",
+  "tooManyItems": "{{pokemonName}} já tem\nmuitos desse item!",
+  "anyEffect": "Isso não terá nenhum efeito.",
+  "unpausedEvolutions": "Evoluções foram despausadas para {{pokemonName}}.",
+  "unspliceConfirmation": "Você realmente deseja desfazer a fusão de {{fusionName}}\ncom {{pokemonName}}? {{fusionName}} será perdido.",
+  "wasReverted": "{{fusionName}} foi revertido para {{pokemonName}}.",
+  "releaseConfirmation": "Você realmente deseja soltar {{pokemonName}}?",
+  "releaseInBattle": "Você não pode soltar um Pokémon que está em batalha!",
+  "doWhat": "O que deseja fazer com este Pokémon?",
+  "selectAMove": "Selecione um movimento.",
+  "changeQuantity": "Selecione um item para transferir.\nUse < e > para mudar a quantidade.",
+  "selectAnotherPokemonToSplice": "Selecione outro Pokémon para fundir.",
+  "cancel": "Cancelar",
+
+  // Slot TM text
+  "able": "Capaz",
+  "notAble": "Incapaz",
+  "learned": "Aprendido",
+
+  // Releasing messages
+  "goodbye": "Adeus, {{pokemonName}}!",
+  "byebye": "Tchau, {{pokemonName}}!",
+  "farewell": "Até mais, {{pokemonName}}!",
+  "soLong": "Até logo, {{pokemonName}}!",
+  "thisIsWhereWePart": "Aqui é onde nos separamos, {{pokemonName}}!",
+  "illMissYou": "Vou sentir saudades, {{pokemonName}}!",
+  "illNeverForgetYou": "Nunca vou esquecer de você, {{pokemonName}}!",
+  "untilWeMeetAgain": "Até nos encontrarmos novamente, {{pokemonName}}!",
+  "sayonara": "Sayonara, {{pokemonName}}!",
+  "smellYaLater": "Te vejo depois, {{pokemonName}}!",
 } as const;
