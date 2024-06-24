2db316768561778f4f969a9c9cb5709cfac59eae
José Ricardo Fleury Oliveira <josefleury@discente.ufg.br>
2024-06-24 00:06:20 -0300

    Localization: Add "added as a starter" translation in multiple languages

--- src/locales/fr/battle.ts
+++ src/locales/fr/battle.ts
@@ -15,6 +15,7 @@ export const battle: SimpleTranslationEntries = {
   "trainerDefeated": "Vous avez battu\n{{trainerName}} !",
   "moneyWon": "Vous remportez\n{{moneyAmount}} ₽ !",
   "pokemonCaught": "Vous avez attrapé {{pokemonName}} !",
+  "addedAsAStarter": "{{pokemonName}} has been\nadded as a starter!",
   "partyFull": "Votre équipe est pleine.\nRelâcher un Pokémon pour {{pokemonName}} ?",
   "pokemon": "Pokémon",
   "sendOutPokemon": "{{pokemonName}} ! Go !",
