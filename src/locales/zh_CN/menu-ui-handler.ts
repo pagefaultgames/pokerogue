3f11d3dbc0f950e79219e51ff223e1bfd9a07f16
Yonmaru40 <47717431+40chyan@users.noreply.github.com>
2024-06-25 14:29:13 +0800

    Update menu-ui-handler.ts

--- src/locales/zh_CN/menu-ui-handler.ts
+++ src/locales/zh_CN/menu-ui-handler.ts
@@ -20,5 +20,5 @@ export const menuUiHandler: SimpleTranslationEntries = {
   "exportData": "导出数据",
   "cancel": "取消",
   "losingProgressionWarning": "你将失去自战斗开始以来的所有进度。是否\n继续？",
-  "noEggs": "You are not hatching\nany eggs at the moment!"
+  "noEggs": "当前没有任何蛋\n正在孵化中！"
 } as const;
