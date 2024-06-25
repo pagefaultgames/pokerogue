49ea8829715c9e4432e69d09410be1ef057ff091
José Ricardo Fleury Oliveira <josericardo.fleury05@gmail.com>
2024-06-25 10:47:17 -0300

    Update src/locales/fr/game-data.ts
    Co-authored-by: Lugiad' <adrien.grivel@hotmail.fr>

--- src/locales/fr/game-data.ts
+++ src/locales/fr/game-data.ts
@@ -8,7 +8,7 @@ export const gameData: SimpleTranslationEntries = {
   "seenDialoguesData": "des dialogues connus",
 
   "dataCouldNotBeLoaded": "Impossible de charger les données {{dataName}}. Elles sont peut-être corrompues.",
-  "dataWillBeOverridden": "Les données {{dataName}} seront effacées et la page rafraichie. Continuer ?",
+  "dataWillBeOverridden": "Les données {{dataName}} seront effacées. Continuer ?",
   "errorContactServer": "Connexion au serveur impossible. Les données {{dataName}} n’ont pas eu être importées.",
-  "errorUpdating": "Une erreur est survenue pendant le chargement des données {{dataName}}. Contactez un administrateur.",
+  "errorUpdating": "Une erreur est survenue lors du chargement des données {{dataName}}. Contactez un administrateur.",
 } as const;
