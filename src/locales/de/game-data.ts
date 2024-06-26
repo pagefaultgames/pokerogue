06a39a084f667c77f17342b31249f63cac52ac9c
José Ricardo Fleury Oliveira <josericardo.fleury05@gmail.com>
2024-06-26 18:13:53 -0300

    Update src/locales/de/game-data.ts
    Co-authored-by: Jannik Tappert <38758606+CodeTappert@users.noreply.github.com>

--- src/locales/de/game-data.ts
+++ src/locales/de/game-data.ts
@@ -8,7 +8,7 @@ export const gameData: SimpleTranslationEntries = {
   "seenDialoguesData": "Gesehener Dialog",
 
   "dataCouldNotBeLoaded": "Deine {{dataName}} Daten konnten nicht geladen werden. Möglicherweise sind sie beschädigt.",
-  "dataWillBeOverridden": "Deine {{dataName}} Daten werden überschrieben und die Seite wird neugeladen. Fortfahren?",
+  "dataWillBeOverridden": "Deine {{dataName}} Daten werden über-/nschrieben und es wird neugeladen. Fortfahren?",
   "errorContactServer": "Konnte den Server nicht kontaktieren. Deine {{dataName}} Daten konnten nicht importiert werden.",
   "errorUpdating": "Es ist ein Fehler aufgetreten während die {{dataName}} aktualisiert wurden. Kontaktiere bitte einen Administrator.",
 } as const;
