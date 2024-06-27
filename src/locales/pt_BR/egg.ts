3fd680c5e25deafcf78f61f7115f543c3eece3c6
José Ricardo Fleury Oliveira <josefleury@discente.ufg.br>
2024-06-27 14:49:44 -0300

    Update Brazilian Portuguese translation for egg.ts

--- src/locales/pt_BR/egg.ts
+++ src/locales/pt_BR/egg.ts
@@ -22,7 +22,7 @@ export const egg: SimpleTranslationEntries = {
   "hatchFromTheEgg": "{{pokemonName}} nasceu do ovo!",
   "eggMoveUnlock": "Movimento de Ovo desbloqueado: {{moveName}}",
   "rareEggMoveUnlock": "Movimento Raro de Ovo desbloqueado: {{moveName}}",
-  "moveUPGacha": "Golpe UP!",
-  "shinyUPGacha": "Shiny UP!",
+  "moveUPGacha": "+ Golpes!",
+  "shinyUPGacha": "+ Shiny!",
   "legendaryUPGacha": "UP!",
 } as const;
