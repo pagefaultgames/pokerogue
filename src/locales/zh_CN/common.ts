564b91a09c471340bc13868059454830faf82862
José Ricardo Fleury Oliveira <josericardo.fleury05@gmail.com>
2024-06-27 00:54:06 -0300

    Update src/locales/zh_CN/common.ts
    Co-authored-by: Yonmaru40 <47717431+40chyan@users.noreply.github.com>

--- src/locales/zh_CN/common.ts
+++ src/locales/zh_CN/common.ts
@@ -2,8 +2,8 @@ import { SimpleTranslationEntries } from "#app/interfaces/locales";
 
 export const common: SimpleTranslationEntries = {
   "start": "开始",
-  "luckIndicator": "Luck:",
-  "shinyOnHover": "Shiny",
+  "luckIndicator": "幸运：",
+  "shinyOnHover": "闪光",
   "commonShiny": "常见",
   "rareShiny": "稀有",
   "epicShiny": "史诗",
