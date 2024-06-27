9794ad23c5dd29e7c5d95a955a5fda42931235b2
José Ricardo Fleury Oliveira <josericardo.fleury05@gmail.com>
2024-06-27 09:40:28 -0300

    Update src/locales/ko/common.ts
    Co-authored-by: Enoch <enoch.jwsong@gmail.com>

--- src/locales/ko/common.ts
+++ src/locales/ko/common.ts
@@ -2,8 +2,8 @@ import { SimpleTranslationEntries } from "#app/interfaces/locales";
 
 export const common: SimpleTranslationEntries = {
   "start": "시작",
-  "luckIndicator": "Luck:",
-  "shinyOnHover": "Shiny",
+  "luckIndicator": "행운:",
+  "shinyOnHover": "색이 다른",
   "commonShiny": "커먼",
   "rareShiny": "레어",
   "epicShiny": "에픽",
