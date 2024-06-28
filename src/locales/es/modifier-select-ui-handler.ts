a8e009fefc5d098277daa659a5a2a8b213ef932c
Lugiad <adrien.grivel@hotmail.fr>
2024-06-28 14:20:19 +0200

    Update src/locales/es/modifier-select-ui-handler.ts
    Co-authored-by: GoldTra <162721984+GoldTra@users.noreply.github.com>

--- src/locales/es/modifier-select-ui-handler.ts
+++ src/locales/es/modifier-select-ui-handler.ts
@@ -1,14 +1,14 @@
 import { SimpleTranslationEntries } from "#app/interfaces/locales";
 
 export const modifierSelectUiHandler: SimpleTranslationEntries = {
-  "transfer": "Transfer",
-  "reroll": "Reroll",
-  "lockRarities": "Lock Rarities",
-  "checkTeam": "Check Team",
-  "transferDesc": "Transfer a held item from one Pokémon to another.",
-  "rerollDesc": "Spend money to reroll your item options.",
-  "lockRaritiesDesc": "Lock item rarities on reroll (affects reroll cost).",
-  "checkTeamDesc": "Check your team or use a form changing item.",
-  "rerollCost": "₽{{formattedMoney}}",
-  "itemCost": "₽{{formattedMoney}}"
+  "transfer": "Transferir",
+  "reroll": "Actualizar",
+  "lockRarities": "Bloquear Rarezas",
+  "checkTeam": "Ver Equipo",
+  "transferDesc": "Transfiere un objeto que lleva un Pokémon a otro.",
+  "rerollDesc": "Gasta dinero para actualizar las opciones de objetos.",
+  "lockRaritiesDesc": "Bloquea las rarezas de los objetos al actualizar (afecta el costo de actualización).",
+  "checkTeamDesc": "Revisa tu equipo o usa un objeto que cambia de forma.",
+  "rerollCost": "{{formattedMoney}} ₽",
+  "itemCost": "{{formattedMoney}} ₽"
 } as const;
