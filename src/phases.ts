9a5f394d3e7d1e88ef39b7c04bcf0556c3d41ad3
torranx <torranicles@gmail.com>
2024-06-30 02:44:33 +0800

    add semi invulnerable state check

--- src/phases.ts
+++ src/phases.ts
@@ -4781,7 +4781,7 @@ export class AttemptCapturePhase extends PokemonPhase {
 
     const pokemon = this.getPokemon() as EnemyPokemon;
 
-    if (!pokemon?.hp) {
+    if (!pokemon?.hp || !!pokemon.getTag(SemiInvulnerableTag) && !pokemon.getTag(BattlerTagType.FLYING)) {
       return this.end();
     }
 
