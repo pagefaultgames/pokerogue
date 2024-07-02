6ca824cd2c4c6cead0e07988eb801be87bf90a9d
Lugiad <adrien.grivel@hotmail.fr>
2024-07-02 13:45:41 +0200

    Update src/locales/es/modifier-select-ui-handler.ts
    Co-authored-by: InnocentGameDev <asdargmng@gmail.com>

--- src/locales/es/modifier-select-ui-handler.ts
+++ src/locales/es/modifier-select-ui-handler.ts
@@ -3,7 +3,7 @@ import { SimpleTranslationEntries } from "#app/interfaces/locales";
 export const modifierSelectUiHandler: SimpleTranslationEntries = {
   "transfer": "Transferir",
   "reroll": "Actualizar",
-  "lockRarities": "Bloquear Rarezas",
+  "lockRarities": "Bloquear rarezas",
   "checkTeam": "Ver Equipo",
   "transferDesc": "Transfiere un objeto que lleva un Pokémon a otro.",
   "rerollDesc": "Gasta dinero para actualizar las opciones de objetos.",
@@ -11,4 +11,5 @@ export const modifierSelectUiHandler: SimpleTranslationEntries = {
   "checkTeamDesc": "Revisa tu equipo o usa un objeto que cambia de forma.",
   "rerollCost": "{{formattedMoney}} ₽",
   "itemCost": "{{formattedMoney}} ₽"
+
 } as const;
