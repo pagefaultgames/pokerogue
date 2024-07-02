dc6f125c7bd023c47678b129d1ba3a8ae0a47c0e
José Ricardo Fleury Oliveira <josericardo.fleury05@gmail.com>
2024-07-02 07:16:16 -0300

    Update src/locales/es/game-data.ts
    Co-authored-by: InnocentGameDev <asdargmng@gmail.com>

--- src/locales/es/game-data.ts
+++ src/locales/es/game-data.ts
@@ -1,14 +1,14 @@
 import { SimpleTranslationEntries } from "#app/interfaces/locales";
 
 export const gameData: SimpleTranslationEntries = {
-  "systemData": "system",
-  "sessionData": "session",
-  "settingsData": "settings",
-  "tutorialsData": "tutorials",
-  "seenDialoguesData": "seen dialogues",
+  "systemData": "sistema",
+  "sessionData": "sesión",
+  "settingsData": "ajustes",
+  "tutorialsData": "tutoriales",
+  "seenDialoguesData": "diálogos leídos",
 
-  "dataCouldNotBeLoaded": "Your {{dataName}} data could not be loaded. It may be corrupted.",
-  "dataWillBeOverridden": "Your {{dataName}} data will be overridden and the page will reload. Proceed?",
-  "errorContactServer": "Could not contact the server. Your {{dataName}} data could not be imported.",
-  "errorUpdating": "An error occurred while updating {{dataName}} data. Please contact the administrator.",
+  "dataCouldNotBeLoaded": "Tus datos "{{dataName}}" no se han podido cargar. Puede que estén corruptos.",
+  "dataWillBeOverridden": "Tus datos "{{dataName}}" se sobreescribirán y la página recargará, ¿estás seguro?",
+  "errorContactServer": "No se pudo contactar con el servidor. Tus datos "{{dataName}}" no han podido ser importados.",
+  "errorUpdating": "Un error ha ocurrido mientras se subían los datos "{{dataName}}". Por favor, ponte en contacto con el administrador.",
 } as const;
