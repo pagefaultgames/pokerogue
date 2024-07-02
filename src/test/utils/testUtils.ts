417ce496f1d2171a817be0c128a76fbbc02cd73e
Felix Staud <felix.staud@headwire.com>
2024-07-01 22:16:46 -0700

    add test coverage for battle-stat

--- src/test/utils/testUtils.ts
+++ src/test/utils/testUtils.ts
@@ -4,3 +4,7 @@ import { vi } from "vitest";
 export function mockI18next() {
   return vi.spyOn(i18next, "t").mockImplementation((key: ParseKeys) => key);
 }
+
+export function arrayOfRange(start: integer, end: integer) {
+  return Array.from({ length: end - start }, (_v, k) => k + start);
+}
