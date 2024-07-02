d84e356866f9f227223f33f3fc9bd401cbefaf83
José Ricardo Fleury Oliveira <josericardo.fleury05@gmail.com>
2024-07-02 07:56:58 -0300

    Update src/locales/es/game-data.ts
    Co-authored-by: InnocentGameDev <asdargmng@gmail.com>

--- src/locales/es/game-data.ts
+++ src/locales/es/game-data.ts
@@ -7,8 +7,8 @@ export const gameData: SimpleTranslationEntries = {
   "tutorialsData": "tutoriales",
   "seenDialoguesData": "diálogos leídos",
 
-  "dataCouldNotBeLoaded": "Tus datos "{{dataName}}" no se han podido cargar. Puede que estén corruptos.",
-  "dataWillBeOverridden": "Tus datos "{{dataName}}" se sobreescribirán y la página recargará, ¿estás seguro?",
-  "errorContactServer": "No se pudo contactar con el servidor. Tus datos "{{dataName}}" no han podido ser importados.",
-  "errorUpdating": "Un error ha ocurrido mientras se subían los datos "{{dataName}}". Por favor, ponte en contacto con el administrador.",
+  "dataCouldNotBeLoaded": "Tus datos {{dataName}} no se han podido cargar. Puede que estén corruptos.",
+  "dataWillBeOverridden": "Tus datos {{dataName}} se sobreescribirán y la página recargará, ¿estás seguro?",
+  "errorContactServer": "No se pudo contactar con el servidor. Tus datos {{dataName}} no han podido ser importados.",
+  "errorUpdating": "Un error ha ocurrido mientras se subían los datos {{dataName}}. Por favor, ponte en contacto con el administrador.",
 } as const;
