176bf01a30d34df3f56c0d9b06c6ec11c9e70933
Philippe <56865723+prateau@users.noreply.github.com>
2024-07-02 10:06:26 +0200

    add Korean tranlation
    Co-authored-by: sodam <66295123+sodaMelon@users.noreply.github.com>

--- src/locales/ko/menu-ui-handler.ts
+++ src/locales/ko/menu-ui-handler.ts
@@ -22,8 +22,8 @@ export const menuUiHandler: SimpleTranslationEntries = {
   "unlinkDiscord": "디스코드 연동해제",
   "linkGoogle": "구글 연동",
   "unlinkGoogle": "구글 연동해제",
-  "importSettings": "Import Settings",
-  "exportSettings": "Export Settings",
+  "importSettings": "설정 불러오기",
+  "exportSettings": "설정 내보내기",
   "cancel": "취소",
   "losingProgressionWarning": "전투 시작으로부터의 진행 상황을 잃게 됩니다. 계속하시겠습니까?",
   "noEggs": "부화중인 알이 없습니다!"
