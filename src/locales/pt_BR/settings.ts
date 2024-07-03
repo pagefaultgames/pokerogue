85b6f419d12003bc972056533373c7fd82a3d418
EmberCM <emberdevteam@gmail.com>
2024-07-03 12:05:32 -0500

    Update src/locales/pt_BR/settings.ts
    Co-authored-by: José Ricardo Fleury Oliveira <josefleury@discente.ufg.br>

--- src/locales/pt_BR/settings.ts
+++ src/locales/pt_BR/settings.ts
@@ -64,7 +64,7 @@ export const settings: SimpleTranslationEntries = {
   "bgmVolume": "Volume de BGM",
   "seVolume": "Volume de SE",
   "musicPreference": "Preferência de Música",
-  "pokemonCries": "Pokemon Battle Cries",
+  "pokemonCries": "Sons de Batalha Pokémon",
   "mixed": "Misto",
   "gamepadPleasePlug": "Conecte um controle ou pressione um botão",
   "delete": "Deletar",
