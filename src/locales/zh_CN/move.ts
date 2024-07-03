2c56ab3bfad0aa02d65cb4b5657559dcd3b857fa
InnocentGameDev <asdargmng@gmail.com>
2024-07-03 17:01:34 +0200

    Update src/locales/zh_CN/move.ts
    zh_CN locale
    
    Co-authored-by: RimKnight <rimknight852@gmail.com>

--- src/locales/zh_CN/move.ts
+++ src/locales/zh_CN/move.ts
@@ -2975,7 +2975,8 @@ export const move: MoveTranslationEntries = {
   },
   "dynamaxCannon": {
     name: "极巨炮",
-    effect: "将凝缩在体内的能量从核心放出进行攻击，\n对手的等级比你高的越多，造成的伤害越高，最多两倍。", // Check new English description
+    effect: "将凝缩在体内的能量从核心放出进行攻击，\n对手等级比当前波次的等级上限越高，造成的伤害越高，最多两倍。", 
+
   },
   "snipeShot": {
     name: "狙击",
