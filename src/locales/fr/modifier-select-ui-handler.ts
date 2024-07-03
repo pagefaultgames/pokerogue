ca5f6539a86b41ef81008c0483a6bcc080051dc2
Lugiad <adrien.grivel@hotmail.fr>
2024-07-03 13:34:30 +0200

    Update modifier-select-ui-handler.ts

--- src/locales/fr/modifier-select-ui-handler.ts
+++ src/locales/fr/modifier-select-ui-handler.ts
@@ -1,14 +1,14 @@
-import { SimpleTranslationEntries } from "#app/interfaces/locales";
+  import { SimpleTranslationEntries } from "#app/interfaces/locales";
 
 export const modifierSelectUiHandler: SimpleTranslationEntries = {
   "transfer": "Transférer",
   "reroll": "Relancer",
   "lockRarities": "Bloquer la rareté",
-  "checkTeam": "Vérifier Équipe",
+  "checkTeam": "Consulter Équipe",
   "transferDesc": "Transférer un objet tenu par un Pokémon vers un autre.",
   "rerollDesc": "Payer pour relancer les objets gratuits proposés.",
-  "lockRaritiesDesc": "La relance proposera des objets gratuits de rareté\négale ou supérieure. Affecte le cout de relance.",
-  "checkTeamDesc": "Vérifier votre équipe ou utiliser\nun objet de changement de forme.",
+  "lockRaritiesDesc": "Assure la relance de proposer des objets gratuits de rareté égale ou supérieure. Affecte le cout de relance.",
+  "checkTeamDesc": "Consulter votre équipe ou utiliser un objet\nde changement de forme.",
   "rerollCost": "{{formattedMoney}} ₽",
   "itemCost": "{{formattedMoney}} ₽"
 } as const;
