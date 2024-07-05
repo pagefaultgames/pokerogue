ee3f8d2ec22c100d9250758cad7fec0387f01158
Jannik Tappert <38758606+CodeTappert@users.noreply.github.com>
2024-07-05 14:17:43 +0200

    Update src/locales/fr/battle.ts
    Co-authored-by: Lugiad' <adrien.grivel@hotmail.fr>

--- src/locales/fr/battle.ts
+++ src/locales/fr/battle.ts
@@ -25,7 +25,7 @@ export const battle: SimpleTranslationEntries = {
   "hitResultNoEffect": "Ça n’affecte pas {{pokemonName}}…",
   "hitResultOneHitKO": "K.O. en un coup !",
   "attackFailed": "Mais cela échoue !",
-  "attackMissed": "{{pokemonNameWithAffix}} avoided the attack!",
+  "attackMissed": "{{pokemonNameWithAffix}}\névite l’attaque !",
   "attackHitsCount": "Touché {{count}} fois !",
   "rewardGain": "Vous recevez\n{{modifierName}} !",
   "expGain": "{{pokemonName}} gagne\n{{exp}} Points d’Exp !",
