f9b926faf1cdd97463cf032159dff31e97c775df
Frutescens <info@laptop>
2024-07-04 18:20:09 -0700

    Weather Test

--- src/test/abilities/magic_guard.test.ts
+++ src/test/abilities/magic_guard.test.ts
@@ -3,14 +3,18 @@ import Phaser from "phaser";
 import GameManager from "#app/test/utils/gameManager";
 import * as overrides from "#app/overrides";
 import { Species } from "#enums/species";
-import { TurnEndPhase, } from "#app/phases";
+import { CommandPhase, TurnEndPhase, WeatherEffectPhase, PostTurnStatusEffectPhase } from "#app/phases";
 import { Moves } from "#enums/moves";
 import { getMovePosition } from "#app/test/utils/gameManagerUtils";
 import { Abilities } from "#enums/abilities";
 import Move, { allMoves } from "#app/data/move.js";
 import { BlockNonDirectDamageAbAttr } from "#app/data/ability.js";
+import { WeatherType } from "#app/data/weather.js";
 import { NumberHolder } from "#app/utils.js";
 import Pokemon from "#app/field/pokemon.js";
+import {Mode} from "#app/ui/ui";
+import {Command} from "#app/ui/command-ui-handler";
+import { StatusEffect } from "#app/data/status-effect";
 
 describe("Abilities - Magic Guard", () => {
   let phaserGame: Phaser.Game;
@@ -29,27 +33,44 @@ describe("Abilities - Magic Guard", () => {
   beforeEach(() => {
     game = new GameManager(phaserGame);
     vi.spyOn(overrides, "SINGLE_BATTLE_OVERRIDE", "get").mockReturnValue(true);
-    vi.spyOn(overrides, "OPP_ABILITY_OVERRIDE", "get").mockReturnValue(Abilities.WONDER_GUARD);
-    vi.spyOn(overrides, "MOVESET_OVERRIDE", "get").mockReturnValue([Moves.TACKLE, Moves.CHARM]);
+    vi.spyOn(overrides, "ABILITY_OVERRIDE", "get").mockReturnValue(Abilities.MAGIC_GUARD);
     vi.spyOn(overrides, "OPP_MOVESET_OVERRIDE", "get").mockReturnValue([Moves.SPLASH, Moves.SPLASH, Moves.SPLASH, Moves.SPLASH]);
   });
 
   it("Magic Guard prevents damage caused by weather", async () => {
+    vi.spyOn(overrides, "WEATHER_OVERRIDE", "get").mockReturnValue(WeatherType.SANDSTORM);
+    vi.spyOn(overrides, "MOVESET_OVERRIDE", "get").mockReturnValue([Moves.SPLASH]);
     await game.startBattle([Species.MAGIKARP]);
-
-    game.doAttack(getMovePosition(game.scene, 0, Moves.CHARM));
-
-    await game.phaseInterceptor.to(TurnEndPhase);
+    const hpPokemon = game.scene.getPlayerPokemon().hp;
+    //Attempting to applying weather damage now...
+    new WeatherEffectPhase(game.scene).start();
+    const hpLost = (hpPokemon === game.scene.getPlayerPokemon().hp);
+    expect(hpLost).toBe(true);
   });
 
   it("Magic Guard prevents damage caused by poison but Pokemon still can be poisoned and take damage upon losing Magic Guard", async () => {
+    //Toxic keeps track of the turn counters -> important that Magic Guard keeps track of post-Toxic turns 
+    vi.spyOn(overrides, "STATUS_OVERRIDE", "get").mockReturnValue(StatusEffect.POISON);
+    vi.spyOn(overrides, "MOVESET_OVERRIDE", "get").mockReturnValue([Moves.SKILL_SWAP, Moves.SPLASH]);
     await game.startBattle([Species.MAGIKARP]);
-
-    game.doAttack(getMovePosition(game.scene, 0, Moves.CHARM));
-
+    const pokemonMG = game.scene.getPlayerPokemon();
+    const startingHP = pokemonMG.hp;
+    game.doAttack(getMovePosition(game.scene, 0, Moves.SPLASH));
     await game.phaseInterceptor.to(TurnEndPhase);
+  	const statusEffectPhase = new PostTurnStatusEffectPhase(game.scene, 0);
+    statusEffectPhase.start();
+    const lostHPWithMG = (pokemonMG.hp === startingHP);
+    //await game.toNextTurn();
+    game.doAttack(getMovePosition(game.scene, 0, Moves.SKILL_SWAP));
+    await game.phaseInterceptor.to(TurnEndPhase);
+    statusEffectPhase.start();
+    const lostHPWithNoMG = (pokemonMG.hp < startingHP);
+
+    expect(lostHPWithMG).toBe(true);
+    expect(lostHPWithNoMG).toBe(true);
   });
 
+/*
   it("Magic Guard prevents damage caused by burn but Pokemon still can be burned and take damage upon losing Magic Guard", async () => {
     await game.startBattle([Species.MAGIKARP]);
 
@@ -129,4 +150,5 @@ describe("Abilities - Magic Guard", () => {
 
     await game.phaseInterceptor.to(TurnEndPhase);
   });
- });
+  */
+});
