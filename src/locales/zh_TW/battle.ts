88ba35459e07988688f5e4832d9e7a00ff49f606
Jannik Tappert <38758606+CodeTappert@users.noreply.github.com>
2024-07-05 14:24:22 +0200

    Update src/locales/zh_TW/battle.ts
    Co-authored-by: Yonmaru40 <47717431+40chyan@users.noreply.github.com>

--- src/locales/zh_TW/battle.ts
+++ src/locales/zh_TW/battle.ts
@@ -22,7 +22,7 @@ export const battle: SimpleTranslationEntries = {
   "hitResultNoEffect": "對 {{pokemonName}} 沒有效果！",
   "hitResultOneHitKO": "一擊切殺！",
   "attackFailed": "但是失敗了！",
-  "attackMissed": "{{pokemonNameWithAffix}} avoided the attack!",
+  "attackMissed": "沒有命中{{pokemonNameWithAffix}}！",
   "attackHitsCount": "擊中 {{count}} 次！",
   "rewardGain": "You received\n{{modifierName}}!",
   "expGain": "{{pokemonName}} 獲得了 {{exp}} 經驗值！",
