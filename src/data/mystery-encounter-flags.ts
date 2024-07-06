b3aa562bc639afebb00f737269ab7a644e71c18d
ImperialSympathizer <imperialsympathizer@gmail.com>
2024-07-05 20:42:25 -0400

    small fixes

--- src/data/mystery-encounter-flags.ts
+++ src/data/mystery-encounter-flags.ts
@@ -1,11 +1,11 @@
 import {MysteryEncounterTier} from "#app/data/mystery-encounter";
 import {MysteryEncounterType} from "#enums/mystery-encounter-type";
-import {BASE_MYSTYERY_ENCOUNTER_WEIGHT} from "#app/data/mystery-encounters/mystery-encounters";
+import {BASE_MYSTERY_ENCOUNTER_WEIGHT} from "#app/data/mystery-encounters/mystery-encounters";
 import {isNullOrUndefined} from "../utils";
 
 export class MysteryEncounterFlags {
   encounteredEvents: [MysteryEncounterType, MysteryEncounterTier][] = [];
-  encounterSpawnChance: number = BASE_MYSTYERY_ENCOUNTER_WEIGHT;
+  encounterSpawnChance: number = BASE_MYSTERY_ENCOUNTER_WEIGHT;
   nextEncounterQueue: [MysteryEncounterType, integer][] = [];
 
   constructor(flags: MysteryEncounterFlags) {
