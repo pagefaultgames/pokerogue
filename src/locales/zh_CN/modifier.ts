87203959ac39cae2345f4ee670ef73d8d41ed92f
Enoch <enoch.jwsong@gmail.com>
2024-07-07 13:48:54 +0900

    Update src/locales/zh_CN/modifier.ts
    Co-authored-by: Yonmaru40 <47717431+40chyan@users.noreply.github.com>

--- src/locales/zh_CN/modifier.ts
+++ src/locales/zh_CN/modifier.ts
@@ -1,12 +1,12 @@
 import { SimpleTranslationEntries } from "#app/interfaces/locales";
 
 export const modifier: SimpleTranslationEntries = {
-  "surviveDamageApply": "{{pokemonNameWithAffix}} hung on\nusing its {{typeName}}!",
-  "turnHealApply": "{{pokemonNameWithAffix}} restored a little HP using\nits {{typeName}}!",
-  "hitHealApply": "{{pokemonNameWithAffix}} restored a little HP using\nits {{typeName}}!",
-  "pokemonInstantReviveApply": "{{pokemonNameWithAffix}} was revived\nby its {{typeName}}!",
-  "moneyInterestApply": "You received interest of ₽{{moneyAmount}}\nfrom the {{typeName}}!",
-  "turnHeldItemTransferApply": "{{pokemonNameWithAffix}}'s {{itemName}} was absorbed\nby {{pokemonName}}'s {{typeName}}!",
-  "contactHeldItemTransferApply": "{{pokemonNameWithAffix}}'s {{itemName}} was snatched\nby {{pokemonName}}'s {{typeName}}!",
-  "enemyTurnHealApply": "{{pokemonNameWithAffix}}\nrestored some HP!",
+  "surviveDamageApply": "{{pokemonNameWithAffix}}用{{typeName}}\n撑住了！",
+  "turnHealApply": "{{pokemonNameWithAffix}}用{{typeName}}\n回复了体力！",
+  "hitHealApply": "{{pokemonNameWithAffix}}用{{typeName}}\n回复了体力！",
+  "pokemonInstantReviveApply": "{{pokemonNameWithAffix}}用{{typeName}}\n恢复了活力！",
+  "moneyInterestApply": "用{{typeName}}\n获得了 ₽{{moneyAmount}} 利息！",
+  "turnHeldItemTransferApply": "{{pokemonNameWithAffix}}的{{itemName}}被\n{{pokemonName}}的{{typeName}}吸收了！",
+  "contactHeldItemTransferApply": "{{pokemonNameWithAffix}}的{{itemName}}被\n{{pokemonName}}的{{typeName}}夺取了！",
+  "enemyTurnHealApply": "{{pokemonNameWithAffix}}\n回复了一些体力！",
 } as const;
