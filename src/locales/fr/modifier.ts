d8db7e6aa01ab5e1c585bc397ffc91ead45dad5d
Enoch <enoch.jwsong@gmail.com>
2024-07-08 08:27:47 +0900

    Update src/locales/fr/modifier.ts
    Co-authored-by: Lugiad' <adrien.grivel@hotmail.fr>

--- src/locales/fr/modifier.ts
+++ src/locales/fr/modifier.ts
@@ -1,12 +1,12 @@
 import { SimpleTranslationEntries } from "#app/interfaces/locales";
 
 export const modifier: SimpleTranslationEntries = {
-  "surviveDamageApply": "{{pokemonNameWithAffix}} tient bon grâce à {{typeName}} !",
-  "turnHealApply": "Les PV du {{pokemonNameWithAffix}}\nsont un peu restaurés par {{typeName}} !",
-  "hitHealApply": "Les PV du {{pokemonNameWithAffix}}\nsont un peu restaurés par {{typeName}} !",
-  "pokemonInstantReviveApply": "{{pokemonNameWithAffix}} a repris connaissance\navec {{typeName}} et est prêt à se battre de nouveau !",
-  "moneyInterestApply": "Vous recevez {{moneyAmount}} ₽\nd’intérets de la {{typeName}} !",
+  "surviveDamageApply": "{{pokemonNameWithAffix}} tient bon\ngrâce à son {{typeName}} !",
+  "turnHealApply": "Les PV de {{pokemonNameWithAffix}}\nsont un peu restaurés par les {{typeName}} !",
+  "hitHealApply": "Les PV de {{pokemonNameWithAffix}}\nsont un peu restaurés par le {{typeName}} !",
+  "pokemonInstantReviveApply": "{{pokemonNameWithAffix}} a repris connaissance\navec sa {{typeName}} et est prêt à se battre de nouveau !",
+  "moneyInterestApply": "La {{typeName}} vous rapporte\n{{moneyAmount}} ₽ d’intérêts !",
   "turnHeldItemTransferApply": "{{itemName}} de {{pokemonNameWithAffix}} est absorbé\npar le {{typeName}} de {{pokemonName}} !",
-  "contactHeldItemTransferApply": "{{itemName}} de {{pokemonNameWithAffix}} est volé\npar {{typeName}} de {{pokemonName}} !",
+  "contactHeldItemTransferApply": "{{itemName}} de {{pokemonNameWithAffix}} est volé\npar l’{{typeName}} de {{pokemonName}} !",
   "enemyTurnHealApply": "{{pokemonNameWithAffix}}\nrestaure un peu ses PV !",
 } as const;
