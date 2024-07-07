aed87ec83502d73c13b412b2953d75d0b8132c2f
Enoch <enoch.jwsong@gmail.com>
2024-07-08 08:51:22 +0900

    Update src/locales/fr/modifier.ts
    Co-authored-by: Lugiad' <adrien.grivel@hotmail.fr>

--- src/locales/fr/modifier.ts
+++ src/locales/fr/modifier.ts
@@ -6,7 +6,7 @@ export const modifier: SimpleTranslationEntries = {
   "hitHealApply": "Les PV de {{pokemonNameWithAffix}}\nsont un peu restaurés par le {{typeName}} !",
   "pokemonInstantReviveApply": "{{pokemonNameWithAffix}} a repris connaissance\navec sa {{typeName}} et est prêt à se battre de nouveau !",
   "moneyInterestApply": "La {{typeName}} vous rapporte\n{{moneyAmount}} ₽ d’intérêts !",
-  "turnHeldItemTransferApply": "{{itemName}} de {{pokemonNameWithAffix}} est absorbé\npar le {{typeName}} de {{pokemonName}} !",
-  "contactHeldItemTransferApply": "{{itemName}} de {{pokemonNameWithAffix}} est volé\npar l’{{typeName}} de {{pokemonName}} !",
+  "turnHeldItemTransferApply": "{{itemName}} de {{pokemonNameWithAffix}} est absorbé·e\npar le {{typeName}} de {{pokemonName}} !",
+  "contactHeldItemTransferApply": "{{itemName}} de {{pokemonNameWithAffix}} est volé·e\npar l’{{typeName}} de {{pokemonName}} !",
   "enemyTurnHealApply": "{{pokemonNameWithAffix}}\nrestaure un peu ses PV !",
 } as const;
