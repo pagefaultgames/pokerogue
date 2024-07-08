2143e88aef3c49ee7cf597c8e67bcd9fa34fcee6
kvngrnr <67913362+kvngrnr@users.noreply.github.com>
2024-07-08 16:35:37 +0200

    Update src/locales/pt_BR/battle.ts
    Co-authored-by: José Ricardo Fleury Oliveira <josefleury@discente.ufg.br>

--- src/locales/pt_BR/battle.ts
+++ src/locales/pt_BR/battle.ts
@@ -133,5 +133,5 @@ export const battle: SimpleTranslationEntries = {
   "battlerTagsSaltCuredLapse": "{{pokemonNameWithAffix}} foi ferido pelo {{moveName}}!",
   "battlerTagsCursedOnAdd": "{{pokemonNameWithAffix}} cortou seus PS pela metade e amaldiçoou {{pokemonName}}!",
   "battlerTagsCursedLapse": "{{pokemonNameWithAffix}} foi ferido pelo Curse!",
-  "useQuickClaw": "{{pokemonName}} usou sua {{quickClaw}} para atacar primeiro!"
+  "useQuickClaw": "{{pokemonName}} se move mais rápido que o normal graças a sua {{quickClaw}}!"
 } as const;
