6734ffc528c3674088aaec49623b1dd7b6aa84fa
EmberCM <emberdevteam@gmail.com>
2024-07-08 12:44:37 -0500

    Update src/locales/fr/battle-info.ts
    Co-authored-by: Lugiad' <adrien.grivel@hotmail.fr>

--- src/locales/fr/battle-info.ts
+++ src/locales/fr/battle-info.ts
@@ -1,5 +1,5 @@
 import { SimpleTranslationEntries } from "#app/interfaces/locales";
 
 export const battleInfo: SimpleTranslationEntries = {
-  "generation": "Generation {{generation}}",
+  "generation": "Génération {{generation}}",
 } as const;
