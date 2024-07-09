172562459c030f0522205842f566acb6669d7a05
Niccolò <123510358+NicusPulcis@users.noreply.github.com>
2024-07-09 15:57:48 +0200

    Update settings.ts

--- src/locales/it/settings.ts
+++ src/locales/it/settings.ts
@@ -43,7 +43,7 @@ export const settings: SimpleTranslationEntries = {
   "moveAnimations": "Move Animations",
   "showStatsOnLevelUp": "Show Stats on Level Up",
   "candyUpgradeNotification": "Candy Upgrade Notification",
-  "passivesFirst": "Passives First", // Changed from "Passives Only" to "Passives First", needs new translation
+  "passivesFirst": "Priorità alle Passive", // Changed from "Passives Only" to "Passives First", needs new translation
   "candyUpgradeDisplay": "Candy Upgrade Display",
   "icon": "Icon",
   "animation": "Animation",
