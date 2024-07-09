77ec9a0ca6232a2a178b1c4a9046e4b562ef6f06
Felix Staud <felix.staud@headwire.com>
2024-07-08 19:22:02 -0700

    add documentation to phaser-extensions.d.ts

--- src/phaser-extensions.d.ts
+++ src/phaser-extensions.d.ts
@@ -1,12 +1,21 @@
 /// <reference types="phaser" />
 
+/**
+ * Defines custom extensions of phaser interfaces and types
+ */
 declare module "phaser" {
   interface Game {
+    /**
+     * Manifest object e.g. for cache busting
+     */
     manifest?: unknown;
   }
 
   namespace Loader {
     interface LoaderPlugin {
+      /**
+       * Manifest object e.g. for cache busting
+       */
       manifest?: unknown;
     }
   }
