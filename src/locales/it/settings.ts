83e9ba7b051bdff9fcd3e7a7cdbbcddd0a446c7d
José Ricardo Fleury Oliveira <josefleury@discente.ufg.br>
2024-07-09 23:49:45 -0300

    Update settings.ts

--- src/locales/it/settings.ts
+++ src/locales/it/settings.ts
@@ -43,7 +43,7 @@ export const settings: SimpleTranslationEntries = {
   "moveAnimations": "Move Animations",
   "showStatsOnLevelUp": "Show Stats on Level Up",
   "candyUpgradeNotification": "Candy Upgrade Notification",
-  "passivesFirst": "Priorità alle Passive", // Changed from "Passives Only" to "Passives First", needs new translation
+  "passivesFirst": "Priorità Passive",
   "candyUpgradeDisplay": "Candy Upgrade Display",
   "icon": "Icon",
   "animation": "Animation",
