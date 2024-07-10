a35f2f6b2ca8ee5fdf21256b33c43708d18ac115
mercurius-00 <j1441258370@163.com>
2024-07-10 11:32:30 +0800

    [Localization]add pokemonName to move-trigger.ts

--- src/data/move.ts
+++ src/data/move.ts
@@ -7054,8 +7054,8 @@ export function initMoves() {
         MovePowerMultiplierAttr,
         (user, target, move) =>  target.status || target.hasAbility(Abilities.COMATOSE)? 2 : 1),
     new AttackMove(Moves.SKY_DROP, Type.FLYING, MoveCategory.PHYSICAL, 60, 100, 10, -1, 0, 5)
-      .attr(ChargeAttr, ChargeAnim.SKY_DROP_CHARGING, i18next.t("moveTriggers:tookTargetIntoSky", {pokemonName: "{USER}"}, {targetName: "{TARGET}"}), BattlerTagType.FLYING) // TODO: Add 2nd turn message
-      // .attr(ChargeAttr, ChargeAnim.SKY_DROP_CHARGING, "took {TARGET} into the 天上!", BattlerTagType.FLYING) // TODO: Add 2nd turn message
+      .attr(ChargeAttr, ChargeAnim.SKY_DROP_CHARGING, i18next.t("moveTriggers:tookTargetIntoSky", {pokemonName: "{USER}", targetName: "{TARGET}"}), BattlerTagType.FLYING) // TODO: Add 2nd turn message
+      // TODO: Add 2nd turn message
       .condition(failOnGravityCondition)
       .ignoresVirtual(),
     new SelfStatusMove(Moves.SHIFT_GEAR, Type.STEEL, -1, 10, -1, 0, 5)
