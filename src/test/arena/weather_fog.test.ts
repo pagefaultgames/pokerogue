482ba233c73b0bfdd182f2451f960699baf94b82
torranx <torranicles@gmail.com>
2024-07-11 16:30:20 +0800

    add unit tests

---
+++ src/test/arena/weather_fog.test.ts
@@ -0,0 +1,49 @@
+import { afterEach, beforeAll, beforeEach, describe, expect, it, vi } from "vitest";
+import Phaser from "phaser";
+import GameManager from "#app/test/utils/gameManager";
+import * as overrides from "#app/overrides";
+import { Species } from "#enums/species";
+import {
+  MoveEndPhase,
+} from "#app/phases";
+import { Moves } from "#enums/moves";
+import { getMovePosition } from "#app/test/utils/gameManagerUtils";
+import { allMoves } from "#app/data/move.js";
+import { WeatherType } from "#app/data/weather.js";
+
+describe("Weather - Fog", () => {
+  let phaserGame: Phaser.Game;
+  let game: GameManager;
+
+  beforeAll(() => {
+    phaserGame = new Phaser.Game({
+      type: Phaser.HEADLESS,
+    });
+  });
+
+  afterEach(() => {
+    game.phaseInterceptor.restoreOg();
+  });
+
+  beforeEach(() => {
+    game = new GameManager(phaserGame);
+    vi.spyOn(overrides, "WEATHER_OVERRIDE", "get").mockReturnValue(WeatherType.FOG);
+    vi.spyOn(overrides, "SINGLE_BATTLE_OVERRIDE", "get").mockReturnValue(true);
+    vi.spyOn(overrides, "OPP_SPECIES_OVERRIDE", "get").mockReturnValue(Species.MAGIKARP);
+    vi.spyOn(overrides, "MOVESET_OVERRIDE", "get").mockReturnValue([Moves.TACKLE]);
+    vi.spyOn(overrides, "OPP_MOVESET_OVERRIDE", "get").mockReturnValue(new Array(4).fill(Moves.SPLASH));
+  });
+
+  it("move accuracy is multiplied by 90%", async () => {
+    const moveToCheck = allMoves[Moves.TACKLE];
+    await game.startBattle([Species.MAGIKARP]);
+
+    game.doAttack(getMovePosition(game.scene, 0, Moves.TACKLE));
+    const moveAccuracy = moveToCheck.calculateAccuracy(game.scene.getPlayerPokemon(), game.scene.getEnemyPokemon());
+
+    await game.phaseInterceptor.to(MoveEndPhase);
+
+    expect(moveAccuracy).not.toBe(moveToCheck.accuracy);
+    expect(moveAccuracy).toBe(moveToCheck.accuracy * 0.9);
+  });
+});
