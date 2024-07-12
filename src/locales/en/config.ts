5567d710f95f7c38c76cec3765a7de9c2535ae4d
mercurius-00 <80205689+mercurius-00@users.noreply.github.com>
2024-07-13 00:23:28 +0800

    [Localization]Localized move-trigger text (#2913)
    * [Localization]Localized move-trigger text
    
    * [Localization]Localized zh-cn move-trigger text
    
    * [Localization]fix typo
    
    * [Localization]fix typo
    
    * Update src/locales/pt_BR/move-trigger.ts
    
    Co-authored-by: José Ricardo Fleury Oliveira <josefleury@discente.ufg.br>
    
    * [Localization]add pokemonName to move-trigger.ts
    
    * [Localization]add pokemonName to move-trigger.ts
    
    * Update zh_TW move-trigger.ts
    
    * Update zh_CN move-trigger.ts
    
    * Update move.ts
    
    * Update src/locales/ko/move-trigger.ts
    
    Co-authored-by: Enoch <enoch.jwsong@gmail.com>
    
    * Update src/locales/ko/move-trigger.ts
    
    Co-authored-by: Enoch <enoch.jwsong@gmail.com>
    
    * Update move-trigger.ts
    
    * Update src/locales/de/move-trigger.ts
    
    Co-authored-by: Jannik Tappert <38758606+CodeTappert@users.noreply.github.com>
    
    * [Localization]add affix to target pokemon names
    
    * Update src/locales/fr/move-trigger.ts
    
    Co-authored-by: Lugiad' <adrien.grivel@hotmail.fr>
    
    * localized type
    
    ---------
    
    Co-authored-by: José Ricardo Fleury Oliveira <josefleury@discente.ufg.br>
    Co-authored-by: Enoch <enoch.jwsong@gmail.com>
    Co-authored-by: Jannik Tappert <38758606+CodeTappert@users.noreply.github.com>
    Co-authored-by: Lugiad' <adrien.grivel@hotmail.fr>

--- src/locales/en/config.ts
+++ src/locales/en/config.ts
@@ -47,6 +47,7 @@ import { tutorial } from "./tutorial";
 import { voucher } from "./voucher";
 import { terrain, weather } from "./weather";
 import { modifierSelectUiHandler } from "./modifier-select-ui-handler";
+import { moveTriggers } from "./move-trigger";
 
 export const enConfig = {
   ability: ability,
@@ -99,5 +100,6 @@ export const enConfig = {
   voucher: voucher,
   weather: weather,
   partyUiHandler: partyUiHandler,
-  modifierSelectUiHandler: modifierSelectUiHandler
+  modifierSelectUiHandler: modifierSelectUiHandler,
+  moveTriggers: moveTriggers
 };
