b386de184b945fbb2a6079aeda15e39e2349bddc
Jannik Tappert <38758606+CodeTappert@users.noreply.github.com>
2024-07-13 20:46:12 +0200

    Charge Attack Text now correctly replaces USER and TARGET

--- src/data/move.ts
+++ src/data/move.ts
@@ -2309,7 +2309,9 @@ export class DelayedAttackAttr extends OverrideMoveEffectAttr {
       if (args.length < 2 || !args[1]) {
         new MoveChargeAnim(this.chargeAnim, move.id, user).play(user.scene, () => {
           (args[0] as Utils.BooleanHolder).value = true;
-          user.scene.queueMessage(getPokemonMessage(user, ` ${this.chargeText.replace("{TARGET}", target.name)}`));
+          this.chargeText = this.chargeText.replace("{USER}", getPokemonNameWithAffix(user));
+          this.chargeText = this.chargeText.replace("{TARGET}", getPokemonNameWithAffix(target));
+          user.scene.queueMessage(this.chargeText);
           user.pushMoveHistory({ move: move.id, targets: [ target.getBattlerIndex() ], result: MoveResult.OTHER });
           user.scene.arena.addTag(this.tagType, 3, move.id, user.id, ArenaTagSide.BOTH, false, target.getBattlerIndex());
 
