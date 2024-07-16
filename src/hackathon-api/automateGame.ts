95d628629467ead3529582a16b2444839d384620
ceeeen <raymondcn2002@gmail.com>
2024-07-16 19:09:17 +1000

    command ui disappears after catch

--- src/hackathon-api/automateGame.ts
+++ src/hackathon-api/automateGame.ts
@@ -17,35 +17,35 @@ import { Mode } from "#app/ui/ui.js";
  * @param game - The Phaser game instance.
  */
 export const automateGame = async (game: Phaser.Game) => {
-    // Initialise the game with three starter Pokémon.
-    // Participants can change the species numbers to select different starters.
-    const battleScene = await initWithStarters(game, [1, 155, 258]);
+  // Initialise the game with three starter Pokémon.
+  // Participants can change the species numbers to select different starters.
+  const battleScene = await initWithStarters(game, [1, 155, 258]);
 
-    phaseApi(battleScene);
+  phaseApi(battleScene);
 
-    // Additional logic to interact with the game via the Pokerogue hackathon API can be added here.
-    // For example, participants can use this space to automate player actions,
-    // simulate gameplay scenarios, or manipulate game data for testing purposes.
+  // Additional logic to interact with the game via the Pokerogue hackathon API can be added here.
+  // For example, participants can use this space to automate player actions,
+  // simulate gameplay scenarios, or manipulate game data for testing purposes.
 
-    // Participants are encouraged to refer to the Pokerogue hackathon API documentation
-    // for detailed information on available functions and their usage.
+  // Participants are encouraged to refer to the Pokerogue hackathon API documentation
+  // for detailed information on available functions and their usage.
 };
 
 const phaseApi = (scene: BattleScene) => {
-    const checkPhase = (currentPhase: Phase) => {
-        if (currentPhase instanceof CommandPhase) {
-            BallCommand(scene);
-        } else {
-            console.log("Some other phase not yet implemented");
-            if (scene.ui.getHandler() instanceof CommandUiHandler) {
-                scene.ui.getHandler().clear();
-                scene.ui.setMode(Mode.MESSAGE);
-            }
-        }
-    };
+  const checkPhase = (currentPhase: Phase) => {
+    if (currentPhase instanceof CommandPhase) {
+      BallCommand(scene);
+    } else {
+      console.log("Some other phase not yet implemented");
+      if (scene.ui.getHandler() instanceof CommandUiHandler) {
+        scene.ui.getHandler().clear();
+        scene.ui.setMode(Mode.MESSAGE);
+      }
+    }
+  };
 
-    // Add the checkPhase function as a listener for phase changes
-    scene.addPhaseChangeListener(checkPhase);
+  // Add the checkPhase function as a listener for phase changes
+  scene.addPhaseChangeListener(checkPhase);
 };
 
 export default automateGame;
--- src/ui/command-ui-handler.ts
+++ src/ui/command-ui-handler.ts
@@ -15,201 +15,201 @@ export enum Command {
 }
 
 export default class CommandUiHandler extends UiHandler {
-    private commandsContainer: Phaser.GameObjects.Container;
-    private cursorObj: Phaser.GameObjects.Image;
+  private commandsContainer: Phaser.GameObjects.Container;
+  private cursorObj: Phaser.GameObjects.Image;
 
-    protected fieldIndex: integer = 0;
-    protected cursor2: integer = 0;
+  protected fieldIndex: integer = 0;
+  protected cursor2: integer = 0;
 
-    constructor(scene: BattleScene) {
-        super(scene, Mode.COMMAND);
+  constructor(scene: BattleScene) {
+    super(scene, Mode.COMMAND);
+  }
+
+  setup() {
+    const ui = this.getUi();
+    const commands = [
+      i18next.t("commandUiHandler:fight"),
+      i18next.t("commandUiHandler:ball"),
+      i18next.t("commandUiHandler:pokemon"),
+      i18next.t("commandUiHandler:run"),
+    ];
+
+    this.commandsContainer = this.scene.add.container(217, -38.7);
+    this.commandsContainer.setName("commands");
+    this.commandsContainer.setVisible(false);
+    ui.add(this.commandsContainer);
+
+    for (let c = 0; c < commands.length; c++) {
+      const commandText = addTextObject(
+        this.scene,
+        c % 2 === 0 ? 0 : 55.8,
+        c < 2 ? 0 : 16,
+        commands[c],
+        TextStyle.WINDOW
+      );
+      commandText.setName(commands[c]);
+      this.commandsContainer.add(commandText);
+    }
+  }
+
+  show(args: any[]): boolean {
+    super.show(args);
+
+    this.fieldIndex = args.length ? (args[0] as integer) : 0;
+
+    this.commandsContainer.setVisible(true);
+
+    let commandPhase: CommandPhase;
+    const currentPhase = this.scene.getCurrentPhase();
+    if (currentPhase instanceof CommandPhase) {
+      commandPhase = currentPhase;
+    } else {
+      commandPhase = this.scene.getStandbyPhase() as CommandPhase;
     }
 
-    setup() {
-        const ui = this.getUi();
-        const commands = [
-            i18next.t("commandUiHandler:fight"),
-            i18next.t("commandUiHandler:ball"),
-            i18next.t("commandUiHandler:pokemon"),
-            i18next.t("commandUiHandler:run"),
-        ];
+    const messageHandler = this.getUi().getMessageHandler();
+    messageHandler.bg.setVisible(true);
+    messageHandler.commandWindow.setVisible(true);
+    messageHandler.movesWindowContainer.setVisible(false);
+    messageHandler.message.setWordWrapWidth(1110);
+    messageHandler.showText(
+      i18next.t("commandUiHandler:actionMessage", {
+        pokemonName: commandPhase.getPokemon().name,
+      }),
+      0
+    );
+    this.setCursor(this.getCursor());
 
-        this.commandsContainer = this.scene.add.container(217, -38.7);
-        this.commandsContainer.setName("commands");
-        this.commandsContainer.setVisible(false);
-        ui.add(this.commandsContainer);
+    return true;
+  }
 
-        for (let c = 0; c < commands.length; c++) {
-            const commandText = addTextObject(
-                this.scene,
-                c % 2 === 0 ? 0 : 55.8,
-                c < 2 ? 0 : 16,
-                commands[c],
-                TextStyle.WINDOW
-            );
-            commandText.setName(commands[c]);
-            this.commandsContainer.add(commandText);
-        }
-    }
+  processInput(button: Button): boolean {
+    const ui = this.getUi();
 
-    show(args: any[]): boolean {
-        super.show(args);
+    let success = false;
 
-        this.fieldIndex = args.length ? (args[0] as integer) : 0;
+    const cursor = this.getCursor();
 
-        this.commandsContainer.setVisible(true);
-
-        let commandPhase: CommandPhase;
-        const currentPhase = this.scene.getCurrentPhase();
-        if (currentPhase instanceof CommandPhase) {
-            commandPhase = currentPhase;
-        } else {
-            commandPhase = this.scene.getStandbyPhase() as CommandPhase;
-        }
-
-        const messageHandler = this.getUi().getMessageHandler();
-        messageHandler.bg.setVisible(true);
-        messageHandler.commandWindow.setVisible(true);
-        messageHandler.movesWindowContainer.setVisible(false);
-        messageHandler.message.setWordWrapWidth(1110);
-        messageHandler.showText(
-            i18next.t("commandUiHandler:actionMessage", {
-                pokemonName: commandPhase.getPokemon().name,
-            }),
-            0
-        );
-        this.setCursor(this.getCursor());
-
-        return true;
-    }
-
-    processInput(button: Button): boolean {
-        const ui = this.getUi();
-
-        let success = false;
-
-        const cursor = this.getCursor();
-
-        if (button === Button.CANCEL || button === Button.ACTION) {
-            if (button === Button.ACTION) {
-                switch (cursor) {
-                    // Fight
-                    case 0:
-                        if (
-                            (
+    if (button === Button.CANCEL || button === Button.ACTION) {
+      if (button === Button.ACTION) {
+        switch (cursor) {
+        // Fight
+        case 0:
+          if (
+            (
                                 this.scene.getCurrentPhase() as CommandPhase
-                            ).checkFightOverride()
-                        ) {
-                            return true;
-                        }
-                        ui.setMode(
-                            Mode.FIGHT,
-                            (
+            ).checkFightOverride()
+          ) {
+            return true;
+          }
+          ui.setMode(
+            Mode.FIGHT,
+            (
                                 this.scene.getCurrentPhase() as CommandPhase
-                            ).getFieldIndex()
-                        );
-                        success = true;
-                        break;
-                    // Ball
-                    case 1:
-                        ui.setModeWithoutClear(Mode.BALL);
-                        success = true;
-                        break;
-                    // Pokemon
-                    case 2:
-                        ui.setMode(
-                            Mode.PARTY,
-                            PartyUiMode.SWITCH,
-                            (this.scene.getCurrentPhase() as CommandPhase)
-                                .getPokemon()
-                                .getFieldIndex(),
-                            null,
-                            PartyUiHandler.FilterNonFainted
-                        );
-                        success = true;
-                        break;
-                    // Run
-                    case 3:
-                        (
+            ).getFieldIndex()
+          );
+          success = true;
+          break;
+          // Ball
+        case 1:
+          ui.setModeWithoutClear(Mode.BALL);
+          success = true;
+          break;
+          // Pokemon
+        case 2:
+          ui.setMode(
+            Mode.PARTY,
+            PartyUiMode.SWITCH,
+            (this.scene.getCurrentPhase() as CommandPhase)
+              .getPokemon()
+              .getFieldIndex(),
+            null,
+            PartyUiHandler.FilterNonFainted
+          );
+          success = true;
+          break;
+          // Run
+        case 3:
+          (
                             this.scene.getCurrentPhase() as CommandPhase
-                        ).handleCommand(Command.RUN, 0);
-                        success = true;
-                        break;
-                }
-            } else {
-                (this.scene.getCurrentPhase() as CommandPhase).cancel();
-            }
-        } else {
-            switch (button) {
-                case Button.UP:
-                    if (cursor >= 2) {
-                        success = this.setCursor(cursor - 2);
-                    }
-                    break;
-                case Button.DOWN:
-                    if (cursor < 2) {
-                        success = this.setCursor(cursor + 2);
-                    }
-                    break;
-                case Button.LEFT:
-                    if (cursor % 2 === 1) {
-                        success = this.setCursor(cursor - 1);
-                    }
-                    break;
-                case Button.RIGHT:
-                    if (cursor % 2 === 0) {
-                        success = this.setCursor(cursor + 1);
-                    }
-                    break;
-            }
+          ).handleCommand(Command.RUN, 0);
+          success = true;
+          break;
         }
-
-        if (success) {
-            ui.playSelect();
+      } else {
+        (this.scene.getCurrentPhase() as CommandPhase).cancel();
+      }
+    } else {
+      switch (button) {
+      case Button.UP:
+        if (cursor >= 2) {
+          success = this.setCursor(cursor - 2);
         }
-
-        return success;
+        break;
+      case Button.DOWN:
+        if (cursor < 2) {
+          success = this.setCursor(cursor + 2);
+        }
+        break;
+      case Button.LEFT:
+        if (cursor % 2 === 1) {
+          success = this.setCursor(cursor - 1);
+        }
+        break;
+      case Button.RIGHT:
+        if (cursor % 2 === 0) {
+          success = this.setCursor(cursor + 1);
+        }
+        break;
+      }
     }
 
-    getCursor(): integer {
-        return !this.fieldIndex ? this.cursor : this.cursor2;
+    if (success) {
+      ui.playSelect();
     }
 
-    setCursor(cursor: integer): boolean {
-        const changed = this.getCursor() !== cursor;
-        if (changed) {
-            if (!this.fieldIndex) {
-                this.cursor = cursor;
-            } else {
-                this.cursor2 = cursor;
-            }
-        }
+    return success;
+  }
 
-        if (!this.cursorObj) {
-            this.cursorObj = this.scene.add.image(0, 0, "cursor");
-            this.commandsContainer.add(this.cursorObj);
-        }
+  getCursor(): integer {
+    return !this.fieldIndex ? this.cursor : this.cursor2;
+  }
 
-        this.cursorObj.setPosition(
-            -5 + (cursor % 2 === 1 ? 56 : 0),
-            8 + (cursor >= 2 ? 16 : 0)
-        );
-
-        return changed;
+  setCursor(cursor: integer): boolean {
+    const changed = this.getCursor() !== cursor;
+    if (changed) {
+      if (!this.fieldIndex) {
+        this.cursor = cursor;
+      } else {
+        this.cursor2 = cursor;
+      }
     }
 
-    clear(): void {
-        super.clear();
-        this.getUi().getMessageHandler().commandWindow.setVisible(false);
-        this.commandsContainer.setVisible(false);
-        this.getUi().getMessageHandler().clearText();
-        this.eraseCursor();
+    if (!this.cursorObj) {
+      this.cursorObj = this.scene.add.image(0, 0, "cursor");
+      this.commandsContainer.add(this.cursorObj);
     }
 
-    eraseCursor(): void {
-        if (this.cursorObj) {
-            this.cursorObj.destroy();
-        }
-        this.cursorObj = null;
+    this.cursorObj.setPosition(
+      -5 + (cursor % 2 === 1 ? 56 : 0),
+      8 + (cursor >= 2 ? 16 : 0)
+    );
+
+    return changed;
+  }
+
+  clear(): void {
+    super.clear();
+    this.getUi().getMessageHandler().commandWindow.setVisible(false);
+    this.commandsContainer.setVisible(false);
+    this.getUi().getMessageHandler().clearText();
+    this.eraseCursor();
+  }
+
+  eraseCursor(): void {
+    if (this.cursorObj) {
+      this.cursorObj.destroy();
     }
+    this.cursorObj = null;
+  }
 }
