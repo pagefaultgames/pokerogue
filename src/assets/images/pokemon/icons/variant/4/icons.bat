0b22e896abec1f28a666ede32abecaa509918f5b
Felix Staud <felix.staud@headwire.com>
2024-07-18 12:11:11 -0700

    remove (for now) obsolete assets

--- src/assets/images/pokemon/icons/variant/4/icons.bat
+++
@@ -1,1 +0,0 @@
-cmd /c TexturePacker ./  ../configuration.tps --sheet ../../../../pokemon_icons_4v.png --data ../../../../pokemon_icons_4v.json --replace .png=
--- src/assets/images/pokemon/input/sheet.bat
+++
@@ -1,3 +0,0 @@
-SET "PATH=C:/Program Files/CodeAndWeb/TexturePacker/bin;%PATH%" 
-
-forfiles /s /m *.gif /c "cmd /c mkdir  @FNAME && ffmpeg -i "@FILE" @FNAME/%%04d.png"
