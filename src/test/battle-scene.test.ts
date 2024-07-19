ea6499fa1251565454fe521f251ea93f843da9f2
Felix Staud <felix.staud@headwire.com>
2024-07-19 08:39:31 -0700

    add battle-scene.test.ts
    add test to verify that LoadingScene is being removed on `BatleScene.create()` call

---
+++ src/test/battle-scene.test.ts
@@ -0,0 +1,27 @@
+import { LoadingScene } from "#app/loading-scene.js";
+import { afterEach, beforeAll, beforeEach, describe, expect, it } from "vitest";
+import GameManager from "./utils/gameManager";
+
+describe("BattleScene", () => {
+  let phaserGame: Phaser.Game;
+  let game: GameManager;
+
+  beforeAll(() => {
+    phaserGame = new Phaser.Game({
+      type: Phaser.HEADLESS,
+    });
+  });
+
+  beforeEach(() => {
+    game = new GameManager(phaserGame);
+  });
+
+  afterEach(() => {
+    game.phaseInterceptor.restoreOg();
+  });
+
+  it("should remove LoadingScene on create", () => {
+    // `BattleScene.create()` is called during the `new GameManager()` call
+    expect(game.scene.scene.remove).toHaveBeenCalledWith(LoadingScene.KEY);
+  });
+});
