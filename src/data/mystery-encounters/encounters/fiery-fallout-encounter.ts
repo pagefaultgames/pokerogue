fc7d7092029aa465c41a5f85158bc860b0cd9cd5
ImperialSympathizer <110984302+ben-lear@users.noreply.github.com>
2024-07-19 17:24:21 -0400

    Update src/data/mystery-encounters/encounters/fiery-fallout-encounter.ts
    Co-authored-by: flx-sta <50131232+flx-sta@users.noreply.github.com>

--- src/data/mystery-encounters/encounters/fiery-fallout-encounter.ts
+++ src/data/mystery-encounters/encounters/fiery-fallout-encounter.ts
@@ -36,7 +36,7 @@ const DAMAGE_PERCENTAGE: number = 20;
 export const FieryFalloutEncounter: IMysteryEncounter =
   MysteryEncounterBuilder.withEncounterType(MysteryEncounterType.FIERY_FALLOUT)
     .withEncounterTier(MysteryEncounterTier.COMMON)
-    .withSceneWaveRangeRequirement(40, 180) // waves 10 to 180
+    .withSceneWaveRangeRequirement(40, 180)
     .withCatchAllowed(true)
     .withIntroSpriteConfigs([]) // Set in onInit()
     .withAnimations(EncounterAnim.MAGMA_BG, EncounterAnim.MAGMA_SPOUT)
