33543f7ccf7d48d3a22ae8470d46a8a2a7348d04
서명인 (Myungin, SEO) <65226760+smee6@users.noreply.github.com>
2024-07-22 00:18:35 +0900

    Add minor Korean translations to ability-trigger file
    Add minor Korean translations to ability-trigger file.
    
    1. Add Korean translation for costar ability trigger
    2. Add Korean translation for trace ability trigger

--- src/locales/ko/ability-trigger.ts
+++ src/locales/ko/ability-trigger.ts
@@ -3,11 +3,11 @@ import { SimpleTranslationEntries } from "#app/interfaces/locales";
 export const abilityTriggers: SimpleTranslationEntries = {
   "blockRecoilDamage" : "{{pokemonName}}[[는]] {{abilityName}} 때문에\n반동 데미지를 받지 않는다!",
   "badDreams": "{{pokemonName}}[[는]]\n나이트메어 때문에 시달리고 있다!",
-  "costar": "{{pokemonName}} copied {{allyName}}'s stat changes!",
+  "costar": "{{pokemonName}}[[는]] {{allyName}} 의 능력 변화를 복사했다!",
   "iceFaceAvoidedDamage": "{{pokemonName}}[[는]] {{abilityName}} 때문에\n데미지를 받지 않는다!",
   "perishBody": "{{pokemonName}}의 {{abilityName}} 때문에\n양쪽 포켓몬 모두는 3턴 후에 쓰러져 버린다!",
   "poisonHeal": "{{pokemonName}}[[는]] {{abilityName}}[[로]]인해\n조금 회복했다.",
-  "trace": "{{pokemonName}} copied {{targetName}}'s\n{{abilityName}}!",
+  "trace": "{{pokemonName}}[[는]] 상대 {{targetName}}'의 \n{{abilityName}}[[를]] 트레이스했다!",
   "windPowerCharged": "{{pokemonName}}[[는]]\n{{moveName}}에 맞아 충전되었다!",
   "quickDraw": "{{pokemonName}}[[는]]\n퀵드로에 의해 행동이 빨라졌다!",
 } as const;
