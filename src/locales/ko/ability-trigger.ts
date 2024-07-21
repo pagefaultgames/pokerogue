70dcb21e366191316832e79ec2c15a87ce39ce16
서명인 (Myungin, SEO) <65226760+smee6@users.noreply.github.com>
2024-07-22 00:32:54 +0900

    Update ability-trigger.ts

--- src/locales/ko/ability-trigger.ts
+++ src/locales/ko/ability-trigger.ts
@@ -3,7 +3,7 @@ import { SimpleTranslationEntries } from "#app/interfaces/locales";
 export const abilityTriggers: SimpleTranslationEntries = {
   "blockRecoilDamage" : "{{pokemonName}}[[는]] {{abilityName}} 때문에\n반동 데미지를 받지 않는다!",
   "badDreams": "{{pokemonName}}[[는]]\n나이트메어 때문에 시달리고 있다!",
-  "costar": "{{pokemonName}}[[는]] {{allyName}} 의 능력 변화를 복사했다!",
+  "costar": "{{pokemonName}}[[는]] {{allyName}}의 능력 변화를 복사했다!",
   "iceFaceAvoidedDamage": "{{pokemonName}}[[는]] {{abilityName}} 때문에\n데미지를 받지 않는다!",
   "perishBody": "{{pokemonName}}의 {{abilityName}} 때문에\n양쪽 포켓몬 모두는 3턴 후에 쓰러져 버린다!",
   "poisonHeal": "{{pokemonName}}[[는]] {{abilityName}}[[로]]인해\n조금 회복했다.",
