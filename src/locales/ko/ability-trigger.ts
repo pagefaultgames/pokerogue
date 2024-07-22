9b3e3d7b59d9886b27d5a662ee6275cc319f990b
서명인 (Myungin, SEO) <65226760+smee6@users.noreply.github.com>
2024-07-22 14:01:41 +0900

    modify trace trigger description (refered from official translation)
    modify trace trigger description (refered from official translation)

--- src/locales/ko/ability-trigger.ts
+++ src/locales/ko/ability-trigger.ts
@@ -7,7 +7,7 @@ export const abilityTriggers: SimpleTranslationEntries = {
   "iceFaceAvoidedDamage": "{{pokemonName}}[[는]] {{abilityName}} 때문에\n데미지를 받지 않는다!",
   "perishBody": "{{pokemonName}}의 {{abilityName}} 때문에\n양쪽 포켓몬 모두는 3턴 후에 쓰러져 버린다!",
   "poisonHeal": "{{pokemonName}}[[는]] {{abilityName}}[[로]]인해\n조금 회복했다.",
-  "trace": "{{pokemonName}}[[는]] {{targetName}}의\n{{abilityName}}[[를]] 복사했다!",
+  "trace": "{{pokemonName}}[[는]] 상대 {{targetName}}'의 \n{{abilityName}}[[를]] 트레이스했다!",
   "windPowerCharged": "{{pokemonName}}[[는]]\n{{moveName}}에 맞아 충전되었다!",
   "quickDraw": "{{pokemonName}}[[는]]\n퀵드로에 의해 행동이 빨라졌다!",
   "blockItemTheft": "{{pokemonNameWithAffix}}의 {{abilityName}}에 의해\n도구를 빼앗기지 않는다!",
