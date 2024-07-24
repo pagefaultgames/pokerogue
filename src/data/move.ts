79e2b1215c7896b6627bcebeb3f82f0b966e3e77
Leo Kim <47556641+KimJeongSun@users.noreply.github.com>
2024-07-24 23:58:26 +0900

    [Move] Water Shuriken guarantees 3 hits with battle bond (#2687)

--- src/data/move.ts
+++ src/data/move.ts
@@ -1719,6 +1719,7 @@ export class MultiHitAttr extends MoveAttr {
     } else {
       const hitType = new Utils.IntegerHolder(this.multiHitType);
       applyMoveAttrs(ChangeMultiHitTypeAttr, user, target, move, hitType);
+      this.multiHitType = hitType.value;
       hitTimes = this.getHitCount(user, target);
     }
 
