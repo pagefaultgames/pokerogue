45d9c65a3efc62404c42bd7dac04de8445117810
Mumble <kimjoanne@protonmail.com>
2024-07-24 15:37:20 -0700

    Bug Fix (#3129)
    Co-authored-by: Frutescens <info@laptop>

--- src/phases.ts
+++ src/phases.ts
@@ -4917,7 +4917,6 @@ export class AttemptCapturePhase extends PokemonPhase {
                   }
                 },
                 onComplete: () => {
-                  this.scene.gameData.setPokemonCaught(pokemon);
                   this.catch();
                 }
               });
@@ -5014,7 +5013,7 @@ export class AttemptCapturePhase extends PokemonPhase {
           }
         });
       };
-      Promise.all([pokemon.hideInfo()]).then(() => {
+      Promise.all([pokemon.hideInfo(), this.scene.gameData.setPokemonCaught(pokemon)]).then(() => {
         if (this.scene.getParty().length === 6) {
           const promptRelease = () => {
             this.scene.ui.showText(i18next.t("battle:partyFull", { pokemonName: getPokemonNameWithAffix(pokemon) }), null, () => {
