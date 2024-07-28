2acea787b25cbda97432e0615ce88643feb89eee
returntoice <dieandbecome@gmail.com>
2024-07-29 06:50:06 +0900

    trans once more

--- src/locales/ko/battle-info.ts
+++ src/locales/ko/battle-info.ts
@@ -1,5 +1,9 @@
 import { SimpleTranslationEntries } from "#app/interfaces/locales";
 
 export const battleInfo: SimpleTranslationEntries = {
+<<<<<<< HEAD
+  "generation": "{{generation}}",
+=======
   "generation": "{{generation}}세대",
+>>>>>>> 118db8c7d0a89a6110e280b82be0b4ccab2ea312
 } as const;
