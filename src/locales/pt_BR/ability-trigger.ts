567516dfadd8a2bac6a83792894296edcf262b18
Enoch <enoch.jwsong@gmail.com>
2024-07-31 11:44:54 +0900

    Update src/locales/pt_BR/ability-trigger.ts
    Co-authored-by: José Ricardo Fleury Oliveira <josefleury@discente.ufg.br>

--- src/locales/pt_BR/ability-trigger.ts
+++ src/locales/pt_BR/ability-trigger.ts
@@ -59,5 +59,5 @@ export const abilityTriggers: SimpleTranslationEntries = {
   "postSummonSwordOfRuin": "Sword of Ruin de {{pokemonNameWithAffix}} reduziu a {{statName}}\nde todos os Pokémon em volta!",
   "postSummonTabletsOfRuin": "Tablets of Ruin de {{pokemonNameWithAffix}} reduziu o {{statName}}\nde todos os Pokémon em volta!",
   "postSummonBeadsOfRuin": "Beads of Ruin de {{pokemonNameWithAffix}} reduziu a {{statName}}\nde todos os Pokémon em volta!",
-  "preventBerryUse": "{{pokemonNameWithAffix}} is too\nnervous to eat berries!",
+  "preventBerryUse": "{{pokemonNameWithAffix}} está nervoso\ndemais para comer frutas!",
 } as const;
