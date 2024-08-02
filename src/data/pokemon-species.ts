aeafe0fddd85fa46c1d7df256dda9276a18c409d
Amani H. <109637146+xsn34kzx@users.noreply.github.com>
2024-08-01 22:20:27 -0400

    [Refactor/Bug] Use Bit Shifting & Masking (#3278)

--- src/data/pokemon-species.ts
+++ src/data/pokemon-species.ts
@@ -435,7 +435,7 @@ export abstract class PokemonSpeciesForm {
     for (const moveId of moveset) {
       if (speciesEggMoves.hasOwnProperty(rootSpeciesId)) {
         const eggMoveIndex = speciesEggMoves[rootSpeciesId].findIndex(m => m === moveId);
-        if (eggMoveIndex > -1 && eggMoves & Math.pow(2, eggMoveIndex)) {
+        if (eggMoveIndex > -1 && (eggMoves & (1 << eggMoveIndex))) {
           continue;
         }
       }
