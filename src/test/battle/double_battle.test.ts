5dc6bf1785dd1f94ef2b4059e12783a5be4eabb3
allen <allen.lo@mail.utoronto.ca>
2024-08-02 19:19:20 -0700

    polish double battle test a bit

--- src/test/battle/double_battle.test.ts
+++ src/test/battle/double_battle.test.ts
@@ -32,9 +32,7 @@ describe("Test Battle Phase", () => {
   // double-battle player's pokemon both fainted in same round, then revive one, and next double battle summons two player's pokemon successfully.
   // (There were bugs that either only summon one when can summon two, player stuck in switchPhase etc)
   it("3v2 edge case: player summons 2 pokemon on the next battle after being fainted and revived", async() => {
-    game.override.battleType("double");
-    game.override.enemyMoveset(SPLASH_ONLY);
-    game.override.moveset(SPLASH_ONLY);
+    game.override.battleType("double").enemyMoveset(SPLASH_ONLY).moveset(SPLASH_ONLY);
     await game.startBattle([
       Species.BULBASAUR,
       Species.CHARIZARD,
@@ -44,26 +42,23 @@ describe("Test Battle Phase", () => {
     game.doAttack(getMovePosition(game.scene, 0, Moves.SPLASH));
     game.doAttack(getMovePosition(game.scene, 1, Moves.SPLASH));
 
-    const pokemon1 = game.scene.getParty()[0];
-    expect(pokemon1).not.toBe(undefined);
+    for (const pokemon of game.scene.getPlayerField()) {
+      expect(pokemon).toBeDefined();
 
-    pokemon1.hp = 0;
-    pokemon1.status = new Status(StatusEffect.FAINT);
-    expect(pokemon1.isFainted()).toBe(true);
-
-    const pokemon2 = game.scene.getParty()[1];
-    expect(pokemon2).not.toBe(undefined);
-
-    pokemon2.hp = 0;
-    pokemon2.status = new Status(StatusEffect.FAINT);
-    expect(pokemon2.isFainted()).toBe(true);
+      pokemon.hp = 0;
+      pokemon.status = new Status(StatusEffect.FAINT);
+      expect(pokemon.isFainted()).toBe(true);
+    }
 
     await game.doKillOpponents();
 
     await game.phaseInterceptor.to(BattleEndPhase);
     game.doSelectModifier();
-    game.doRevivePokemon(1);
+
+    const charizard = game.scene.getParty().findIndex(p => p.species.speciesId === Species.CHARIZARD);
+    game.doRevivePokemon(charizard);
+
     await game.phaseInterceptor.to(TurnInitPhase);
-    expect(game.scene.getPlayerField().filter(p => !p.isFainted()).length).toBe(2);
+    expect(game.scene.getPlayerField().filter(p => !p.isFainted())).toHaveLength(2);
   }, 20000);
 });
