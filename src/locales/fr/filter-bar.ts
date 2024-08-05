51bdce224a67d8c2b88994f647263ccb27d7051d
Leo Kim <47556641+KimJeongSun@users.noreply.github.com>
2024-08-06 02:32:40 +0900

    Update src/locales/fr/filter-bar.ts
    Co-authored-by: Lugiad' <adrien.grivel@hotmail.fr>

--- src/locales/fr/filter-bar.ts
+++ src/locales/fr/filter-bar.ts
@@ -3,7 +3,7 @@ import { SimpleTranslationEntries } from "#app/interfaces/locales";
 export const filterBar: SimpleTranslationEntries = {
   "genFilter": "Gen",
   "typeFilter": "Type",
-  "caughtFilter": "Caught",
+  "caughtFilter": "Capturés",
   "unlocksFilter": "Débloq.",
   "miscFilter": "Divers",
   "sortFilter": "Tri",
