6c516f32a0e0803b0b525c2e8ca2c2677a30572d
Frutescens <info@laptop>
2024-08-07 14:31:26 -0700

    New Localizations

---
+++ src/locales/ja/run-history-ui-handler.ts
@@ -0,0 +1,36 @@
+import { SimpleTranslationEntries } from "#app/interfaces/locales";
+
+export const runHistory: SimpleTranslationEntries = {
+  "victory": "Victory!",
+  "defeatedWild": "Defeated by ",
+  "defeatedTrainer": "Defeated by ",
+  "defeatedTrainerDouble": "Defeated by Duo",
+  "defeatedRival": "Defeated by Rival",
+  "defeated":"Defeated",
+  "luck":"Luck",
+  "score":"Score",
+  "mode":"Mode",
+  "challengeRules":"Rule(s)",
+  "challengeMonoGen1":"Gen I",
+  "challengeMonoGen2":"Gen II",
+  "challengeMonoGen3":"Gen III",
+  "challengeMonoGen4":"Gen IV",
+  "challengeMonoGen5":"Gen V",
+  "challengeMonoGen6":"Gen VI",
+  "challengeMonoGen7":"Gen VII",
+  "challengeMonoGen8":"Gen VIII",
+  "challengeMonoGen9":"Gen IX",
+  "playerItems":"Player Items",
+  "personalBest":"Personal Best!",
+  "SPDshortened":"Vel.",
+  "runInfo":"Run Info",
+  "money":"Money",
+  "runLength":"Run Length",
+  "viewHeldItems":"Held Items",
+  "hallofFameText":"Welcome to the Hall of Fame!",
+  "viewHallOfFame":"View Hall of Fame!",
+  "viewEndingSplash":"View ending art!"
+} as const;
+
+//Mode Information found in game-mode.ts
+//Wave / Lv found in save-slot-select-ui-handler.ts
