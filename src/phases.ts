9eb57c0c661cde84064ae5a5319bd27e6d01be5f
Opaque02 <66582645+Opaque02@users.noreply.github.com>
2024-08-08 15:04:12 +1000

    [QoL] IV scanner update to hide the prompt and let you see the stats mid battle on the stats screen (#3285)
    * IV scanner PR fix
    
    Updated locales to have updated keys for IV scanner in menu
    
    Updated legacy UI to work with IV scanner changes
    
    Updated code to have player and enemy battle stats for ease of use
    
    Updated logic to make the player side work exactly the same as previously since there should be no HP stat shown for players. Also updated the colours so there's no more grey option for unknown stats
    
    Added HP, updated logic and colours. Need to undo changes to player pokemon to not be as squished because of no HP, and need to see what happens for trainers
    
    Fixing up some git errors
    
    Fixed a bug with double battles not updating the pokemon properly
    
    Updated settings to allow for the ability to skip the IV scanning prompts
    
    Adding functionality to IV scanner to skip prompt and see IVs mid battle
    
    * Merged with latest + fixed bug with IV scanner vs pokemon info container getRootSpeciesId being different
    
    * Updated as per merge comment
    
    * Updating to fix comments
    
    * Updated comments
    
    * Update src/locales/fr/settings.ts
    
    Co-authored-by: Lugiad' <adrien.grivel@hotmail.fr>
    
    * Update src/locales/de/settings.ts
    
    Co-authored-by: Jannik Tappert <38758606+CodeTappert@users.noreply.github.com>
    
    * Update src/locales/pt_BR/settings.ts
    
    Co-authored-by: José Ricardo Fleury Oliveira <josefleury@discente.ufg.br>
    
    * Update src/locales/ko/settings.ts
    
    Co-authored-by: Enoch <enoch.jwsong@gmail.com>
    
    * Update src/locales/zh_CN/settings.ts
    
    Co-authored-by: mercurius-00 <80205689+mercurius-00@users.noreply.github.com>
    
    * Update src/locales/zh_TW/settings.ts
    
    Co-authored-by: mercurius-00 <80205689+mercurius-00@users.noreply.github.com>
    
    * Updating text of IV scanner to be gold
    
    * Updated text colour to use one of the existing golds instead of a custom gold
    
    * Japanese locale
    
    * Updating docs
    
    ---------
    
    Co-authored-by: Lugiad' <adrien.grivel@hotmail.fr>
    Co-authored-by: Jannik Tappert <38758606+CodeTappert@users.noreply.github.com>
    Co-authored-by: José Ricardo Fleury Oliveira <josefleury@discente.ufg.br>
    Co-authored-by: Enoch <enoch.jwsong@gmail.com>
    Co-authored-by: mercurius-00 <80205689+mercurius-00@users.noreply.github.com>

--- src/phases.ts
+++ src/phases.ts
@@ -50,7 +50,7 @@ import { GameMode, GameModes, getGameMode } from "./game-mode";
 import PokemonSpecies, { getPokemonSpecies, speciesStarters } from "./data/pokemon-species";
 import i18next from "./plugins/i18n";
 import Overrides from "#app/overrides";
-import { TextStyle, addTextObject } from "./ui/text";
+import { TextStyle, addTextObject, getTextColor } from "./ui/text";
 import { Type } from "./data/type";
 import { BerryUsedEvent, EncounterPhaseEvent, MoveUsedEvent, TurnEndEvent, TurnInitEvent } from "./events/battle-scene";
 import { Abilities } from "#enums/abilities";
@@ -5599,19 +5599,45 @@ export class ScanIvsPhase extends PokemonPhase {
 
     const pokemon = this.getPokemon();
 
-    this.scene.ui.showText(i18next.t("battle:ivScannerUseQuestion", { pokemonName: getPokemonNameWithAffix(pokemon) }), null, () => {
-      this.scene.ui.setMode(Mode.CONFIRM, () => {
-        this.scene.ui.setMode(Mode.MESSAGE);
-        this.scene.ui.clearText();
-        new CommonBattleAnim(CommonAnim.LOCK_ON, pokemon, pokemon).play(this.scene, () => {
-          this.scene.ui.getMessageHandler().promptIvs(pokemon.id, pokemon.ivs, this.shownIvs).then(() => this.end());
+    let enemyIvs: number[] = [];
+    let statsContainer: Phaser.GameObjects.Sprite[] = [];
+    let statsContainerLabels: Phaser.GameObjects.Sprite[] = [];
+    const enemyField = this.scene.getEnemyField();
+    const uiTheme = (this.scene as BattleScene).uiTheme; // Assuming uiTheme is accessible
+    for (let e = 0; e < enemyField.length; e++) {
+      enemyIvs = enemyField[e].ivs;
+      const currentIvs = this.scene.gameData.dexData[enemyField[e].species.getRootSpeciesId()].ivs;  // we are using getRootSpeciesId() here because we want to check against the baby form, not the mid form if it exists
+      const ivsToShow = this.scene.ui.getMessageHandler().getTopIvs(enemyIvs, this.shownIvs);
+      statsContainer = enemyField[e].getBattleInfo().getStatsValueContainer().list as Phaser.GameObjects.Sprite[];
+      statsContainerLabels = statsContainer.filter(m => m.name.indexOf("icon_stat_label") >= 0);
+      for (let s = 0; s < statsContainerLabels.length; s++) {
+        const ivStat = Stat[statsContainerLabels[s].frame.name];
+        if (enemyIvs[ivStat] > currentIvs[ivStat] && ivsToShow.indexOf(Number(ivStat)) >= 0) {
+          const hexColour = enemyIvs[ivStat] === 31 ? getTextColor(TextStyle.PERFECT_IV, false, uiTheme) : getTextColor(TextStyle.SUMMARY_GREEN, false, uiTheme);
+          const hexTextColour = Phaser.Display.Color.HexStringToColor(hexColour).color;
+          statsContainerLabels[s].setTint(hexTextColour);
+        }
+        statsContainerLabels[s].setVisible(true);
+      }
+    }
+
+    if (!this.scene.hideIvs) {
+      this.scene.ui.showText(i18next.t("battle:ivScannerUseQuestion", { pokemonName: getPokemonNameWithAffix(pokemon) }), null, () => {
+        this.scene.ui.setMode(Mode.CONFIRM, () => {
+          this.scene.ui.setMode(Mode.MESSAGE);
+          this.scene.ui.clearText();
+          new CommonBattleAnim(CommonAnim.LOCK_ON, pokemon, pokemon).play(this.scene, () => {
+            this.scene.ui.getMessageHandler().promptIvs(pokemon.id, pokemon.ivs, this.shownIvs).then(() => this.end());
+          });
+        }, () => {
+          this.scene.ui.setMode(Mode.MESSAGE);
+          this.scene.ui.clearText();
+          this.end();
         });
-      }, () => {
-        this.scene.ui.setMode(Mode.MESSAGE);
-        this.scene.ui.clearText();
-        this.end();
       });
-    });
+    } else {
+      this.end();
+    }
   }
 }
 
