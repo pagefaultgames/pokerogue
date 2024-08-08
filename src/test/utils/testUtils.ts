c46357dc1e74e16cd2e1c365a99d1c8eea331a22
torranx <torranicles@gmail.com>
2024-08-08 08:24:48 +0800

    add mockHitCheck helper

--- src/test/utils/testUtils.ts
+++ src/test/utils/testUtils.ts
@@ -3,7 +3,7 @@ import i18next, { type ParseKeys } from "i18next";
 import { vi } from "vitest";
 import GameManager from "./gameManager";
 import { BattlerIndex } from "#app/battle.js";
-import { TurnStartPhase } from "#app/phases.js";
+import { MoveEffectPhase, TurnStartPhase } from "#app/phases.js";
 
 /** Ready to use array of Moves.SPLASH x4 */
 export const SPLASH_ONLY = [Moves.SPLASH, Moves.SPLASH, Moves.SPLASH, Moves.SPLASH];
@@ -52,4 +52,20 @@ export async function changeTurnOrder(game: GameManager, order: BattlerIndex[]):
   await game.phaseInterceptor.to(TurnStartPhase, false);
 
   vi.spyOn(game.scene.getCurrentPhase() as TurnStartPhase, "getOrder").mockReturnValue(order);
+}
+
+/**
+ * Mocks the hitCheck's return value {@linkcode MoveEffectPhase.hitCheck}.
+ * Used to force a move to either hit or miss.
+ * Note that this uses `mockReturnValue()`, meaning it will also apply to a
+ * succeeding `MoveEffectPhase` immediately following the first one
+ * (in the case of a multi-target move)
+ *
+ * @param {GameManager} game The GameManager instance
+ * @param shouldHit Whether the move should hit
+ */
+export async function mockHitCheck(game: GameManager, shouldHit: boolean): Promise<void> {
+  await game.phaseInterceptor.to(MoveEffectPhase, false);
+
+  vi.spyOn(game.scene.getCurrentPhase() as MoveEffectPhase, "hitCheck").mockReturnValue(shouldHit);
 }
