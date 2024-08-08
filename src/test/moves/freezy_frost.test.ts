8a6b2634bebd85dd49afff69a0452bb0ac80a54c
Christopher Schmidt <schmidtc086@outlook.com>
2024-08-08 19:28:55 -0400

    Updates title of haze tests, creates freezy frost tests

--- src/test/moves/freezy_frost.test.ts
+++ src/test/moves/freezy_frost.test.ts
@@ -6,8 +6,9 @@ import { Abilities } from "#enums/abilities";
 import { Moves } from "#enums/moves";
 import { Species } from "#enums/species";
 import Phaser from "phaser";
-import { afterEach, beforeAll, beforeEach, describe, expect, it } from "vitest";
+import { afterEach, beforeAll, beforeEach, describe, expect, it, vi } from "vitest";
 import { SPLASH_ONLY } from "#test/utils/testUtils";
+import { allMoves } from "#app/data/move.js";
 
 describe("Moves - Haze", () => {
   describe("integration tests", () => {
@@ -33,11 +34,12 @@ describe("Moves - Haze", () => {
       game.override.enemyAbility(Abilities.NONE);
 
       game.override.startingLevel(100);
-      game.override.moveset([Moves.HAZE, Moves.SWORDS_DANCE, Moves.CHARM, Moves.SPLASH]);
+      game.override.moveset([Moves.FREEZY_FROST, Moves.SWORDS_DANCE, Moves.CHARM, Moves.SPLASH]);
+      vi.spyOn(allMoves[Moves.FREEZY_FROST], "accuracy", "get").mockReturnValue(100);
       game.override.ability(Abilities.NONE);
     });
 
-    it("Uses Swords Dance to raise own ATK by 2, Charm to lower enemy ATK by 2, player uses Haze to clear all stat changes", { timeout: 10000 }, async () => {
+    it("Uses Swords Dance to raise own ATK by 2, Charm to lower enemy ATK by 2, player uses Freezy Frost to clear all stat changes", { timeout: 10000 }, async () => {
       await game.startBattle([Species.RATTATA]);
       const user = game.scene.getPlayerPokemon()!;
       const enemy = game.scene.getEnemyPokemon()!;
@@ -54,14 +56,14 @@ describe("Moves - Haze", () => {
       expect(userAtkBefore).toBe(2);
       expect(enemyAtkBefore).toBe(-2);
 
-      game.doAttack(getMovePosition(game.scene, 0, Moves.HAZE));
+      game.doAttack(getMovePosition(game.scene, 0, Moves.FREEZY_FROST));
       await game.phaseInterceptor.to(TurnInitPhase);
       expect(user.summonData.battleStats[BattleStat.ATK]).toBe(0);
       expect(enemy.summonData.battleStats[BattleStat.ATK]).toBe(0);
     });
 
-    it("Uses Swords Dance to raise own ATK by 2, Charm to lower enemy ATK by 2, enemy uses Haze to clear all stat changes", { timeout: 10000 }, async () => {
-      game.override.enemyMoveset([Moves.HAZE, Moves.HAZE, Moves.HAZE, Moves.HAZE]);
+    it("Uses Swords Dance to raise own ATK by 2, Charm to lower enemy ATK by 2, enemy uses Freezy Frost to clear all stat changes", { timeout: 10000 }, async () => {
+      game.override.enemyMoveset([Moves.FREEZY_FROST, Moves.FREEZY_FROST, Moves.FREEZY_FROST, Moves.FREEZY_FROST]);
       await game.startBattle([Species.SHUCKLE]); // Shuckle for slower Swords Dance on first turn so Haze doesn't affect it.
       const user = game.scene.getPlayerPokemon()!;
       expect(user.summonData.battleStats[BattleStat.ATK]).toBe(0);
