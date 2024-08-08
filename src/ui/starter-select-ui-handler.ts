4a429f13627f39045d4e33fdf3bf5ac92e0ae0ad
damocleas <damocleas25@gmail.com>
2024-08-08 01:20:28 -0600

    Candy per Species-Egg Adjusted

--- src/ui/starter-select-ui-handler.ts
+++ src/ui/starter-select-ui-handler.ts
@@ -119,12 +119,12 @@ const starterCandyCosts: { passive: integer, costReduction: [integer, integer], 
   { passive: 45, costReduction: [25, 60], egg: 30 }, // 2 Cost
   { passive: 40, costReduction: [20, 50], egg: 25 }, // 3 Cost
   { passive: 30, costReduction: [15, 40], egg: 20 }, // 4 Cost
-  { passive: 25, costReduction: [12, 35], egg: 15 }, // 5 Cost
-  { passive: 20, costReduction: [10, 30], egg: 12 }, // 6 Cost
-  { passive: 15, costReduction: [8, 20], egg: 10 },  // 7 Cost
-  { passive: 10, costReduction: [5, 15], egg: 8 },  // 8 Cost
-  { passive: 10, costReduction: [3, 10], egg: 5 },  // 9 Cost
-  { passive: 10, costReduction: [3, 10], egg: 5 },  // 10 Cost
+  { passive: 25, costReduction: [12, 35], egg: 18 }, // 5 Cost
+  { passive: 20, costReduction: [10, 30], egg: 15 }, // 6 Cost
+  { passive: 15, costReduction: [8, 20], egg: 12 },  // 7 Cost
+  { passive: 10, costReduction: [5, 15], egg: 10 },  // 8 Cost
+  { passive: 10, costReduction: [3, 10], egg: 8 },  // 9 Cost
+  { passive: 10, costReduction: [3, 10], egg: 8 },  // 10 Cost
 ];
 
 // Position of UI elements
