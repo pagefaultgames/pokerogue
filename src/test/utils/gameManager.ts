bfdcd4fc1eb062be3563545ae24b23d7849957de
Adrian T. <68144167+torranx@users.noreply.github.com>
2024-08-09 23:07:55 +0800

    [Dev] Move function from testUtils.ts to gameManager.ts (2/3)  (#3432)
    * move mockTurnOrder to gameManager
    
    * change name
    
    * fix test failing
    
    * fix purify test
    
    * fix typo

--- src/test/utils/gameManager.ts
+++ src/test/utils/gameManager.ts
@@ -29,6 +29,7 @@ import overrides from "#app/overrides.js";
 import { removeEnemyHeldItems } from "./testUtils";
 import ModifierSelectUiHandler from "#app/ui/modifier-select-ui-handler.js";
 import { MoveHelper } from "./moveHelper";
+import { vi } from "vitest";
 
 /**
  * Class to manage the game state and transitions between phases.
@@ -356,5 +357,20 @@ export default class GameManager {
       partyHandler.processInput(Button.ACTION); // select party slot
       partyHandler.processInput(Button.ACTION); // send out (or whatever option is at the top)
     });
+  }
+
+  /**
+   * Intercepts `TurnStartPhase` and mocks the getOrder's return value {@linkcode TurnStartPhase.getOrder}
+   * Used to modify the turn order.
+   * @param {BattlerIndex[]} order The turn order to set
+   * @example
+   * ```ts
+   * await game.setTurnOrder([BattlerIndex.PLAYER, BattlerIndex.ENEMY, BattlerIndex.ENEMY_2, BattlerIndex.PLAYER_2]);
+   * ```
+   */
+  async setTurnOrder(order: BattlerIndex[]): Promise<void> {
+    await this.phaseInterceptor.to(TurnStartPhase, false);
+
+    vi.spyOn(this.scene.getCurrentPhase() as TurnStartPhase, "getOrder").mockReturnValue(order);
   }
 }
