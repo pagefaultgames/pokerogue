a143612d3b1186f40c64a9fc5610a3ff30cdbf11
Aleco <113533233+VentiPlenti@users.noreply.github.com>
2024-08-10 12:02:48 +0100

    Complete common.ts

--- src/locales/ca_ES/common.ts
+++ src/locales/ca_ES/common.ts
@@ -1,10 +1,10 @@
 import { SimpleTranslationEntries } from "#app/interfaces/locales";
 
 export const common: SimpleTranslationEntries = {
-  "start": "Start",
-  "luckIndicator": "Luck:",
-  "shinyOnHover": "Shiny",
-  "commonShiny": "Common",
-  "rareShiny": "Rare",
-  "epicShiny": "Epic",
+  "start": "Començar",
+  "luckIndicator": "Sort:",
+  "shinyOnHover": "Variocolor",
+  "commonShiny": "Comú",
+  "rareShiny": "Rar",
+  "epicShiny": "Èpica",
 } as const;
