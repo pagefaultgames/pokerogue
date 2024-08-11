15bc97e25d179c31bd4913221b923c8b42fa88a7
José Ricardo Fleury Oliveira <josericardo.fleury05@gmail.com>
2024-08-11 15:58:13 -0300

    Update src/locales/fr/battle.ts
    Co-authored-by: Lugiad' <adrien.grivel@hotmail.fr>

--- src/locales/fr/battle.ts
+++ src/locales/fr/battle.ts
@@ -60,7 +60,7 @@ export const battle: SimpleTranslationEntries = {
   "turnEndHpRestore": "{{pokemonName}} récupère des PV !",
   "hpIsFull": "Les PV de {{pokemonName}}\nsont au maximum !",
   "skipItemQuestion": "Êtes-vous sûr·e de ne pas vouloir prendre d’objet ?",
-  "itemStackFull": "The stack for this item is full.\nYou will receive {{itemName}} instead.",
+  "itemStackFull": "Vous avez atteint la quantité maximale pour cet objet.\nVous recevez {{itemName}} à la place.",
   "eggHatching": "Hein ?",
   "ivScannerUseQuestion": "Utiliser le Scanner d’IV\nsur {{pokemonName}} ?",
   "wildPokemonWithAffix": "{{pokemonName}} sauvage",
