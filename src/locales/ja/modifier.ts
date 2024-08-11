e761adb79f24a5f3f5fc0f7cbf3318192623d624
protimita <protimitajp@gmail.com>
2024-08-11 19:10:07 +0900

    Update src/locales/ja/modifier.ts
    Co-authored-by: Chapybara-jp <charlie.beer@hotmail.com>

--- src/locales/ja/modifier.ts
+++ src/locales/ja/modifier.ts
@@ -7,7 +7,7 @@ export const modifier: SimpleTranslationEntries = {
   "pokemonInstantReviveApply": "{{pokemonNameWithAffix}}は\n{{typeName}}で　復活した！",
   "pokemonResetNegativeStatStageApply": "{{pokemonNameWithAffix}}は　{{typeName}}で\n下がった能力が　元に戻った！",
   "moneyInterestApply": "{{typeName}}から　{{moneyAmount}}円　取得した！",
-  "turnHeldItemTransferApply": "{{pokemonName}}の {{typeName}}が\n{{pokemonNameWithAffix}}の {{itemName}}を すいとった！",
+  "turnHeldItemTransferApply": "{{pokemonName}}の {{typeName}}が\n{{pokemonNameWithAffix}}の {{itemName}}を 吸い取った！",
   "contactHeldItemTransferApply": "{{pokemonName}}の {{typeName}}が\n{{pokemonNameWithAffix}}の {{itemName}}を うばいとった！",
   "enemyTurnHealApply": "{{pokemonNameWithAffix}}は\n体力を　回復！",
   "bypassSpeedChanceApply": "{{pokemonName}}は　{{itemName}}で\n行動が　はやくなった！",
