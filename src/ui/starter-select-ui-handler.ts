6d515d58b552a76acd5dec03c12886a540b4e2a5
damocleas <damocleas25@gmail.com>
2024-08-12 16:27:33 -0400

    [Balance] Egg and Starter Adjustments (increase HA chance, decrease candy costs) (#3419)
    * [Balance] Egg Adjustments
    
    * Reduced Cost of Same-Species Eggs
    
    * Update egg.ts
    
    * check errors
    
    * Candy per Species-Egg Adjusted
    
    * Set egg cost back to 10 for 9/10 cost mons
    
    * 8-10 Legendaries made to 8 cost Eggs
    
    * Updated some Passive Costs and Cost Reductions
    
    * Change HA rate of eggs to 192
    
    Also clean up the if to make it more readable
    
    * Changed HA Rate from Same-Species Eggs up 1/4 -> 1/8
    
    * Rename "voucher*" variables to "gacha*"
    
    ---------
    
    Co-authored-by: NightKev <34855794+DayKev@users.noreply.github.com>

--- src/ui/starter-select-ui-handler.ts
+++ src/ui/starter-select-ui-handler.ts
@@ -118,16 +118,16 @@ const languageSettings: { [key: string]: LanguageSetting } = {
 };
 
 const starterCandyCosts: { passive: integer, costReduction: [integer, integer], egg: integer }[] = [
-  { passive: 50, costReduction: [30, 75], egg: 35 }, // 1
-  { passive: 45, costReduction: [25, 60], egg: 35 }, // 2
-  { passive: 40, costReduction: [20, 50], egg: 35 }, // 3
-  { passive: 30, costReduction: [15, 40], egg: 30 }, // 4
-  { passive: 25, costReduction: [12, 35], egg: 25 }, // 5
-  { passive: 20, costReduction: [10, 30], egg: 20 }, // 6
-  { passive: 15, costReduction: [8, 20], egg: 15 },  // 7
-  { passive: 10, costReduction: [5, 15], egg: 10 },  // 8
-  { passive: 10, costReduction: [3, 10], egg: 10 },  // 9
-  { passive: 10, costReduction: [3, 10], egg: 10 },  // 10
+  { passive: 40, costReduction: [25, 60], egg: 30 }, // 1 Cost
+  { passive: 40, costReduction: [25, 60], egg: 30 }, // 2 Cost
+  { passive: 35, costReduction: [20, 50], egg: 25 }, // 3 Cost
+  { passive: 30, costReduction: [15, 40], egg: 20 }, // 4 Cost
+  { passive: 25, costReduction: [12, 35], egg: 18 }, // 5 Cost
+  { passive: 20, costReduction: [10, 30], egg: 15 }, // 6 Cost
+  { passive: 15, costReduction: [8, 20], egg: 12 },  // 7 Cost
+  { passive: 10, costReduction: [5, 15], egg: 8 },  // 8 Cost
+  { passive: 10, costReduction: [5, 15], egg: 8 },  // 9 Cost
+  { passive: 10, costReduction: [5, 15], egg: 8 },  // 10 Cost
 ];
 
 // Position of UI elements
