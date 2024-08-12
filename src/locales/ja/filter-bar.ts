00d03594679f5804579edbb7a21d47d91f502651
protimita <protimitajp@gmail.com>
2024-08-12 20:23:13 +0900

    [Localization][JA]Update filter-bar.ts
    Added hyphens for constancy.
    Added 4 new translations.

--- src/locales/ja/filter-bar.ts
+++ src/locales/ja/filter-bar.ts
@@ -11,13 +11,13 @@ export const filterBar: SimpleTranslationEntries = {
   "normal": "非色違い",
   "uncaught": "未捕獲",
   "passive": "パッシブ",
-  "passiveUnlocked": "パッシブ　解放済み",
-  "passiveLocked": "パッシブ　未解放",
-  "passiveUnlockable": "Passive - Can Unlock",//Add translation here
+  "passiveUnlocked": "パッシブ - 解放済み",
+  "passiveLocked": "パッシブ - 未解放",
+  "passiveUnlockable": "パッシブ - 解放可能",//Add translation here
   "costReduction": "ポイント削減",
-  "costReductionUnlocked": "ポイント削減　解放済み",
-  "costReductionLocked": "ポイント削減　未解放",
-  "costReductionUnlockable": "Cost Reduction - Can Unlock",//Add translation here
+  "costReductionUnlocked": "ポイント削減 - 解放済み",
+  "costReductionLocked": "ポイント削減 - 未解放",
+  "costReductionUnlockable": "ポイント削減 - 解放可能",//Add translation here
   "favorite": "お気に入り",
   "isFavorite": "お気に入り - あり",
   "notFavorite": "お気に入り - なし",
@@ -27,8 +27,8 @@ export const filterBar: SimpleTranslationEntries = {
   "hiddenAbility": "隠れ特性",
   "hasHiddenAbility": "隠れ特性 - あり",
   "noHiddenAbility": "隠れ特性 - なし",
-  "egg": "Egg",//Add translation here
-  "eggPurchasable": "Purchasable Egg",//Add translation here
+  "egg": "タマゴ",//Add translation here
+  "eggPurchasable": "タマゴ - 購入可能",//Add translation here
   "pokerus": "ポケルス",
   "hasPokerus": "ポケルス - あり",
   "noPokerus": "ポケルス - なし",
