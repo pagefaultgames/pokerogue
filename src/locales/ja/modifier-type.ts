7e9973633b0982cf9ed500188e6539187d5410b8
prime <10091050+prime-dialga@users.noreply.github.com>
2024-08-12 15:27:02 +0200

    Added translations from Adri1
    Co-authored-by: Lugiad' <adrien.grivel@hotmail.fr>

--- src/locales/ja/modifier-type.ts
+++ src/locales/ja/modifier-type.ts
@@ -414,7 +414,7 @@ export const modifierType: ModifierTypeTranslationEntries = {
     "BURN_DRIVE": "ブレイズカセット",
     "CHILL_DRIVE": "フリーズカセット",
     "DOUSE_DRIVE": "アクアカセット",
-    "ULTRANECROZIUM_Z": "Ultranecrozium Z",
+    "ULTRANECROZIUM_Z": "ウルトラネクロZ",
 
     "FIST_PLATE": "Fist Plate",
     "SKY_PLATE": "Sky Plate",
