00ace7961057485a017628788ccfd2ee419705b8
Blitzy <118096277+Blitz425@users.noreply.github.com>
2024-08-13 13:59:36 -0500

    Update pokemon-forms.ts

--- src/data/pokemon-forms.ts
+++ src/data/pokemon-forms.ts
@@ -632,7 +632,6 @@ export const pokemonFormChanges: PokemonFormChanges = {
   [Species.MELOETTA]: [
     new SpeciesFormChange(Species.MELOETTA, "aria", "pirouette", new SpeciesFormChangePostMoveTrigger(Moves.RELIC_SONG), true),
     new SpeciesFormChange(Species.MELOETTA, "pirouette", "aria", new SpeciesFormChangePostMoveTrigger(Moves.RELIC_SONG), true),
-    new SpeciesFormChange(Species.MELOETTA, "pirouette", "aria", new SpeciesFormChangeActiveTrigger(false), true)
   ],
   [Species.GENESECT]: [
     new SpeciesFormChange(Species.GENESECT, "", "shock", new SpeciesFormChangeItemTrigger(FormChangeItem.SHOCK_DRIVE)),
