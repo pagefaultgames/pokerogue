847ee1651f06edd9867fad5182a4395254841528
Frutescens <info@laptop>
2024-08-17 15:01:19 -0700

    removed debugging

--- src/phases.ts
+++ src/phases.ts
@@ -2036,10 +2036,7 @@ export class CommandPhase extends FieldPhase {
       }
       break;
     case Command.BALL:
-      const notInDex = (this.scene.getEnemyField().filter(p => p.isActive(true)).some(p => !p.scene.gameData.dexData[p.species.speciesId].caughtAttr) && this.scene.gameData.getStarterCount(d => !!d.caughtAttr) < Object.keys(speciesStarters).length - 1); //&& !this.scene.gameMode.isFreshStartChallenge();
-      console.log(notInDex);
-      console.log(!this.scene.gameMode.isClassic);
-      console.log(this.scene.gameMode.isFreshStartChallenge());
+      const notInDex = (this.scene.getEnemyField().filter(p => p.isActive(true)).some(p => !p.scene.gameData.dexData[p.species.speciesId].caughtAttr) && this.scene.gameData.getStarterCount(d => !!d.caughtAttr) < Object.keys(speciesStarters).length - 1);
       if (this.scene.arena.biomeType === Biome.END && (!this.scene.gameMode.isClassic || this.scene.gameMode.isFreshStartChallenge() || notInDex )) {
         this.scene.ui.setMode(Mode.COMMAND, this.fieldIndex);
         this.scene.ui.setMode(Mode.MESSAGE);
