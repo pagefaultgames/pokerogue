9421e2507af8cb8f6ea38329b0868e7ed64fdfda
Jannik Tappert <38758606+CodeTappert@users.noreply.github.com>
2024-08-17 20:40:41 +0200

    Fix Curse Message not beeing properly shown when casted by a ghost pokemon

--- src/data/move.ts
+++ src/data/move.ts
@@ -4441,7 +4441,7 @@ export class CurseAttr extends MoveEffectAttr {
       const curseRecoilDamage = Math.max(1, Math.floor(user.getMaxHp() / 2));
       user.damageAndUpdate(curseRecoilDamage, HitResult.OTHER, false, true, true);
       user.scene.queueMessage(
-        i18next.t("battle:cursedOnAdd", {
+        i18next.t("battlerTags:cursedOnAdd", {
           pokemonNameWithAffix: getPokemonNameWithAffix(user),
           pokemonName: getPokemonNameWithAffix(target)
         })
