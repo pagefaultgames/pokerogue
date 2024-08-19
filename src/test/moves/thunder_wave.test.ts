a251ba9622526243b36fd55f1d9ae55d445f0115
frutescens <info@laptop>
2024-08-19 08:38:48 -0700

    thunder wave test

---
+++ src/test/moves/thunder_wave.test.ts
@@ -0,0 +1,122 @@
+import { Status, StatusEffect } from "#app/data/status-effect";
+import { EnemyPokemon } from "#app/field/pokemon";
+import { allMoves } from "#app/data/move";
+import GameManager from "#test/utils/gameManager";
+import { getMovePosition } from "#test/utils/gameManagerUtils";
+import { Abilities } from "#app/enums/abilities";
+import { Moves } from "#enums/moves";
+import { Species } from "#enums/species";
+import Phaser from "phaser";
+import { afterEach, beforeAll, beforeEach, describe, expect, test, vi } from "vitest";
+import { TurnEndPhase } from "#app/phases/turn-end-phase";
+
+const TIMEOUT = 20 * 1000;
+
+describe("Moves - Thunder Wave", () => {
+  let phaserGame: Phaser.Game;
+  let game: GameManager;
+
+  beforeAll(() => {
+    phaserGame = new Phaser.Game({
+      type: Phaser.HEADLESS,
+    });
+  });
+
+  afterEach(() => {
+    game.phaseInterceptor.restoreOg();
+  });
+
+  beforeEach(() => {
+    game = new GameManager(phaserGame);
+    game.override.battleType("single");
+    game.override.starterSpecies(Species.PIKACHU);
+    game.override.moveset([Moves.THUNDER_WAVE]);
+    game.override.enemyMoveset([Moves.SPLASH, Moves.NONE, Moves.NONE, Moves.NONE]);
+    vi.spyOn(allMoves[Moves.THUNDER_WAVE], "accuracy", "get").mockReturnValue(100);
+  });
+
+  // References: https://bulbapedia.bulbagarden.net/wiki/Thunder_Wave_(move)
+
+  test(
+    "Thunder Wave as-is paralyzes non-statused Pokemon that are not Ground types",
+    async () => {
+      game.override.enemySpecies(Species.MAGIKARP);
+      await game.startBattle();
+
+      const enemyPokemon: EnemyPokemon = game.scene.getEnemyPokemon()!;
+
+      game.doAttack(getMovePosition(game.scene, 0, Moves.THUNDER_WAVE));
+      await game.phaseInterceptor.to(TurnEndPhase);
+
+      expect(enemyPokemon.status?.effect).toBe(StatusEffect.PARALYSIS);
+    },
+    TIMEOUT
+  );
+
+  test(
+    "Thunder Wave as-is does not paralyze if the Pokemon is a Ground-type",
+    async () => {
+      game.override.enemySpecies(Species.DIGLETT);
+      await game.startBattle();
+
+      const enemyPokemon: EnemyPokemon = game.scene.getEnemyPokemon()!;
+
+      game.doAttack(getMovePosition(game.scene, 0, Moves.THUNDER_WAVE));
+      await game.phaseInterceptor.to(TurnEndPhase);
+
+      expect(enemyPokemon.status).toBeUndefined();
+    },
+    TIMEOUT
+  );
+
+  test(
+    "Thunder Wave does not paralyze if the Pokemon is already status-ed",
+    async () => {
+      game.override.enemySpecies(Species.MAGIKARP);
+      await game.startBattle();
+
+      const enemyPokemon: EnemyPokemon = game.scene.getEnemyPokemon()!;
+      enemyPokemon.status = new Status(StatusEffect.BURN);
+
+      game.doAttack(getMovePosition(game.scene, 0, Moves.THUNDER_WAVE));
+      await game.phaseInterceptor.to(TurnEndPhase);
+
+      expect(enemyPokemon.status?.effect).not.toBe(StatusEffect.PARALYSIS);
+    },
+    TIMEOUT
+  );
+
+  test(
+    "Thunder Wave's effectiveness is based on its type",
+    async () => {
+      game.override.ability(Abilities.NORMALIZE);
+      game.override.enemySpecies(Species.DIGLETT);
+      await game.startBattle();
+
+      const enemyPokemon: EnemyPokemon = game.scene.getEnemyPokemon()!;
+
+      game.doAttack(getMovePosition(game.scene, 0, Moves.THUNDER_WAVE));
+      await game.phaseInterceptor.to(TurnEndPhase);
+
+      expect(enemyPokemon.status?.effect).toBe(StatusEffect.PARALYSIS);
+    },
+    TIMEOUT
+  );
+
+  test(
+    "Thunder Wave respects type immunities",
+    async () => {
+      game.override.ability(Abilities.NORMALIZE);
+      game.override.enemySpecies(Species.HAUNTER);
+      await game.startBattle();
+
+      const enemyPokemon: EnemyPokemon = game.scene.getEnemyPokemon()!;
+
+      game.doAttack(getMovePosition(game.scene, 0, Moves.THUNDER_WAVE));
+      await game.phaseInterceptor.to(TurnEndPhase);
+
+      expect(enemyPokemon.status).toBeUndefined();
+    },
+    TIMEOUT
+  );
+});
