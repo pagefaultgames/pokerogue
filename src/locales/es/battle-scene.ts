b653a4231428066f06e31690d263fb9db6a26366
Lugiad <adrien.grivel@hotmail.fr>
2024-08-19 19:17:33 +0200

    Update battle-scene.ts

--- src/locales/es/battle-scene.ts
+++ src/locales/es/battle-scene.ts
@@ -1,5 +1,5 @@
 import { SimpleTranslationEntries } from "#app/interfaces/locales";
 
 export const battleScene: SimpleTranslationEntries = {
-  "moneyOwned": "₽{{formattedMoney}}"
+  "moneyOwned": "{{formattedMoney}} ₽"
 } as const;
