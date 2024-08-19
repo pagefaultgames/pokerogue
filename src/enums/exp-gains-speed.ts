06e13800e574da2de2b235c7b6c7f2a83e76e634
flx-sta <0131232+flx-sta@users.noreply.github.com>
2024-08-19 09:10:26 -0700

    add ExpGainsSpeed enum and apply it

---
+++ src/enums/exp-gains-speed.ts
@@ -0,0 +1,13 @@
+/**
+ * Determines exp gains speeed.
+ */
+export enum ExpGainsSpeed {
+    /** Normal animation speed */
+    NORMAL,
+    /** Fast animation speed */
+    FAST,
+    /** Faster animation speed */
+    FASTER,
+    /** Skip animation (instant) */
+    SKIP,
+}
