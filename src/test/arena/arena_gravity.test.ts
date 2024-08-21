4920cdfda766ff295d44b1d6190f6e67865cf5ed
Christopher Schmidt <schmidtc086@outlook.com>
2024-08-21 13:57:13 -0400

    Moves Gravity test to pre-existing arena_gravity.test.ts

--- src/test/arena/arena_gravity.test.ts
+++ src/test/arena/arena_gravity.test.ts
@@ -9,6 +9,8 @@ import Phaser from "phaser";
 import { afterEach, beforeAll, beforeEach, describe, expect, it, vi } from "vitest";
 import { MoveEffectPhase } from "#app/phases/move-effect-phase.js";
 import { TurnEndPhase } from "#app/phases/turn-end-phase.js";
+import { ArenaTagSide } from "#app/data/arena-tag.js";
+import { BattlerTagType } from "#app/enums/battler-tag-type.js";
 
 describe("Arena - Gravity", () => {
   let phaserGame: Phaser.Game;
@@ -27,11 +29,11 @@ describe("Arena - Gravity", () => {
   beforeEach(() => {
     game = new GameManager(phaserGame);
     game.override.battleType("single");
-    game.override.moveset([Moves.TACKLE, Moves.GRAVITY, Moves.FISSURE]);
+    game.override.moveset([Moves.TACKLE, Moves.GRAVITY, Moves.FISSURE, Moves.FLY]);
     game.override.ability(Abilities.UNNERVE);
     game.override.enemyAbility(Abilities.BALL_FETCH);
     game.override.enemySpecies(Species.SHUCKLE);
-    game.override.enemyMoveset(new Array(4).fill(Moves.SPLASH));
+    game.override.enemyMoveset(new Array(4).fill(Moves.GROWL)); // Splash is unselectable under gravity effects.
   });
 
   it("non-OHKO move accuracy is multiplied by 1.67", async () => {
@@ -77,4 +79,40 @@ describe("Arena - Gravity", () => {
 
     expect(moveToCheck.calculateBattleAccuracy).toHaveReturnedWith(30);
   });
+
+  it(
+    "should last for 5 turns on both sides",
+    async () => {
+      await game.startBattle([Species.RATTATA]);
+
+      game.doAttack(getMovePosition(game.scene, 0, Moves.GRAVITY));
+
+      for (let i = 0; i < 4; i++) { // Gravity tag should be defined for 5 turns (1 turn before this loop, 4 turns from this loop)
+        await game.toNextTurn();
+        expect(game.scene.arena.getTagOnSide(ArenaTagType.GRAVITY, ArenaTagSide.PLAYER)).toBeDefined();
+        expect(game.scene.arena.getTagOnSide(ArenaTagType.GRAVITY, ArenaTagSide.ENEMY)).toBeDefined();
+        game.doAttack(getMovePosition(game.scene, 0, Moves.GROWL));
+      }
+
+      await game.toNextTurn();
+      expect(game.scene.arena.getTagOnSide(ArenaTagType.GRAVITY, ArenaTagSide.PLAYER)).toBeUndefined();
+      expect(game.scene.arena.getTagOnSide(ArenaTagType.GRAVITY, ArenaTagSide.ENEMY)).toBeUndefined();
+    });
+
+  it(
+    "should interrupt any pokemon in the semi-invulnerable state of Fly",
+    async () => {
+      game.override.enemyMoveset(Array(4).fill(Moves.GRAVITY));
+      await game.startBattle([Species.REGIELEKI]);
+      const playerPokemon = game.scene.getPlayerPokemon()!;
+
+      game.doAttack(getMovePosition(game.scene, 0, Moves.FLY));
+      // Finish Fly's MoveEffectPhase and check for BattlerTagType.FLYING
+      await game.phaseInterceptor.to(MoveEffectPhase, true);
+      expect(playerPokemon.getTag(BattlerTagType.FLYING)).toBeTruthy();
+
+      // Enemy Shuckle uses Gravity, removes playerPokemon from Fly
+      await game.toNextTurn();
+      expect(playerPokemon.getTag(BattlerTagType.FLYING)).toBeFalsy();
+    });
 });
