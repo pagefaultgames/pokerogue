ff0a8974fbdccec5e5948da071e1f2309aa851a5
flx-sta <50131232+flx-sta@users.noreply.github.com>
2024-08-22 10:04:14 -0700

    EN: migrate `starter-select-ui-handler`

--- src/locales/en/config.ts
+++ src/locales/en/config.ts
@@ -41,7 +41,7 @@ import pokemonInfoContainer from "./pokemon-info-container.json";
 import pokemonSummary from "./pokemon-summary.json";
 import saveSlotSelectUiHandler from "./save-slot-select-ui-handler.json";
 import splashMessages from "./splash-messages.json";
-import { starterSelectUiHandler } from "./starter-select-ui-handler";
+import starterSelectUiHandler from "./starter-select-ui-handler.json";
 import { statusEffect } from "./status-effect";
 import { titles, trainerClasses, trainerNames } from "./trainers";
 import { tutorial } from "./tutorial";
