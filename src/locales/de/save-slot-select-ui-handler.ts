9e35ac96f7672b9011bfb71b6368dd66b2295608
flx-sta <50131232+flx-sta@users.noreply.github.com>
2024-08-22 13:21:18 -0700

    Migrate DE to JSON

--- src/locales/de/save-slot-select-ui-handler.ts
+++
@@ -1,9 +0,0 @@
-import { SimpleTranslationEntries } from "#app/interfaces/locales";
-
-export const saveSlotSelectUiHandler: SimpleTranslationEntries = {
-  "overwriteData": "Den ausgewählten Speicherstand überschreiben?",
-  "loading": "Läd...",
-  "wave": "Welle",
-  "lv": "Lv. ",
-  "empty": "Leer",
-} as const;
