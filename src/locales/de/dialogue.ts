e086a983e0e92a022c61790e389d03ce5a9c84a2
Blitzy <118096277+Blitz425@users.noreply.github.com>
2024-08-22 17:03:36 -0500

    Change line per request of Dertapp

--- src/locales/de/dialogue.ts
+++ src/locales/de/dialogue.ts
@@ -482,7 +482,7 @@ export const PGMdialogue: DialogueTranslationEntries = {
       3: "Was machst du hier? Bist du uns gefolgt?"
     },
     "victory": {
-      1: "Na gut, bis der Boss Zeit für dich hat, werde ich dein Gegner sein!",
+      1: "Muwuhahaha! That battle was fun even though I lost!",
       2: `Ich kann es fühlen! Ich kann es spüren, das ist klar! Die Stärke, die von dir ausgeht!
           $Mehr! Ich will noch mehr! Aber es sieht so aus, als hätten wir keine Zeit mehr...`,
       3: `Das war Spaß! Ich wusste, dass du mir eine gute Zeit bieten würdest!
