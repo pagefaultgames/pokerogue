ed000a4fa40b1554690ed836fd55f3d1902a6060
NightKev <34855794+DayKev@users.noreply.github.com>
2024-08-22 22:02:16 -0700

    Fix inverse types tests (#1)
    * Fix Inverse Battle tests
    
    * Add timeout parameter to tests

--- src/test/battle/inverse_battle.test.ts
+++ src/test/battle/inverse_battle.test.ts
@@ -1,21 +1,20 @@
-import { TurnEndPhase } from "#app/phases/turn-end-phase.js";
-import { MoveEndPhase } from "#app/phases/move-end-phase.js";
-import GameManager from "#test/utils/gameManager";
+import { BattlerIndex } from "#app/battle";
+import { allMoves } from "#app/data/move";
+import { Type } from "#app/data/type";
+import { MoveEndPhase } from "#app/phases/move-end-phase";
+import { TurnEndPhase } from "#app/phases/turn-end-phase";
 import { Abilities } from "#enums/abilities";
+import { ArenaTagType } from "#enums/arena-tag-type";
+import { Challenges } from "#enums/challenges";
 import { Moves } from "#enums/moves";
 import { Species } from "#enums/species";
-import Phaser from "phaser";
-import { Type } from "#app/data/type.js";
-import { afterEach, beforeAll, beforeEach, describe, expect, it } from "vitest";
-import { Challenges } from "#enums/challenges";
-import { copyChallenge } from "data/challenge";
-import { ArenaTagType } from "#enums/arena-tag-type";
-import { SPLASH_ONLY } from "../utils/testUtils";
-import { allMoves } from "#app/data/move";
-import { BattlerTagType } from "#enums/battler-tag-type";
 import { StatusEffect } from "#enums/status-effect";
+import GameManager from "#test/utils/gameManager";
+import { copyChallenge } from "data/challenge";
+import Phaser from "phaser";
+import { afterEach, beforeAll, beforeEach, describe, expect, it } from "vitest";
 
-
+const TIMEOUT = 20 * 1000;
 
 describe("Inverse Battle", () => {
   let phaserGame: Phaser.Game;
@@ -41,55 +40,52 @@ describe("Inverse Battle", () => {
     };
 
     game.scene.gameMode.challenges = [copyChallenge(challenge)];
-    game.override.battleType("single");
-    game.override.startingLevel(10);
+    game.override
+      .battleType("single")
+      .starterSpecies(Species.FEEBAS)
+      .ability(Abilities.BALL_FETCH)
+      .enemySpecies(Species.MAGIKARP)
+      .enemyAbility(Abilities.BALL_FETCH);
   });
 
   it("1. immune types are 2x effective - Thunderbolt against Ground Type", async () => {
     game.override.enemySpecies(Species.SANDSHREW);
-    game.override.enemyAbility(Abilities.SAND_VEIL);
-    game.override.starterSpecies(Species.PIKACHU);
 
     await game.startBattle(undefined, false);
 
-    const pikachu = game.scene.getPlayerPokemon()!;
-    const sandshrew = game.scene.getEnemyPokemon()!;
+    const player = game.scene.getPlayerPokemon()!;
+    const enemy = game.scene.getEnemyPokemon()!;
 
-    expect(sandshrew.getAttackTypeEffectiveness(allMoves[Moves.THUNDERBOLT].type, pikachu)).toBe(2);
-  });
+    expect(enemy.getAttackTypeEffectiveness(allMoves[Moves.THUNDERBOLT].type, player)).toBe(2);
+  }, TIMEOUT);
 
   it("2. 2x effective types are 0.5x effective - Thunderbolt against Flying Type", async () => {
     game.override.enemySpecies(Species.PIDGEY);
-    game.override.enemyAbility(Abilities.KEEN_EYE);
-    game.override.starterSpecies(Species.PIKACHU);
 
     await game.startBattle(undefined, false);
 
-    const pikachu = game.scene.getPlayerPokemon()!;
-    const pidgey = game.scene.getEnemyPokemon()!;
+    const player = game.scene.getPlayerPokemon()!;
+    const enemy = game.scene.getEnemyPokemon()!;
 
-    expect(pidgey.getAttackTypeEffectiveness(allMoves[Moves.THUNDERBOLT].type, pikachu)).toBe(0.5);
-  });
+    expect(enemy.getAttackTypeEffectiveness(allMoves[Moves.THUNDERBOLT].type, player)).toBe(0.5);
+  }, TIMEOUT);
 
   it("3. 0.5x effective types are 2x effective - Thunderbolt against Electric Type", async () => {
     game.override.enemySpecies(Species.CHIKORITA);
-    game.override.enemyAbility(Abilities.OVERGROW);
-    game.override.starterSpecies(Species.PIKACHU);
 
     await game.startBattle(undefined, false);
 
-    const pikachu = game.scene.getPlayerPokemon()!;
-    const chikorita = game.scene.getEnemyPokemon()!;
+    const player = game.scene.getPlayerPokemon()!;
+    const enemy = game.scene.getEnemyPokemon()!;
 
-    expect(chikorita.getAttackTypeEffectiveness(allMoves[Moves.THUNDERBOLT].type, pikachu)).toBe(2);
-  });
+    expect(enemy.getAttackTypeEffectiveness(allMoves[Moves.THUNDERBOLT].type, player)).toBe(2);
+  }, TIMEOUT);
 
   it("4. Stealth Rock follows the inverse matchups - Stealth Rock against Charizard deals 1/32 of max HP", async () => {
     game.scene.arena.addTag(ArenaTagType.STEALTH_ROCK, 1, Moves.STEALTH_ROCK, 0);
-    game.override.enemySpecies(Species.CHARIZARD);
-    game.override.enemyAbility(Abilities.BLAZE);
-    game.override.starterSpecies(Species.PIKACHU);
-    game.override.enemyLevel(100);
+    game.override
+      .enemySpecies(Species.CHARIZARD)
+      .enemyLevel(100);
 
     await game.startBattle(undefined, false);
 
@@ -102,105 +98,99 @@ describe("Inverse Battle", () => {
     const expectedHP = maxHp - damage_prediction;
 
     console.log("Charizard's max HP: " + maxHp, "Damage: " + damage_prediction, "Current HP: " + currentHp, "Expected HP: " + expectedHP);
-    expect(expectedHP).toBeGreaterThan(maxHp*31/32-1);
-  });
+    expect(expectedHP).toBeGreaterThan(maxHp * 31 / 32 - 1);
+  }, TIMEOUT);
 
   it("5. Freeze Dry is 2x effective against Water Type like other Ice type Move - Freeze Dry against Squirtle", async () => {
     game.override.enemySpecies(Species.SQUIRTLE);
-    game.override.enemyAbility(Abilities.TORRENT);
-    game.override.starterSpecies(Species.ARTICUNO);
 
     await game.startBattle(undefined, false);
 
-    const squirtle = game.scene.getEnemyPokemon()!;
-    const articuno = game.scene.getPlayerPokemon()!;
+    const player = game.scene.getPlayerPokemon()!;
+    const enemy = game.scene.getEnemyPokemon()!;
 
-    expect(squirtle.getAttackTypeEffectiveness(allMoves[Moves.FREEZE_DRY].type, articuno)).toBe(2);
-  });
+    expect(enemy.getAttackTypeEffectiveness(allMoves[Moves.FREEZE_DRY].type, player)).toBe(2);
+  }, TIMEOUT);
 
   it("6. Water Absorb should heal against water moves - Water Absorb against Water gun", async () => {
-    game.override.starterSpecies(Species.SQUIRTLE);
-    game.override.moveset([Moves.WATER_GUN]);
-    game.override.enemySpecies(Species.PIKACHU);
-    game.override.enemyAbility(Abilities.WATER_ABSORB);
+    game.override
+      .moveset([Moves.WATER_GUN])
+      .enemyAbility(Abilities.WATER_ABSORB);
 
     await game.startBattle(undefined, false);
 
-    const pikachu = game.scene.getEnemyPokemon()!;
-    pikachu.hp = pikachu.getMaxHp() - 1;
+    const enemy = game.scene.getEnemyPokemon()!;
+    enemy.hp = enemy.getMaxHp() - 1;
     game.move.select(Moves.WATER_GUN);
+    await game.setTurnOrder([BattlerIndex.PLAYER, BattlerIndex.ENEMY]);
 
     await game.phaseInterceptor.to(MoveEndPhase);
 
-    expect(pikachu.hp).toBe(pikachu.getMaxHp());
-  });
+    expect(enemy.hp).toBe(enemy.getMaxHp());
+  }, TIMEOUT);
 
   it("7. Fire type does not get burned - Will-O-Wisp against Charmander", async () => {
-    game.override.starterSpecies(Species.PIKACHU);
-    game.override.moveset([Moves.WILL_O_WISP]);
-    game.override.enemySpecies(Species.CHARMANDER);
-    game.override.enemyMoveset(SPLASH_ONLY);
+    game.override
+      .moveset([Moves.WILL_O_WISP])
+      .enemySpecies(Species.CHARMANDER);
 
     await game.startBattle(undefined, false);
 
-    const charmander = game.scene.getEnemyPokemon()!;
-    charmander.addTag(BattlerTagType.ALWAYS_GET_HIT, 99);
+    const enemy = game.scene.getEnemyPokemon()!;
 
     game.move.select(Moves.WILL_O_WISP);
+    await game.setTurnOrder([BattlerIndex.PLAYER, BattlerIndex.ENEMY]);
+    await game.move.forceHit();
 
     await game.phaseInterceptor.to(MoveEndPhase);
 
-    expect(charmander.status?.effect).not.toBe(StatusEffect.BURN);
-  });
+    expect(enemy.status?.effect).not.toBe(StatusEffect.BURN);
+  }, TIMEOUT);
 
   it("8. Electric type does not get paralyzed - Nuzzle against Pikachu", async () => {
-    game.override.starterSpecies(Species.PICHU);
-    game.override.moveset([Moves.NUZZLE]);
-    game.override.enemySpecies(Species.PIKACHU);
-    game.override.enemyMoveset(SPLASH_ONLY);
+    game.override
+      .moveset([Moves.NUZZLE])
+      .enemySpecies(Species.PIKACHU)
+      .enemyLevel(50);
 
     await game.startBattle(undefined, false);
 
-    const pikachu = game.scene.getEnemyPokemon()!;
+    const enemy = game.scene.getEnemyPokemon()!;
 
     game.move.select(Moves.NUZZLE);
+    await game.setTurnOrder([BattlerIndex.PLAYER, BattlerIndex.ENEMY]);
 
     await game.phaseInterceptor.to(MoveEndPhase);
 
-    expect(pikachu.status?.effect).not.toBe(StatusEffect.PARALYSIS);
-  });
+    expect(enemy.status?.effect).not.toBe(StatusEffect.PARALYSIS);
+  }, TIMEOUT);
 
 
   it("10. Anticipation should trigger on 2x effective moves - Anticipation against Thunderbolt", async () => {
-    game.override.starterSpecies(Species.PIKACHU);
-    game.override.moveset([Moves.THUNDERBOLT]);
-    game.override.enemySpecies(Species.SANDSHREW);
-    game.override.enemyAbility(Abilities.ANTICIPATION);
-    game.override.enemyMoveset(SPLASH_ONLY);
+    game.override
+      .moveset([Moves.THUNDERBOLT])
+      .enemySpecies(Species.SANDSHREW)
+      .enemyAbility(Abilities.ANTICIPATION);
 
     await game.startBattle(undefined, false);
 
-    game.move.select(Moves.THUNDERBOLT);
-
-    await game.phaseInterceptor.to(MoveEndPhase);
-  });
+    expect(game.scene.getEnemyPokemon()?.summonData.abilitiesApplied[0]).toBe(Abilities.ANTICIPATION);
+  }, TIMEOUT);
 
   it("11. Conversion 2 should change the type to the resistive type - Conversion 2 against Dragonite", async () => {
-    game.override.starterSpecies(Species.PORYGON);
-    game.override.moveset([Moves.CONVERSION_2]);
-    game.override.enemySpecies(Species.DRAGONITE);
-    game.override.enemyAbility(Abilities.MULTISCALE);
-    game.override.enemyMoveset([Moves.DRAGON_CLAW, Moves.DRAGON_CLAW, Moves.DRAGON_CLAW, Moves.DRAGON_CLAW]);
-    game.override.enemyLevel(10);
+    game.override
+      .moveset([Moves.CONVERSION_2])
+      .enemyMoveset([Moves.DRAGON_CLAW, Moves.DRAGON_CLAW, Moves.DRAGON_CLAW, Moves.DRAGON_CLAW]);
 
     await game.startBattle(undefined, false);
 
-    const porygon = game.scene.getPlayerPokemon()!;
+    const player = game.scene.getPlayerPokemon()!;
 
     game.move.select(Moves.CONVERSION_2);
+    await game.setTurnOrder([BattlerIndex.ENEMY, BattlerIndex.PLAYER]);
 
     await game.phaseInterceptor.to(TurnEndPhase);
 
-    expect(porygon.getTypes()[0]).toBe(Type.DRAGON);
-  });
+    expect(player.getTypes()[0]).toBe(Type.DRAGON);
+  }, TIMEOUT);
 });
