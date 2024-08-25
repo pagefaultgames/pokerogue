f839b68ac236596b917f079d4a07826ac25040a5
NightKev <34855794+DayKev@users.noreply.github.com>
2024-08-25 15:56:21 -0700

    Optimize effectiveness test and make others more stylish (#3)

--- src/test/moves/effectiveness.test.ts
+++ src/test/moves/effectiveness.test.ts
@@ -1,24 +1,21 @@
-import { Abilities } from "#app/enums/abilities";
-import { Species } from "#app/enums/species";
-import { Moves } from "#app/enums/moves";
+import { allMoves } from "#app/data/move";
 import { getPokemonSpecies } from "#app/data/pokemon-species";
 import { TrainerSlot } from "#app/data/trainer-config";
-import { allMoves } from "#app/data/move";
+import { Abilities } from "#app/enums/abilities";
+import { Moves } from "#app/enums/moves";
+import { Species } from "#app/enums/species";
 import * as Messages from "#app/messages";
-import Phaser from "phaser";
 import GameManager from "#test/utils/gameManager";
-import { afterEach, beforeAll, beforeEach, describe, expect, it, vi } from "vitest";
+import Phaser from "phaser";
+import { afterEach, beforeAll, describe, expect, it, vi } from "vitest";
 
 function testMoveEffectiveness(game: GameManager, move: Moves, targetSpecies: Species,
   expected: number, targetAbility: Abilities = Abilities.BALL_FETCH): void {
   // Suppress getPokemonNameWithAffix because it calls on a null battle spec
   vi.spyOn(Messages, "getPokemonNameWithAffix").mockReturnValue("");
+  game.override.enemyAbility(targetAbility);
   const user = game.scene.addPlayerPokemon(getPokemonSpecies(Species.SNORLAX), 5);
   const target = game.scene.addEnemyPokemon(getPokemonSpecies(targetSpecies), 5, TrainerSlot.NONE);
-
-  game.override
-    .ability(Abilities.BALL_FETCH)
-    .enemyAbility(targetAbility);
 
   expect(target.getMoveEffectiveness(user, allMoves[move])).toBe(expected);
 }
@@ -31,53 +28,43 @@ describe("Moves - Type Effectiveness", () => {
     phaserGame = new Phaser.Game({
       type: Phaser.HEADLESS,
     });
+    game = new GameManager(phaserGame);
+    game.override.ability(Abilities.BALL_FETCH);
   });
 
   afterEach(() => {
     game.phaseInterceptor.restoreOg();
   });
 
-  beforeEach(() => {
-    game = new GameManager(phaserGame);
-  });
-
-  it(
-    "Normal-type attacks are neutrally effective against Normal-type Pokemon",
+  it("Normal-type attacks are neutrally effective against Normal-type Pokemon",
     () => testMoveEffectiveness(game, Moves.TACKLE, Species.SNORLAX, 1)
   );
 
-  it(
-    "Normal-type attacks are not very effective against Steel-type Pokemon",
+  it("Normal-type attacks are not very effective against Steel-type Pokemon",
     () => testMoveEffectiveness(game, Moves.TACKLE, Species.REGISTEEL, 0.5)
   );
 
-  it(
-    "Normal-type attacks are doubly resisted by Steel/Rock-type Pokemon",
+  it("Normal-type attacks are doubly resisted by Steel/Rock-type Pokemon",
     () => testMoveEffectiveness(game, Moves.TACKLE, Species.AGGRON, 0.25)
   );
 
-  it(
-    "Normal-type attacks have no effect on Ghost-type Pokemon",
+  it("Normal-type attacks have no effect on Ghost-type Pokemon",
     () => testMoveEffectiveness(game, Moves.TACKLE, Species.DUSCLOPS, 0)
   );
 
-  it(
-    "Normal-type status moves are not affected by type matchups",
+  it("Normal-type status moves are not affected by type matchups",
     () => testMoveEffectiveness(game, Moves.GROWL, Species.DUSCLOPS, 1)
   );
 
-  it(
-    "Electric-type attacks are super-effective against Water-type Pokemon",
+  it("Electric-type attacks are super-effective against Water-type Pokemon",
     () => testMoveEffectiveness(game, Moves.THUNDERBOLT, Species.BLASTOISE, 2)
   );
 
-  it(
-    "Electric-type attacks are doubly super-effective against Water/Flying-type Pokemon",
+  it("Electric-type attacks are doubly super-effective against Water/Flying-type Pokemon",
     () => testMoveEffectiveness(game, Moves.THUNDERBOLT, Species.GYARADOS, 4)
   );
 
-  it(
-    "Electric-type attacks are negated by Volt Absorb",
+  it("Electric-type attacks are negated by Volt Absorb",
     () => testMoveEffectiveness(game, Moves.THUNDERBOLT, Species.GYARADOS, 0, Abilities.VOLT_ABSORB)
   );
 });
--- src/test/moves/thunder_wave.test.ts
+++ src/test/moves/thunder_wave.test.ts
@@ -1,12 +1,12 @@
 import { StatusEffect } from "#app/data/status-effect";
-import { EnemyPokemon } from "#app/field/pokemon";
-import GameManager from "#test/utils/gameManager";
 import { Abilities } from "#app/enums/abilities";
+import { EnemyPokemon } from "#app/field/pokemon";
 import { Moves } from "#enums/moves";
 import { Species } from "#enums/species";
+import GameManager from "#test/utils/gameManager";
 import Phaser from "phaser";
 import { afterEach, beforeAll, beforeEach, describe, expect, it } from "vitest";
-import { TurnEndPhase } from "#app/phases/turn-end-phase";
+import { SPLASH_ONLY } from "../utils/testUtils";
 
 const TIMEOUT = 20 * 1000;
 
@@ -26,96 +26,77 @@ describe("Moves - Thunder Wave", () => {
 
   beforeEach(() => {
     game = new GameManager(phaserGame);
-    game.override.battleType("single")
+    game.override
+      .battleType("single")
       .starterSpecies(Species.PIKACHU)
       .moveset([Moves.THUNDER_WAVE])
-      .enemyMoveset([Moves.SPLASH, Moves.NONE, Moves.NONE, Moves.NONE]);
+      .enemyMoveset(SPLASH_ONLY);
   });
 
   // References: https://bulbapedia.bulbagarden.net/wiki/Thunder_Wave_(move)
 
-  it(
-    "paralyzes non-statused Pokemon that are not Ground types",
-    async () => {
-      game.override.enemySpecies(Species.MAGIKARP);
-      await game.startBattle();
+  it("paralyzes non-statused Pokemon that are not Ground types", async () => {
+    game.override.enemySpecies(Species.MAGIKARP);
+    await game.startBattle();
 
-      const enemyPokemon: EnemyPokemon = game.scene.getEnemyPokemon()!;
+    const enemyPokemon: EnemyPokemon = game.scene.getEnemyPokemon()!;
 
-      game.move.select(Moves.THUNDER_WAVE);
-      await game.move.forceHit();
-      await game.phaseInterceptor.to(TurnEndPhase);
+    game.move.select(Moves.THUNDER_WAVE);
+    await game.move.forceHit();
+    await game.phaseInterceptor.to("BerryPhase");
 
-      expect(enemyPokemon.status?.effect).toBe(StatusEffect.PARALYSIS);
-    },
-    TIMEOUT
-  );
+    expect(enemyPokemon.status?.effect).toBe(StatusEffect.PARALYSIS);
+  }, TIMEOUT);
 
-  it(
-    "does not paralyze if the Pokemon is a Ground-type",
-    async () => {
-      game.override.enemySpecies(Species.DIGLETT);
-      await game.startBattle();
+  it("does not paralyze if the Pokemon is a Ground-type", async () => {
+    game.override.enemySpecies(Species.DIGLETT);
+    await game.startBattle();
 
-      const enemyPokemon: EnemyPokemon = game.scene.getEnemyPokemon()!;
+    const enemyPokemon: EnemyPokemon = game.scene.getEnemyPokemon()!;
 
-      game.move.select(Moves.THUNDER_WAVE);
-      await game.move.forceHit();
-      await game.phaseInterceptor.to(TurnEndPhase);
+    game.move.select(Moves.THUNDER_WAVE);
+    await game.move.forceHit();
+    await game.phaseInterceptor.to("BerryPhase");
 
-      expect(enemyPokemon.status).toBeUndefined();
-    },
-    TIMEOUT
-  );
+    expect(enemyPokemon.status).toBeUndefined();
+  }, TIMEOUT);
 
-  it(
-    "does not paralyze if the Pokemon already has a status effect",
-    async () => {
-      game.override.enemySpecies(Species.MAGIKARP).enemyStatusEffect(StatusEffect.BURN);
-      await game.startBattle();
+  it("does not paralyze if the Pokemon already has a status effect", async () => {
+    game.override.enemySpecies(Species.MAGIKARP).enemyStatusEffect(StatusEffect.BURN);
+    await game.startBattle();
 
-      const enemyPokemon: EnemyPokemon = game.scene.getEnemyPokemon()!;
+    const enemyPokemon: EnemyPokemon = game.scene.getEnemyPokemon()!;
 
-      game.move.select(Moves.THUNDER_WAVE);
-      await game.move.forceHit();
-      await game.phaseInterceptor.to(TurnEndPhase);
+    game.move.select(Moves.THUNDER_WAVE);
+    await game.move.forceHit();
+    await game.phaseInterceptor.to("BerryPhase");
 
-      expect(enemyPokemon.status?.effect).not.toBe(StatusEffect.PARALYSIS);
-    },
-    TIMEOUT
-  );
+    expect(enemyPokemon.status?.effect).not.toBe(StatusEffect.PARALYSIS);
+  }, TIMEOUT);
 
-  it(
-    "affects Ground types if the user has Normalize",
-    async () => {
-      game.override.ability(Abilities.NORMALIZE).enemySpecies(Species.DIGLETT);
-      await game.startBattle();
+  it("affects Ground types if the user has Normalize", async () => {
+    game.override.ability(Abilities.NORMALIZE).enemySpecies(Species.DIGLETT);
+    await game.startBattle();
 
-      const enemyPokemon: EnemyPokemon = game.scene.getEnemyPokemon()!;
+    const enemyPokemon: EnemyPokemon = game.scene.getEnemyPokemon()!;
 
-      game.move.select(Moves.THUNDER_WAVE);
-      await game.move.forceHit();
-      await game.phaseInterceptor.to(TurnEndPhase);
+    game.move.select(Moves.THUNDER_WAVE);
+    await game.move.forceHit();
+    await game.phaseInterceptor.to("BerryPhase");
 
-      expect(enemyPokemon.status?.effect).toBe(StatusEffect.PARALYSIS);
-    },
-    TIMEOUT
-  );
+    expect(enemyPokemon.status?.effect).toBe(StatusEffect.PARALYSIS);
+  }, TIMEOUT);
 
-  it(
-    "does not affect Ghost types if the user has Normalize",
-    async () => {
-      game.override.ability(Abilities.NORMALIZE).enemySpecies(Species.HAUNTER);
-      await game.startBattle();
+  it("does not affect Ghost types if the user has Normalize", async () => {
+    game.override.ability(Abilities.NORMALIZE).enemySpecies(Species.HAUNTER);
+    await game.startBattle();
 
-      const enemyPokemon: EnemyPokemon = game.scene.getEnemyPokemon()!;
+    const enemyPokemon: EnemyPokemon = game.scene.getEnemyPokemon()!;
 
-      game.move.select(Moves.THUNDER_WAVE);
-      await game.move.forceHit();
-      await game.phaseInterceptor.to(TurnEndPhase);
+    game.move.select(Moves.THUNDER_WAVE);
+    await game.move.forceHit();
+    await game.phaseInterceptor.to("BerryPhase");
 
-      expect(enemyPokemon.status).toBeUndefined();
-    },
-    TIMEOUT
-  );
+    expect(enemyPokemon.status).toBeUndefined();
+  }, TIMEOUT);
 });
