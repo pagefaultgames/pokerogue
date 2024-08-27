b2ec9612945f9ea117bd5e8846acec7e13b47718
Christopher Schmidt <schmidtc086@outlook.com>
2024-08-27 11:23:11 -0400

    Applies ClearWeatherAbAttr and PostFaintAbAttr separately for edge case of fused pokemon

--- src/phases/faint-phase.ts
+++ src/phases/faint-phase.ts
@@ -62,7 +62,10 @@ export class FaintPhase extends PokemonPhase {
 
     if (pokemon.turnData?.attacksReceived?.length) {
       const lastAttack = pokemon.turnData.attacksReceived[0];
-      if (pokemon.hasAbilityWithAttr(PostFaintClearWeatherAbAttr) || lastAttack.sourceId !== pokemon.id) { // prevent applying faint abilities to self unless its clear weather
+      if (pokemon.hasAbilityWithAttr(PostFaintClearWeatherAbAttr)) {
+        applyPostFaintAbAttrs(PostFaintClearWeatherAbAttr, pokemon, this.scene.getPokemonById(lastAttack.sourceId)!, new PokemonMove(lastAttack.move).getMove(), lastAttack.result);
+      }
+      if (lastAttack.sourceId !== pokemon.id) { // prevent applying faint abilities to self unless its clear weather
         applyPostFaintAbAttrs(PostFaintAbAttr, pokemon, this.scene.getPokemonById(lastAttack.sourceId)!, new PokemonMove(lastAttack.move).getMove(), lastAttack.result);
       }
     }
