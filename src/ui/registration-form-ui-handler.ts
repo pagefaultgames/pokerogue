0a92ece08ce2c2916cbb6cb604b9098449271d42
KimJeongSun <leo@atlaslabs.ai>
2024-08-28 01:30:02 +0900

    remove deprecated comments

--- src/ui/registration-form-ui-handler.ts
+++ src/ui/registration-form-ui-handler.ts
@@ -65,9 +65,7 @@ export default class RegistrationFormUiHandler extends FormModalUiHandler {
     super.setup();
 
     this.modalContainer.list.forEach((child: Phaser.GameObjects.GameObject) => {
-      // if child is a text object, and not the title text, set the font size to 42px
       if (child instanceof Phaser.GameObjects.Text && child !== this.titleText) {
-        // if buttonLabelTextSize is defined in languageSettings, set the font size to the value
         const inputFieldFontSize = languageSettings[i18next.language]?.inputFieldFontSize;
         if (inputFieldFontSize) {
           child.setFontSize(inputFieldFontSize);
