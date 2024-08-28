d66ecf8dece1a40f601199334680559f90c9c0aa
AJ Fontaine <fontbane@gmail.com>
2024-08-28 16:36:30 -0400

    Moved up Mimic in Mime Jr's moveset

--- src/data/pokemon-level-moves.ts
+++ src/data/pokemon-level-moves.ts
@@ -7550,14 +7550,15 @@ export const pokemonSpeciesLevelMoves: PokemonSpeciesLevelMoves = {
     [ 1, Moves.POUND ],
     [ 1, Moves.COPYCAT ],
     [ 1, Moves.BARRIER ],
+    [ 1, Moves.TICKLE ], //USUM
     [ 4, Moves.BATON_PASS ],
     [ 8, Moves.ENCORE ],
     [ 12, Moves.CONFUSION ],
-    [ 16, Moves.ROLE_PLAY ],
+    [ 16, Moves.MIMIC ], //Custom, swapped with Role Play to be closer to USUM
     [ 20, Moves.PROTECT ],
     [ 24, Moves.RECYCLE ],
     [ 28, Moves.PSYBEAM ],
-    [ 32, Moves.MIMIC ],
+    [ 32, Moves.ROLE_PLAY ], //Custom, swapped with Mimic
     [ 36, Moves.LIGHT_SCREEN ],
     [ 36, Moves.REFLECT ],
     [ 36, Moves.SAFEGUARD ],
