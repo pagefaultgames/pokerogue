eb559fd20d56a1c11b7a6d462c0e10a7fd58acb2
EmberCM <kooly213@hotmail.com>
2024-08-29 12:15:59 -0500

    Allow False Swipe and Hold Back to hit even at 1 hp

--- src/data/move.ts
+++ src/data/move.ts
@@ -1261,10 +1261,6 @@ export class SurviveDamageAttr extends ModifiedDamageAttr {
     return Math.min(damage, target.hp - 1);
   }
 
-  getCondition(): MoveConditionFunc {
-    return (user, target, move) => target.hp > 1;
-  }
-
   getUserBenefitScore(user: Pokemon, target: Pokemon, move: Move): integer {
     return target.hp > 1 ? 0 : -20;
   }
