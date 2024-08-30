7ae87c5af7dea599e70ec1aec21440490bc0b2df
Colin Kincaid <colin.kincaid.school@gmail.com>
2024-08-30 00:10:32 -0700

    Copy met level as well

--- src/field/pokemon.ts
+++ src/field/pokemon.ts
@@ -3598,6 +3598,7 @@ export class PlayerPokemon extends Pokemon {
         newPokemon.moveset = this.moveset.slice();
         newPokemon.moveset = this.copyMoveset();
         newPokemon.luck = this.luck;
+        newPokemon.metLevel = this.metLevel;
         newPokemon.metBiome = this.metBiome;
         newPokemon.metSpecies = this.metSpecies;
         newPokemon.fusionSpecies = this.fusionSpecies;
