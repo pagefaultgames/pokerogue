c76642798dc8273730e30c94c63fa3658685c8d9
NightKev <34855794+DayKev@users.noreply.github.com>
2024-08-31 22:25:31 -0700

    Update Toxic Orb test

--- src/test/items/toxic_orb.test.ts
+++ src/test/items/toxic_orb.test.ts
@@ -1,15 +1,14 @@
 import { StatusEffect } from "#app/data/status-effect";
-import { EnemyCommandPhase } from "#app/phases/enemy-command-phase";
-import { MessagePhase } from "#app/phases/message-phase";
-import { TurnEndPhase } from "#app/phases/turn-end-phase";
 import i18next, { initI18n } from "#app/plugins/i18n";
 import { Abilities } from "#enums/abilities";
 import { Moves } from "#enums/moves";
 import { Species } from "#enums/species";
 import GameManager from "#test/utils/gameManager";
+import { SPLASH_ONLY } from "#test/utils/testUtils";
 import Phaser from "phaser";
 import { afterEach, beforeAll, beforeEach, describe, expect, it } from "vitest";
 
+const TIMEOUT = 20 * 1000;
 
 describe("Items - Toxic orb", () => {
   let phaserGame: Phaser.Game;
@@ -27,40 +26,34 @@ describe("Items - Toxic orb", () => {
 
   beforeEach(() => {
     game = new GameManager(phaserGame);
-    const moveToUse = Moves.GROWTH;
-    const oppMoveToUse = Moves.TACKLE;
-    game.override.battleType("single");
-    game.override.enemySpecies(Species.RATTATA);
-    game.override.ability(Abilities.INSOMNIA);
-    game.override.enemyAbility(Abilities.INSOMNIA);
-    game.override.startingLevel(2000);
-    game.override.moveset([moveToUse]);
-    game.override.enemyMoveset([oppMoveToUse, oppMoveToUse, oppMoveToUse, oppMoveToUse]);
-    game.override.startingHeldItems([{
-      name: "TOXIC_ORB",
-    }]);
+    game.override
+      .battleType("single")
+      .enemySpecies(Species.RATTATA)
+      .ability(Abilities.BALL_FETCH)
+      .enemyAbility(Abilities.BALL_FETCH)
+      .moveset([Moves.SPLASH])
+      .enemyMoveset(SPLASH_ONLY)
+      .startingHeldItems([{
+        name: "TOXIC_ORB",
+      }]);
   });
 
-  it("TOXIC ORB", async () => {
+  it("badly poisons the holder", async () => {
     initI18n();
     i18next.changeLanguage("en");
-    const moveToUse = Moves.GROWTH;
-    await game.startBattle([
-      Species.MIGHTYENA,
-      Species.MIGHTYENA,
-    ]);
-    expect(game.scene.modifiers[0].type.id).toBe("TOXIC_ORB");
+    await game.classicMode.startBattle([Species.MIGHTYENA]);
 
-    game.move.select(moveToUse);
+    const player = game.scene.getPlayerField()[0];
 
-    // will run the 13 phase from enemyCommandPhase to TurnEndPhase
-    await game.phaseInterceptor.runFrom(EnemyCommandPhase).to(TurnEndPhase);
+    game.move.select(Moves.SPLASH);
+
+    await game.phaseInterceptor.to("TurnEndPhase");
     // Toxic orb should trigger here
-    await game.phaseInterceptor.run(MessagePhase);
+    await game.phaseInterceptor.run("MessagePhase");
     const message = game.textInterceptor.getLatestMessage();
     expect(message).toContain("was badly poisoned by the Toxic Orb");
-    expect(game.scene.getParty()[0].status!.effect).toBe(StatusEffect.TOXIC);
+    expect(player.status?.effect).toBe(StatusEffect.TOXIC);
     // Damage should not have ticked yet.
-    expect(game.scene.getParty()[0].status!.turnCount).toBe(0);
-  }, 2000);
+    expect(player.status?.turnCount).toBe(0);
+  }, TIMEOUT);
 });
