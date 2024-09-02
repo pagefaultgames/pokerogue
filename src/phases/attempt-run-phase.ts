ec4c3f559a603cb0808899ddb7f927ef393d610f
Opaque02 <66582645+Opaque02@users.noreply.github.com>
2024-09-02 17:06:47 +1000

    Changed escape calculation as per Mega

--- src/phases/attempt-run-phase.ts
+++ src/phases/attempt-run-phase.ts
@@ -22,10 +22,41 @@ export class AttemptRunPhase extends PokemonPhase {
 
     const enemySpeed = enemyField.reduce((total: integer, enemyPokemon: Pokemon) => total + enemyPokemon.getStat(Stat.SPD), 0) / enemyField.length;
 
-    const escapeChance = new Utils.IntegerHolder((((playerPokemon.getStat(Stat.SPD) * 128) / enemySpeed) + (30 * this.scene.currentBattle.escapeAttempts++)) % 256);
+    /*  The way the escape chance works is by looking at the difference between your speed and the enemy field's average speed as a ratio. The higher this ratio, the higher your chance of success.
+     *  However, there is a cap for the ratio of your speed vs enemy speed which beyond that point, you won't gain any advantage. It also looks at how many times you've tried to escape.
+     *  Again, the more times you've tried to escape, the higher your odds of escaping. Bosses and non-bosses are calculated differently - bosses are harder to escape from vs non-bosses
+     *  Finally, there's a minimum and maximum escape chance as well so that escapes aren't guaranteed, yet they are never 0 either.
+     *  The percentage chance to escape from a pokemon for both bosses and non bosses is linear and based on the minimum and maximum chances, and the speed ratio cap.
+     *
+     *  At the time of writing, these conditions should be met:
+     *   - The minimum escape chance should be 5% for bosses and non bosses
+     *   - Bosses should have a maximum escape chance of 25%, whereas non-bosses should be 95%
+     *   - The bonus per previous escape attempt should be 2% for bosses and 10% for non-bosses
+     *   - The speed ratio cap should be 6x for bosses and 4x for non-bosses
+     *   - The "default" escape chance when your speed equals the enemy speed should be 8.33% for bosses and 27.5% for non-bosses
+     *
+     *  From the above, we can calculate the below values
+     */
+
+    let isBoss = false;
+    for (let e = 0; e < enemyField.length; e++) {
+      isBoss = isBoss || enemyField[e].isBoss(); // this line checks if any of the enemy pokemon on the field are bosses; if so, the calculation for escaping is different
+    }
+
+    const speedRatio = playerPokemon.getStat(Stat.SPD) / enemySpeed; // this gets the speed ration between you and the average enemy speed
+    const speedCap = isBoss ? 6 : 4; // this gets the speed cap depending whether there's a boss or not in the enemy field
+    const minChance = 5; // this is the minimum chance to escape
+    const maxChance = isBoss ? 25 : 95; // this gets the max escape chance depending whether there's a boss or not in the enemy field
+    const escapeBonus = isBoss ? 2 : 10; // this is the bonus per previous escape attempt depending whether there's a boss or not in the enemy field
+    const escapeSlope = (maxChance - minChance) / speedCap; // this is just a helper const to help calculate the "slope" of the escape chance
+
+    // this will calculate the escape chance given all of the above and clamp it; this ensures the value never goes outside the range [minChance, maxChance]
+    const escapeChance = new Utils.IntegerHolder(Phaser.Math.Clamp((escapeSlope * speedRatio) + minChance + (escapeBonus * this.scene.currentBattle.escapeAttempts++), minChance, maxChance));
+
+    //const escapeChance = new Utils.IntegerHolder((((playerPokemon.getStat(Stat.SPD) * 128) / enemySpeed) + (30 * this.scene.currentBattle.escapeAttempts++)) % 256);
     applyAbAttrs(RunSuccessAbAttr, playerPokemon, null, false, escapeChance);
 
-    if (playerPokemon.randSeedInt(256) < escapeChance.value) {
+    if (playerPokemon.randSeedInt(100) < escapeChance.value) {
       this.scene.playSound("se/flee");
       this.scene.queueMessage(i18next.t("battle:runAwaySuccess"), null, true, 500);
 
