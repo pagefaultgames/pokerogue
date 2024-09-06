8926c58f1e7df7862fc9f14dfe9f6a3d03d1153e
xsn34kzx <xsn34kzx@gmail.com>
2024-09-06 13:48:02 -0400

    Update Unit Tests for X Items and Lures

--- src/test/items/double_battle_chance_booster.test.ts
+++ src/test/items/double_battle_chance_booster.test.ts
@@ -1,6 +1,14 @@
+import { Moves } from "#app/enums/moves.js";
+import { Species } from "#app/enums/species.js";
+import { DoubleBattleChanceBoosterModifier } from "#app/modifier/modifier";
 import GameManager from "#test/utils/gameManager";
 import Phaser from "phaser";
 import { afterEach, beforeAll, beforeEach, describe, expect, it } from "vitest";
+import { SPLASH_ONLY } from "../utils/testUtils";
+import { ShopCursorTarget } from "#app/enums/shop-cursor-target.js";
+import { Mode } from "#app/ui/ui.js";
+import ModifierSelectUiHandler from "#app/ui/modifier-select-ui-handler.js";
+import { Button } from "#app/enums/buttons.js";
 
 describe("Items - Double Battle Chance Boosters", () => {
   let phaserGame: Phaser.Game;
@@ -50,5 +58,48 @@ describe("Items - Double Battle Chance Boosters", () => {
     expect(enemyField.length).toBe(2);
     expect(enemyField[0].isBoss()).toBe(true);
     expect(enemyField[1].isBoss()).toBe(true);
+  }, TIMEOUT);
+
+  it("should renew how many battles are left of existing booster when picking up new booster of same tier", async() => {
+    game.override
+      .startingModifier([{ name: "LURE" }])
+      .itemRewards([{ name: "LURE" }])
+      .moveset(SPLASH_ONLY)
+      .startingLevel(200);
+
+    await game.classicMode.startBattle([
+      Species.PIKACHU
+    ]);
+
+    game.move.select(Moves.SPLASH);
+
+    await game.doKillOpponents();
+
+    await game.phaseInterceptor.to("BattleEndPhase");
+
+    const modifier = game.scene.findModifier(m => m instanceof DoubleBattleChanceBoosterModifier) as DoubleBattleChanceBoosterModifier;
+    expect(modifier.getBattleCount()).toBe(9);
+
+    // Forced LURE to spawn in the first slot with override
+    game.onNextPrompt("SelectModifierPhase", Mode.MODIFIER_SELECT, () => {
+      const handler = game.scene.ui.getHandler() as ModifierSelectUiHandler;
+      // Traverse to first modifier slot
+      handler.setCursor(0);
+      handler.setRowCursor(ShopCursorTarget.REWARDS);
+      handler.processInput(Button.ACTION);
+    }, () => game.isCurrentPhase("CommandPhase") || game.isCurrentPhase("NewBattlePhase"), true);
+
+    await game.phaseInterceptor.to("TurnInitPhase");
+
+    // Making sure only one booster is in the modifier list even after picking up another
+    let count = 0;
+    for (const m of game.scene.modifiers) {
+      if (m instanceof DoubleBattleChanceBoosterModifier) {
+        count++;
+        const modifierInstance = m as DoubleBattleChanceBoosterModifier;
+        expect(modifierInstance.getBattleCount()).toBe(modifierInstance.getMaxBattles());
+      }
+    }
+    expect(count).toBe(1);
   }, TIMEOUT);
 });
