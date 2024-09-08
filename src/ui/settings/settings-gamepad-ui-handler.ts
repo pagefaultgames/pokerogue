2bd07cb84e02d51345a4e2c85543b023a35bdec7
flx-sta <50131232+flx-sta@users.noreply.github.com>
2024-09-07 21:37:37 -0700

    fix and optimize imports (#4061)
    - remove any `.js` extension imports
    - remove unncessary dynamic imports of `modifier.ts` file. The file was being imported statically & dynamically. Made it pure static
    - increase vite chunk-size warning limit
    
    Co-authored-by: Mumble <171087428+frutescens@users.noreply.github.com>

--- src/ui/settings/settings-gamepad-ui-handler.ts
+++ src/ui/settings/settings-gamepad-ui-handler.ts
@@ -12,7 +12,7 @@ import pad_xbox360 from "#app/configs/inputs/pad_xbox360";
 import pad_dualshock from "#app/configs/inputs/pad_dualshock";
 import pad_unlicensedSNES from "#app/configs/inputs/pad_unlicensedSNES";
 import {InterfaceConfig} from "#app/inputs-controller";
-import AbstractControlSettingsUiHandler from "#app/ui/settings/abstract-control-settings-ui-handler.js";
+import AbstractControlSettingsUiHandler from "#app/ui/settings/abstract-control-settings-ui-handler";
 import {Device} from "#enums/devices";
 import {truncateString} from "#app/utils";
 import i18next from "i18next";
