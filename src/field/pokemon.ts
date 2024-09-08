4a2b67f2edbab6a50f8663146a5133143c6d09d9
frutescens <info@laptop>
2024-09-07 23:50:21 -0700

    Revert "[DOCS] adding JSDocs to `arena.ts` (#3590)"
    This reverts commit 0742efd1963b8b9e0b17797ceb3dd576a765ecf6.

--- src/field/pokemon.ts
+++ src/field/pokemon.ts
@@ -4501,7 +4501,6 @@ export interface AttackMoveResult {
 }
 
 export class PokemonSummonData {
-  /** [Atk, Def, SpAtk, SpDef, Spd, Acc, Eva] */
   public statStages: number[] = [ 0, 0, 0, 0, 0, 0, 0 ];
   public moveQueue: QueuedMove[] = [];
   public tags: BattlerTag[] = [];
