a87cf817a3193916d67e4ac7aa40335dc79e0fa2
cadi <dbtmdgns4296@naver.com>
2024-09-12 14:14:44 +0900

    annotation fix

--- src/data/move.ts
+++ src/data/move.ts
@@ -5951,7 +5951,7 @@ export class TransformAttr extends MoveEffectAttr {
       user.summonData.gender = target.getGender();
       user.summonData.fusionGender = target.getFusionGender();
 
-      // Power Trick's effect will not preserved after using Transform.
+      // Power Trick's effect will not preserved after using Transform
       user.removeTag(BattlerTagType.POWER_TRICK);
 
       // Copy all stats (except HP)
