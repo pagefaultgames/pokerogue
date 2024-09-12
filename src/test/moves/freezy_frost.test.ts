7c7408d14a3764f605c5f0a3cdb8baac235a4983
schmidtc1 <62030095+schmidtc1@users.noreply.github.com>
2024-09-11 22:02:37 -0400

    Update freezy_frost.test.ts
    Remove js file extension

--- src/test/moves/freezy_frost.test.ts
+++ src/test/moves/freezy_frost.test.ts
@@ -6,7 +6,7 @@ import GameManager from "#test/utils/gameManager";
 import Phaser from "phaser";
 import { afterEach, beforeAll, beforeEach, describe, expect, it, vi } from "vitest";
 import { allMoves } from "#app/data/move";
-import { CommandPhase } from "#app/phases/command-phase.js";
+import { CommandPhase } from "#app/phases/command-phase";
 
 describe("Moves - Freezy Frost", () => {
   let phaserGame: Phaser.Game;
