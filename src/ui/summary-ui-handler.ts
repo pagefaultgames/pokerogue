a27bd1f098ca95a266f99cf749b95ceeae8f00f8
Jannik Tappert <38758606+CodeTappert@users.noreply.github.com>
2024-09-17 12:09:11 +0200

    Have English Status Symbols show up again :)

--- src/ui/summary-ui-handler.ts
+++ src/ui/summary-ui-handler.ts
@@ -214,7 +214,7 @@ export default class SummaryUiHandler extends UiHandler {
 
     this.statusContainer.add(statusLabel);
 
-    this.status = this.scene.add.sprite(91, 4, `statuses_${i18next.resolvedLanguage}`);
+    this.status = this.scene.add.sprite(91, 4, `statuses${Utils.verifyLang(i18next.language) ? `_${i18next.language}` : ""}`);
     this.status.setOrigin(0.5, 0);
 
     this.statusContainer.add(this.status);
