60dd2a254f9f0c78261582bff70016465e1311a0
PrabbyDD <dhaliprabh@gmail.com>
2024-09-19 13:39:40 -0700

    adding immunity check

--- src/data/ability.ts
+++ src/data/ability.ts
@@ -353,6 +353,10 @@ export class TypeImmunityAbAttr extends PreDefendAbAttr {
     return false;
   }
 
+  getImmuneType(): Type | null {
+    return this.immuneType;
+  }
+
   override getCondition(): AbAttrCondition | null {
     return this.condition;
   }
