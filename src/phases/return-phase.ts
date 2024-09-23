c39efe6e264e9b4d14f8c98de43fff9180801a27
innerthunder <brandonerickson98@gmail.com>
2024-09-22 23:05:41 -0700

    Remove unneeded default arg in ReturnPhase

--- src/phases/return-phase.ts
+++ src/phases/return-phase.ts
@@ -5,7 +5,7 @@ import { SwitchSummonPhase } from "./switch-summon-phase";
 
 export class ReturnPhase extends SwitchSummonPhase {
   constructor(scene: BattleScene, fieldIndex: integer) {
-    super(scene, SwitchType.SWITCH, fieldIndex, -1, true, true);
+    super(scene, SwitchType.SWITCH, fieldIndex, -1, true);
   }
 
   switchAndSummon(): void {
