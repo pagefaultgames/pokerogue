88d0e3529e500810be3ac2c6b6cf410bb42b3b3c
Lance Xing <lancexing@gmail.com>
2024-09-24 04:49:35 -0700

    Fix Toxic to hit through semi-invlnerability when used by a Poison-type.

---
+++ src/test/moves/toxic.test.ts
@@ -0,0 +1,77 @@
+import { BerryPhase } from "#app/phases/berry-phase";
+import { Moves } from "#enums/moves";
+import { Species } from "#enums/species";
+import GameManager from "#test/utils/gameManager";
+import Phaser from "phaser";
+import { afterEach, beforeAll, beforeEach, describe, expect, it, vi } from "vitest";
+import { StatusEffect } from "#enums/status-effect";
+import { BattlerIndex } from "#app/battle";
+import { allMoves } from "#app/data/move";
+
+describe("Moves - Toxic", () => {
+  let phaserGame: Phaser.Game;
+  let game: GameManager;
+
+  beforeAll(() => {
+    phaserGame = new Phaser.Game({
+      type: Phaser.HEADLESS,
+    });
+  });
+
+  afterEach(() => {
+    game.phaseInterceptor.restoreOg();
+  });
+
+  beforeEach(() => {
+    game = new GameManager(phaserGame);
+    game.override
+      .battleType("single")
+      .moveset([Moves.TOXIC])
+      .enemySpecies(Species.MAGIKARP)
+      .enemyMoveset(Moves.SPLASH);
+  });
+
+  it("should be guaranteed to hit if user is Poison-type", async () => {
+    vi.spyOn(allMoves[Moves.TOXIC], "accuracy", "get").mockReturnValue(0);
+    await game.startBattle([Species.TOXAPEX]);
+
+    game.move.select(Moves.TOXIC);
+    await game.phaseInterceptor.to(BerryPhase, false);
+
+    expect(game.scene.getEnemyPokemon()!.status?.effect).toBe(StatusEffect.TOXIC);
+  });
+
+  it("may miss if user is not Poison-type", async () => {
+    vi.spyOn(allMoves[Moves.TOXIC], "accuracy", "get").mockReturnValue(0);
+    await game.startBattle([Species.UMBREON]);
+
+    game.move.select(Moves.TOXIC);
+    await game.phaseInterceptor.to(BerryPhase, false);
+
+    expect(game.scene.getEnemyPokemon()!.status).toBeUndefined();
+  });
+
+  it("should hit semi-invulnerable targets if user is Poison-type", async () => {
+    vi.spyOn(allMoves[Moves.TOXIC], "accuracy", "get").mockReturnValue(0);
+    game.override.enemyMoveset([Moves.FLY]);
+    await game.startBattle([Species.TOXAPEX]);
+
+    game.move.select(Moves.TOXIC);
+    await game.setTurnOrder([BattlerIndex.ENEMY, BattlerIndex.PLAYER]);
+    await game.phaseInterceptor.to(BerryPhase, false);
+
+    expect(game.scene.getEnemyPokemon()!.status?.effect).toBe(StatusEffect.TOXIC);
+  });
+
+  it("should miss semi-invulnerable targets if user is not Poison-type", async () => {
+    vi.spyOn(allMoves[Moves.TOXIC], "accuracy", "get").mockReturnValue(-1);
+    game.override.enemyMoveset([Moves.FLY]);
+    await game.startBattle([Species.UMBREON]);
+
+    game.move.select(Moves.TOXIC);
+    await game.setTurnOrder([BattlerIndex.ENEMY, BattlerIndex.PLAYER]);
+    await game.phaseInterceptor.to(BerryPhase, false);
+
+    expect(game.scene.getEnemyPokemon()!.status).toBeUndefined();
+  });
+});
