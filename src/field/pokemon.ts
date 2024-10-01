8c1adbc2f08a6888e74d7ebb31dc83efcd1f4b75
innerthunder <brandonerickson98@gmail.com>
2024-10-01 16:13:30 -0700

    fix undefined property error

--- src/field/pokemon.ts
+++ src/field/pokemon.ts
@@ -1538,9 +1538,10 @@ export default abstract class Pokemon extends Phaser.GameObjects.Container {
    * @returns The type damage multiplier, indicating the effectiveness of the move
    */
   getMoveEffectiveness(source: Pokemon, move: Move, ignoreAbility: boolean = false, simulated: boolean = true, cancelled?: Utils.BooleanHolder): TypeDamageMultiplier {
-    if (this.turnData.moveEffectiveness !== null) {
+    if (!Utils.isNullOrUndefined(this.turnData.moveEffectiveness)) {
       return this.turnData.moveEffectiveness;
     }
+
     if (move.hasAttr(TypelessAttr)) {
       return 1;
     }
