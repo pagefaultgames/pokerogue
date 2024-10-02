7473c31c779d262329924587b51ea2c8f5d6b9a1
innerthunder <168692175+innerthunder@users.noreply.github.com>
2024-10-01 18:58:56 -0700

    [P2] Fix Tera Shell to apply to all hits of multi-strike moves (#4541)
    * Apply Tera Shell to all hits for multi-hit moves
    
    * fix undefined property error
    
    * ugh
    
    * Remove obsolete bangs

--- src/field/pokemon.ts
+++ src/field/pokemon.ts
@@ -1538,6 +1538,10 @@ export default abstract class Pokemon extends Phaser.GameObjects.Container {
    * @returns The type damage multiplier, indicating the effectiveness of the move
    */
   getMoveEffectiveness(source: Pokemon, move: Move, ignoreAbility: boolean = false, simulated: boolean = true, cancelled?: Utils.BooleanHolder): TypeDamageMultiplier {
+    if (!Utils.isNullOrUndefined(this.turnData?.moveEffectiveness)) {
+      return this.turnData?.moveEffectiveness;
+    }
+
     if (move.hasAttr(TypelessAttr)) {
       return 1;
     }
@@ -5019,6 +5023,7 @@ export class PokemonTurnData {
   public order: number;
   public statStagesIncreased: boolean = false;
   public statStagesDecreased: boolean = false;
+  public moveEffectiveness: TypeDamageMultiplier | null = null;
 }
 
 export enum AiType {
