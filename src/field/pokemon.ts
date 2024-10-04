d2d47b430fb69ba0748ef174d3799f8a9f935121
innerthunder <brandonerickson98@gmail.com>
2024-10-04 13:39:06 -0700

    Implement Ion Deluge

--- src/field/pokemon.ts
+++ src/field/pokemon.ts
@@ -1527,7 +1527,7 @@ export default abstract class Pokemon extends Phaser.GameObjects.Container {
     applyMoveAttrs(VariableMoveTypeAttr, this, null, move, moveTypeHolder);
     applyPreAttackAbAttrs(MoveTypeChangeAbAttr, this, null, move, simulated, moveTypeHolder);
 
-    this.scene.arena.applyTags(ArenaTagType.PLASMA_FISTS, moveTypeHolder);
+    this.scene.arena.applyTags(ArenaTagType.ION_DELUGE, moveTypeHolder);
     if (this.getTag(BattlerTagType.ELECTRIFIED)) {
       moveTypeHolder.value = Type.ELECTRIC;
     }
