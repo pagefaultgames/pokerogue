47572f8a36b6a7b08a96bc99ea74be67399bfc59
Lugiad <2070109+Adri1@users.noreply.github.com>
2024-10-07 18:39:44 +0200

    Update i18n.ts

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -151,7 +151,7 @@ export async function initI18n(): Promise<void> {
   i18next.use(new KoreanPostpositionProcessor());
   await i18next.init({
     fallbackLng: "en",
-    supportedLngs: ["en", "es", "fr", "it", "de", "zh-CN", "zh-TW", "pt-BR", "ko", "ja", "ca-ES", "da"],
+    supportedLngs: [ "en", "es", "fr", "it", "de", "zh-CN", "zh-TW", "pt-BR", "ko", "ja", "ca-ES", "da" ],
     backend: {
       loadPath(lng: string, [ ns ]: string[]) {
         let fileName: string;
