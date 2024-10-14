36b981500c06a92057769cd221bad1a74a16452e
innerthunder <brandonerickson98@gmail.com>
2024-10-13 23:06:08 -0700

    back to `any` again lol

--- src/data/arena-tag.ts
+++ src/data/arena-tag.ts
@@ -36,7 +36,7 @@ export abstract class ArenaTag {
     public side: ArenaTagSide = ArenaTagSide.BOTH
   ) {}
 
-  apply(arena: Arena, simulated: boolean, ...args: unknown[]): boolean {
+  apply(arena: Arena, simulated: boolean, ...args: any[]): boolean {
     return true;
   }
 
