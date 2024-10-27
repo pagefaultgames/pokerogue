b8e87eb4885acb3f057e9e89c4a370ea6228ac27
Madmadness65 <blaze.the.fireman@gmail.com>
2024-10-27 02:56:16 -0500

    Add missing challenge data entries

--- src/system/challenge-data.ts
+++ src/system/challenge-data.ts
@@ -4,11 +4,13 @@ export default class ChallengeData {
   public id: integer;
   public value: integer;
   public severity: integer;
+  public rand: integer;
 
   constructor(source: Challenge | any) {
     this.id = source.id;
     this.value = source.value;
     this.severity = source.severity;
+    this.rand = source.rand;
   }
 
   toChallenge(): Challenge {
