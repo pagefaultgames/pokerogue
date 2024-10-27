87e869c02cf96370f7405a366f8b339f1b446bcf
muscode13 <mmustafafareed@hotmail.com>
2024-10-27 02:49:07 -0600

    added documentation for tag

--- src/data/arena-tag.ts
+++ src/data/arena-tag.ts
@@ -1202,6 +1202,13 @@ class GrassWaterPledgeTag extends ArenaTag {
   }
 }
 
+/**
+ * Arena Tag class for {@link https://bulbapedia.bulbagarden.net/wiki/Fairy_Lock_(move) Fairy Lock}.
+ * Fairy Lock prevents all Pokémon (except Ghost types) on the field from switching out or
+ * fleeing during their next turn.
+ * If a Pokémon that's on the field when Fairy Lock is used goes on to faint later in the same turn,
+ * the Pokémon that replaces it will still be unable to switch out in the following turn.
+ */
 export class FairyLockTag extends ArenaTag {
   constructor(turnCount: number, sourceId: number) {
     super(ArenaTagType.FAIRY_LOCK, turnCount, Moves.FAIRY_LOCK, sourceId);
