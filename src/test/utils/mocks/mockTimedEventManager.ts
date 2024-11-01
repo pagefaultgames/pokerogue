ef7f55ca10d992fce9e4ca389467a9f3a2014d85
Moka <millennium.stitcher@gmail.com>
2024-11-02 00:25:36 +0100

    disable timed events in tests

---
+++ src/test/utils/mocks/mockTimedEventManager.ts
@@ -0,0 +1,21 @@
+import { TimedEventManager } from "#app/timed-event-manager";
+
+/** Mock TimedEventManager so that ongoing events don't impact tests */
+export class MockTimedEventManager extends TimedEventManager {
+  constructor() {
+    super();
+  }
+
+  override activeEvent() {
+    return undefined;
+  }
+  override isEventActive(): boolean {
+    return false;
+  }
+  override getFriendshipMultiplier(): number {
+    return 1;
+  }
+  override getShinyMultiplier(): number {
+    return 1;
+  }
+}
