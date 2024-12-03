0ccb9df405e0e05178c1401831c7d02207cfd118
PigeonBar <56974298+PigeonBar@users.noreply.github.com>
2024-12-02 22:01:26 -0500

    Fix Heavy Rain check to account for weather suppression
    Co-authored-by: NightKev <34855794+DayKev@users.noreply.github.com>

--- src/data/battler-tags.ts
+++ src/data/battler-tags.ts
@@ -885,7 +885,8 @@ export class PowderTag extends BattlerTag {
       const movePhase = pokemon.scene.getCurrentPhase();
       if (movePhase instanceof MovePhase) {
         const move = movePhase.move.getMove();
-        if (pokemon.getMoveType(move) === Type.FIRE && pokemon.scene.arena.weather?.weatherType !== WeatherType.HEAVY_RAIN) {
+        const weather = pokemon.scene.arena.weather;
+        if (pokemon.getMoveType(move) === Type.FIRE && !(weather && weather.weatherType === WeatherType.HEAVY_RAIN && !weather.isEffectSuppressed(pokemon.scene))) {
           movePhase.fail();
 
           pokemon.scene.unshiftPhase(new CommonAnimPhase(pokemon.scene, pokemon.getBattlerIndex(), pokemon.getBattlerIndex(), CommonAnim.POWDER));
