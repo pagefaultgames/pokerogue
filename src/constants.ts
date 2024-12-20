29a079cfd3f188c3975ab0eff1ae9f73e44e139d
NightKev <34855794+DayKev@users.noreply.github.com>
2024-12-19 16:04:12 -0800

    Revert "[UI/UX] Refactor and enable seasonal splash messages (#5009)" (#5014)
    This reverts commit 806585f1a69a8059444c00aa87ca5ecedc71cf9b.

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@
 export const PLAYER_PARTY_MAX_SIZE: number = 6;
 
 /** Whether to use seasonal splash messages in general */
-export const USE_SEASONAL_SPLASH_MESSAGES: boolean = true;
+export const USE_SEASONAL_SPLASH_MESSAGES: boolean = false;
 
 /** Name of the session ID cookie */
 export const SESSION_ID_COOKIE_NAME: string = "pokerogue_sessionId";
