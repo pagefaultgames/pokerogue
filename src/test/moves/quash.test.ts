88724b83eb7fb3b147571487a6cd56f06397bcbb
Dean <me@deann.dev>
2024-12-27 13:01:07 -0800

    Basic test case

---
+++ src/test/moves/quash.test.ts
@@ -0,0 +1,49 @@
+import { Species } from "#enums/species";
+import { Moves } from "#enums/moves";
+import { Abilities } from "#app/enums/abilities";
+import { BattlerIndex } from "#app/battle";
+import { WeatherType } from "#enums/weather-type";
+import GameManager from "#test/utils/gameManager";
+import Phaser from "phaser";
+import { describe, beforeAll, afterEach, beforeEach, it, expect } from "vitest";
+
+describe("Moves - Quash", () => {
+  let phaserGame: Phaser.Game;
+  let game: GameManager;
+
+  beforeAll(() => {
+    phaserGame = new Phaser.Game({
+      type: Phaser.HEADLESS,
+    });
+  });
+
+  afterEach(() => {
+    game.phaseInterceptor.restoreOg();
+  });
+
+  beforeEach(() => {
+    game = new GameManager(phaserGame);
+    game.override
+      .battleType("double")
+      .enemyLevel(1)
+      .enemySpecies(Species.SLOWPOKE)
+      .enemyAbility(Abilities.BALL_FETCH)
+      .enemyMoveset([ Moves.RAIN_DANCE, Moves.SPLASH ])
+      .ability(Abilities.BALL_FETCH)
+      .moveset([ Moves.QUASH, Moves.SUNNY_DAY, Moves.SPLASH ]);
+  });
+
+  it("makes the target move last in a turn, ignoring priority", async () => {
+    await game.classicMode.startBattle([ Species.ACCELGOR, Species.RATTATA ]);
+
+    game.move.select(Moves.QUASH, 0, BattlerIndex.PLAYER_2);
+    game.move.select(Moves.SUNNY_DAY, 1);
+    await game.forceEnemyMove(Moves.SPLASH);
+    await game.forceEnemyMove(Moves.RAIN_DANCE);
+
+    await game.phaseInterceptor.to("TurnEndPhase", false);
+    // will be sunny if player_2 moved last because of quash, rainy otherwise
+    expect(game.scene.arena.weather?.weatherType).toBe(WeatherType.SUNNY);
+  });
+
+});
