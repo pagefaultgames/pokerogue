5f1e4b23a40cc2925dd6ea8bf2f32c887c58c631
Wlowscha <54003515+Wlowscha@users.noreply.github.com>
2024-12-27 19:37:30 +0100

    Adding options to see mons with only one or only two cost reductions

--- src/ui/dropdown.ts
+++ src/ui/dropdown.ts
@@ -8,7 +8,9 @@ export enum DropDownState {
     ON = 0,
     OFF = 1,
     EXCLUDE = 2,
-    UNLOCKABLE = 3
+    UNLOCKABLE = 3,
+    ONE = 4,
+    TWO = 5
 }
 
 export enum DropDownType {
@@ -56,6 +58,8 @@ export class DropDownOption extends Phaser.GameObjects.Container {
   private offColor = 0x272727;
   private excludeColor = 0xff5555;
   private unlockableColor = 0xffff00;
+  private oneColor = 0x33bbff;
+  private twoColor = 0x33bbff;
 
   constructor(scene: SceneBase, val: any, labels: DropDownLabel | DropDownLabel[]) {
     super(scene);
@@ -126,6 +130,12 @@ export class DropDownOption extends Phaser.GameObjects.Container {
         break;
       case DropDownState.UNLOCKABLE:
         this.toggle.setTint(this.unlockableColor);
+        break;
+      case DropDownState.ONE:
+        this.toggle.setTint(this.oneColor);
+        break;
+      case DropDownState.TWO:
+        this.toggle.setTint(this.twoColor);
         break;
     }
   }
