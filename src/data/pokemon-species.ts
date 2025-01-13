ad530bdcb7a67799041b1f2edc1b7a490ba1124e
Jimmybald1 <147992650+IBBCalc@users.noreply.github.com>
2025-01-13 10:36:27 +0100

    [Bug] Fix #5034 removed unnecessary caught data block for certain forms

--- src/data/pokemon-species.ts
+++ src/data/pokemon-species.ts
@@ -948,19 +948,6 @@ export class PokemonForm extends PokemonSpeciesForm {
   }
 }
 
-export const noStarterFormKeys: string[] = [
-  SpeciesFormKey.MEGA,
-  SpeciesFormKey.MEGA_X,
-  SpeciesFormKey.MEGA_Y,
-  SpeciesFormKey.PRIMAL,
-  SpeciesFormKey.ORIGIN,
-  SpeciesFormKey.THERIAN,
-  SpeciesFormKey.GIGANTAMAX,
-  SpeciesFormKey.GIGANTAMAX_RAPID,
-  SpeciesFormKey.GIGANTAMAX_SINGLE,
-  SpeciesFormKey.ETERNAMAX
-].map(k => k.toString());
-
 /**
 * Method to get the daily list of starters with Pokerus.
 * @returns A list of starters with Pokerus
