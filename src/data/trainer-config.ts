39b6043e46aaf95c9e0ff9a924f55d58011dc83e
Blitzy <118096277+Blitz425@users.noreply.github.com>
2025-01-22 13:26:44 -0600

    Fix formatting

--- src/data/trainer-config.ts
+++ src/data/trainer-config.ts
@@ -1976,11 +1976,10 @@ export const trainerConfigs: TrainerConfigs = {
     .setPartyMemberFunc(2, getRandomPartyMemberFunc([ Species.OGERPON ], TrainerSlot.TRAINER, true, p => {
       p.formIndex = Utils.randSeedInt(4); // Random Ogerpon Mask
       p.generateAndPopulateMoveset();
+      p.pokeball = PokeballType.ULTRA_BALL;
       if (!p.moveset.some(move => move.moveId === Moves.IVY_CUDGEL)) { // Check if Ivy Cudgel is in the moveset, if not, replace the first move with Ivy Cudgel.
         p.moveset[0] = new PokemonMove(Moves.IVY_CUDGEL);
       }
-    }))
-      p.pokeball = PokeballType.ULTRA_BALL;
     }))
     .setPartyMemberFunc(3, getRandomPartyMemberFunc([ Species.URSALUNA, Species.BLOODMOON_URSALUNA ], TrainerSlot.TRAINER, true, p => {
       p.generateAndPopulateMoveset();
@@ -2349,11 +2348,10 @@ export const trainerConfigs: TrainerConfigs = {
     .setPartyMemberFunc(2, getRandomPartyMemberFunc([ Species.SILVALLY ], TrainerSlot.TRAINER, true, p => {
       p.formIndex = Utils.randSeedInt(18); // Random Silvally Form
       p.generateAndPopulateMoveset();
+      p.pokeball = PokeballType.ROGUE_BALL;
       if (!p.moveset.some(move => move.moveId === Moves.MULTI_ATTACK)) { // // Check if Multi Attack is in the moveset, if not, replace the first move with Multi Attack.
         p.moveset[0] = new PokemonMove(Moves.MULTI_ATTACK);
       }
-    }))
-      p.pokeball = PokeballType.ROGUE_BALL;
     }))
     .setPartyMemberFunc(3, getRandomPartyMemberFunc([ Species.PHEROMOSA ], TrainerSlot.TRAINER, true, p => {
       p.generateAndPopulateMoveset();
