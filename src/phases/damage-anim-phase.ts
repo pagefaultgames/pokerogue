ae4e94254fa1e2b3c5ee19deedb90315b1389154
Christopher Schmidt <schmidtc086@outlook.com>
2025-01-25 20:59:37 -0500

    Adds HitResult.SELF to damage sound effect switch

--- src/phases/damage-anim-phase.ts
+++ src/phases/damage-anim-phase.ts
@@ -42,6 +42,7 @@ export class DamageAnimPhase extends PokemonPhase {
   applyDamage() {
     switch (this.damageResult) {
       case HitResult.EFFECTIVE:
+      case HitResult.SELF:
         globalScene.playSound("se/hit");
         break;
       case HitResult.SUPER_EFFECTIVE:
