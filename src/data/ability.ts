cd732ef27cc06f0261bfaa61ec71deecc76131c0
Sirz Benjie <142067137+SirzBenjie@users.noreply.github.com>
2025-01-30 18:38:59 -0600

    Accept suggested change
    Co-authored-by: NightKev <34855794+DayKev@users.noreply.github.com>

--- src/data/ability.ts
+++ src/data/ability.ts
@@ -5619,8 +5619,9 @@ export function applyStatMultiplierAbAttrs(
  * @param args - unused
  */
 export function applyAllyStatMultiplierAbAttrs(attrType: Constructor<AllyStatMultiplierAbAttr>,
-  pokemon: Pokemon, stat: BattleStat, statValue: Utils.NumberHolder, simulated: boolean = false, checkedPokemon, ignore, ...args: any[]): void {
-  return applyAbAttrsInternal<AllyStatMultiplierAbAttr>(attrType, pokemon, (attr, passive) => attr.applyAllyStat(pokemon, passive, simulated, stat, statValue, checkedPokemon, ignore, args), args);
+  pokemon: Pokemon, stat: BattleStat, statValue: Utils.NumberHolder, simulated: boolean = false, checkedPokemon: Pokemon, ignoreAbility: boolean, ...args: any[]
+): void {
+  return applyAbAttrsInternal<AllyStatMultiplierAbAttr>(attrType, pokemon, (attr, passive) => attr.applyAllyStat(pokemon, passive, simulated, stat, statValue, checkedPokemon, ignoreAbility, args), args);
 }
 
 export function applyPostSetStatusAbAttrs(
