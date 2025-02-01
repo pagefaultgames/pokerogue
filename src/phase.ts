c55e60ea0cfbe74a27a1ce6769bb25e04e12d1c3
Dean <me@deann.dev>
2025-01-31 23:26:52 -0800

    Remove abilityBar reference from base Phase class

--- src/phase.ts
+++ src/phase.ts
@@ -2,9 +2,7 @@ import { globalScene } from "#app/global-scene";
 
 export class Phase {
   start() {
-    if (globalScene.abilityBar.shown) {
-      globalScene.abilityBar.resetAutoHideTimer();
-    }
+
   }
 
   end() {
