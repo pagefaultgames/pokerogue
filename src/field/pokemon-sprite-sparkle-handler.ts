820d51bba0e17698ded14d7e2af0ed27402d415b
Xavion3 <xavion333@gmail.com>
2025-02-02 04:30:47 +1100

    Experimental sparkle fix

--- src/field/pokemon-sprite-sparkle-handler.ts
+++ src/field/pokemon-sprite-sparkle-handler.ts
@@ -27,7 +27,7 @@ export default class PokemonSpriteSparkleHandler {
       if (!s.visible || (s.parentContainer instanceof Pokemon && !s.parentContainer.parentContainer)) {
         continue;
       }
-      if (s.parentContainer instanceof Pokemon && !(s.parentContainer as Pokemon).isTerastallized) {
+      if (!(s.parentContainer instanceof Pokemon) || !(s.parentContainer as Pokemon).isTerastallized) {
         continue;
       }
       const pokemon = s.parentContainer instanceof Pokemon ? s.parentContainer as Pokemon : null;
