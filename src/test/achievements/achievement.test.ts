d1dd366b8dcd95f9e82c78c14c19e5809a360266
Madmadness65 <59298170+Madmadness65@users.noreply.github.com>
2025-02-16 14:44:13 -0600

    Remove duplicate NumberHolder

--- src/test/achievements/achievement.test.ts
+++ src/test/achievements/achievement.test.ts
@@ -1,6 +1,6 @@
 import { TurnHeldItemTransferModifier } from "#app/modifier/modifier";
 import { Achv, AchvTier, DamageAchv, HealAchv, LevelAchv, ModifierAchv, MoneyAchv, RibbonAchv, achvs } from "#app/system/achv";
-import { NumberHolder, NumberHolder } from "#app/utils";
+import { NumberHolder } from "#app/utils";
 import GameManager from "#test/utils/gameManager";
 import Phaser from "phaser";
 import { afterEach, beforeAll, beforeEach, describe, expect, it, vi } from "vitest";
