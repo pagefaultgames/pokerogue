Despair-Games/poketernity

Added mockContext and mockCanvas objects

test/testUtils/mocks/mockCanvas.ts
@@ -1,0 +1,11 @@
+import { mockContext } from "#test/testUtils/mocks/mockContext";
+
+/**
+ * A minimal stub object to mock HTMLCanvasElement
+ */
+export const mockCanvas: any = {
+  width: 0,
+  getContext() {
+    return mockContext;
+  },
+};

test/testUtils/mocks/mockContext.ts
@@ -1,0 +1,15 @@
+import { mockCanvas } from "#test/testUtils/mocks/mockCanvas";
+
+/**
+ * A minimal stub object to mock CanvasRenderingContext2D
+ */
+export const mockContext: any = {
+  font: "",
+  //@ts-ignore
+  measureText: () => new TextMetrics(""),
+  save: () => {},
+  scale: () => {},
+  clearRect: () => {},
+  canvas: mockCanvas,
+  restore: () => {},
+};

pagefaultgames/pokerogue

test/utils/mocks/mockCanvas.ts
@@ -1,0 +1,11 @@
+import { mockContext } from "#test/utils/mocks/mockContext";
+
+/**
+ * A minimal stub object to mock HTMLCanvasElement
+ */
+export const mockCanvas: any = {
+  width: 0,
+  getContext() {
+    return mockContext;
+  },
+};

test/utils/mocks/mockContext.ts
@@ -1,0 +1,15 @@
+import { mockCanvas } from "#test/utils/mocks/mockCanvas";
+
+/**
+ * A minimal stub object to mock CanvasRenderingContext2D
+ */
+export const mockContext: any = {
+  font: "",
+  //@ts-ignore
+  measureText: () => new TextMetrics(""),
+  save: () => {},
+  scale: () => {},
+  clearRect: () => {},
+  canvas: mockCanvas,
+  restore: () => {},
+};