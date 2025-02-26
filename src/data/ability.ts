f5963c87c422bd2a6074a10ebc2f510a865cdd16
Sirz Benjie <142067137+SirzBenjie@users.noreply.github.com>
2025-02-25 21:00:04 -0600

    Revert "[Bug] Fix canApplyAbility check #5413"
    This reverts commit 2cc0ac90f88ac6cc1e79a439a4cdf4392d1a650a.

--- src/data/ability.ts
+++ src/data/ability.ts
@@ -5152,10 +5152,6 @@ function applySingleAbAttrs<TAttr extends AbAttr>(
   showAbilityInstant: boolean = false,
   messages: string[] = []
 ) {
-  if (!pokemon?.canApplyAbility(passive) || (passive && (pokemon.getPassiveAbility().id === pokemon.getAbility().id))) {
-    return;
-  }
-
   const ability = passive ? pokemon.getPassiveAbility() : pokemon.getAbility();
   if (gainedMidTurn && ability.getAttrs(attrType).some(attr => attr instanceof PostSummonAbAttr && !attr.shouldActivateOnGain())) {
     return;
@@ -5449,10 +5445,12 @@ function applyAbAttrsInternal<TAttr extends AbAttr>(
   gainedMidTurn: boolean = false
 ) {
   for (const passive of [ false, true ]) {
-    if (pokemon) {
-      applySingleAbAttrs(pokemon, passive, attrType, applyFunc, args, gainedMidTurn, simulated, showAbilityInstant, messages);
-      globalScene.clearPhaseQueueSplice();
+    if (!pokemon?.canApplyAbility(passive) || (passive && (pokemon.getPassiveAbility().id === pokemon.getAbility().id))) {
+      continue;
     }
+
+    applySingleAbAttrs(pokemon, passive, attrType, applyFunc, args, gainedMidTurn, simulated, showAbilityInstant, messages);
+    globalScene.clearPhaseQueueSplice();
   }
 }
 
