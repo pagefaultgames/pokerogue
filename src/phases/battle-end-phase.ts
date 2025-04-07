17a56cc6c1d2dfebfd6fbb0045fb742b421ccb7b
NightKev <34855794+DayKev@users.noreply.github.com>
2025-04-07 15:57:50 -0700

    Move `try/catch` inside `for` loop
    Co-authored-by: Sirz Benjie <142067137+SirzBenjie@users.noreply.github.com>

--- src/phases/battle-end-phase.ts
+++ src/phases/battle-end-phase.ts
@@ -73,10 +73,12 @@ export class BattleEndPhase extends BattlePhase {
     }
 
     globalScene.clearEnemyHeldItemModifiers();
-    try {
-      globalScene.getEnemyParty().forEach(p => p.destroy());
-    } catch {
-      console.warn("Unable to destroy stale pokemon objects in BattleEndPhase.");
+    for (const p of globalScene.getEnemyParty()) {
+      try {
+        p.destroy();
+      } catch {
+        console.warn("Unable to destroy stale pokemon objects in BattleEndPhase.");
+      }
     }
 
     const lapsingModifiers = globalScene.findModifiers(
