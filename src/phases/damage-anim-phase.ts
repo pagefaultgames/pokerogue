f67813ec89e227cfc02f108e8c26ac217722ded1
Sirz Benjie <142067137+SirzBenjie@users.noreply.github.com>
2025-04-10 21:49:36 -0500

    Move DamageResult to its own file

--- src/phases/damage-anim-phase.ts
+++ src/phases/damage-anim-phase.ts
@@ -1,7 +1,7 @@
 import { globalScene } from "#app/global-scene";
 import type { BattlerIndex } from "#app/battle";
 import { BattleSpec } from "#enums/battle-spec";
-import type { DamageResult } from "#app/field/pokemon";
+import type { DamageResult } from "#app/@types/damage-result";
 import { HitResult } from "#enums/hit-result";
 import { fixedInt } from "#app/utils";
 import { PokemonPhase } from "#app/phases/pokemon-phase";
