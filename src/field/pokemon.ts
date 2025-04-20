57fc651696c419852f74016e0529c2eda9a017f6
zaccie <zaccie.adams@gmail.com>
2025-04-21 00:40:43 +1200

    order of operations in creating boss battleInfo
    fixed a bug where because of an order of operations in this file it affected the position (ignored the position) of the boss life value set in battle-info.ts (around line 562)

--- src/field/pokemon.ts
+++ src/field/pokemon.ts
@@ -7157,8 +7157,8 @@ export class EnemyPokemon extends Pokemon {
   initBattleInfo(): void {
     if (!this.battleInfo) {
       this.battleInfo = new EnemyBattleInfo();
+            this.battleInfo.initInfo(this);
       this.battleInfo.updateBossSegments(this);
-      this.battleInfo.initInfo(this);
     } else {
       this.battleInfo.updateBossSegments(this);
     }
