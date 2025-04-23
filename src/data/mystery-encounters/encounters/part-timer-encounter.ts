110fd2f0a1888787f7b267f71b233ed1d1a63675
Sirz Benjie <142067137+SirzBenjie@users.noreply.github.com>
2025-04-22 19:10:27 -0500

    [Refactor][Move] Refactor move effect phase (#5678)
    * Add enum for hit check result
    
    Co-authored-by: innerthunder <brandonerickson98@gmail.com>
    
    * Refactor parameter list for pokemon#getBaseDamage and pokemon#getAttackDamage
    
    * Rewrite move phase
    
    Co-authored-by: innerthunder <brandonerickson98@gmail.com>
    
    * Update tests to reflect move effect phase changes
    
    Co-authored-by: innerthunder <brandonerickson98@gmail.com>
    
    * Fix pluck / bug bite
    
    Co-authored-by: innerthunder <brandonerickson98@gmail.com>
    
    * Fix reviver seed ohko, remove leftover dead code
    
    Co-authored-by: innerthunder <brandonerickson98@gmail.com>
    
    * Cleanup jsdoc comments
    
    * Remove hitsSubstitute check from postDefend abilities
    
    * Fix improper i18nkey in moveEffectPhase#applyToTargets
    
    * Cleanup comments
    
    * Fix type issue with substitute test
    
    * Move MYSTERY_ENCOUNTER_WAVES to constants.ts
    
    * Update linkcode in damageparams to use proper tsdoc syntax
    
    ---------
    
    Co-authored-by: innerthunder <brandonerickson98@gmail.com>

--- src/data/mystery-encounters/encounters/part-timer-encounter.ts
+++ src/data/mystery-encounters/encounters/part-timer-encounter.ts
@@ -20,7 +20,7 @@ import { showEncounterDialogue, showEncounterText } from "#app/data/mystery-enco
 import i18next from "i18next";
 import type { PlayerPokemon } from "#app/field/pokemon";
 import type Pokemon from "#app/field/pokemon";
-import { CLASSIC_MODE_MYSTERY_ENCOUNTER_WAVES } from "#app/game-mode";
+import { CLASSIC_MODE_MYSTERY_ENCOUNTER_WAVES } from "#app/constants";
 import { isPokemonValidForEncounterOptionSelection } from "#app/data/mystery-encounters/utils/encounter-pokemon-utils";
 
 /** the i18n namespace for the encounter */
