262b4f28d8c4a6a03bf28b23af8f3cbd6a43d2b9
Sirz Benjie <142067137+SirzBenjie@users.noreply.github.com>
2025-04-23 09:46:53 -0500

    Make statOrder specific to subclass

--- src/ui/battle-info/player-battle-info.ts
+++ src/ui/battle-info/player-battle-info.ts
@@ -1,17 +1,18 @@
 import { globalScene } from "#app/global-scene";
+import { Stat } from "#enums/stat";
 import BattleInfo from "./battle-info";
 
 export class PlayerBattleInfo extends BattleInfo {
+  override get statOrder(): Stat[] {
+    return [Stat.ATK, Stat.DEF, Stat.SPATK, Stat.SPDEF, Stat.ACC, Stat.EVA, Stat.SPD];
+  }
   constructor() {
     super(Math.floor(globalScene.game.canvas.width / 6) - 10, -72, true);
 
-    this.hpNumbersContainer = globalScene.add.container(-15, 10);
-    this.hpNumbersContainer.setName("container_hp");
+    this.hpNumbersContainer = globalScene.add.container(-15, 10).setName("container_hp");
     this.add(this.hpNumbersContainer);
 
-    const expBar = globalScene.add.image(-98, 18, "overlay_exp");
-    expBar.setName("overlay_exp");
-    expBar.setOrigin(0);
+    const expBar = globalScene.add.image(-98, 18, "overlay_exp").setName("overlay_exp").setOrigin(0);
     this.add(expBar);
 
     const expMaskRect = globalScene.make
