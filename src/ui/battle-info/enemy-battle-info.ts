ec3a54273dd7373068ddcd838b9f128bbec92183
Sirz Benjie <142067137+SirzBenjie@users.noreply.github.com>
2025-04-23 09:09:39 -0500

    Move subclass specific parts of constructor to subclass constructor

--- src/ui/battle-info/enemy-battle-info.ts
+++ src/ui/battle-info/enemy-battle-info.ts
@@ -1,8 +1,42 @@
 import BattleInfo from "./battle-info";
+import { globalScene } from "#app/global-scene";
+import BattleFlyout from "../battle-flyout";
+import { addTextObject, TextStyle } from "#app/ui/text";
+import { addWindow, WindowVariant } from "#app/ui/ui-theme";
 
 export class EnemyBattleInfo extends BattleInfo {
   constructor() {
     super(140, -141, false);
+
+    this.ownedIcon = globalScene.add.sprite(0, 0, "icon_owned");
+    this.ownedIcon.setName("icon_owned");
+    this.ownedIcon.setVisible(false);
+    this.ownedIcon.setOrigin(0, 0);
+    this.ownedIcon.setPositionRelative(this.nameText, 0, 11.75);
+    this.add(this.ownedIcon);
+
+    this.championRibbon = globalScene.add.sprite(0, 0, "champion_ribbon");
+    this.championRibbon.setName("icon_champion_ribbon");
+    this.championRibbon.setVisible(false);
+    this.championRibbon.setOrigin(0, 0);
+    this.championRibbon.setPositionRelative(this.nameText, 8, 11.75);
+    this.add(this.championRibbon);
+
+    this.flyoutMenu = new BattleFlyout(this.player);
+    this.add(this.flyoutMenu);
+
+    this.moveBelow<Phaser.GameObjects.GameObject>(this.flyoutMenu, this.box);
+
+    this.effectivenessContainer = globalScene.add.container(0, 0);
+    this.effectivenessContainer.setPositionRelative(this.type1Icon, 22, 4);
+    this.effectivenessContainer.setVisible(false);
+    this.add(this.effectivenessContainer);
+
+    this.effectivenessText = addTextObject(5, 4.5, "", TextStyle.BATTLE_INFO);
+    this.effectivenessWindow = addWindow(0, 0, 0, 20, undefined, false, undefined, undefined, WindowVariant.XTHIN);
+
+    this.effectivenessContainer.add(this.effectivenessWindow);
+    this.effectivenessContainer.add(this.effectivenessText);
   }
 
   setMini(_mini: boolean): void {} // Always mini
--- src/ui/battle-info/player-battle-info.ts
+++ src/ui/battle-info/player-battle-info.ts
@@ -4,5 +4,27 @@ import BattleInfo from "./battle-info";
 export class PlayerBattleInfo extends BattleInfo {
   constructor() {
     super(Math.floor(globalScene.game.canvas.width / 6) - 10, -72, true);
+
+    this.hpNumbersContainer = globalScene.add.container(-15, 10);
+    this.hpNumbersContainer.setName("container_hp");
+    this.add(this.hpNumbersContainer);
+
+    const expBar = globalScene.add.image(-98, 18, "overlay_exp");
+    expBar.setName("overlay_exp");
+    expBar.setOrigin(0);
+    this.add(expBar);
+
+    const expMaskRect = globalScene.make.graphics({});
+    expMaskRect.setScale(6);
+    expMaskRect.fillStyle(0xffffff);
+    expMaskRect.beginPath();
+    expMaskRect.fillRect(127, 126, 85, 2);
+
+    const expMask = expMaskRect.createGeometryMask();
+
+    expBar.setMask(expMask);
+
+    this.expBar = expBar;
+    this.expMaskRect = expMaskRect;
   }
 }
