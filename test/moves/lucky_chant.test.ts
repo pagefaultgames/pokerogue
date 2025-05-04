4b2ad66bc78e6a94ad44f3e9143cf2790078767e
Bertie690 <taylormw163@gmail.com>
2025-05-04 08:51:33 -0400

    Fixed failing tests

--- test/moves/lucky_chant.test.ts
+++ test/moves/lucky_chant.test.ts
@@ -28,51 +28,54 @@ describe("Moves - Lucky Chant", () => {
       .moveset([Moves.LUCKY_CHANT, Moves.SPLASH, Moves.FOLLOW_ME])
       .enemySpecies(Species.SNORLAX)
       .enemyAbility(Abilities.INSOMNIA)
-      .enemyMoveset(Moves.FLOWER_TRICK)
+      .enemyMoveset(Moves.TACKLE)
       .startingLevel(100)
       .enemyLevel(100);
   });
 
   it("should prevent random critical hits from moves", async () => {
-    game.override.enemyMoveset(Moves.TACKLE).criticalHits(true);
+    game.override.criticalHits(true);
     await game.classicMode.startBattle([Species.CHARIZARD]);
 
     const charizard = game.scene.getPlayerPokemon()!;
+    expect(charizard).toBeDefined();
     const critSpy = vi.spyOn(charizard, "getCriticalHitResult"); // called on the defender (ie player)
 
     game.move.select(Moves.SPLASH);
     await game.phaseInterceptor.to("TurnEndPhase");
 
     expect(critSpy).toHaveLastReturnedWith(true);
-    const firstTurnDamage = charizard.getInverseHp();
+    const firstTurnDamage = charizard.turnData.damageTaken;
 
     game.move.select(Moves.LUCKY_CHANT);
     await game.phaseInterceptor.to("TurnEndPhase");
 
     expect(critSpy).toHaveLastReturnedWith(false);
-    const secondTurnDamage = charizard.getInverseHp() - firstTurnDamage;
+    const secondTurnDamage = charizard.turnData.damageTaken;
     expect(secondTurnDamage).toBeLessThan(firstTurnDamage);
   });
 
   it("should prevent guaranteed critical hits from moves", async () => {
+    game.override.enemyMoveset(Moves.FLOWER_TRICK);
     await game.classicMode.startBattle([Species.CHARIZARD]);
 
-    const playerPokemon = game.scene.getPlayerPokemon()!;
+    const charizard = game.scene.getPlayerPokemon()!;
+    expect(charizard).toBeDefined();
 
     game.move.select(Moves.SPLASH);
     await game.phaseInterceptor.to("TurnEndPhase");
 
-    const firstTurnDamage = playerPokemon.getInverseHp();
+    const firstTurnDamage = charizard.getInverseHp();
 
     game.move.select(Moves.LUCKY_CHANT);
     await game.phaseInterceptor.to("TurnEndPhase");
 
-    const secondTurnDamage = playerPokemon.getInverseHp() - firstTurnDamage;
+    const secondTurnDamage = charizard.getInverseHp() - firstTurnDamage;
     expect(secondTurnDamage).toBeLessThan(firstTurnDamage);
   });
 
   it("should prevent critical hits against the user's ally", async () => {
-    game.override.battleStyle("double");
+    game.override.battleStyle("double").criticalHits(true);
 
     await game.classicMode.startBattle([Species.CHARIZARD, Species.BLASTOISE]);
 
@@ -83,36 +86,35 @@ describe("Moves - Lucky Chant", () => {
     game.move.select(Moves.SPLASH, BattlerIndex.PLAYER_2);
     await game.phaseInterceptor.to("TurnEndPhase");
 
-    const firstTurnDamage = charizard.getInverseHp();
+    const firstTurnDamage = charizard.turnData.damageTaken;
 
     game.move.select(Moves.FOLLOW_ME, BattlerIndex.PLAYER);
     game.move.select(Moves.LUCKY_CHANT, BattlerIndex.PLAYER_2);
 
     await game.phaseInterceptor.to("TurnEndPhase");
 
-    const secondTurnDamage = charizard.getInverseHp() - firstTurnDamage;
+    const secondTurnDamage = charizard.turnData.damageTaken;
     expect(secondTurnDamage).toBeLessThan(firstTurnDamage);
   });
 
   it("should prevent critical hits from field effects", async () => {
-    game.override.enemyMoveset(Moves.TACKLE);
-
     await game.classicMode.startBattle([Species.CHARIZARD]);
 
-    const playerPokemon = game.scene.getPlayerPokemon()!;
-    const enemyPokemon = game.scene.getEnemyPokemon()!;
+    const charizard = game.scene.getPlayerPokemon()!;
+    expect(charizard).toBeDefined();
 
-    enemyPokemon.addTag(BattlerTagType.ALWAYS_CRIT, 2, Moves.NONE, 0);
+    const snorlax = game.scene.getEnemyPokemon()!;
+    snorlax.addTag(BattlerTagType.ALWAYS_CRIT, 2, Moves.NONE, 0);
 
     game.move.select(Moves.SPLASH);
     await game.phaseInterceptor.to("TurnEndPhase");
 
-    const firstTurnDamage = playerPokemon.getInverseHp();
+    const firstTurnDamage = charizard.turnData.damageTaken;
 
     game.move.select(Moves.LUCKY_CHANT);
     await game.phaseInterceptor.to("TurnEndPhase");
 
-    const secondTurnDamage = playerPokemon.getInverseHp() - firstTurnDamage;
+    const secondTurnDamage = charizard.turnData.damageTaken;
     expect(secondTurnDamage).toBeLessThan(firstTurnDamage);
   });
 });
