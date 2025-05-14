9425a877d707da3d9f700c6d1ea07189188e21ff
Wlowscha <54003515+Wlowscha@users.noreply.github.com>
2025-05-15 00:25:58 +0200

    Clearing options when switching out

--- src/ui/party-ui-handler.ts
+++ src/ui/party-ui-handler.ts
@@ -809,6 +809,7 @@ export default class PartyUiHandler extends MessageUiHandler {
 
       // This is used when switching out using the Pokemon command (possibly holding a Baton held item).
       if ((option === PartyOption.PASS_BATON || option === PartyOption.SEND_OUT) && !this.selectCallback) {
+        this.clearOptions();
         (globalScene.getCurrentPhase() as CommandPhase).handleCommand(
           Command.POKEMON,
           this.cursor,
