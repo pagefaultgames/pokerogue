0713eb358687ea46d8cc3a590469030609b3efe5
Sirz Benjie <142067137+SirzBenjie@users.noreply.github.com>
2025-05-26 23:18:04 -0500

    Explicitly check against Moves.NONE
    Co-authored-by: Bertie690 <136088738+Bertie690@users.noreply.github.com>

--- src/phases/command-phase.ts
+++ src/phases/command-phase.ts
@@ -280,7 +280,7 @@ export class CommandPhase extends FieldPhase {
             multiple: move.targets.length > 1,
           };
 
-    if (!moveId) {
+    if (moveId === Moves.NONE) {
       turnCommand.targets = [this.fieldIndex];
     }
 
