ccd9480240ea26285683fb997fcb1af67e3d4a9b
Lugiad <2070109+Adri1@users.noreply.github.com>
2025-05-31 01:54:17 +0200

    [Localization] Secondary pending languages inclusion (#5903)
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Update mysterious-chest-dialogue.json
    
    * Update mysterious-chest-dialogue.json
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Update i18n.ts
    
    * Update settings.ts
    
    * Update settings-display-ui-handler.ts
    
    * Update starter-select-ui-handler.ts
    
    * Update i18n.ts
    
    * Update i18n.ts
    
    * Update settings.ts
    
    * Update settings-display-ui-handler.ts
    
    * Update i18n.ts
    
    * Update starter-select-ui-handler.ts
    
    * Update utils.ts
    
    * Update utils.ts
    
    * Add files via upload
    
    * Rename statuses_dk.json to statuses_da.json
    
    * Update statuses_da.json
    
    * Update and rename types_dk.json to types_da.json
    
    * Rename statuses_dk.png to statuses_da.png
    
    * Rename types_dk.png to types_da.png
    
    * Delete src/locales/dk directory
    
    * Add files via upload
    
    * Apply suggestions from code review
    
    * Delete src/locales/da directory
    
    * Delete src/locales/tr directory
    
    * Update i18n.ts
    
    * Update i18n.ts
    
    * Update utils.ts
    
    * Main -> Beta (1.1.6) (#4751)
    
    * Comment out startGame call on manifest fetch failure
    
    * [Hotfix] Fix status damage triggering before berry usage (#4732)
    
    * [Hotfix] Fix Eternatus egg tier (#4734)
    
    * [Hotfix] Fix manifest getting loaded before the game is initialized (#4739)
    
    * fix locales path for offline builds (#4739)
    
    * [Sprite] Hotfix cut off Binacle sprite (#4741)
    
    * [Sprite][hotfix] Fixed cropping on 658 static greninja and ash greninja (#4743)
    
    * [Sprite][hotfix] Fixed cropping on static greninja and ash greninja
    
    * [Hotfix] Fix crash when Mist would block a stat drop (#4746)
    
    ---------
    
    Co-authored-by: Frederico Santos <frederico.f.santos@tecnico.ulisboa.pt>
    Co-authored-by: innerthunder <168692175+innerthunder@users.noreply.github.com>
    Co-authored-by: PigeonBar <56974298+PigeonBar@users.noreply.github.com>
    Co-authored-by: chaosgrimmon <31082757+chaosgrimmon@users.noreply.github.com>
    Co-authored-by: pom-eranian <lrlrliwoo@gmail.com>
    
    * Update settings-display-ui-handler.ts
    
    * Delete src/utils.ts
    
    * Update common.ts
    
    * Delete src/utils.ts
    
    * Update common.ts
    
    * Romanian workspace (#25)
    
    * Russian workspace (#26)
    
    * Update settings-display-ui-handler.ts
    
    * Update settings-display-ui-handler.ts
    
    * Update settings.ts
    
    * Update settings-display-ui-handler.ts
    
    * Update i18n.ts
    
    * Update settings.ts
    
    * Update settings-display-ui-handler.ts
    
    * Update common.ts
    
    * Update and rename statuses_ca-ES.json to statuses_ca.json
    
    * Update and rename types_ca-ES.json to types_ca.json
    
    * Add files via upload
    
    * Delete public/images/statuses_ca-ES.png
    
    * Delete public/images/types_ca-ES.png
    
    * Update locales submodule
    
    ---------
    
    Co-authored-by: Moka <54149968+MokaStitcher@users.noreply.github.com>
    Co-authored-by: Frederico Santos <frederico.f.santos@tecnico.ulisboa.pt>
    Co-authored-by: innerthunder <168692175+innerthunder@users.noreply.github.com>
    Co-authored-by: PigeonBar <56974298+PigeonBar@users.noreply.github.com>
    Co-authored-by: chaosgrimmon <31082757+chaosgrimmon@users.noreply.github.com>
    Co-authored-by: pom-eranian <lrlrliwoo@gmail.com>
    Co-authored-by: NightKev <34855794+DayKev@users.noreply.github.com>

--- src/ui/starter-select-ui-handler.ts
+++ src/ui/starter-select-ui-handler.ts
@@ -162,6 +162,24 @@ const languageSettings: { [key: string]: LanguageSetting } = {
     starterInfoYOffset: 0.5,
     starterInfoXPos: 29,
   },
+  da:{
+    starterInfoTextSize: "56px",
+    instructionTextSize: "38px",
+  },
+  tr:{
+    starterInfoTextSize: "56px",
+    instructionTextSize: "38px",
+  },
+  ro:{
+    starterInfoTextSize: "56px",
+    instructionTextSize: "38px",
+  },
+  ru: {
+    starterInfoTextSize: "46px",
+    instructionTextSize: "38px",
+    starterInfoYOffset: 0.5,
+    starterInfoXPos: 26,
+  },
 };
 
 const valueReductionMax = 2;
