9e1ed4932b90a047db8435d092255d69ad9dfccb
NightKev <34855794+DayKev@users.noreply.github.com>
2025-06-07 23:45:18 -0700

    Minor comment reformatting

--- src/@types/phase-types.ts
+++ src/@types/phase-types.ts
@@ -1,12 +1,11 @@
 import type { PhaseConstructorMap } from "#app/phase-manager";
 
-// Intentionally export the types of everything in phase-manager, as this file is meant to be the centralized
-// type definitions for the phase system.
+// Intentionally export the types of everything in phase-manager, as this file is meant to be
+// the centralized place for type definitions for the phase system.
 export type * from "#app/phase-manager";
 
-/*
-This file includes helpful types for the phase system.
-It intentionally imports the phase constructor map from the phase manager (and re-exports it)
+// This file includes helpful types for the phase system.
+// It intentionally imports the phase constructor map from the phase manager (and re-exports it)
 
 /**
  * Map of phase names to constructors for said phase
