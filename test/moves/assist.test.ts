c56ed1d56467f90ded5b763a1a761e4596515194
Bertie690 <136088738+Bertie690@users.noreply.github.com>
2025-07-06 13:18:38 +0200

    Update assist.test.ts import

--- test/moves/assist.test.ts
+++ test/moves/assist.test.ts
@@ -4,6 +4,7 @@ import { MoveResult } from "#enums/move-result";
 import { AbilityId } from "#enums/ability-id";
 import { MoveId } from "#enums/move-id";
 import { SpeciesId } from "#enums/species-id";
+import { MoveUseMode } from "#enums/move-use-mode";
 import GameManager from "#test/testUtils/gameManager";
 import Phaser from "phaser";
 import { afterEach, beforeAll, beforeEach, describe, expect, it, vi } from "vitest";
