2d1da02ca028164288ddf61931be6e3f4662970e
Dean <69436131+emdeann@users.noreply.github.com>
2025-07-19 18:54:18 -0700

    Remove implicit ordering from DQM

--- src/queues/dynamic-queue-manager.ts
+++ src/queues/dynamic-queue-manager.ts
@@ -3,13 +3,11 @@ import type { PhaseString, DynamicPhase } from "#app/@types/phase-types";
 import type { PokemonMove } from "#app/data/moves/pokemon-move";
 import type Pokemon from "#app/field/pokemon";
 import type { Phase } from "#app/phase";
-import type { MoveHeaderPhase } from "#app/phases/move-header-phase";
 import type { MovePhase } from "#app/phases/move-phase";
 import { MovePhasePriorityQueue } from "#app/queues/move-phase-priority-queue";
 import type { PhasePriorityQueue } from "#app/queues/phase-priority-queue";
 import { PokemonPhasePriorityQueue } from "#app/queues/pokemon-phase-priority-queue";
 import { PostSummonPhasePriorityQueue } from "#app/queues/post-summon-phase-priority-queue";
-import { SwitchSummonPhasePriorityQueue } from "#app/queues/switch-summon-phase-priority-queue";
 import type { BattlerIndex } from "#enums/battler-index";
 import type { MovePhaseTimingModifier } from "#enums/move-phase-timing-modifier";
 
@@ -27,13 +25,10 @@ const nonDynamicPokemonPhases: PhaseString[] = [
 
 export class DynamicQueueManager {
   private dynamicPhaseMap: Map<PhaseString, PhasePriorityQueue<Phase>>;
-  private alwaysDynamic: PhaseString[] = ["SwitchSummonPhase", "PostSummonPhase", "MoveHeaderPhase", "MovePhase"];
 
   constructor() {
     this.dynamicPhaseMap = new Map();
-    this.dynamicPhaseMap.set("SwitchSummonPhase", new SwitchSummonPhasePriorityQueue());
     this.dynamicPhaseMap.set("PostSummonPhase", new PostSummonPhasePriorityQueue());
-    this.dynamicPhaseMap.set("MoveHeaderPhase", new PokemonPhasePriorityQueue<MoveHeaderPhase>());
     this.dynamicPhaseMap.set("MovePhase", new MovePhasePriorityQueue());
   }
 
@@ -57,21 +52,11 @@ export class DynamicQueueManager {
   }
 
   public popNextPhase(type: PhaseString): Phase | undefined {
-    if (!this.alwaysDynamic.includes(type)) {
-      return this.dynamicPhaseMap.get(type)?.pop();
-    }
-    return this.alwaysDynamic
-      .map((p: PhaseString) => this.dynamicPhaseMap.get(p))
-      .find(q => q && !q.isEmpty())
-      ?.pop();
+    return this.dynamicPhaseMap.get(type)?.pop();
   }
 
   public findPhaseOfType<T extends PhaseString>(type: T, condition?: PhaseConditionFunc<T>): Phase | undefined {
     return this.dynamicPhaseMap.get(type)?.findPhase(condition);
-  }
-
-  public activeQueueExists(type: PhaseString) {
-    return this.alwaysDynamic.includes(type) || this.dynamicPhaseMap.get(type)?.isEmpty() === false;
   }
 
   public exists<T extends PhaseString>(type: T, condition?: PhaseConditionFunc<T>): boolean {
