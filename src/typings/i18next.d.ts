b26124501e102585e09709ec7cbc3902f47e327b
Bertie690 <taylormw163@gmail.com>
2025-07-27 23:41:37 -0400

    Reverted biome breaking i18next

--- src/typings/i18next.d.ts
+++ src/typings/i18next.d.ts
@@ -2,5 +2,8 @@ import type { TOptions } from "i18next";
 
 // Module declared to make referencing keys in the localization files type-safe.
 declare module "i18next" {
-  type TFunction = (key: string | string[], options?: TOptions & Record<string, unknown>) => string;
+  interface TFunction {
+    // biome-ignore lint/style/useShorthandFunctionType: IDK why this breaks everything but it does
+    (key: string | string[], options?: TOptions & Record<string, unknown>): string;
+  }
 }
