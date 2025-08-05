a6007cef1011f175eaf65c163c6ed85ef874442b
Matheus Alves <matheus.r.noya.alves@tecnico.ulisboa.pt>
2025-08-05 10:46:15 +0100

    Dependency resolved

--- test/system/rename_run.test.ts
+++ test/system/rename_run.test.ts
@@ -4,7 +4,7 @@ import { pokerogueApi } from "#app/plugins/api/pokerogue-api";
 import type { SessionSaveData } from "#app/system/game-data";
 import { AbilityId } from "#enums/ability-id";
 import { MoveId } from "#enums/move-id";
-import { GameManager } from "#test/testUtils/gameManager";
+import { GameManager } from "#test/test-utils/game-manager";
 import Phaser from "phaser";
 import { afterEach, beforeAll, beforeEach, describe, expect, it, vi } from "vitest";
 
