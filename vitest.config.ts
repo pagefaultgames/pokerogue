88189d5c9f2b4466968ff98863e2bedb1c558b77
Bertie690 <taylormw163@gmail.com>
2025-08-19 19:33:32 -0400

    Moved all the setup files into their own folder

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,7 +9,7 @@ export default defineProject(({ mode }) => ({
       TZ: "UTC",
     },
     testTimeout: 20000,
-    setupFiles: ["./test/font-face.setup.ts", "./test/vitest.setup.ts", "./test/matchers.setup.ts"],
+    setupFiles: ["./test/setup/font-face.setup.ts", "./test/setup/vitest.setup.ts", "./test/setup/matchers.setup.ts"],
     sequence: {
       sequencer: MySequencer,
     },
