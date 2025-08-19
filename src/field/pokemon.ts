5fbffc10309034f632a9f2e610498e80d0280c93
Sirz Benjie <142067137+SirzBenjie@users.noreply.github.com>
2025-08-19 18:04:45 -0500

    Fix getBattlerIndex for enemyPokemon

--- src/field/pokemon.ts
+++ src/field/pokemon.ts
@@ -7036,7 +7036,11 @@ export class EnemyPokemon extends Pokemon {
     return globalScene.getEnemyField().indexOf(this);
   }
 
-  getBattlerIndex(): Exclude<BattlerIndex, BattlerIndex.ATTACKER> {
+  getBattlerIndex(): BattlerIndex {
+    const fieldIndex = this.getFieldIndex();
+    if (fieldIndex === -1) {
+      return BattlerIndex.ATTACKER;
+    }
     return BattlerIndex.ENEMY + this.getFieldIndex();
   }
 
