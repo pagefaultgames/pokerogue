56752d6f4a951da4524c41c0459d772b7a65807d
Sirz Benjie <142067137+SirzBenjie@users.noreply.github.com>
2025-08-25 14:08:05 -0500

    Ensure users that install pokerogue as a PWA do not remain on the old version

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,9 @@
+/// <reference lib = "WebWorker" />
 self.addEventListener('install', function () {
   console.log('Service worker installing...');
 });
+
+self.addEventListener('activate', (event) => {
+  // @ts-expect-error: See https://github.com/microsoft/TypeScript/issues/14877
+  event.waitUntil(self.clients.claim());
+})
