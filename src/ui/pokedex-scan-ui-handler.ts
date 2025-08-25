f250c71f8eac81940775c1e3ee9a6cc9b727b9c2
fabske0 <192151969+fabske0@users.noreply.github.com>
2025-08-25 23:07:24 +0200

    auto focus first input

--- src/ui/pokedex-scan-ui-handler.ts
+++ src/ui/pokedex-scan-ui-handler.ts
@@ -106,10 +106,6 @@ export class PokedexScanUiHandler extends FormModalUiHandler {
 
     this.reduceKeys();
 
-    setTimeout(() => {
-      input.setFocus(); // Focus after a short delay to avoid unwanted input
-    }, 50);
-
     input.on("keydown", (inputObject, evt: KeyboardEvent) => {
       if (
         ["escape", "space"].some(v => v === evt.key.toLowerCase() || v === evt.code.toLowerCase()) &&
