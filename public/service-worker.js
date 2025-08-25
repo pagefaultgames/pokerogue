f0c24cd16e02be946a8abb141394c7f5e1eea19a
Sirz Benjie <142067137+SirzBenjie@users.noreply.github.com>
2025-08-25 18:45:41 -0500

    Remove unnecessary tsdoc comments from `service-worker.js` (#6417)
    Remove typescript comments form `service-worker.js`

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,7 @@
-/// <reference lib = "WebWorker" />
 self.addEventListener('install', function () {
   console.log('Service worker installing...');
 });
 
 self.addEventListener('activate', (event) => {
-  // @ts-expect-error: See https://github.com/microsoft/TypeScript/issues/14877
   event.waitUntil(self.clients.claim());
 })
