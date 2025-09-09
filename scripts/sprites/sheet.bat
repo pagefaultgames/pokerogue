a6c3f137fe816404d9a4d9cb61727bcd5287a2b5
Sirz Benjie <142067137+SirzBenjie@users.noreply.github.com>
2025-09-09 11:10:39 -0500

    Move ps1 files out of public

--- scripts/sprites/sheet.bat
+++ scripts/sprites/sheet.bat
@@ -1,3 +1,7 @@
+:: SPDX-FileCopyrightText: 2025 Pagefault Games
+::
+:: SPDX-License-Identifier: AGPL-3.0-only
+
 SET "PATH=C:/Program Files/CodeAndWeb/TexturePacker/bin;%PATH%" 
 
 forfiles /s /m *.gif /c "cmd /c mkdir  @FNAME && ffmpeg -i "@FILE" @FNAME/%%04d.png"
