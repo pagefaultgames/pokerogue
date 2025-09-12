65e185d12c63678d9e780f93b86744506157f016
Sirz Benjie <142067137+SirzBenjie@users.noreply.github.com>
2025-09-12 08:49:02 -0500

    Add new tests for array utilities

--- src/data/pokemon/pokemon-data.ts
+++ src/data/pokemon/pokemon-data.ts
@@ -16,7 +16,7 @@ import type { AttackMoveResult } from "#types/attack-move-result";
 import type { IllusionData } from "#types/illusion-data";
 import type { TurnMove } from "#types/turn-move";
 import type { CoerceNullPropertiesToUndefined } from "#types/type-helpers";
-import { setTypedArray } from "#utils/common";
+import { setTypedArray } from "#utils/array";
 import { getPokemonSpeciesForm } from "#utils/pokemon-utils";
 
 /**
