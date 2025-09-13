008cecb79c1795e27c5f603965e396ea5e2058b7
fabske0 <192151969+fabske0@users.noreply.github.com>
2025-09-13 23:19:31 +0200

    biome

--- src/battle-scene.ts
+++ src/battle-scene.ts
@@ -1350,14 +1350,14 @@ export class BattleScene extends SceneBase {
         // Forcing a double battle on wave 1 causes a bug where only one enemy is sent out,
         // making it impossible to complete the fight without a reload
         const overrideVariant =
-          Overrides.RANDOM_TRAINER_OVERRIDE?.trainerVariant === TrainerVariant.DOUBLE &&
-          (!hasDouble || newWaveIndex <= 1)
+          Overrides.RANDOM_TRAINER_OVERRIDE?.trainerVariant === TrainerVariant.DOUBLE
+          && (!hasDouble || newWaveIndex <= 1)
             ? TrainerVariant.DEFAULT
             : Overrides.RANDOM_TRAINER_OVERRIDE?.trainerVariant;
 
         const variant =
-          overrideVariant ??
-          (doubleTrainer ? TrainerVariant.DOUBLE : randSeedInt(2) ? TrainerVariant.FEMALE : TrainerVariant.DEFAULT);
+          overrideVariant
+          ?? (doubleTrainer ? TrainerVariant.DOUBLE : randSeedInt(2) ? TrainerVariant.FEMALE : TrainerVariant.DEFAULT);
 
         newTrainer = trainerData !== undefined ? trainerData.toTrainer() : new Trainer(trainerType, variant);
         this.field.add(newTrainer);
