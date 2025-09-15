8013093513fd272b6f8aa5d76329c682a098813d
Sirz Benjie <142067137+SirzBenjie@users.noreply.github.com>
2025-09-15 14:04:13 -0500

    [Bug] Secondary fusions with gender evo condition can now evolve (#6510)

--- src/field/pokemon.ts
+++ src/field/pokemon.ts
@@ -2635,7 +2635,7 @@ export abstract class Pokemon extends Phaser.GameObjects.Container {
         e => new FusionSpeciesFormEvolution(this.species.speciesId, e),
       );
       for (const fe of fusionEvolutions) {
-        if (fe.validate(this)) {
+        if (fe.validate(this, true)) {
           return fe;
         }
       }
