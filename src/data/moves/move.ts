f82e5b40f1e58f0914df9b5ab7ca39c1bd4590f7
Sirz Benjie <142067137+SirzBenjie@users.noreply.github.com>
2025-09-16 19:07:51 -0500

    Remove latent isNullOrUndefined

--- src/data/moves/move.ts
+++ src/data/moves/move.ts
@@ -10827,7 +10827,7 @@ export function initMoves() {
       .condition(
         () => {
           const weather = globalScene.arena.weather;
-          if (isNullOrUndefined(weather) || weather.isEffectSuppressed()) {
+          if (weather == null || weather.isEffectSuppressed()) {
             return false;
           }
           return weather.weatherType === WeatherType.HAIL || weather.weatherType === WeatherType.SNOW;
