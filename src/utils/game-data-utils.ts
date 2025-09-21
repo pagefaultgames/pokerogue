36a2e526206031425dad38cfc48aaa943db14bf2
Bertie690 <136088738+Bertie690@users.noreply.github.com>
2025-09-21 10:45:13 -0400

    Update game-data-utils.ts
    Co-authored-by: Sirz Benjie <142067137+SirzBenjie@users.noreply.github.com>

--- src/utils/game-data-utils.ts
+++ src/utils/game-data-utils.ts
@@ -1,7 +1,7 @@
 import { loggedInUser } from "#app/account";
 
 /**
- * Utility function to obtain the local storage key for a given save slot.
+ * Obtain the local storage key corresponding to a given save slot.
  * @param slotId - The numerical save slot ID
  * Will throw an error if `<0` (in line with standard util functions)
  * @returns The local storage key used to access the save data for the given slot.
