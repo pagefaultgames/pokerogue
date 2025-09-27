aab320432cda05104344c7af6f082a0729e6d25c
Bertie690 <136088738+Bertie690@users.noreply.github.com>
2025-09-27 01:15:04 -0400

    Update sprite-types.ts
    Co-authored-by: NightKev <34855794+DayKev@users.noreply.github.com>

--- src/@types/sprite-types.ts
+++ src/@types/sprite-types.ts
@@ -2,4 +2,4 @@
  * Tuple type representing a set of RGB color values.
  * Values should be in integer form (inside the interval `[0, 255]`).
  */
-export type RGBArray = [r: number, b: number, g: number];
+export type RGBArray = [r: number, g: number, b: number];
