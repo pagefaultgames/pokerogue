41db1ac3212de6e45142580940dfdb1442c831c0
NightKev <34855794+DayKev@users.noreply.github.com>
2025-10-23 13:12:11 -0700

    Apply test suggestions

--- test/arena/psychic-terrain.test.ts
+++ test/arena/psychic-terrain.test.ts
@@ -1,3 +1,5 @@
+import { allMoves } from "#data/data-lists";
+import { TerrainType } from "#data/terrain";
 import { AbilityId } from "#enums/ability-id";
 import { MoveId } from "#enums/move-id";
 import { SpeciesId } from "#enums/species-id";
@@ -59,27 +61,39 @@ describe("Arena - Psychic Terrain", () => {
 
   it("should not block non-priority moves boosted by Quick Claw", async () => {
     game.override.startingHeldItems([{ name: "QUICK_CLAW", count: 10 }]);
-    await game.classicMode.startBattle([SpeciesId.MAGIKARP]);
+    await game.classicMode.startBattle([SpeciesId.FEEBAS]);
 
-    game.move.select(MoveId.PSYCHIC_TERRAIN);
+    game.move.use(MoveId.PSYCHIC_TERRAIN);
     await game.toNextTurn();
+
+    expect(game).toHaveTerrain(TerrainType.PSYCHIC);
 
     game.move.use(MoveId.POUND);
+    await game.phaseInterceptor.to("MovePhase", false);
+    const feebas = game.field.getPlayerPokemon();
+    expect(allMoves[MoveId.POUND].getPriority(feebas)).toBe(0.2);
     await game.toEndOfTurn();
 
-    expect(game.field.getEnemyPokemon().hp).toBeLessThan(game.field.getEnemyPokemon().getMaxHp());
+    const shuckle = game.field.getEnemyPokemon();
+    expect(shuckle).not.toHaveFullHp();
   });
 
-  it("Quick claw boosted priority move to be blocked", async () => {
+  it("should block priority moves boosted by Quick Claw", async () => {
     game.override.startingHeldItems([{ name: "QUICK_CLAW", count: 10 }]);
-    await game.classicMode.startBattle([SpeciesId.MAGIKARP]);
+    await game.classicMode.startBattle([SpeciesId.FEEBAS]);
 
-    game.move.select(MoveId.PSYCHIC_TERRAIN);
+    game.move.use(MoveId.PSYCHIC_TERRAIN);
     await game.toNextTurn();
 
+    expect(game).toHaveTerrain(TerrainType.PSYCHIC);
+
     game.move.use(MoveId.QUICK_ATTACK);
+    await game.phaseInterceptor.to("MovePhase", false);
+    const feebas = game.field.getPlayerPokemon();
+    expect(allMoves[MoveId.QUICK_ATTACK].getPriority(feebas)).toBe(1.2);
     await game.toEndOfTurn();
 
-    expect(game.field.getEnemyPokemon().hp).toHaveFullHp;
+    const shuckle = game.field.getEnemyPokemon();
+    expect(shuckle).toHaveFullHp();
   });
 });
