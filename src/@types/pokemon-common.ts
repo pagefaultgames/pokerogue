ca0a169d92ce861a0e4a1ac2d4e6617c54737b1c
AJ Fontaine <fontbane@gmail.com>
2025-10-29 21:51:34 -0400

    >'less janky'>look inside>linting error

--- src/@types/pokemon-common.ts
+++ src/@types/pokemon-common.ts
@@ -1,5 +1,4 @@
 import type { PokemonSpecies, PokemonSpeciesFilter } from "#data/pokemon-species";
-import type { getRandomEncounterPokemon } from "#data/mystery-encounters/utils/encounter-phase-utils";
 import type { SpeciesId } from "#enums/species-id";
 import type { BooleanHolder } from "#utils/common";
 
@@ -18,28 +17,28 @@ export interface RandomEncounterParams {
   /** A custom function used to return the {@linkcode PokemonSpecies} to generate */
   speciesFunction?: () => PokemonSpecies;
 
-  /** 
+  /**
    * Whether the Pokemon should be a Boss.
    * @defaultValue `false`
-  */
+   */
   isBoss?: boolean;
 
-  /** 
+  /**
    * Whether Sub-legendaries can be encountered, mainly for event encounters
    * @defaultValue `true`
    */
   includeSubLegendary?: boolean;
 
-  /** 
+  /**
    * Whether Legendaries can be encountered
    * @defaultValue `true`
    */
   includeLegendary?: boolean;
 
-  /** 
+  /**
    * Whether Mythicals can be encountered
    * @defaultValue `true`
-   */  
+   */
   includeMythical?: boolean;
 
   /**
@@ -48,47 +47,47 @@ export interface RandomEncounterParams {
    */
   eventChance?: number;
 
-  /** 
+  /**
    * Number of rerolls for Hidden Ability (HA) that should be attempted
    * @defaultValue `0`
    */
   hiddenRerolls?: number;
 
-  /** 
+  /**
    * Number of rerolls for shininess/variants that should be attempted
    * @defaultValue `0`
    */
   shinyRerolls?: number;
 
-  /** 
-   * Number of extra HA rerolls for event mons 
+  /**
+   * Number of extra HA rerolls for event mons
    * @defaultValue `0`
    */
   eventHiddenRerolls?: number;
 
-  /** 
-   * Number of extra shiny rerolls for event mons 
+  /**
+   * Number of extra shiny rerolls for event mons
    * @defaultValue `0`
    */
   eventShinyRerolls?: number;
 
-  /** 
-   * The overridden HA chance, defaults to base 
+  /**
+   * The overridden HA chance, defaults to base
    */
   hiddenAbilityChance?: number;
 
-  /** 
-   * The overridden shiny chance, defaults to base 
+  /**
+   * The overridden shiny chance, defaults to base
    */
   shinyChance?: number;
 
-  /** 
+  /**
    * The max shiny threshold after modifiers are applied. Values below 1 mean no maximum
    * @defaultValue `0` (no maximum)
    */
   maxShinyChance?: number;
 
-  /** 
+  /**
    * An optional filter for eligible mons, applied to the event encounter pool.
    * If omitted, no filter will be applied.
    */
