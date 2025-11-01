62c13c9f38f5caa29320bf6e9204053f49f30708
fabske0 <192151969+fabske0@users.noreply.github.com>
2025-11-01 23:23:52 +0100

    move dailyBiomeWeights to own file

--- src/data/daily-run.ts
+++ src/data/daily-run.ts
@@ -12,6 +12,7 @@ import { isBetween, randSeedGauss, randSeedInt, randSeedItem } from "#utils/comm
 import { getEnumValues } from "#utils/enums";
 import { getPokemonSpecies, getPokemonSpeciesForm } from "#utils/pokemon-utils";
 import { chunkString } from "#utils/strings";
+import { dailyBiomeWeights } from "./daily-biome-weights";
 
 export interface DailyRunConfig {
   seed: number;
@@ -85,54 +86,6 @@ function getDailyRunStarter(starterSpeciesForm: PokemonSpeciesForm, startingLeve
   pokemon.destroy();
   return starter;
 }
-
-interface BiomeWeights {
-  [key: number]: number;
-}
-
-// Initially weighted by amount of exits each biome has
-// Town and End are set to 0 however
-// And some other biomes were balanced +1/-1 based on average size of the total daily.
-const dailyBiomeWeights: BiomeWeights = {
-  [BiomeId.CAVE]: 3,
-  [BiomeId.LAKE]: 3,
-  [BiomeId.PLAINS]: 3,
-  [BiomeId.SNOWY_FOREST]: 3,
-  [BiomeId.SWAMP]: 3, // 2 -> 3
-  [BiomeId.TALL_GRASS]: 3, // 2 -> 3
-
-  [BiomeId.ABYSS]: 2, // 3 -> 2
-  [BiomeId.RUINS]: 2,
-  [BiomeId.BADLANDS]: 2,
-  [BiomeId.BEACH]: 2,
-  [BiomeId.CONSTRUCTION_SITE]: 2,
-  [BiomeId.DESERT]: 2,
-  [BiomeId.DOJO]: 2, // 3 -> 2
-  [BiomeId.FACTORY]: 2,
-  [BiomeId.FAIRY_CAVE]: 2,
-  [BiomeId.FOREST]: 2,
-  [BiomeId.GRASS]: 2, // 1 -> 2
-  [BiomeId.MEADOW]: 2,
-  [BiomeId.MOUNTAIN]: 2, // 3 -> 2
-  [BiomeId.SEA]: 2,
-  [BiomeId.SEABED]: 2,
-  [BiomeId.SLUM]: 2,
-  [BiomeId.TEMPLE]: 2, // 3 -> 2
-  [BiomeId.VOLCANO]: 2,
-
-  [BiomeId.GRAVEYARD]: 1,
-  [BiomeId.ICE_CAVE]: 1,
-  [BiomeId.ISLAND]: 1,
-  [BiomeId.JUNGLE]: 1,
-  [BiomeId.LABORATORY]: 1,
-  [BiomeId.METROPOLIS]: 1,
-  [BiomeId.POWER_PLANT]: 1,
-  [BiomeId.SPACE]: 1,
-  [BiomeId.WASTELAND]: 1,
-
-  [BiomeId.TOWN]: 0,
-  [BiomeId.END]: 0,
-};
 
 export function getDailyStartingBiome(): BiomeId {
   const eventBiome = getDailyEventSeedBiome(globalScene.seed);
