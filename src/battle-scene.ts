18e0c2b9f7b76b1ea966e7b991e8abb244d9a192
Bertie690 <136088738+Bertie690@users.noreply.github.com>
2025-11-06 21:14:37 -0500

    Update erorr msg

--- src/battle-scene.ts
+++ src/battle-scene.ts
@@ -1315,7 +1315,7 @@ export class BattleScene extends SceneBase {
     }
 
     if (resolved.battleType == null) {
-      throw new Error("Whoopsie! I guess my type checks were wrong");
+      throw new Error("BattleScene.newBattle lacked battle type information inside new battle config!\nData:" + JSON.stringify(resolved));
     }
     resolved.double = this.checkIsDouble(resolved as NewBattleConstructedProps);
 
