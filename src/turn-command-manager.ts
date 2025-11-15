c3e362f689d5ccc7839e6005b294650822e4de85
Bertie690 <136088738+Bertie690@users.noreply.github.com>
2025-11-15 11:47:53 -0500

    Update turn-command-manager.ts

--- src/turn-command-manager.ts
+++ src/turn-command-manager.ts
@@ -1,12 +1,11 @@
-import type { FieldBattlerIndex } from "#enums/battler-index";
+import type { BattlerIndex } from "#enums/battler-index";
 
 // TODO: Add more methods and move `lastTurnOrder` here
 /** A manager for the commands and moves used in the current battle. */
 export class TurnCommandManager {
   /**
-   * A preset order of `BattlerIndex`es, used by tests and MEs to "fix" speed order-related effects.
+   * A preset order of {@linkcode BattlerIndex}es, used by tests and MEs to "fix" speed order-related effects.
    */
-  // TODO: Add a way to specify "partial" ordering of Pokemon
   public setOrder: readonly FieldBattlerIndex[] | undefined;
 
   /** Reset a prior set turn order. */
