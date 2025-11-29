6239e5578f1b39b32a72b43984eebecf9de7b6f3
Bertie690 <taylormw163@gmail.com>
2025-11-29 14:02:04 -0500

    reverted changes

--- src/battle-scene.ts
+++ src/battle-scene.ts
@@ -3197,61 +3197,55 @@ export class BattleScene extends SceneBase {
     return null;
   }
 
-  // TODO: Remove boolean return
   triggerPokemonFormChange(
     pokemon: Pokemon,
     formChangeTriggerType: Constructor<SpeciesFormChangeTrigger>,
     delayed = false,
     modal = false,
   ): boolean {
-    if (!pokemonFormChanges.hasOwnProperty(pokemon.species.speciesId)) {
-      return false;
+    if (pokemonFormChanges.hasOwnProperty(pokemon.species.speciesId)) {
+      // in case this is NECROZMA, determine which forms this
+      const matchingFormChangeOpts = pokemonFormChanges[pokemon.species.speciesId].filter(
+        fc => fc.findTrigger(formChangeTriggerType) && fc.canChange(pokemon),
+      );
+      let matchingFormChange: SpeciesFormChange | null;
+      if (pokemon.species.speciesId === SpeciesId.NECROZMA && matchingFormChangeOpts.length > 1) {
+        // Ultra Necrozma is changing its form back, so we need to figure out into which form it devolves.
+        const formChangeItemModifiers = (
+          this.findModifiers(
+            m => m instanceof PokemonFormChangeItemModifier && m.pokemonId === pokemon.id,
+          ) as PokemonFormChangeItemModifier[]
+        )
+          .filter(m => m.active)
+          .map(m => m.formChangeItem);
+
+        matchingFormChange = formChangeItemModifiers.includes(FormChangeItem.N_LUNARIZER)
+          ? matchingFormChangeOpts[0]
+          : formChangeItemModifiers.includes(FormChangeItem.N_SOLARIZER)
+            ? matchingFormChangeOpts[1]
+            : null;
+      } else {
+        matchingFormChange = matchingFormChangeOpts[0];
+      }
+      if (matchingFormChange) {
+        let phase: Phase;
+        if (pokemon.isPlayer() && !matchingFormChange.quiet) {
+          phase = this.phaseManager.create("FormChangePhase", pokemon, matchingFormChange, modal);
+        } else {
+          phase = this.phaseManager.create("QuietFormChangePhase", pokemon, matchingFormChange);
+        }
+        if (pokemon.isPlayer() && !matchingFormChange.quiet && modal) {
+          this.phaseManager.overridePhase(phase);
+        } else if (delayed) {
+          this.phaseManager.pushPhase(phase);
+        } else {
+          this.phaseManager.unshiftPhase(phase);
+        }
+        return true;
+      }
     }
 
-    // in case this is NECROZMA, determine which forms this
-    const matchingFormChangeOpts = pokemonFormChanges[pokemon.species.speciesId].filter(
-      fc => fc.findTrigger(formChangeTriggerType) && fc.canChange(pokemon),
-    );
-
-    let matchingFormChange: SpeciesFormChange | undefined;
-    if (pokemon.species.speciesId === SpeciesId.NECROZMA && matchingFormChangeOpts.length > 1) {
-      // Ultra Necrozma is changing its form back, so we need to figure out into which form it devolves.
-      const formChangeItemModifiers = (
-        this.findModifiers(
-          m => m instanceof PokemonFormChangeItemModifier && m.pokemonId === pokemon.id && m.active,
-        ) as PokemonFormChangeItemModifier[]
-      ).map(m => m.formChangeItem);
-
-      matchingFormChange = formChangeItemModifiers.includes(FormChangeItem.N_LUNARIZER)
-        ? matchingFormChangeOpts[0]
-        : formChangeItemModifiers.includes(FormChangeItem.N_SOLARIZER)
-          ? matchingFormChangeOpts[1]
-          : undefined;
-    } else {
-      matchingFormChange = matchingFormChangeOpts[0];
-    }
-
-    if (!matchingFormChange) {
-      return false;
-    }
-
-    let phase: Phase;
-    if (pokemon.isPlayer() && !matchingFormChange.quiet) {
-      phase = this.phaseManager.create("FormChangePhase", pokemon, matchingFormChange, modal);
-    } else {
-      phase = this.phaseManager.create("QuietFormChangePhase", pokemon, matchingFormChange);
-    }
-
-    // TODO: OverridePhase is bad
-    if (pokemon.isPlayer() && !matchingFormChange.quiet && modal) {
-      this.phaseManager.overridePhase(phase);
-    } else if (delayed) {
-      this.phaseManager.pushPhase(phase);
-    } else {
-      this.phaseManager.unshiftPhase(phase);
-    }
-
-    return true;
+    return false;
   }
 
   triggerPokemonBattleAnim(
