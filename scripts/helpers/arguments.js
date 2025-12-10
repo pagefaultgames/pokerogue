b91634056339e4731aed0e8557393b8dcdbbd5b5
Bertie690 <taylormw163@gmail.com>
2025-12-10 00:26:42 -0500

    doc fixes

--- scripts/helpers/arguments.js
+++ scripts/helpers/arguments.js
@@ -2,22 +2,22 @@
  * Obtain the value of a property value CLI argument (one of the form
  * `-x=y` or `-x y`).
  * @param {string[]} args - The command line arguments array to parse
- * @param {readonly string[]} aliases - An array containing all valid properties to check.
- * @returns {string | undefined} The outfile location, or `undefined` if none is provided
+ * @param {readonly string[]} flags - An array containing all valid flags to check.
+ * @returns {string | undefined} The provided arguments, or `undefined` if none is provided
  * @remarks
  * This will mutate the `args` array by removing the first specified match.
  */
-export function getPropertyValue(args, aliases) {
-  let /** @type {string | undefined} */ outFile;
-  // Extract the outfile as either the form "-o=y" or "-o y".
+export function getPropertyValue(args, flags) {
+  let /** @type {string | undefined} */ arg;
+  // Extract the prop as either the form "-o=y" or "-o y".
   const hasEquals = /^.*=(.+)$/g.exec(args[0]);
   if (hasEquals) {
-    outFile = hasEquals[1];
+    arg = hasEquals[1];
     args.splice(0, 1);
-  } else if (aliases.includes(args[0])) {
-    outFile = args[1];
+  } else if (flags.includes(args[0])) {
+    arg = args[1];
     args.splice(0, 2);
   }
 
-  return outFile;
+  return arg;
 }
