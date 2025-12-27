6846e48b911c857e37c968e6e27b4c8fe5cef459
Bertie690 <136088738+Bertie690@users.noreply.github.com>
2025-12-27 18:32:03 -0500

    Update mock-phase.ts
    Co-authored-by: NightKev <34855794+DayKev@users.noreply.github.com>

--- test/test-utils/mocks/mock-phase.ts
+++ test/test-utils/mocks/mock-phase.ts
@@ -4,7 +4,7 @@ import { Phase } from "#app/phase";
  * A rudimentary mock of a phase used for unit tests.
  * Ends upon starting by default.
  */
-export abstract class mockPhase extends Phase {
+export abstract class MockPhase extends Phase {
   public phaseName: any;
   public override start() {
     this.end();
