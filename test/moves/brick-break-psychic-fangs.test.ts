7c338f1991e329ea038a88072dc7288def69af16
NightKev <34855794+DayKev@users.noreply.github.com>
2025-12-29 03:07:27 -0800

    Add tests for Brick Break and Psychic Fangs

---
+++ test/moves/brick-break-psychic-fangs.test.ts
@@ -0,0 +1,74 @@
+/*
+ * SPDX-FileCopyrightText: 2024-2025 Pagefault Games
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { AbilityId } from "#enums/ability-id";
+import { ArenaTagSide } from "#enums/arena-tag-side";
+import { ArenaTagType } from "#enums/arena-tag-type";
+import { MoveId } from "#enums/move-id";
+import { SpeciesId } from "#enums/species-id";
+import { GameManager } from "#test/test-utils/game-manager";
+import Phaser from "phaser";
+import { afterEach, beforeAll, beforeEach, describe, expect, it } from "vitest";
+
+describe.each([
+  { moveId: MoveId.BRICK_BREAK, moveName: "Brick Break" },
+  { moveId: MoveId.PSYCHIC_FANGS, moveName: "Psychic Fangs" },
+])("Move - $moveName", ({ moveId }) => {
+  let phaserGame: Phaser.Game;
+  let game: GameManager;
+
+  beforeAll(() => {
+    phaserGame = new Phaser.Game({
+      type: Phaser.HEADLESS,
+    });
+  });
+
+  afterEach(() => {
+    game.phaseInterceptor.restoreOg();
+  });
+
+  beforeEach(() => {
+    game = new GameManager(phaserGame);
+    game.override
+      .ability(AbilityId.NO_GUARD)
+      .battleStyle("single")
+      .criticalHits(false)
+      .enemySpecies(SpeciesId.MAGIKARP)
+      .enemyAbility(AbilityId.BALL_FETCH)
+      .enemyMoveset(MoveId.SPLASH)
+      .startingLevel(100)
+      .enemyLevel(100);
+  });
+
+  it.each<{ tagType: ArenaTagType; tagName: string }>([
+    { tagType: ArenaTagType.REFLECT, tagName: "Reflect" },
+    { tagType: ArenaTagType.LIGHT_SCREEN, tagName: "Light Screen" },
+    { tagType: ArenaTagType.AURORA_VEIL, tagName: "Aurora Veil" },
+  ])("should remove $tagName only from the target's side of the field", async ({ tagType }) => {
+    await game.classicMode.startBattle([SpeciesId.FEEBAS]);
+
+    game.scene.arena.addTag(tagType, 0, undefined, game.field.getEnemyPokemon().id, ArenaTagSide.ENEMY);
+    game.scene.arena.addTag(tagType, 0, undefined, game.field.getPlayerPokemon().id, ArenaTagSide.PLAYER);
+
+    game.move.use(moveId);
+    await game.toEndOfTurn();
+
+    expect(game).toHaveArenaTag({ tagType, side: ArenaTagSide.PLAYER });
+    expect(game).not.toHaveArenaTag({ tagType, side: ArenaTagSide.ENEMY });
+  });
+
+  it("should not remove screens if the target is immune to the move", async () => {
+    game.override.enemySpecies(SpeciesId.SABLEYE);
+    await game.classicMode.startBattle([SpeciesId.FEEBAS]);
+
+    game.scene.arena.addTag(ArenaTagType.REFLECT, 0, undefined, game.field.getEnemyPokemon().id, ArenaTagSide.ENEMY);
+
+    game.move.use(moveId);
+    await game.toEndOfTurn();
+
+    expect(game).toHaveArenaTag({ tagType: ArenaTagType.REFLECT, side: ArenaTagSide.ENEMY });
+  });
+});
