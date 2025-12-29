f370b8c5d186494f1a5edd91b508aae4a6c872b8
NightKev <34855794+DayKev@users.noreply.github.com>
2025-12-29 02:14:18 -0800

    Add full test suite for Defog

--- test/moves/defog.test.ts
+++ test/moves/defog.test.ts
@@ -1,6 +1,8 @@
+import { TerrainType } from "#data/terrain";
 import { AbilityId } from "#enums/ability-id";
 import { ArenaTagSide } from "#enums/arena-tag-side";
 import { ArenaTagType } from "#enums/arena-tag-type";
+import { BattlerIndex } from "#enums/battler-index";
 import { MoveId } from "#enums/move-id";
 import { SpeciesId } from "#enums/species-id";
 import { Stat } from "#enums/stat";
@@ -33,7 +35,7 @@ describe("Moves - Defog", () => {
       .enemyMoveset(MoveId.SPLASH);
   });
 
-  it("should not allow opponent Safeguard to be active", async () => {
+  it("should remove opponent Safeguard", async () => {
     await game.classicMode.startBattle([SpeciesId.REGIELEKI]);
 
     game.scene.arena.addTag(ArenaTagType.SAFEGUARD, 0, 0, game.field.getPlayerPokemon().id, ArenaTagSide.PLAYER);
@@ -46,7 +48,7 @@ describe("Moves - Defog", () => {
     expect(game).not.toHaveArenaTag({ tagType: ArenaTagType.SAFEGUARD, side: ArenaTagSide.ENEMY });
   });
 
-  it("should not allow opponent Mist to be active", async () => {
+  it("should remove opponent Mist", async () => {
     await game.classicMode.startBattle([SpeciesId.REGIELEKI]);
 
     game.move.use(MoveId.MIST);
@@ -60,5 +62,63 @@ describe("Moves - Defog", () => {
     await game.phaseInterceptor.to("BerryPhase");
 
     expect(game.field.getPlayerPokemon()).toHaveStatStage(Stat.ATK, -1);
+  });
+
+  it("should remove terrains", async () => {
+    await game.classicMode.startBattle([SpeciesId.FEEBAS]);
+
+    game.move.use(MoveId.DEFOG);
+    await game.move.forceEnemyMove(MoveId.ELECTRIC_TERRAIN);
+    await game.setTurnOrder([BattlerIndex.ENEMY, BattlerIndex.PLAYER]);
+
+    await game.phaseInterceptor.to("MoveEndPhase");
+    expect(game).toHaveTerrain(TerrainType.ELECTRIC);
+
+    await game.toEndOfTurn();
+    expect(game).not.toHaveTerrain(TerrainType.ELECTRIC);
+  });
+
+  it("should lower opponent's evasion by 1 stage", async () => {
+    await game.classicMode.startBattle([SpeciesId.FEEBAS]);
+
+    game.move.use(MoveId.DEFOG);
+    await game.toEndOfTurn();
+
+    expect(game.field.getEnemyPokemon()).toHaveStatStage(Stat.EVA, -1);
+  });
+
+  it.each<{ tagType: ArenaTagType; tagName: string }>([
+    { tagType: ArenaTagType.SPIKES, tagName: "Spikes" },
+    { tagType: ArenaTagType.STEALTH_ROCK, tagName: "Stealth Rocks" },
+    { tagType: ArenaTagType.TOXIC_SPIKES, tagName: "Toxic Spikes" },
+    { tagType: ArenaTagType.STICKY_WEB, tagName: "Sticky Web" },
+  ])("should remove $tagName from the field", async ({ tagType }) => {
+    await game.classicMode.startBattle([SpeciesId.FEEBAS]);
+
+    game.scene.arena.addTag(tagType, 0, undefined, game.field.getEnemyPokemon().id, ArenaTagSide.PLAYER);
+    game.scene.arena.addTag(tagType, 0, undefined, game.field.getPlayerPokemon().id, ArenaTagSide.ENEMY);
+
+    game.move.use(MoveId.DEFOG);
+    await game.toEndOfTurn();
+
+    expect(game).not.toHaveArenaTag({ tagType, side: ArenaTagSide.PLAYER });
+    expect(game).not.toHaveArenaTag({ tagType, side: ArenaTagSide.ENEMY });
+  });
+
+  it.each<{ tagType: ArenaTagType; tagName: string }>([
+    { tagType: ArenaTagType.REFLECT, tagName: "Reflect" },
+    { tagType: ArenaTagType.LIGHT_SCREEN, tagName: "Light Screen" },
+    { tagType: ArenaTagType.AURORA_VEIL, tagName: "Aurora Veil" },
+  ])("should remove $tagName only from the target's side of the field", async ({ tagType }) => {
+    await game.classicMode.startBattle([SpeciesId.FEEBAS]);
+
+    game.scene.arena.addTag(tagType, 0, undefined, game.field.getEnemyPokemon().id, ArenaTagSide.ENEMY);
+    game.scene.arena.addTag(tagType, 0, undefined, game.field.getPlayerPokemon().id, ArenaTagSide.PLAYER);
+
+    game.move.use(MoveId.DEFOG);
+    await game.toEndOfTurn();
+
+    expect(game).toHaveArenaTag({ tagType, side: ArenaTagSide.PLAYER });
+    expect(game).not.toHaveArenaTag({ tagType, side: ArenaTagSide.ENEMY });
   });
 });
