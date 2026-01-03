29c4b32ec7421533a53ea3da66e248025099a5c6
Ovdoes-dd <13681184556@163.com>
2026-01-03 23:34:21 +0800

    Refactor target selection logic in select-target-phase

--- src/phases/select-target-phase.ts
+++ src/phases/select-target-phase.ts
@@ -18,59 +18,49 @@ export class SelectTargetPhase extends PokemonPhase {
     super.start();
 
     const turnCommand = globalScene.currentBattle.turnCommands[this.fieldIndex];
-    const move = turnCommand?.move?.move;
-    const moveObject = move ? allMoves[move] : null;
-    const fieldSide = globalScene.getField();
-    const { double } = globalScene.currentBattle;
-
-    let defaultTarget: BattlerIndex | undefined;
-    const allyDefault = moveObject?.hasFlag(MoveFlags.ALLY_TARGET_DEFAULT) ?? false;
-
-    if (!moveObject) {
+    const moveId = turnCommand?.move?.move;
+    if (!moveId) {
       this.end();
       return;
     }
 
-    if (allyDefault && double) {
-      const user = fieldSide[this.fieldIndex];
-      const allyIndex = user.getAlly()?.getBattlerIndex();
+    // TODO: Move the logic for computing default targets here instead of `target-select-ui-handler`
+    const move = allMoves[moveId];
+    const fieldSide = globalScene.getField();
 
-      if (allyIndex !== undefined) {
-        const ally = fieldSide[allyIndex];
-        if (ally && ally.hp > 0) {
-          defaultTarget = allyIndex;
-        }
-      }
-    }
-
-    const defaultTargetArray = defaultTarget !== undefined ? [defaultTarget] : undefined;
-
+    const user = fieldSide[this.fieldIndex];
+    const ally = user.getAlly();
+    const shouldDefaultToAlly =
+      globalScene.currentBattle.double // formatting
+      && move.hasFlag(MoveFlags.ALLY_TARGET_DEFAULT)
+      && ally != null
+      && !ally.isFainted();
+    const defaultTargets = shouldDefaultToAlly ? [ally.getBattlerIndex()] : undefined;
     globalScene.ui.setMode(
       UiMode.TARGET_SELECT,
       this.fieldIndex,
-      move,
+      moveId,
       (targets: BattlerIndex[]) => {
         globalScene.ui.setMode(UiMode.MESSAGE);
-        const user = fieldSide[this.fieldIndex];
-        if (moveObject && targets[0] && user.isMoveTargetRestricted(moveObject.id, user, fieldSide[targets[0]])) {
+        if (targets[0] && user.isMoveTargetRestricted(move.id, user, fieldSide[targets[0]])) {
           const errorMessage = user
-            .getRestrictingTag(move!, user, fieldSide[targets[0]])!
-            .selectionDeniedText(user, moveObject.id);
-          globalScene.phaseManager.queueMessage(i18next.t(errorMessage, { moveName: moveObject.name }), 0, true);
+            .getRestrictingTag(moveId!, user, fieldSide[targets[0]])!
+            .selectionDeniedText(user, move.id);
+          globalScene.phaseManager.queueMessage(i18next.t(errorMessage, { moveName: move.name }), 0, true);
           targets = [];
         }
         if (targets.length === 0) {
           globalScene.currentBattle.turnCommands[this.fieldIndex] = null;
           globalScene.phaseManager.unshiftNew("CommandPhase", this.fieldIndex);
         } else {
-          turnCommand!.targets = targets;
+          turnCommand.targets = targets;
         }
-        if (turnCommand?.command === Command.BALL && this.fieldIndex) {
+        if (turnCommand.command === Command.BALL && this.fieldIndex) {
           globalScene.currentBattle.turnCommands[this.fieldIndex - 1]!.skip = true;
         }
         this.end();
       },
-      defaultTargetArray,
+      defaultTargets,
     );
   }
 }
