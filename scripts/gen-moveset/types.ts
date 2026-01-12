335fbdbaab048618e21ec30f3d7cc5c4e3343524
Sirz Benjie <142067137+SirzBenjie@users.noreply.github.com>
2026-01-12 15:30:07 -0600

    Cleanup licenses in scripts

--- scripts/gen-moveset/types.ts
+++ scripts/gen-moveset/types.ts
@@ -1,3 +1,9 @@
+/*
+ * SPDX-Copyright-Text: 2026 Pagefault Games
+ * SPDX-FileContributor: SirzBenjie
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
 import type { SpeciesId } from "#enums/species-id";
 
 export interface SamplerPayload {
