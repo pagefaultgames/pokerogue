d66c1b6af5fe853218430c8791b33ff6f70d6dd7
Bertie690 <taylormw163@gmail.com>
2026-01-27 13:44:14 -0500

    fix type import

--- src/phases/move-effect-phase.ts
+++ src/phases/move-effect-phase.ts
@@ -1,4 +1,4 @@
-import type { PostMoveInteractionAbAttrParams } from "#abilities/ability";
+import type { PostMoveInteractionAbAttrParams } from "#abilities/ab-attrs";
 import { applyAbAttrs } from "#abilities/apply-ab-attrs";
 import { globalScene } from "#app/global-scene";
 import { getPokemonNameWithAffix } from "#app/messages";
