51396d3945a626e9477fd085ffed8fbee8b8fa73
Bertie690 <136088738+Bertie690@users.noreply.github.com>
2026-01-28 03:04:58 -0500

    refactor: add `TIME_OF_DAY_OVERRIDE` & refactor arena tint code
    https://github.com/pagefaultgames/pokerogue/pull/6600
    
    * Removed `ARENA_TINT_OVERRIDE` + minor cleanup of arena tint code
    
    * Added `TIME_OF_DAY_OVERRIDE` to replace tint override
    
    * Fixed visual bug with time of day override
    
    * Fixed the frag stuff and blah blah blah
    
    * Update sprite-types.ts
    
    Co-authored-by: NightKev <34855794+DayKev@users.noreply.github.com>
    
    * Fixed sprite shader errors again
    
    * Update src/pipelines/glsl/sprite-shader.vert
    
    Co-authored-by: NightKev <34855794+DayKev@users.noreply.github.com>
    
    * Did the chain of the method and such
    
    * Fixed / 255 issue
    
    * foo
    
    * Ran biome
    
    * Update sprite.ts
    
    Co-authored-by: NightKev <34855794+DayKev@users.noreply.github.com>
    
    * Update sprite.ts
    
    Co-authored-by: NightKev <34855794+DayKev@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: NightKev <34855794+DayKev@users.noreply.github.com>

--- src/field/pokemon.ts
+++ src/field/pokemon.ts
@@ -1143,7 +1143,7 @@ export abstract class Pokemon extends Phaser.GameObjects.Container {
   }
 
   getSprite(): Phaser.GameObjects.Sprite {
-    return this.getAt(0) as Phaser.GameObjects.Sprite;
+    return this.getAt(0);
   }
 
   getTintSprite(): Phaser.GameObjects.Sprite | null {
@@ -1201,14 +1201,14 @@ export abstract class Pokemon extends Phaser.GameObjects.Container {
     this.setScale(this.getSpriteScale());
   }
 
-  updateSpritePipelineData(): void {
+  async updateSpritePipelineData(): Promise<void> {
     [this.getSprite(), this.getTintSprite()]
       .filter(s => !!s)
       .map(s => {
         s.pipelineData["teraColor"] = getTypeRgb(this.getTeraType());
         s.pipelineData["isTerastallized"] = this.isTerastallized;
       });
-    this.updateInfo(true);
+    await this.updateInfo(true);
   }
 
   initShinySparkle(): void {
