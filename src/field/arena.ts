94a437e1579d3551a0a84401afcf22a57c09dfa5
Bertie690 <136088738+Bertie690@users.noreply.github.com>
2026-02-02 04:03:38 -0500

    refactor: optimize implementation of `Arena#applyTagsForSide`
    https://github.com/pagefaultgames/pokerogue/pull/6881

--- src/field/arena.ts
+++ src/field/arena.ts
@@ -557,14 +557,23 @@ export class Arena {
     side: ArenaTagSide,
     ...args: Parameters<T["apply"]>
   ): void {
-    let tags =
-      typeof tagType === "string"
-        ? this.tags.filter(t => t.tagType === tagType)
-        : this.tags.filter(t => t instanceof tagType);
-    if (side !== ArenaTagSide.BOTH) {
-      tags = tags.filter(t => t.side === side);
+    /** A lambda function to filter out incompatible tag types. */
+    const sameTagType: (tag: ArenaTag) => tag is T =
+      typeof tagType === "string" // formatting
+        ? (t): t is T => t.tagType === tagType
+        : (t): t is T => t instanceof tagType;
+
+    for (const tag of this.tags) {
+      if (!sameTagType(tag)) {
+        continue;
+      }
+
+      if (side !== ArenaTagSide.BOTH && tag.side !== ArenaTagSide.BOTH && side !== tag.side) {
+        continue;
+      }
+
+      tag.apply(...args);
     }
-    tags.forEach(t => t.apply(...args));
   }
 
   /**
