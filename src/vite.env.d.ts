5a9537f3ae1b424667aeed54ca553b50a500762a
Bertie690 <taylormw163@gmail.com>
2026-02-05 19:44:06 -0500

    Stricten up allowed types

--- src/vite.env.d.ts
+++ src/vite.env.d.ts
@@ -1,17 +1,22 @@
 /// <reference types="vite/client" />
 
+// biome-ignore lint/style/useNamingConvention: HTTP and URL are fullcaps acronyms
+type HTTP_URL = `http${"" | "s"}://${string}`;
+
+// Declaration merging for vite's `import.meta.env`.
+
 interface ImportMetaEnv {
-  readonly VITE_PORT?: string;
-  readonly VITE_BYPASS_LOGIN?: string;
-  readonly VITE_BYPASS_TUTORIAL?: string;
-  readonly VITE_API_BASE_URL?: string;
-  readonly VITE_SERVER_URL?: string;
+  readonly VITE_PORT?: `${number}`;
+  readonly VITE_BYPASS_LOGIN?: "0" | "1";
+  readonly VITE_BYPASS_TUTORIAL?: "0" | "1";
+  readonly VITE_API_BASE_URL?: HTTP_URL;
+  readonly VITE_SERVER_URL?: HTTP_URL;
   readonly VITE_DISCORD_CLIENT_ID?: string;
   readonly VITE_GOOGLE_CLIENT_ID?: string;
-  readonly VITE_I18N_DEBUG?: string;
-  readonly NODE_ENV?: string;
+  readonly VITE_I18N_DEBUG?: "0" | "1";
 }
 
+// tell vite to disallow missing env vars
 interface ViteTypeOptions {
   strictImportMetaEnv: unknown;
 }
