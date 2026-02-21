7c4e2e7b877fca32f235ae166467dc22877b4a0a
NightKev <34855794+DayKev@users.noreply.github.com>
2026-02-21 03:23:28 -0800

    Move matchers to `test/matchers/`

--- test/setup/matchers.setup.ts
+++ test/setup/matchers.setup.ts
@@ -4,26 +4,26 @@
  * @module
  */
 
-import { toBeAtPhase } from "#test/utils/matchers/to-be-at-phase";
-import { toEqualUnsorted } from "#test/utils/matchers/to-equal-unsorted";
-import { toHaveAbilityApplied } from "#test/utils/matchers/to-have-ability-applied";
-import { toHaveArenaTag } from "#test/utils/matchers/to-have-arena-tag";
-import { toHaveBattlerTag } from "#test/utils/matchers/to-have-battler-tag";
-import { toHaveEffectiveStat } from "#test/utils/matchers/to-have-effective-stat";
-import { toHaveFainted } from "#test/utils/matchers/to-have-fainted";
-import { toHaveFullHp } from "#test/utils/matchers/to-have-full-hp";
-import { toHaveHp } from "#test/utils/matchers/to-have-hp";
-import { toHaveKey } from "#test/utils/matchers/to-have-key";
-import { toHavePositionalTag } from "#test/utils/matchers/to-have-positional-tag";
-import { toHaveShownMessage } from "#test/utils/matchers/to-have-shown-message";
-import { toHaveStatStage } from "#test/utils/matchers/to-have-stat-stage";
-import { toHaveStatusEffect } from "#test/utils/matchers/to-have-status-effect";
-import { toHaveTakenDamage } from "#test/utils/matchers/to-have-taken-damage";
-import { toHaveTerrain } from "#test/utils/matchers/to-have-terrain";
-import { toHaveTypes } from "#test/utils/matchers/to-have-types";
-import { toHaveUsedMove } from "#test/utils/matchers/to-have-used-move";
-import { toHaveUsedPP } from "#test/utils/matchers/to-have-used-pp";
-import { toHaveWeather } from "#test/utils/matchers/to-have-weather";
+import { toBeAtPhase } from "#test/matchers/to-be-at-phase";
+import { toEqualUnsorted } from "#test/matchers/to-equal-unsorted";
+import { toHaveAbilityApplied } from "#test/matchers/to-have-ability-applied";
+import { toHaveArenaTag } from "#test/matchers/to-have-arena-tag";
+import { toHaveBattlerTag } from "#test/matchers/to-have-battler-tag";
+import { toHaveEffectiveStat } from "#test/matchers/to-have-effective-stat";
+import { toHaveFainted } from "#test/matchers/to-have-fainted";
+import { toHaveFullHp } from "#test/matchers/to-have-full-hp";
+import { toHaveHp } from "#test/matchers/to-have-hp";
+import { toHaveKey } from "#test/matchers/to-have-key";
+import { toHavePositionalTag } from "#test/matchers/to-have-positional-tag";
+import { toHaveShownMessage } from "#test/matchers/to-have-shown-message";
+import { toHaveStatStage } from "#test/matchers/to-have-stat-stage";
+import { toHaveStatusEffect } from "#test/matchers/to-have-status-effect";
+import { toHaveTakenDamage } from "#test/matchers/to-have-taken-damage";
+import { toHaveTerrain } from "#test/matchers/to-have-terrain";
+import { toHaveTypes } from "#test/matchers/to-have-types";
+import { toHaveUsedMove } from "#test/matchers/to-have-used-move";
+import { toHaveUsedPP } from "#test/matchers/to-have-used-pp";
+import { toHaveWeather } from "#test/matchers/to-have-weather";
 import { expect } from "vitest";
 
 expect.extend({
