51a09db8f00eeb5ad85bbe74ff82761b9a4c81f7
Bertie690 <taylormw163@gmail.com>
2026-03-15 20:34:05 -0400

    convert even more stuff

--- scripts/utils/arguments.ts
+++ scripts/utils/arguments.ts
@@ -5,22 +5,21 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
+import type { HelpConfiguration } from "@commander-js/extra-typings";
 import chalk from "chalk";
-
-/** @import {Help} from "@commander-js/extra-typings" */
 
 /**
  * Obtain the value of a property value CLI argument (one of the form
  * `-x=y` or `-x y`).
- * @param {string[]} args - The command line arguments array to parse
- * @param {readonly string[]} flags - An array containing all valid flags to check.
- * @returns {string | undefined} The provided arguments, or `undefined` if none is provided
+ * @param args - The command line arguments array to parse
+ * @param flags - An array containing all valid flags to check.
+ * @returns The provided arguments, or `undefined` if none is provided
  * @remarks
  * This will mutate the `args` array by removing the first specified match.
  */
 // TODO: Remove and migrate existing scripts to `commander` (which does this for us)
-export function getPropertyValue(args, flags) {
-  let /** @type {string | undefined} */ arg;
+export function getPropertyValue(args: string[], flags: readonly string[]): string | undefined {
+  let arg: string | undefined;
   // Extract the prop as either the form "-o=y" or "-o y".
   const hasEquals = /^.*=(.+)$/g.exec(args[0]);
   if (hasEquals) {
@@ -42,20 +41,18 @@ export const OPTION_COLOR = chalk.hex("#8a2be2");
 export const USAGE_COLOR = chalk.blue;
 
 /**
- * Standardized arguments to pass to Commander's help text formatter.
- * @type {Partial<Help>}
+ * Standardized arguments for Commander's help text formatter.
  * @remarks
  * Extending this config is discouraged; its explicit purpose is to standardize a potential source of bikeshedding.
  */
-export const defaultCommanderHelpArgs = {
+export const defaultCommanderHelpArgs: HelpConfiguration = {
   styleUsage: str => USAGE_COLOR(str),
   optionTerm: option => OPTION_COLOR(option.flags),
   argumentTerm: argument => ARGUMENT_COLOR(argument.name()),
   styleTitle: title => getTitleColor(title)(title),
   optionDescription: option => {
-    // Inspired by the default reporter from `commander` source, but in Title Case to match existing conventions
-    /** @type {string[]} */
-    const extraInfo = [];
+    // Inspired by the default reporter from `commander`'s source code, but making all headers Title Case to match existing conventions
+    const extraInfo: string[] = [];
 
     if (option.argChoices) {
       extraInfo.push(`Choices: ${option.argChoices.map(choice => JSON.stringify(choice)).join(", ")}`);
@@ -88,12 +85,11 @@ export const defaultCommanderHelpArgs = {
 };
 
 /**
- * Color title headers the same as their corresponding contents.
- * @param {string} title - The title header being colored.
- * @returns {(s: string) => string}
- * A function to color the resulting output.
+ * Color section header titles with the color corresponding to their contents.
+ * @param title - The title header being colored
+ * @returns A function to color the resulting output.
  */
-function getTitleColor(title) {
+function getTitleColor(title: string): (str: string) => string {
   const titleLower = title.toLowerCase();
   if (titleLower.includes("option")) {
     return OPTION_COLOR.bold;
