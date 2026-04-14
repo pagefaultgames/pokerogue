c28f31b3a824a1ffd5f4b6a6b47c388e0ec9d39b
Bertie690 <taylormw163@gmail.com>
2026-04-14 15:12:14 -0400

    Remove redundant comment

--- src/items/held-items/damage-money-reward.ts
+++ src/items/held-items/damage-money-reward.ts
@@ -13,10 +13,6 @@ import { NumberHolder } from "#utils/common";
 export class DamageMoneyRewardHeldItem extends HeldItem<[typeof HeldItemEffect.DAMAGE_MONEY_REWARD]> {
   public readonly effects = [HeldItemEffect.DAMAGE_MONEY_REWARD] as const;
 
-  /**
-   * Applies {@linkcode DamageMoneyRewardModifier}
-   * @returns always `true`
-   */
   apply(_effect: typeof HeldItemEffect.DAMAGE_MONEY_REWARD, { pokemon, damage }: DamageMoneyRewardParams): void {
     const stackCount = pokemon.heldItemManager.getStack(this.type);
     const moneyAmount = new NumberHolder(Math.floor(damage * (0.5 * stackCount)));
