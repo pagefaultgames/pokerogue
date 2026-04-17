703b76577318401078ad057023b381fca2b7939f
Dean <69436131+emdeann@users.noreply.github.com>
2026-04-17 15:45:12 -0700

    Update src/enums/stat-change-source.ts
    Co-authored-by: NightKev <34855794+DayKev@users.noreply.github.com>

--- src/enums/stat-change-source.ts
+++ src/enums/stat-change-source.ts
@@ -1,5 +1,9 @@
-export enum StatChangeSource {
-  STICKY_WEB,
-  MIRROR_ARMOR,
-  OPPORTUNIST,
+import type { ValueOf } from "type-fest";
+
+export const StatChangeSource = {
+  STICKY_WEB: 1,
+  MIRROR_ARMOR: 2,
+  OPPORTUNIST: 3,
 }
+
+export type StatChangeSource = ValueOf<typeof StatChangeSource>;
