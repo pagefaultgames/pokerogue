c713c2dd2c5cd0f6c082451fe212adb5bbd54728
NightKev <34855794+DayKev@users.noreply.github.com>
2026-06-19 19:37:47 -0700

    Fix version number in migrator

--- src/system/version-migration/version-converter.ts
+++ src/system/version-migration/version-converter.ts
@@ -58,7 +58,7 @@ import * as v1_8_3 from "#system/v1_8_3";
 import * as v1_9_0 from "#system/v1_9_0";
 import * as v1_10_0 from "#system/v1_10_0";
 import * as v1_11_19 from "#system/v1_11_19";
-import * as v1_12_0 from "#system/v1_12_0";
+import * as v1_12_0_0 from "#system/v1_12_0_0";
 
 // To add a new set of migrators, add them to the appropriate array of migrators
 
@@ -75,7 +75,7 @@ const sessionMigrators: SessionSaveMigrator[] = [
   ...v1_7_0.sessionMigrators,
   ...v1_9_0.sessionMigrators,
   ...v1_10_0.sessionMigrators,
-  ...v1_12_0.sessionMigrators,
+  ...v1_12_0_0.sessionMigrators,
 ];
 
 /** All settings migrators */
--- src/system/version-migration/versions/v1_12_0_0.ts
+++ src/system/version-migration/versions/v1_12_0_0.ts
@@ -2,7 +2,7 @@ import type { SessionSaveData } from "#types/save-data";
 import type { SessionSaveMigrator } from "#types/save-migrators";
 
 const migrateRageFistHitCount: SessionSaveMigrator = {
-  version: "1.12.0",
+  version: "1.12.0.0",
   migrate: (data: SessionSaveData): void => {
     for (const p of data.party.concat(data.enemyParty)) {
       p.summonData.hitCount = p.battleData.hitCount;
