335cbc35537f087745c1b4048b9db21874d738bf
NightKev <34855794+DayKev@users.noreply.github.com>
2026-06-19 19:36:00 -0700

    Remove migration for enemy party `CustomPokemonData`
    It shouldn't be possible for enemy Pokemon to have `CustomPokemonData`
    types serialized to save data

--- src/system/version-migration/versions/v1_12_0_0.ts
+++ src/system/version-migration/versions/v1_12_0_0.ts
@@ -3,9 +3,9 @@ import type { SessionSaveData } from "#types/save-data";
 import type { SessionSaveMigrator } from "#types/save-migrators";
 
 const convertCustomPokemonDataTypes: SessionSaveMigrator = {
-  version: "1.12.0",
+  version: "1.12.0.0",
   migrate: (data: SessionSaveData): void => {
-    for (const p of data.party.concat(data.enemyParty)) {
+    for (const p of data.party) {
       if (p.customPokemonData.types.length > 0) {
         p.customPokemonData.types = p.customPokemonData.types.map(t =>
           (t as PokemonType) === PokemonType.UNKNOWN ? null : t,
