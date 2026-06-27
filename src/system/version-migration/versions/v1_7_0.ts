bf3f7ee04654cd07805ae35830cca8e7b51629bf
NightKev <34855794+DayKev@users.noreply.github.com>
2026-06-27 15:53:18 -0700

    Change check to be "array length of at least 2"

--- src/system/version-migration/versions/v1_7_0.ts
+++ src/system/version-migration/versions/v1_7_0.ts
@@ -38,8 +38,8 @@ const migrateUnselectableForms: SystemSaveMigrator = {
 
 export const systemMigrators: readonly SystemSaveMigrator[] = [migrateUnselectableForms] as const;
 
-function isArrayOfLengthThree(arr: unknown): arr is [unknown, unknown, unknown] {
-  return Array.isArray(arr) && arr.length === 3;
+function isArrayOfAtLeastTwo(arr: unknown): arr is unknown[] {
+  return Array.isArray(arr) && arr.length >= 2;
 }
 
 const migrateTera: SessionSaveMigrator = {
@@ -54,7 +54,7 @@ const migrateTera: SessionSaveMigrator = {
           // Assert the modifier has the expected args structure
           const modifierArgs = data.modifiers[i].args;
           // Skip malformed modifiers (it is not the migrator's responsibility to fix/remove)
-          if (!isArrayOfLengthThree(modifierArgs)) {
+          if (!isArrayOfAtLeastTwo(modifierArgs)) {
             continue;
           }
           data.party.forEach(p => {
@@ -87,7 +87,7 @@ const migrateTera: SessionSaveMigrator = {
         // Assert the modifier has the expected args structure
         const modifierArgs = data.enemyModifiers[i].args;
 
-        if (!isArrayOfLengthThree(modifierArgs)) {
+        if (!isArrayOfAtLeastTwo(modifierArgs)) {
           data.enemyModifiers.splice(i, 1);
           continue;
         }
