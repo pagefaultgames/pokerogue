716acf2422c2f52152e423c4f5f2e9739387254d
NightKev <34855794+DayKev@users.noreply.github.com>
2026-07-03 03:43:35 -0700

    fix: the data loss failsafe now checks for `abilityAttr > 0`

--- src/system/version-migration/versions/v1_12_0_3.ts
+++ src/system/version-migration/versions/v1_12_0_3.ts
@@ -5,7 +5,7 @@ import type { IEggOptions } from "#data/egg";
 import { DexAttr } from "#enums/dex-attr";
 import { EggSourceType } from "#enums/egg-source-types";
 import { EggTier } from "#enums/egg-type";
-import type { SpeciesId } from "#enums/species-id";
+import { SpeciesId } from "#enums/species-id";
 import { UiMode } from "#enums/ui-mode";
 import { EggData } from "#system/egg-data";
 import { VoucherType } from "#system/voucher";
@@ -172,7 +172,56 @@ const voucherCompensationMigrator: SystemSaveMigrator = {
   },
 };
 
+// a copy of the 1.12.0.1 migrator with the `.abilityAttr` check fixed
+const fixDexData: SystemSaveMigrator = {
+  version: "1.12.0.3",
+  migrate: (data): void => {
+    const defaultStarterAttr =
+      DexAttr.NON_SHINY | DexAttr.MALE | DexAttr.FEMALE | DexAttr.DEFAULT_VARIANT | DexAttr.DEFAULT_FORM;
+
+    for (const speciesId of speciesDataRegistry.getAllStarters()) {
+      if (defaultStarterSpecies.includes(speciesId)) {
+        continue;
+      }
+
+      const starterEntry = data.starterData[speciesId];
+      const dexEntry = data.dexData[speciesId];
+
+      const species = SpeciesId[speciesId];
+
+      if (starterEntry == null) {
+        console.warn("Missing starter data for %s (%d)!", species, speciesId);
+      }
+      if (dexEntry == null) {
+        console.warn("Missing dex data for %s (%d)!", species, speciesId);
+      }
+
+      const hasStarterData =
+        starterEntry.abilityAttr > 0 // starters are initialized with 0 and not 1
+        || starterEntry.eggMoves > 0
+        || starterEntry.moveset != null
+        || starterEntry.passiveAttr > 0
+        || starterEntry.valueReduction > 0;
+
+      const noDexData = dexEntry.caughtCount === 0 && dexEntry.hatchedCount === 0 && dexEntry.caughtAttr === 0n;
+
+      if (hasStarterData && noDexData) {
+        console.warn("Missing dex data for %s (%d), creating backup data.", species, speciesId);
+
+        data.dexData[speciesId] = {
+          ...data.dexData[speciesId],
+          caughtCount: 1,
+          caughtAttr: defaultStarterAttr,
+          natureAttr: 1,
+          ivs: [15, 15, 15, 15, 15, 15],
+        };
+      }
+    }
+  },
+};
+
 export const systemMigrators: readonly SystemSaveMigrator[] = [
+  fixDexData,
   shinyCompensationMigrator,
   voucherCompensationMigrator,
 ] as const;
