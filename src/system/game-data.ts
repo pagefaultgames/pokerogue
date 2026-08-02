b3e1c59dea874bb1ab590913a709a0ca8d4126c6
Fabi <192151969+fabske0@users.noreply.github.com>
2026-08-02 20:58:14 +0200

    only allow `.prsv` for ios import

--- src/system/game-data.ts
+++ src/system/game-data.ts
@@ -1422,7 +1422,7 @@ export class GameData {
     const saveFile = document.createElement("input");
     saveFile.id = "saveFile";
     saveFile.type = "file";
-    saveFile.accept = ".prsv, .json, .txt";
+    saveFile.accept = isIos() ? ".prsv" : ".prsv, .json, .txt";
 
     // iOS requires user interaction with a visible element to trigger file input
     if (isIos()) {
