053222a7c255eaa10b834b48f2f46387ac00763b
NightKev <34855794+DayKev@users.noreply.github.com>
2026-08-06 23:42:15 -0700

    refactor(ui): overhaul Option Select and Confirm UI handlers
    Co-authored-by: Moka <millennium.stitcher@gmail.com>

--- src/ui/settings/option-select-ui-handler.ts
+++
@@ -1,7 +0,0 @@
-import { BaseOptionSelectUiHandler } from "#ui/base-option-select-ui-handler";
-
-export class OptionSelectUiHandler extends BaseOptionSelectUiHandler {
-  getWindowWidth(): number {
-    return 64;
-  }
-}
