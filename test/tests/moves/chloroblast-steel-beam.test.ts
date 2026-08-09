a32fe18a86d815c85810c3172c2210c51575f11d
Bertie690 <taylormw163@gmail.com>
2026-08-09 19:02:00 -0400

    fix chloroblast test + add more

--- test/tests/moves/chloroblast-steel-beam.test.ts
+++ test/tests/moves/chloroblast-steel-beam.test.ts
@@ -1,8 +1,10 @@
 import { AbilityId } from "#enums/ability-id";
+import { BattlerIndex } from "#enums/battler-index";
 import { MoveId } from "#enums/move-id";
 import { MoveResult } from "#enums/move-result";
 import { SpeciesId } from "#enums/species-id";
 import { GameManager } from "#test/framework/game-manager";
+import { toDmgValue } from "#utils/common";
 import Phaser from "phaser";
 import { beforeAll, beforeEach, describe, expect, it } from "vitest";
 
@@ -34,11 +36,43 @@ describe("Moves - Chloroblast and Steel Beam", () => {
 
     game.move.use(move);
     await game.move.forceEnemyMove(MoveId.SPLASH);
+    game.setTurnOrder([BattlerIndex.PLAYER, BattlerIndex.ENEMY]);
+    await game.move.forceHit();
     await game.toEndOfTurn();
 
     const player = game.field.getPlayerPokemon();
     expect(player).toHaveUsedMove({ move, result: MoveResult.SUCCESS });
     expect(player).toHaveTakenDamage(player.getMaxHp() / 2);
+  });
+
+  it("should deal recoil damage even if the move misses", async () => {
+    await game.classicMode.startBattle(SpeciesId.FEEBAS);
+
+    game.move.use(MoveId.CHLOROBLAST);
+    await game.move.forceEnemyMove(MoveId.SPLASH);
+    game.setTurnOrder([BattlerIndex.PLAYER, BattlerIndex.ENEMY]);
+    await game.move.forceMiss();
+    await game.toEndOfTurn();
+
+    const player = game.field.getPlayerPokemon();
+    expect(player).toHaveUsedMove({ move: MoveId.CHLOROBLAST, result: MoveResult.MISS });
+    expect(player).toHaveTakenDamage(player.getMaxHp() / 2);
+  });
+
+  it("should be able to KO the user", async () => {
+    await game.classicMode.startBattle(SpeciesId.FEEBAS);
+
+    const player = game.field.getPlayerPokemon();
+    player.hp = toDmgValue(player.getMaxHp() / 2);
+
+    game.move.use(MoveId.CHLOROBLAST);
+    await game.move.forceEnemyMove(MoveId.SPLASH);
+    game.setTurnOrder([BattlerIndex.PLAYER, BattlerIndex.ENEMY]);
+    await game.move.forceHit();
+    await game.toEndOfTurn();
+
+    expect(player).toHaveUsedMove({ move: MoveId.CHLOROBLAST, result: MoveResult.SUCCESS });
+    expect(player).toHaveFainted();
   });
 
   it("should not deal recoil damage if the opponent uses protect", async () => {
