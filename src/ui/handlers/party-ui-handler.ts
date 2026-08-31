7a6055c222df3c95100e18d72b874693aec1309b
NightKev <34855794+DayKev@users.noreply.github.com>
2026-08-30 17:37:42 -0700

    Move "TODO" out of TSDoc

--- src/ui/handlers/party-ui-handler.ts
+++ src/ui/handlers/party-ui-handler.ts
@@ -81,10 +81,10 @@ export type PartyItemTransferSelectCallback = (
 ) => void;
 export type PartyRewardSpliceSelectCallback = (fromCursor: number, toCursor?: number) => void;
 /**
- * A filter that returns the error message to display for an invalid selection, or `null` if the selection is valid.
- *
- * TODO: This is highly convoluted and generally painful
+ * A filter that returns the error message to display for an invalid selection,
+ * or `null` if the selection is valid.
  */
+// TODO: This is highly convoluted and generally painful
 export type PokemonSelectFilter = (pokemon: PlayerPokemon) => string | null;
 export type PokemonItemTransferSelectFilter = (pokemon: PlayerPokemon, item: HeldItemId) => string | null;
 export type PokemonMoveSelectFilter = (pokemonMove: PokemonMove) => string | null;
