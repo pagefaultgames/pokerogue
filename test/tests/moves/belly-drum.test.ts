b00be7cf06f4677b97c93f77c53cf6a89d08021c
Bertie690 <136088738+Bertie690@users.noreply.github.com>
2026-09-06 07:54:18 -0400

    Update belly-drum.test.ts
    Co-authored-by: NightKev <34855794+DayKev@users.noreply.github.com>

--- test/tests/moves/belly-drum.test.ts
+++ test/tests/moves/belly-drum.test.ts
@@ -4,7 +4,6 @@ import { MoveId } from "#enums/move-id";
 import { MoveResult } from "#enums/move-result";
 import { SpeciesId } from "#enums/species-id";
 import { getStatKey, Stat } from "#enums/stat";
-import { StatStageChangePhase } from "#phases/stat-stage-change-phase";
 import { GameManager } from "#test/framework/game-manager";
 import { toDmgValue } from "#utils/common";
 import i18next from "i18next";
