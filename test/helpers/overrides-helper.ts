e03eb1a4fcc2bfbadd2b542c73645a1eebdc5488
NightKev <34855794+DayKev@users.noreply.github.com>
2026-09-19 18:43:08 -0700

    chore: update TSDoc for `OverridesHelper#starterForms`

--- test/helpers/overrides-helper.ts
+++ test/helpers/overrides-helper.ts
@@ -185,8 +185,10 @@ export class OverridesHelper extends GameManagerHelper {
    * Override the player pokemon's forms
    * @param forms - The forms to set
    * @returns `this`
-   *
-   * Example of input: { [SpeciesId.MELOETTA]: 1 }
+   * @example
+   * ```ts
+   * game.override.starterForms({ [SpeciesId.MELOETTA]: 1 });
+   * ```
    */
   public starterForms(forms: Partial<Record<SpeciesId, number>>): this {
     vi.spyOn(activeOverrides, "STARTER_FORM_OVERRIDES", "get").mockReturnValue(forms);
