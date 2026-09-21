72b27bd5011f54b87439e91d85ac5dadf786ec4c
Matthew Taylor <136088738+Bertie690@users.noreply.github.com>
2026-09-21 09:47:47 -0400

    Apply suggestion from @DayKev
    Co-authored-by: NightKev <34855794+DayKev@users.noreply.github.com>

--- src/data/moves/move.ts
+++ src/data/moves/move.ts
@@ -6694,7 +6694,7 @@ export class JawLockAttr extends AddBattlerTagAttr {
 
     const moveChance = this.getMoveChance(user, target, move, this.selfTarget);
     if (moveChance < 0 || moveChance === 100 || user.randBattleSeedInt(100) < moveChance) {
-      /**
+      /*
        * Add the tag to both the user and the target.
        * The target's tag source is considered to be the user and vice versa
        */
